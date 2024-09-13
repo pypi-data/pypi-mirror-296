4c88eca7d3a674882e19ff9c60736dbf11e58343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:02:54 +0000

    Add package baizhanMath-zhangdian/1.0/baizhanMath-zhangdian-1.0.tar.gz

---
+++ packages/baizhanMath-zhangdian/baizhanMath-zhangdian-1.0.tar.gz/baizhanMath-zhangdian-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='baizhanMath-zhangdian',
+      version='1.0',
+      py_modules=['baizhanMath.demo1', 'baizhanMath.demo2'],
+      description='测试自定义模块的发布与安装',
+      author='gaoqi',  # 作者
+      author_email='gaoqi110@163.com')
