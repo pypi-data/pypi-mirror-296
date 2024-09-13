232a506c025d538a1298b8098e317f6fde87f497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:13 +0000

    Add package tcra/0.1.3/tcra-0.1.3.tar.gz

---
+++ packages/tcra/tcra-0.1.3.tar.gz/tcra-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tcra',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'folium',
+    ],
+    description='A Python package for estimating cyclone hazard parameters and calculating wind speeds at structure sites.',  # Make sure this is under 512 characters
+    long_description=open('README.md').read(),  # Detailed description is handled by this field
+    long_description_content_type='text/markdown',
+    url='https://github.com/rxm562/TCRA.git',
+    author='Ram Krishna Mazumder',
+    author_email='rkmazumder@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    )
---
+++ packages/tcra/tcra-0.1.3.tar.gz/tcra-0.1.3/tcra/fragility/__init__.py
@@ -0,0 +1,12 @@
+# TCRA/fragility/__init__.py
+
+# Import specific functions or classes from the modules
+from .damageanalysis import damageanalysis
+from .fragility_curves import fragility_curves
+from .fragility_curves_epn import fragility_curves_epn
+from .fragility_rehab import fragility_rehab
+from .hurricane import hurricane
+from .interactive import interactive
+from .montecarlosimulation import montecarlosimulation
+from .plot import plot
+from .Recovery import Recovery
