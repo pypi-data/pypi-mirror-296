98348e3d7c31946e948fd43fc911556bdaa28bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:00 +0000

    Add package nifty-ls/1.0.1/nifty_ls-1.0.1-cp39-cp39-macosx_12_0_arm64.whl

---
+++ packages/nifty-ls/nifty_ls-1.0.1-cp39-cp39-macosx_12_0_arm64.whl/nifty_ls/backends.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from importlib import import_module
+from typing import Literal, get_args
+
+__all__ = ['available_backends', 'BACKEND_TYPE', 'BACKEND_NAMES']
+
+BACKEND_TYPE = Literal['finufft', 'cufinufft']
+BACKEND_NAMES = list(get_args(BACKEND_TYPE))
+
+
+def available_backends(verbose: bool = False) -> list[str]:
+    """Return a list of available backends.  Backends may be unavailable if their dependencies are not installed."""
+    backends = []
+
+    for backend in BACKEND_NAMES:
+        try:
+            # from . import backend
+            import_module(f'.{backend}', __package__)
+        except ImportError as e:
+            if verbose:
+                print(f'[nifty-ls] Backend {backend} is unavailable: {e}')
+        else:
+            backends.append(backend)
+
+    return backends
