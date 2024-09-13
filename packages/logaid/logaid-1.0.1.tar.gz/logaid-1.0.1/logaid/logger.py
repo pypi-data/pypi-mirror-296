b956ce015d38a1663625691d69698c7d4690bcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:15 +0000

    Add package logaid/1.0.1/logaid-1.0.1.tar.gz

---
+++ packages/logaid/logaid-1.0.1.tar.gz/logaid-1.0.1/logaid/logger.py
@@ -0,0 +1,31 @@
+import logging
+import inspect
+def add_context_info(func):
+    def wrapper(*args, **kwargs):
+        [logging.root.removeHandler(handler) or handler.close() for handler in logging.root.handlers[:]]
+        frame = inspect.currentframe().f_back
+        func_name = frame.f_code.co_name
+        if func_name == '<module>':
+            func_name = 'None'
+        co_filename = frame.f_code.co_filename.split('\\')[-1]
+        lineno = frame.f_lineno
+        logging.basicConfig(level=logging.INFO,
+                            format=f'File "{co_filename}", line {lineno}, func {func_name}, level %(levelname)s: %(message)s')
+        if func.__name__ == 'debug':
+            args = (' '.join([f"\033[35m{i}\033[0m" for i in args]),)
+        elif func.__name__ == 'info':
+            args = (' '.join([f"\033[36m{i}\033[0m" for i in args]),)
+        elif func.__name__ == 'warning':
+            args = (' '.join([f"\033[33m{i}\033[0m" for i in args]),)
+        elif func.__name__ == 'error':
+            args = (' '.join([f"\033[34m{i}\033[0m" for i in args]),)
+        elif func.__name__ == 'fatal':
+            args = (' '.join([f"\033[31m{i}\033[0m" for i in args]),)
+        return func(*args, **kwargs)
+    return wrapper
+
+debug = add_context_info(logging.debug)
+info = add_context_info(logging.info)
+warning = add_context_info(logging.warning)
+error = add_context_info(logging.error)
+fatal = add_context_info(logging.fatal)
