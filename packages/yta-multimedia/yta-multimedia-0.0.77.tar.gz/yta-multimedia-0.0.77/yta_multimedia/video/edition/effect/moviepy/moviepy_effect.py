53de6e14634bbe53f03ed20ad092025668fc6ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:03:57 +0000

    Add package yta-multimedia/0.0.77/yta-multimedia-0.0.77.tar.gz

---
+++ packages/yta-multimedia/yta-multimedia-0.0.77.tar.gz/yta-multimedia-0.0.77/yta_multimedia/video/edition/effect/moviepy/moviepy_effect.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class MoviepyEffect(ABC):
+    """
+    Abstract class to be inherited by all my custom moviepy effects
+    so I can control they belong to this family.
+
+    A moviepy effect (or what I call like that) is an effect that is
+    applied directly to the video by using only the moviepy editor
+    and/or moviepy vfx module. It could be a simple moviepy effect
+    made an object to simplify the work with it, or a more complex
+    effect that is build with some different small effects.
+    """
+    @abstractmethod
+    def get_moviepy_vfx_effect(self):
+        #return getattr(vfx, self.__MOVIEPY_EFFECT_NAME, None)
+        # TODO: This could be real, not abstract, I think
+        pass
+    
+    @abstractmethod
+    def process_parameters(self):
+        pass
+    
+    @abstractmethod
+    def apply(self):
+        pass
