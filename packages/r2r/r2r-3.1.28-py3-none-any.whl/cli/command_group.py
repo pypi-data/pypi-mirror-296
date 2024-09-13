98b624d9f70e24039eb338825e37236b679ab2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:00 +0000

    Add package r2r/3.1.28/r2r-3.1.28-py3-none-any.whl

---
+++ packages/r2r/r2r-3.1.28-py3-none-any.whl/cli/command_group.py
@@ -0,0 +1,14 @@
+import click
+
+from sdk.client import R2RClient
+
+
+@click.group()
+@click.option(
+    "--base-url", default="http://localhost:7272", help="Base URL for the API"
+)
+@click.pass_context
+def cli(ctx, base_url):
+    """R2R CLI for all core operations."""
+
+    ctx.obj = R2RClient(base_url=base_url)
