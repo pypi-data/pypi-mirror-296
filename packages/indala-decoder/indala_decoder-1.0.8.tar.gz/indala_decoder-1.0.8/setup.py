975d58487a0019af3581c9b21a0a492c8386112b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:02 +0000

    Add package indala-decoder/1.0.8/indala_decoder-1.0.8.tar.gz

---
+++ packages/indala-decoder/indala_decoder-1.0.8.tar.gz/indala_decoder-1.0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='indala-decoder',
+    py_modules=['indala_decoder'],
+    version='1.0.8',
+    description='A Python library for decoding 32-bit Indala26 codes into facility code and card numbers, '
+                'and vice versa',
+    author='Logan Nommensen'
+)
