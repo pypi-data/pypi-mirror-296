5c9f9e72c93eac84439afbbc5745b42db3d69f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:32 +0000

    Add package syneto_api/0.3.2/syneto_api-0.3.2-py3-none-any.whl

---
+++ packages/syneto_api/syneto_api-0.3.2-py3-none-any.whl/syneto_api/__init__.py
@@ -0,0 +1,8 @@
+from .api_client import APIException, InvalidInputException, APIClientBase
+from .auth_api import Authentication
+from .virt_api import Virtualization
+from .storage_api import Storage
+from .protection_api import Protection
+from .jobs_api import Jobs
+from .central_api import Central
+from .node_api import Node
