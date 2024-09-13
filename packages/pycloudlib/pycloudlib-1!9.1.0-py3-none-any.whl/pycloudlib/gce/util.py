4a622e6381ee4d5856dc964be8a51b7d1d427857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:04:04 +0000

    Add package pycloudlib/1!9.1.0/pycloudlib-1!9.1.0-py3-none-any.whl

---
+++ packages/pycloudlib/pycloudlib-1!9.1.0-py3-none-any.whl/pycloudlib/gce/util.py
@@ -0,0 +1,43 @@
+"""Common GCE utils."""
+
+import os
+from urllib.error import HTTPError
+
+import google.auth
+from google.oauth2 import service_account
+
+from pycloudlib.gce.errors import GceException
+
+
+def raise_on_error(response):
+    """Look for errors in response and raise if found."""
+    if "httpErrorStatusCode" in response:
+        raise HTTPError(
+            url=response["selfLink"],
+            code=response["httpErrorStatusCode"],
+            msg=response["httpErrorMessage"],
+            hdrs={},
+            fp=None,
+        )
+    if "error" in response:
+        raise GceException(
+            "Received error(s)!\n" "Errors: {}".format(
+                response["error"]["errors"]
+            )
+        )
+
+
+def get_credentials(credentials_path):
+    """Get GCE account credentials.
+
+    Try service account credentials first. If those fail, try the environment
+    """
+    credentials_path = os.path.expandvars(os.path.expanduser(credentials_path))
+    if credentials_path:
+        try:
+            return service_account.Credentials.from_service_account_file(
+                credentials_path
+            )
+        except ValueError:
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+    return google.auth.default()[0]
