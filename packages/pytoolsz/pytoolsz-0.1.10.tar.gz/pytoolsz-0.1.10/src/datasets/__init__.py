605f8555b8714fc19bd0a779232bc3e287991355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:24 +0000

    Add package pytoolsz/0.1.10/pytoolsz-0.1.10.tar.gz

---
+++ packages/pytoolsz/pytoolsz-0.1.10.tar.gz/pytoolsz-0.1.10/src/datasets/__init__.py
@@ -0,0 +1,22 @@
+#  ____       _____           _
+# |  _ \ _   |_   _|__   ___ | |___ ____
+# | |_) | | | || |/ _ \ / _ \| / __|_  /
+# |  __/| |_| || | (_) | (_) | \__ \/ /
+# |_|    \__, ||_|\___/ \___/|_|___/___|
+#        |___/
+
+# Copyright (c) 2024 Sidney Zhang <zly@lyzhang.me>
+# PyToolsz is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+import datasets.iris as iris
+import datasets.covid as covid
+import datasets.tstest as tstest
+
+__all__ = ["iris", "covid", "tstest"]
