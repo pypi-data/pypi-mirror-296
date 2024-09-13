9a020b61244a2d091904981abbcaab6239ad7889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:20 +0000

    Add package gncpy/2.0.3/gncpy-2.0.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gncpy/gncpy-2.0.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/include/gncpy/dynamics/Parameters.h
@@ -0,0 +1,86 @@
+#pragma once
+#include <cereal/access.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/polymorphic.hpp>
+
+#include "gncpy/SerializeMacros.h"
+
+namespace lager::gncpy::dynamics {
+
+/**
+ * @brief Base class for all state transition parameters
+ *
+ * Polymorphic base class such that any child class can be passed into filters,
+ * dynamics, etc. and the object using the child can dynamic cast to the
+ * appropriate polymorphic type.
+ *
+ */
+class StateTransParams {
+    friend class cereal::access;
+
+    GNCPY_SERIALIZE_CLASS(StateTransParams)
+
+   public:
+    virtual ~StateTransParams() = default;
+
+   private:
+    template <class Archive>
+    void serialize([[maybe_unused]] Archive& ar) {
+        /* nothing to serialize */
+    }
+};
+
+/**
+ * @brief Base class for all control parameters
+ *
+ * Polymorphic base class such that any child class can be passed into filters,
+ * dynamics, etc. and the object using the child can dynamic cast to the
+ * appropriate polymorphic type.
+ *
+ */
+// class ControlParams {
+//     friend class cereal::access;
+
+//     GNCPY_SERIALIZE_CLASS(ControlParams)
+
+//    public:
+//     virtual ~ControlParams() = default;
+
+//    private:
+//     template <class Archive>
+//     void serialize([[maybe_unused]] Archive& ar) {
+//         /* nothing to serialize */
+//     }
+// };
+
+/**
+ * @brief Base class for all state constraint parameters
+ *
+ * Polymorphic base class such that any child class can be passed into filters,
+ * dynamics, etc. and the object using the child can dynamic cast to the
+ * appropriate polymorphic type.
+ *
+ */
+class ConstraintParams {
+    friend class cereal::access;
+
+    GNCPY_SERIALIZE_CLASS(ConstraintParams)
+
+   public:
+    virtual ~ConstraintParams() = default;
+
+   private:
+    template <class Archive>
+    void serialize([[maybe_unused]] Archive& ar) {
+        /* nothing to serialize */
+    }
+};
+
+}  // namespace lager::gncpy::dynamics
+
+CEREAL_REGISTER_TYPE(lager::gncpy::dynamics::StateTransParams)
+// CEREAL_REGISTER_TYPE(lager::gncpy::dynamics::ControlParams)
+CEREAL_REGISTER_TYPE(lager::gncpy::dynamics::ConstraintParams)
