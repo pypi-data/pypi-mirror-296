8344047bc65a30dceafdde0176166d93699886db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:23 +0000

    Add package eolymp/0.11.1/eolymp-0.11.1-py3-none-any.whl

---
+++ packages/eolymp/eolymp-0.11.1-py3-none-any.whl/eolymp/judge/template_pb2.pyi
@@ -0,0 +1,16 @@
+from eolymp.executor import file_pb2 as _file_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Template(_message.Message):
+    __slots__ = ["id", "runtime", "source"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    RUNTIME_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    runtime: str
+    source: str
+    def __init__(self, id: _Optional[str] = ..., runtime: _Optional[str] = ..., source: _Optional[str] = ...) -> None: ...
