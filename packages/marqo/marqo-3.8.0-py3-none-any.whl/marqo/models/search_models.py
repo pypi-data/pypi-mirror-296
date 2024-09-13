b9147642ae99cd92e4af49fbf9eb4fdc6f7389bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:36 +0000

    Add package marqo/3.8.0/marqo-3.8.0-py3-none-any.whl

---
+++ packages/marqo/marqo-3.8.0-py3-none-any.whl/marqo/models/search_models.py
@@ -0,0 +1,44 @@
+from typing import Dict, List, Optional, Union
+from marqo.models.marqo_models import StrictBaseModel
+from abc import ABC
+from enum import Enum
+
+from pydantic import validator, BaseModel, root_validator
+
+
+class SearchBody(StrictBaseModel):
+    q: Optional[Union[str, Dict[str, float]]] = None
+    searchableAttributes: Union[None, List[str]] = None
+    searchMethod: Union[None, str] = "TENSOR"
+    limit: int = 10
+    offset: int = 0
+    showHighlights: bool = True
+    reRanker: str = None
+    filter: str = None
+    attributesToRetrieve: Union[None, List[str]] = None
+    boost: Optional[Dict] = None
+    image_download_headers: Optional[Dict] = None
+    context: Optional[Dict] = None
+    scoreModifiers: Optional[Dict] = None
+    modelAuth: Optional[Dict] = None
+
+
+class BulkSearchBody(SearchBody):
+    index: str
+
+
+class BulkSearchQuery(StrictBaseModel):
+    queries: List[BulkSearchBody]
+
+
+class RetrievalMethod(str, Enum):
+    Disjunction = 'disjunction'
+    Tensor = 'tensor'
+    Lexical = 'lexical'
+
+
+class RankingMethod(str, Enum):
+    RRF = 'rrf'
+    NormalizeLinear = 'normalize_linear'
+    Tensor = 'tensor'
+    Lexical = 'lexical'
