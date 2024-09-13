acf43bceadf4a4def6c09b0e1166b28b333a08d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:49 +0000

    Add package visual-contextual-classifier/1.1/visual_contextual_classifier-1.1.tar.gz

---
+++ packages/visual-contextual-classifier/visual_contextual_classifier-1.1.tar.gz/visual_contextual_classifier-1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='visual-contextual-classifier',
+      version='1.1',
+      description='Predicts whether a sentence is a visual sentence or a contextual sentence',
+      author='James Gore',
+      author_email='james.gore@maine.edu',
+      url='https://github.com/jgore077/visual-contextual-classifier/blob/main/README.md',
+      install_requires=[
+        'transformers>=4.44.2',
+        'torch>=2.4.1',
+        'requests>=2.32.3',
+        'tqdm>=4.66.5'
+      ],
+      packages=setuptools.find_packages(),
+     )
