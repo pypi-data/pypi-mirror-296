adff659a53638fc2df9c13beb3df38c5553e0f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:22 +0000

    Add package gncpy/2.0.3/gncpy-2.0.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gncpy/gncpy-2.0.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/include/gncpy/Utilities.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <memory>
+
+namespace lager::gncpy::utilities {
+
+/**
+ * @brief Check if the pointer is an instance of a class type
+ *
+ * @tparam Base testing class type
+ * @tparam T Current class type
+ * @param ptr The pointer to a class to check
+ * @return true class is an ancestor of type Base
+ * @return false
+ */
+template <typename Base, typename T>
+inline bool instanceof (const T *ptr) {
+    return dynamic_cast<const Base *>(ptr) != nullptr;
+}
+
+template <typename Base, typename T>
+inline bool instanceof (const std::shared_ptr<T> ptr) {
+    return std::dynamic_pointer_cast<const Base>(ptr) != nullptr;
+}
+
+}  // namespace lager::gncpy::utilities
---
+++ packages/gncpy/gncpy-2.0.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/include/gncpy/measurements/INonLinearMeasModel.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Eigen/Dense>
+#include <functional>
+#include <vector>
+
+#include "gncpy/SerializeMacros.h"
+#include "gncpy/measurements/IMeasModel.h"
+#include "gncpy/measurements/Parameters.h"
+
+namespace lager::gncpy::measurements {
+
+class INonLinearMeasModel : public IMeasModel {
+    friend class cereal::access;
+
+   public:
+    Eigen::VectorXd measure(const Eigen::VectorXd& state,
+                            const MeasParams* params = nullptr) const override;
+    Eigen::MatrixXd getMeasMat(
+        const Eigen::VectorXd& state,
+        const MeasParams* params = nullptr) const override;
+
+   protected:
+    virtual std::vector<std::function<double(const Eigen::VectorXd&)>>
+    getMeasFuncLst(const MeasParams* params = nullptr) const = 0;
+
+   private:
+    template <class Archive>
+    void serialize([[maybe_unused]] Archive& ar);
+};
+
+template <class Archive>
+void INonLinearMeasModel::serialize([[maybe_unused]] Archive& ar) {
+    ar(cereal::make_nvp("IMeasModel",
+                        cereal::virtual_base_class<IMeasModel>(this)));
+}
+
+}  // namespace lager::gncpy::measurements
+
+CEREAL_REGISTER_TYPE(lager::gncpy::measurements::INonLinearMeasModel)
