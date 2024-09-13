4c668e00d90fdac2f00811387ad8353c9cf0f93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:03:37 +0000

    Add package aiida-workgraph/0.4.0/aiida_workgraph-0.4.0-py3-none-any.whl

---
+++ packages/aiida-workgraph/aiida_workgraph-0.4.0-py3-none-any.whl/aiida_workgraph/executors/test.py
@@ -0,0 +1,31 @@
+from typing import Union, Dict
+import time
+from aiida.engine import calcfunction
+from aiida.orm import Int, Float
+
+
+@calcfunction
+def add(
+    x: Union[Int, Float], y: Union[Int, Float], t: Union[Int, Float] = 1.0
+) -> Dict[str, Union[Int, Float]]:
+    """Add node."""
+    time.sleep(t.value)
+    return {"sum": x + y}
+
+
+@calcfunction
+def greater(
+    x: Union[Int, Float], y: Union[Int, Float], t: Union[Int, Float] = 1.0
+) -> Dict[str, bool]:
+    """Compare node."""
+    time.sleep(t.value)
+    return {"result": x > y}
+
+
+@calcfunction
+def sum_diff(
+    x: Union[Int, Float], y: Union[Int, Float], t: Union[Int, Float] = 1.0
+) -> Dict[str, Union[Int, Float]]:
+    """Add node."""
+    time.sleep(t.value)
+    return {"sum": x + y, "diff": x - y}
