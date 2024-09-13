4ee93757dea7be42f30516cf4b6cfcf37d79921e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:03:49 +0000

    Add package CheckExplore-Defect-Package/0.1/CheckExplore_Defect_Package-0.1.tar.gz

---
+++ packages/CheckExplore-Defect-Package/CheckExplore_Defect_Package-0.1.tar.gz/CheckExplore_Defect_Package-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CheckExplore_Defect_Package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        #eg. numpy>=1.11.1
+    ]
+)
