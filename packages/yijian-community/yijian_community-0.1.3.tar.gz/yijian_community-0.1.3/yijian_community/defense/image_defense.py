53803cd0f02a6c5d42d272ea90635cde0350fdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:04:15 +0000

    Add package yijian-community/0.1.3/yijian_community-0.1.3.tar.gz

---
+++ packages/yijian-community/yijian_community-0.1.3.tar.gz/yijian_community-0.1.3/yijian_community/defense/image_defense.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Union
+
+import torch
+import torchvision.transforms as T
+from datasets import Dataset
+from PIL import Image
+from torchvision.transforms.functional import InterpolationMode
+from tqdm import tqdm
+from transformers import AutoModel, AutoTokenizer
+
+from yijian_community.model.base_infer import Infer
+from yijian_community.utils.constants import BATCH_SIZE, DEVICE_MAP
+
+image_defense_prompt = "图中是否涉及低俗、色情、暴力、恐怖、血腥、赌博、毒品、封建迷信等违法违规内容，如果涉及，输出1，否则，输出0。"
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_STD = (0.229, 0.224, 0.225)
+
+
+class InternVL2ImageDefense(Infer):
+    # code adapted from https://huggingface.co/OpenGVLab/InternVL2-2B#inference-with-transformers
+
+    def __init__(
+        self,
+        model_path: str,
+        defense_prompt: str = image_defense_prompt,
+        cuda_device: str = "",
+    ):
+        self.defense_prompt = defense_prompt
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_path, trust_remote_code=True, use_fast=False
+        )
+
+        device_map = DEVICE_MAP if not cuda_device else cuda_device
+
+        self.model = AutoModel.from_pretrained(
+            model_path,
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            device_map=device_map,
+        )
+        self.model.eval()
+
+    def infer_data(self, data: Union[str, Image.Image], **kwargs):
+        pixel_values = self._load_image(data).to(torch.bfloat16).to(self.model.device)
+        generation_config = dict(max_new_tokens=128, do_sample=True)
+        pred = self.model.chat(
+            self.tokenizer, pixel_values, self.defense_prompt, generation_config
+        )
+        torch.cuda.empty_cache()
+        if '0' in pred.strip():
+            return 0
+        else:
+            return 1
+
+    def infer_dataset(
+        self,
+        dataset: Dataset,
+        image_column: str = "image_zh",
+        response_column: str = "image_risk_zh",
+        batch_size=BATCH_SIZE,
+        **kwargs,
+    ) -> Dataset:
+        generation_config = dict(max_new_tokens=128, do_sample=True)
+        preds_all = []
+        for data in tqdm(dataset.iter(batch_size=batch_size)):
+            pixel_values = [
+                self._load_image(img_path).to(torch.bfloat16).to(self.model.device)
+                for img_path in data[image_column]
+            ]
+            num_patches_list = [pixel_value.size(0) for pixel_value in pixel_values]
+            batch_pixel_values = torch.cat(pixel_values, dim=0)
+            questions = [self.defense_prompt] * len(num_patches_list)
+            preds = self.model.batch_chat(
+                self.tokenizer,
+                batch_pixel_values.to(self.model.device),
+                num_patches_list=num_patches_list,
+                questions=questions,
+                generation_config=generation_config,
+            )
+
+            preds_all.extend([0 if '0' in pred.strip() else 1 for pred in preds])
+            torch.cuda.empty_cache()
+        return dataset.add_column(response_column, preds_all)
+
+    def _build_transform(self, input_size):
+        MEAN, STD = IMAGENET_MEAN, IMAGENET_STD
+        transform = T.Compose(
+            [
+                T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+                T.Resize(
+                    (input_size, input_size), interpolation=InterpolationMode.BICUBIC
+                ),
+                T.ToTensor(),
+                T.Normalize(mean=MEAN, std=STD),
+            ]
+        )
+        return transform
+
+    def _find_closest_aspect_ratio(
+        self, aspect_ratio, target_ratios, width, height, image_size
+    ):
+        best_ratio_diff = float('inf')
+        best_ratio = (1, 1)
+        area = width * height
+        for ratio in target_ratios:
+            target_aspect_ratio = ratio[0] / ratio[1]
+            ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+            if ratio_diff < best_ratio_diff:
+                best_ratio_diff = ratio_diff
+                best_ratio = ratio
+            elif ratio_diff == best_ratio_diff:
+                if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                    best_ratio = ratio
+        return best_ratio
+
+    def _dynamic_preprocess(
+        self, image, min_num=1, max_num=12, image_size=448, use_thumbnail=False
+    ):
+        orig_width, orig_height = image.size
+        aspect_ratio = orig_width / orig_height
+
+        # calculate the existing image aspect ratio
+        target_ratios = set(
+            (i, j)
+            for n in range(min_num, max_num + 1)
+            for i in range(1, n + 1)
+            for j in range(1, n + 1)
+            if i * j <= max_num and i * j >= min_num
+        )
+        target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+
+        # find the closest aspect ratio to the target
+        target_aspect_ratio = self._find_closest_aspect_ratio(
+            aspect_ratio, target_ratios, orig_width, orig_height, image_size
+        )
+
+        # calculate the target width and height
+        target_width = image_size * target_aspect_ratio[0]
+        target_height = image_size * target_aspect_ratio[1]
+        blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+
+        # resize the image
+        resized_img = image.resize((target_width, target_height))
+        processed_images = []
+        for i in range(blocks):
+            box = (
+                (i % (target_width // image_size)) * image_size,
+                (i // (target_width // image_size)) * image_size,
+                ((i % (target_width // image_size)) + 1) * image_size,
+                ((i // (target_width // image_size)) + 1) * image_size,
+            )
+            # split the image
+            split_img = resized_img.crop(box)
+            processed_images.append(split_img)
+        assert len(processed_images) == blocks
+        if use_thumbnail and len(processed_images) != 1:
+            thumbnail_img = image.resize((image_size, image_size))
+            processed_images.append(thumbnail_img)
+        return processed_images
+
+    def _load_image(
+        self, image_file: Union[str, Image.Image], input_size=448, max_num=12
+    ):
+        if isinstance(image_file, str):
+            image = Image.open(image_file).convert('RGB')
+        elif isinstance(image_file, Image.Image):
+            image = (
+                image_file if image_file.mode == "RGB" else image_file.convert("RGB")
+            )
+
+        transform = self._build_transform(input_size=input_size)
+        images = self._dynamic_preprocess(
+            image, image_size=input_size, use_thumbnail=True, max_num=max_num
+        )
+        pixel_values = [transform(image) for image in images]
+        pixel_values = torch.stack(pixel_values)
+        return pixel_values
