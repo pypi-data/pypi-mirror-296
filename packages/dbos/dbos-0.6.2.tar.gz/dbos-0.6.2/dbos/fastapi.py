11e8653e1091c6bc0a5fb38fdb1a873f0e1c834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:32 +0000

    Add package dbos/0.6.2/dbos-0.6.2.tar.gz

---
+++ packages/dbos/dbos-0.6.2.tar.gz/dbos-0.6.2/dbos/fastapi.py
@@ -0,0 +1,57 @@
+import uuid
+from typing import Any, Callable
+
+from fastapi import FastAPI
+from fastapi import Request as FastAPIRequest
+
+from .context import (
+    EnterDBOSHandler,
+    OperationType,
+    SetWorkflowID,
+    TracedAttributes,
+    assert_current_dbos_context,
+)
+from .request import Address, Request, request_id_header
+
+
+def get_or_generate_request_id(request: FastAPIRequest) -> str:
+    request_id = request.headers.get(request_id_header, None)
+    if request_id is not None:
+        return request_id
+    else:
+        return str(uuid.uuid4())
+
+
+def make_request(request: FastAPIRequest) -> Request:
+    return Request(
+        headers=request.headers,
+        path_params=request.path_params,
+        query_params=request.query_params,
+        url=str(request.url),
+        base_url=str(request.base_url),
+        client=Address(*request.client) if request.client is not None else None,
+        cookies=request.cookies,
+        method=request.method,
+    )
+
+
+def setup_fastapi_middleware(app: FastAPI) -> None:
+    @app.middleware("http")
+    async def dbos_fastapi_middleware(
+        request: FastAPIRequest, call_next: Callable[..., Any]
+    ) -> Any:
+        attributes: TracedAttributes = {
+            "name": str(request.url.path),
+            "requestID": get_or_generate_request_id(request),
+            "requestIP": request.client.host if request.client is not None else None,
+            "requestURL": str(request.url),
+            "requestMethod": request.method,
+            "operationType": OperationType.HANDLER.value,
+        }
+        with EnterDBOSHandler(attributes):
+            ctx = assert_current_dbos_context()
+            ctx.request = make_request(request)
+            workflow_id = request.headers.get("dbos-idempotency-key", "")
+            with SetWorkflowID(workflow_id):
+                response = await call_next(request)
+        return response
---
+++ packages/dbos/dbos-0.6.2.tar.gz/dbos-0.6.2/dbos/scheduler/scheduler.py
@@ -0,0 +1,43 @@
+import threading
+import traceback
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from dbos.dbos import _DBOSRegistry
+
+from ..context import SetWorkflowID
+from ..logger import dbos_logger
+from .croniter import croniter  # type: ignore
+
+ScheduledWorkflow = Callable[[datetime, datetime], None]
+
+
+def scheduler_loop(
+    func: ScheduledWorkflow, cron: str, stop_event: threading.Event
+) -> None:
+    iter = croniter(cron, datetime.now(timezone.utc), second_at_beginning=True)
+    while not stop_event.is_set():
+        nextExecTime = iter.get_next(datetime)
+        sleepTime = nextExecTime - datetime.now(timezone.utc)
+        if stop_event.wait(timeout=sleepTime.total_seconds()):
+            return
+        with SetWorkflowID(f"sched-{func.__qualname__}-{nextExecTime.isoformat()}"):
+            try:
+                func(nextExecTime, datetime.now(timezone.utc))
+            except Exception as e:
+                dbos_logger.error(
+                    f"Exception encountered in scheduled workflow: {traceback.format_exc()}"
+                )
+                pass  # Let the thread keep running
+
+
+def scheduled(
+    dbosreg: "_DBOSRegistry", cron: str
+) -> Callable[[ScheduledWorkflow], ScheduledWorkflow]:
+    def decorator(func: ScheduledWorkflow) -> ScheduledWorkflow:
+        stop_event = threading.Event()
+        dbosreg.register_poller(stop_event, scheduler_loop, func, cron, stop_event)
+        return func
+
+    return decorator
