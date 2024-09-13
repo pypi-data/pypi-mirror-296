5898585b4ff81e251099ed21fed40473d1d9c110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:04:08 +0000

    Add package odoo14-addon-ssi-general-audit-worksheet-understanding-entity/14.0.1.2.0/odoo14_addon_ssi_general_audit_worksheet_understanding_entity-14.0.1.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-general-audit-worksheet-understanding-entity/odoo14_addon_ssi_general_audit_worksheet_understanding_entity-14.0.1.2.0-py3-none-any.whl/odoo/addons/ssi_general_audit_worksheet_understanding_entity/models/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from . import (
+    general_audit_ws_e78a3c6,
+    general_audit_ws_e78a3c6_organization_structure,
+    general_audit_ws_a13a30e,
+    general_audit_ws_a13a30e_detail,
+    general_audit_ws_bdcdfc5,
+    general_audit_ws_bdcdfc5_detail,
+)
