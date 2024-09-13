c1abf853a762f947932776fd8e2f4ce3742ee103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:21 +0000

    Add package Loglan-Core/0.4.1/loglan_core-0.4.1.tar.gz

---
+++ packages/Loglan-Core/loglan_core-0.4.1.tar.gz/loglan_core-0.4.1/loglan_core/__init__.py
@@ -0,0 +1,27 @@
+"""
+Main point for export DB models
+"""
+
+# fmt: off
+
+__author__ = "torrua"
+__copyright__ = "Copyright 2024, loglan_core project"
+__email__ = "torrua@gmail.com"
+
+from loglan_core.author import BaseAuthor as Author
+from loglan_core.connect_tables import (
+    t_connect_authors,
+    t_connect_words,
+    t_connect_keys,
+)
+from loglan_core.definition import BaseDefinition as Definition
+from loglan_core.event import BaseEvent as Event
+from loglan_core.key import BaseKey as Key
+from loglan_core.setting import BaseSetting as Setting
+from loglan_core.syllable import BaseSyllable as Syllable
+from loglan_core.type import BaseType as Type
+from loglan_core.word import BaseWord as Word
+from loglan_core.word_spell import BaseWordSpell as WordSpell
+
+from loglan_core.base import BaseModel as Base
+from loglan_core.addons.word_selector import WordSelector
