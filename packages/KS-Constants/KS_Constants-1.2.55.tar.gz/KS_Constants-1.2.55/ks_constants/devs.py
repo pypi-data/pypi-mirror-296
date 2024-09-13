2c75f955014709b5af54d27736e62c26fdec4d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:01 +0000

    Add package KS-Constants/1.2.55/KS_Constants-1.2.55.tar.gz

---
+++ packages/KS-Constants/KS_Constants-1.2.55.tar.gz/KS_Constants-1.2.55/ks_constants/devs.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Developer(Enum):
+    Susu = 0
+    Templar = 1
+    Luminous = 2
+    Sox = 3
+    Fatline = 4
+    Duck = 5
+    Understudy = 6
+    Korneel = 7
+    Archlei = 8
+    Legacy = 9
+    hex = 10
+    geo = 11
+    Gcloud = 12
+    Azaz = 13
+    Feanor = 14
---
+++ packages/KS-Constants/KS_Constants-1.2.55.tar.gz/KS_Constants-1.2.55/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='KS_Constants',
+    version='1.2.55',
+    license='MIT',
+    author="Steven Su",
+    author_email='ks2devteam@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/kerrigan-survival-team/ks_constants_py'
+)
