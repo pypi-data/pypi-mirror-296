f9ce439999b12610dde4bb0087d7e9b9cf51a581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:04:18 +0000

    Add package drf-integrations-framework/0.8.7/drf_integrations_framework-0.8.7-py3-none-any.whl

---
+++ packages/drf-integrations-framework/drf_integrations_framework-0.8.7-py3-none-any.whl/drf_integrations/integrations/registry.py
@@ -0,0 +1,86 @@
+from typing import Dict, Iterable, List, Optional, Set, Type, Union
+
+import inspect
+from django.urls import include, path
+
+from drf_integrations.exceptions import ImproperlyConfigured
+from drf_integrations.integrations.base import BaseIntegration
+from drf_integrations.utils import is_instance_of_all
+
+
+class Registry:
+    class IntegrationUnavailableException(Exception):
+        pass
+
+    def __init__(self):
+        self.integrations: Dict[str, BaseIntegration] = {}
+
+    def register(self, integration_cls: Type[BaseIntegration], **kwargs) -> BaseIntegration:
+        """Register an integration."""
+        if integration_cls.name in self.integrations:
+            raise ImproperlyConfigured(
+                f"Integration with name {integration_cls.name} already registered"
+            )
+        self.integrations[integration_cls.name] = integration_cls(**kwargs)
+        return self.integrations[integration_cls.name]
+
+    def get_all(
+        self, *implements: Iterable[type], is_local: Optional[bool] = None
+    ) -> Set[BaseIntegration]:
+        """
+        Get all integrations. If ``implements`` is provided, only return integrations
+        that are instances of **all** of the classes in ``implements``.
+
+        :raises TypeError: If any element of ``implements`` is not a type.
+        """
+        if is_local is not None:
+            integrations = {
+                value for value in self.integrations.values() if value.is_local is is_local
+            }
+        else:
+            integrations = set(self.integrations.values())
+
+        # Filter by implements
+        return set(
+            integration
+            for integration in integrations
+            if is_instance_of_all(integration, implements)
+        )
+
+    def get(self, name_or_class: Union[str, Type[BaseIntegration]]) -> BaseIntegration:
+        """
+        Get integration by name or class.
+        Raises Registry.IntegrationUnavailableException
+        """
+        try:
+            if isinstance(name_or_class, str):
+                integration = self.integrations[name_or_class]
+            elif inspect.isclass(name_or_class) and issubclass(name_or_class, BaseIntegration):
+                integration = self.integrations[name_or_class.name]
+            else:
+                raise ValueError("invalid name or base integration class")
+        except KeyError as err:
+            raise Registry.IntegrationUnavailableException() from err
+
+        return integration
+
+    def get_urls(self, basepath="api/integrations/") -> List:
+        """Get URLConf for all registered integrations."""
+        urls = []
+        for name, integration_cls in self.integrations.items():
+            # Legacy unprefixed URLs
+            urls.extend(integration_cls.get_legacy_unprefixed_urls())
+
+            # Integration URLs
+            integration_urls = integration_cls.get_urls()
+            if integration_urls:
+                urls += [
+                    path(
+                        f"{basepath}{name}/",
+                        include(
+                            (integration_urls, integration_cls.namespace),
+                            namespace=integration_cls.namespace,
+                        ),
+                    )
+                ]
+        return urls
---
+++ packages/drf-integrations-framework/drf_integrations_framework-0.8.7-py3-none-any.whl/drf_integrations/utils.py
@@ -0,0 +1,117 @@
+from typing import Any, Iterable, Iterator, List, Optional, Union
+
+import django
+import logging
+from django.utils.module_loading import import_string
+from environ import Env
+
+AnyString = Union[str, Iterable[Any]]
+
+env = Env()
+
+
+def split_string(string: Optional[AnyString], separator: str = ",") -> List[str]:
+    """
+    Breaks given *string* by the specified *separator*.
+
+    If *string* is a non-``str`` iterable, then return a list if it is not already.
+
+    >>> split_string('A, B, C')  # Str
+    ['A', 'B', 'C']
+
+    >>> split_string(['A', 'B', 'C'])  # List, a non-str iterable
+    ['A', 'B', 'C']
+
+    >>> split_string(('A', 'B', 'C'))  # Tuple, a non-str iterable
+    ['A', 'B', 'C']
+    """
+    return list(iter_split_string(string=string, separator=separator))
+
+
+def iter_split_string(string: Optional[AnyString], separator: str = ",") -> Iterator[str]:
+    """Generator version of :func:`split_string`."""
+
+    if string is None:
+        return
+
+    elif isinstance(string, str):
+        parts = str(string).split(separator)
+        for part in parts:
+            part = part.strip()
+            if part:
+                yield part
+
+    elif isinstance(string, Iterable):
+        # NOTE: Text is also an Iterable, so this should always be after the Text check.
+        for part in string:
+            part = str(part).strip()
+            if part:
+                yield part
+
+    else:
+        raise TypeError("Cannot split string of {!r}".format(type(string)))
+
+
+def is_instance_of_all(obj, classes: Iterable[type]) -> bool:
+    """
+    Returns ``True`` if the ``obj`` argument is an instance of all of the
+    classes in the ``classes`` argument.
+
+    :raises TypeError: If any element of classes is not a type.
+    """
+    if any(not isinstance(classinfo, type) for classinfo in classes):
+        raise TypeError("classes must contain types")
+    return all(isinstance(obj, classinfo) for classinfo in classes)
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_json_form_field_import_name():
+    # if the Django version >= 3.2 then user the new default JSONField
+    if django.VERSION[0:2] >= (3, 2):
+        default = "django.forms.JSONField"
+    else:
+        default = "django.contrib.postgres.forms.JSONField"
+
+    # Allow for override
+    form_field = env.str("DB_BACKEND_JSON_FORM_FIELD", default)
+    logger.info(f"Using django form JSONField: {form_field}")
+    return form_field
+
+
+def get_json_form_field():
+    form_field = get_json_form_field_import_name()
+    try:
+        return import_string(form_field)
+    except ImportError:
+        raise ImportError(
+            "drf_integrations can only work with a backend that supports JSON fields, "
+            "please make sure you set the DB_BACKEND_JSON_FORM_FIELD setting to the "
+            "JSONField of your backend."
+        ) from None
+
+
+def get_json_model_field_import_name():
+    # if the Django version >= 3.2 then user the new default JSONField
+    if django.VERSION[0:2] >= (3, 2):
+        default = "django.db.models.JSONField"
+    else:
+        default = "django.contrib.postgres.fields.JSONField"
+
+    # Allow for override
+    model_field = env.str("DB_BACKEND_JSON_FIELD", default)
+    logger.info(f"Using django model JSONField: {model_field}")
+    return model_field
+
+
+def get_json_model_field():
+    model_field = get_json_model_field_import_name()
+    try:
+        return import_string(model_field)
+    except ImportError:
+        raise ImportError(
+            "drf_integrations can only work with a backend that supports JSON fields, "
+            "please make sure you set the DB_BACKEND_JSON_FIELD setting to the "
+            "JSONField of your backend."
+        ) from None
