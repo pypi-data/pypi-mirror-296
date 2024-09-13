0a62e413be3b390a235ea69cfefd78b6b2eefd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:04:02 +0000

    Add package peptdeep/1.3.0/peptdeep-1.3.0.tar.gz

---
+++ packages/peptdeep/peptdeep-1.3.0.tar.gz/peptdeep-1.3.0/tests/test_cli.py
@@ -0,0 +1,11 @@
+#!python -m unittest tests.test_utils
+"""This module provides unit tests for peptdeep.cli."""
+
+# builtin
+import unittest
+
+# local
+import peptdeep.cli
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/peptdeep/peptdeep-1.3.0.tar.gz/peptdeep-1.3.0/tests/test_gui.py
@@ -0,0 +1,11 @@
+#!python -m unittest tests.test_utils
+"""This module provides unit tests for peptdeep.gui."""
+
+# builtin
+import unittest
+
+# local
+import peptdeep.gui
+
+if __name__ == "__main__":
+    unittest.main()
