bbc7d79c30ff8ef932748916aaf44a5fa67c70cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 15:03:57 +0000

    Add package django-pygwalker/0.0.9/django_pygwalker-0.0.9.tar.gz

---
+++ packages/django-pygwalker/django_pygwalker-0.0.9.tar.gz/django_pygwalker-0.0.9/README.md
@@ -0,0 +1,137 @@
+# django-pygwalker
+![](https://img.shields.io/pypi/v/django-pygwalker?color=blue) 
+[![Downloads](https://static.pepy.tech/badge/django-pygwalker)](https://pepy.tech/project/django-pygwalker)
+![](https://img.shields.io/pypi/status/django-pygwalker)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/23aa8dc3c2e30ac40cb4/maintainability)](https://codeclimate.com/github/djangoaddicts/django-pygwalker/maintainability)
+[![OpenSSF Best Practices](https://bestpractices.coreinfrastructure.org/projects/7682/badge)](https://bestpractices.coreinfrastructure.org/projects/7682)
+
+![PyPI - Python](https://img.shields.io/pypi/pyversions/django-pygwalker)
+![PyPI - Django](https://img.shields.io/pypi/djversions/django-pygwalker)
+
+
+
+This utility creates user interfaces for visual analysis using PyGWalker in your django application. A PyGWalker page can be created from a Django queryset, a static csv file, or an uploaded csv file.
+
+For more information on PyGWalker see the github repo available here: https://github.com/Kanaries/pygwalker 
+
+
+<br/>
+
+## Code Quality
+| Workflow | Description             | Status                                                                       |
+|----------|-------------------------|------------------------------------------------------------------------------|
+|Bandit|security checks|![Bandit](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/bandit.yaml/badge.svg)|
+|Black|code formatting|![Black](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/black.yaml/badge.svg)|
+|CodeQL|security analysis|[![CodeQL](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/github-code-scanning/codeql/badge.svg)](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/github-code-scanning/codeql)|
+|Coveralls|code coverage status|![Coveralls](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/coveralls.yaml/badge.svg)|
+|Isort|python import ordering|![Isort](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/isort.yaml/badge.svg)|
+|Mypy|static type checking|![Mypy](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/mypy.yaml/badge.svg)|
+|Pytest|unit testing|![Pytest](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/pytest.yaml/badge.svg)|
+|Radon|code complexity analysis|![Radon](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/radon.yaml/badge.svg)|
+|Ruff|static code analysis|![Ruff](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/ruff.yaml/badge.svg)|
+|Safety|dependency scanner|![Saftey](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/safety.yaml/badge.svg)|
+|Tox|python/django versions testing|![Tox](https://github.com/djangoaddicts/django-pygwalker/actions/workflows/tox.yaml/badge.svg)|
+
+
+### Code Coverage 
+[![Coverage Status](https://coveralls.io/repos/github/djangoaddicts/django-pygwalker/badge.svg)](https://coveralls.io/github/djangoaddicts/django-pygwalker)
+
+Dashboard:
+https://coveralls.io/github/djangoaddicts/django-pygwalker
+
+
+<br/>
+
+## Documentation
+[![Documentation Status](https://readthedocs.org/projects/django-pygwalker/badge/?version=latest)](https://django-pygwalker.readthedocs.io/en/latest/?badge=latest)
+
+Full documentation can be found on http://django-pygwalker.readthedocs.org. 
+
+Documentation source files are available in the docs folder.
+
+
+<br/>
+
+## Installation 
+- install via pip:
+    ``` 
+    pip install django-pygwalker
+    ```
+- add the following to your INSTALLED_APPS in settings.py:
+
+    ```python 
+    djangoaddicts.pygwalker
+    ```
+
+    ***NOTE:*** *adding djangoaddicts.pygwalker to INSTALLED_APPS is only required if you intend to use the built-in templates or the 'generic' PyGWalker view.* 
+
+- to include the generic PyGWalker view (creates the PyGWalker page from an upload of a csv file) add the following to your project-level urls.py:
+
+    ```python
+    path("pygwalker/", include("djangoaddicts.pygwalker.urls"), ),
+    ```
+
+
+<br/>
+
+## Features
+
+### PygWalkerView
+The PygWalkerView renders a page containing PyGWalker html. This view takes a queryset parameter and includes all data in the queryset for visualizations. By default fields defined in the model will be included. To exclude fields or include additional fields (such as related fields), use the field_list parameter to specify exact fields desired for visualizations.  
+
+A Bootstrap 5 template is included, but can be overwritten using the template_name parameter. 
+
+#### Parameters
+- **field_list:** list of model fields to include (defaults to fields defined in the model)
+- **queryset:** queryset providing data available to visualization
+- **theme:** PyGWalker theme to use for pyg html (defaults to "media")
+- **title:** title used on html render
+- **template_name:** template used when rendering page; (defaults to pygwalker/bs5/pygwalker.html)
+
+
+<br/>
+
+## Usage Examples
+
+```python
+from djangoaddicts.pygwalker.views import PygWalkerView
+
+class MyPygWalkerView(PygWalkerView):
+    queryset = MyModel.objects.all()
+```
+
+#### Explicitly Defined Fields
+
+```python
+from djangoaddicts.pygwalker.views import PygWalkerView
+
+class MyPygWalkerView(PygWalkerView):
+    queryset = MyModel.objects.all()
+    title = "MyModel Data Analysis"
+    theme = "light"
+    field_list = ["name", "some_field", "some_other__related_field", "id", "created_at", "updated_at"]
+```
+
+#### Custom Template
+Custom views/templates can be used to override the Bootstrap 5 templates provided by default view. Here is an example:
+
+```python
+from djangoaddicts.pygwalker.views import PygWalkerView
+
+class MyPygWalkerView(PygWalkerView):
+    queryset = MyModel.objects.all()
+    template_name = "my_custom_template.html"
+```
+
+<br/>
+
+## License
+django-pygwalker is licensed under the GNU-3 license (see the LICENSE file for details).
+
+https://github.com/djangoaddicts/django-pygwalker/blob/main/LICENSE
+
+<br/>
+
+## Contributing
+To contribute to django-pygwalker, please see [Contributing](.github/CONTRIBUTING.md)
---
+++ packages/django-pygwalker/django_pygwalker-0.0.9.tar.gz/django_pygwalker-0.0.9/src/djangoaddicts/pygwalker/views.py
@@ -0,0 +1,196 @@
+from __future__ import annotations
+
+import mimetypes
+
+import pandas as pd
+import pygwalker as pyg
+from django.apps import apps
+from django.conf import settings
+from django.contrib import messages
+from django.db.models import QuerySet
+from django.shortcuts import render
+from django.views.generic import View
+from handyhelpers.views.gui import HandyHelperListView, HandyHelperPaginatedListView
+
+from djangoaddicts.pygwalker.forms import UploadFileForm
+
+
+class PygWalkerView(View):
+    """View to create a PyGWalker visualization interface from a Django queryset.
+    See https://github.com/Kanaries/pygwalker for more information on PyGWalker.
+
+    class parameters:
+        field_list    - list of model fields to include; defaults to fields defined in the model
+        queryset      - queryset providing data available to visualization
+        template_name - template used when rendering page; defaults to: pygwalker/bs5/pyg.html
+        theme         - PyGWalker theme to use for pyg html; defaults to: "media"
+        title         - title used on html render
+
+    example:
+
+        from djangoaddicts.pygwalker.views import PygWalkerView
+        class MyPygView(PygWalkerView):
+            queryset = Order.objects.all()
+            title = "Order Data Analysis"
+            theme = "light"
+            field_list = ["status__name", "customer", "order_id", "created_at", "updated_at", "products"]
+    """
+
+    field_list: list = []
+    queryset: QuerySet = None
+    template_name: str = "pygwalker/bs5/pygwalker.html"
+    theme: str = getattr(settings, "PYGWALKER_THEME", "media")
+    title: str | None = None
+
+    def get(self, request):
+        referrer = request.META.get("HTTP_REFERER", "")
+        if "?" in referrer:
+            query_dict = {
+                key: value for key, value in (item.split("=") for item in referrer.split("?")[1].split("&") if item)
+            }
+        else:
+            query_dict = {}
+        self.queryset = self.queryset.filter(**query_dict)
+        if not self.title:
+            self.title = f"{self.queryset.model._meta.model_name.title()} Analysis"
+        pd_data = pd.DataFrame(list(self.queryset.values(*self.field_list)))
+        if pd_data.empty:
+            context = {"no_data": f"no {self.queryset.model.__name__} data found", "title": self.title}
+        else:
+            context = {"pyg": pyg.to_html(pd_data, appearance=self.theme), "title": self.title}
+        return render(request, self.template_name, context)
+
+
+class StaticCsvPygWalkerView(View):
+    """View to create a PyGWalker visualization interface from a statically definied csv file.
+    See https://github.com/Kanaries/pygwalker for more information on PyGWalker.
+
+    class parameters:
+        csv_file      - csv file containing data to visualize
+        template_name - template used when rendering page; defaults to: pygwalker/bs5/pyg.html
+        theme         - PyGWalker theme to use for pyg html; defaults to: "media"
+        title         - title used on html render
+
+    example:
+
+        from djangoaddicts.pygwalker.views import StaticCsvPygWalkerView
+        class MyPygView(StaticCsvPygWalkerView):
+            csv_file = "my_csv_file.csv
+            title = "Order Data Analysis"
+            theme = "light"
+    """
+
+    csv_file = None
+    template_name: str = "pygwalker/bs5/pygwalker.html"
+    theme: str = getattr(settings, "PYGWALKER_THEME", "media")
+    title: str = "Data Analysis"
+
+    def get(self, request):
+        pd_data = pd.read_csv(self.csv_file)
+        context = {"pyg": pyg.to_html(pd_data, appearance=self.theme), "title": self.title}
+        return render(request, self.template_name, context)
+
+
+class DynamicCsvPygWalkerView(View):
+    """View to create a PyGWalker visualization interface from a provided csv file.
+    See https://github.com/Kanaries/pygwalker for more information on PyGWalker.
+
+    class parameters:
+        template_name - template used when rendering page; defaults to: pygwalker/bs5/pygwalker_dynamic.html
+        theme         - PyGWalker theme to use for pyg html; defaults to "media"
+        title         - title used on html render
+    """
+
+    template_name: str = "pygwalker/bs5/pygwalker_dynamic.html"
+    theme: str = getattr(settings, "PYGWALKER_THEME", "media")
+    title: str = "Upload a csv file"
+
+    def get(self, request):
+        context = {"form": UploadFileForm(), "title": "Upload a csv file"}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        context = {}
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            csv_file = form.cleaned_data["csv_file"]
+            if mimetypes.guess_type(csv_file.name)[0] not in ["text/csv"]:
+                messages.add_message(
+                    request, messages.ERROR, "file provided is not a csv file", extra_tags="alert-danger"
+                )
+                return render(request, self.template_name, context)
+            pd_data = pd.read_csv(csv_file)
+            context["pyg"] = pyg.to_html(pd_data, appearance=self.theme)
+            context[
+                "title"
+            ] = f"""Showing data from <span class="text-secondary">{csv_file.name.split("/")[-1]}</span>"""
+            return render(request, self.template_name, context)
+        else:
+            for error in form.errors:
+                for i in form.errors[error].data:
+                    for msg in i.messages:
+                        messages.add_message(
+                            self.request,
+                            messages.ERROR,
+                            msg,
+                            extra_tags="alert-danger",
+                        )
+        return self.get(request)
+
+
+class GenericPygWalkerView(View):
+    """View to create a PyGWalker visualization interface from an app and model passed as kwargs. If query
+    parameters are present, include only filtered data, based on query parameters, in the PyGWalker interface."""
+
+    field_list: list = []
+    queryset: QuerySet = None
+    template_name: str = "pygwalker/bs5/pygwalker.html"
+    theme: str = getattr(settings, "PYGWALKER_THEME", "media")
+
+    def get(self, request, **kwargs):
+        """process GET request"""
+        referrer = request.META.get("HTTP_REFERER", "")
+        if "?" in referrer:
+            query_dict = {
+                key: value for key, value in (item.split("=") for item in referrer.split("?")[1].split("&") if item)
+            }
+        else:
+            query_dict = {}
+        model = apps.get_model(kwargs["app_name"], kwargs["model_name"])
+        self.queryset = model.objects.filter(**query_dict)
+        title = f"{self.queryset.model._meta.model_name.title()} Analysis"
+        pd_data = pd.DataFrame(list(self.queryset.values(*self.field_list)))
+        if pd_data.empty:
+            context = {"no_data": f"no {self.queryset.model.__name__} data found", "title": title}
+        else:
+            context = {"pyg": pyg.to_html(pd_data, appearance=self.theme), "title": title}
+        return render(request, self.template_name, context)
+
+
+class PygWalkerListViewMixin:
+    pygwalker_icon: str = """<i class="fa-solid fa-magnifying-glass-chart"></i>"""
+    pygwalker_title: str = "PyGWalker view"
+    pygwalker_url: str | None = None
+
+    def get(self, request, *args, **kwargs):
+        if not self.pygwalker_url:
+            self.pygwalker_url = (
+                f"/pygwalker/generic_pyg/{self.queryset.model._meta.app_label}/{self.queryset.model._meta.model_name}"
+            )
+
+        self.extra_controls["pygwalker"] = {
+            "icon": self.pygwalker_icon,
+            "title": self.pygwalker_title,
+            "url": self.pygwalker_url,
+        }
+        return super().get(request, *args, **kwargs)
+
+
+class PygWalkerListView(PygWalkerListViewMixin, HandyHelperListView):
+    """extend the HandyHelperListView (from handyhelpers) to add an icon for a PyGWalker
+    visualzation interface. If the list view is filtered, include only filtered data in the PyGWalker interface."""
+
+
+class PygWalkerPaginatedListView(PygWalkerListViewMixin, HandyHelperPaginatedListView):
+    """extend the HandyHelperPaginatedListView (from handyhelpers) to add an icon for a PyGWalker
+    visualzation interface. If the list view is filtered, include only filtered data in the PyGWalker interface."""
