230cf04d18517903fd917f7431a9a33be4c3a8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:04:21 +0000

    Add package graph-datasets/0.14.0/graph_datasets-0.14.0.tar.gz

---
+++ packages/graph-datasets/graph_datasets-0.14.0.tar.gz/graph_datasets-0.14.0/graph_datasets/__init__.py
@@ -0,0 +1,6 @@
+"""Graph Datasets
+"""
+__version__ = "0.14.0"
+
+from .load_data import load_data
+from .utils import *
---
+++ packages/graph-datasets/graph_datasets-0.14.0.tar.gz/graph_datasets-0.14.0/graph_datasets/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Utils
+"""
+# pylint:disable=invalid-name
+from .common import *
+from .statistics import edge_homo
+from .statistics import node_homo
+from .statistics import statistics
