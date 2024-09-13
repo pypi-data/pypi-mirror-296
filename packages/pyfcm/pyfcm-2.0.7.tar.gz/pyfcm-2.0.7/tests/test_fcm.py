e7d695ee90cd43e224f614437a60367566816dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 14:03:17 +0000

    Add package pyfcm/2.0.7/pyfcm-2.0.7.tar.gz

---
+++ packages/pyfcm/pyfcm-2.0.7.tar.gz/pyfcm-2.0.7/tests/test_fcm.py
@@ -0,0 +1,21 @@
+import pytest
+from pyfcm import FCMNotification, errors
+
+
+def test_push_service_without_credentials():
+    try:
+        FCMNotification(service_account_file="", project_id="", credentials=None)
+        assert False, "Should raise AuthenticationError without credentials"
+    except errors.AuthenticationError:
+        pass
+
+
+def test_notify(push_service, generate_response):
+    response = push_service.notify(
+        fcm_token="Test",
+        notification_body="Test",
+        notification_title="Test",
+        dry_run=True,
+    )
+
+    assert isinstance(response, dict)
