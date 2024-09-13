b937d40cf8f16d027839a2905ac646ff02beb35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 16:03:47 +0000

    Add package jaxsim/0.4.3.dev31/jaxsim-0.4.3.dev31-py3-none-any.whl

---
+++ packages/jaxsim/jaxsim-0.4.3.dev31-py3-none-any.whl/jaxsim/api/joint.py
@@ -0,0 +1,201 @@
+import functools
+from collections.abc import Sequence
+
+import jax
+import jax.numpy as jnp
+
+import jaxsim.api as js
+import jaxsim.typing as jtp
+from jaxsim import exceptions
+
+# =======================
+# Index-related functions
+# =======================
+
+
+@functools.partial(jax.jit, static_argnames="joint_name")
+def name_to_idx(model: js.model.JaxSimModel, *, joint_name: str) -> jtp.Int:
+    """
+    Convert the name of a joint to its index.
+
+    Args:
+        model: The model to consider.
+        joint_name: The name of the joint.
+
+    Returns:
+        The index of the joint.
+    """
+
+    if joint_name not in model.joint_names():
+        raise ValueError(f"Joint '{joint_name}' not found in the model.")
+
+    # Note: the index of the joint for RBDAs starts from 1, but the index for
+    # accessing the right element starts from 0. Therefore, there is a -1.
+    return (
+        jnp.array(
+            model.kin_dyn_parameters.joint_model.joint_names.index(joint_name) - 1
+        )
+        .astype(int)
+        .squeeze()
+    )
+
+
+def idx_to_name(model: js.model.JaxSimModel, *, joint_index: jtp.IntLike) -> str:
+    """
+    Convert the index of a joint to its name.
+
+    Args:
+        model: The model to consider.
+        joint_index: The index of the joint.
+
+    Returns:
+        The name of the joint.
+    """
+
+    exceptions.raise_value_error_if(
+        condition=jnp.array(
+            [joint_index < 0, joint_index >= model.number_of_joints()]
+        ).any(),
+        msg="Invalid joint index '{idx}'",
+        idx=joint_index,
+    )
+
+    return model.kin_dyn_parameters.joint_model.joint_names[joint_index + 1]
+
+
+@functools.partial(jax.jit, static_argnames="joint_names")
+def names_to_idxs(
+    model: js.model.JaxSimModel, *, joint_names: Sequence[str]
+) -> jax.Array:
+    """
+    Convert a sequence of joint names to their corresponding indices.
+
+    Args:
+        model: The model to consider.
+        joint_names: The names of the joints.
+
+    Returns:
+        The indices of the joints.
+    """
+
+    return jnp.array(
+        [name_to_idx(model=model, joint_name=name) for name in joint_names],
+    ).astype(int)
+
+
+def idxs_to_names(
+    model: js.model.JaxSimModel,
+    *,
+    joint_indices: Sequence[jtp.IntLike] | jtp.VectorLike,
+) -> tuple[str, ...]:
+    """
+    Convert a sequence of joint indices to their corresponding names.
+
+    Args:
+        model: The model to consider.
+        joint_indices: The indices of the joints.
+
+    Returns:
+        The names of the joints.
+    """
+
+    return tuple(idx_to_name(model=model, joint_index=idx) for idx in joint_indices)
+
+
+# ============
+# Joint limits
+# ============
+
+
+@jax.jit
+def position_limit(
+    model: js.model.JaxSimModel, *, joint_index: jtp.IntLike
+) -> tuple[jtp.Float, jtp.Float]:
+    """
+    Get the position limits of a joint.
+
+    Args:
+        model: The model to consider.
+        joint_index: The index of the joint.
+
+    Returns:
+        The position limits of the joint.
+    """
+
+    if model.number_of_joints() <= 1:
+        return jnp.empty(0).astype(float), jnp.empty(0).astype(float)
+
+    exceptions.raise_value_error_if(
+        condition=jnp.array(
+            [joint_index < 0, joint_index >= model.number_of_joints()]
+        ).any(),
+        msg="Invalid joint index '{idx}'",
+        idx=joint_index,
+    )
+
+    s_min = model.kin_dyn_parameters.joint_parameters.position_limits_min[joint_index]
+    s_max = model.kin_dyn_parameters.joint_parameters.position_limits_max[joint_index]
+
+    return s_min.astype(float), s_max.astype(float)
+
+
+@functools.partial(jax.jit, static_argnames=["joint_names"])
+def position_limits(
+    model: js.model.JaxSimModel, *, joint_names: Sequence[str] | None = None
+) -> tuple[jtp.Vector, jtp.Vector]:
+    """
+    Get the position limits of a list of joint.
+
+    Args:
+        model: The model to consider.
+        joint_names: The names of the joints.
+
+    Returns:
+        The position limits of the joints.
+    """
+
+    joint_names = joint_names if joint_names is not None else model.joint_names()
+
+    if len(joint_names) == 0:
+        return jnp.empty(0).astype(float), jnp.empty(0).astype(float)
+
+    joint_idxs = names_to_idxs(joint_names=joint_names, model=model)
+    return jax.vmap(lambda i: position_limit(model=model, joint_index=i))(joint_idxs)
+
+
+# ======================
+# Random data generation
+# ======================
+
+
+@functools.partial(jax.jit, static_argnames=["joint_names"])
+def random_joint_positions(
+    model: js.model.JaxSimModel,
+    *,
+    joint_names: Sequence[str] | None = None,
+    key: jax.Array | None = None,
+) -> jtp.Vector:
+    """
+    Generate random joint positions.
+
+    Args:
+        model: The model to consider.
+        joint_names: The names of the joints.
+        key: The random key.
+
+    Returns:
+        The random joint positions.
+    """
+
+    key = key if key is not None else jax.random.PRNGKey(seed=0)
+
+    s_min, s_max = position_limits(model=model, joint_names=joint_names)
+
+    s_random = jax.random.uniform(
+        minval=s_min,
+        maxval=s_max,
+        key=key,
+        shape=s_min.shape,
+    )
+
+    return s_random
---
+++ packages/jaxsim/jaxsim-0.4.3.dev31-py3-none-any.whl/jaxsim/terrain/terrain.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+
+import abc
+import dataclasses
+
+import jax.numpy as jnp
+import jax_dataclasses
+import numpy as np
+
+import jaxsim.typing as jtp
+
+
+class Terrain(abc.ABC):
+
+    delta = 0.010
+
+    @abc.abstractmethod
+    def height(self, x: jtp.FloatLike, y: jtp.FloatLike) -> jtp.Float:
+        pass
+
+    def normal(self, x: jtp.FloatLike, y: jtp.FloatLike) -> jtp.Vector:
+        """
+        Compute the normal vector of the terrain at a specific (x, y) location.
+
+        Args:
+            x: The x-coordinate of the location.
+            y: The y-coordinate of the location.
+
+        Returns:
+            The normal vector of the terrain surface at the specified location.
+        """
+
+        # https://stackoverflow.com/a/5282364
+        h_xp = self.height(x=x + self.delta, y=y)
+        h_xm = self.height(x=x - self.delta, y=y)
+        h_yp = self.height(x=x, y=y + self.delta)
+        h_ym = self.height(x=x, y=y - self.delta)
+
+        n = jnp.array(
+            [(h_xm - h_xp) / (2 * self.delta), (h_ym - h_yp) / (2 * self.delta), 1.0]
+        )
+
+        return n / jnp.linalg.norm(n)
+
+
+@jax_dataclasses.pytree_dataclass
+class FlatTerrain(Terrain):
+
+    z: float = dataclasses.field(default=0.0, kw_only=True)
+
+    @staticmethod
+    def build(height: jtp.FloatLike) -> FlatTerrain:
+
+        return FlatTerrain(z=float(height))
+
+    def height(self, x: jtp.FloatLike, y: jtp.FloatLike) -> jtp.Float:
+
+        return jnp.array(self.z, dtype=float)
+
+    def __hash__(self) -> int:
+
+        return hash(self.z)
+
+    def __eq__(self, other: FlatTerrain) -> bool:
+
+        if not isinstance(other, FlatTerrain):
+            return False
+
+        return self.z == other.z
+
+
+@jax_dataclasses.pytree_dataclass
+class PlaneTerrain(FlatTerrain):
+
+    plane_normal: tuple[float, float, float] = jax_dataclasses.field(
+        default=(0.0, 0.0, 1.0), kw_only=True
+    )
+
+    @staticmethod
+    def build(
+        plane_normal: jtp.VectorLike, plane_height_over_origin: jtp.FloatLike = 0.0
+    ) -> PlaneTerrain:
+        """
+        Create a PlaneTerrain instance with a specified plane normal vector.
+
+        Args:
+            plane_normal: The normal vector of the terrain plane.
+            plane_height_over_origin: The height of the plane over the origin.
+
+        Returns:
+            PlaneTerrain: A PlaneTerrain instance.
+        """
+
+        plane_normal = jnp.array(plane_normal, dtype=float)
+        plane_height_over_origin = jnp.array(plane_height_over_origin, dtype=float)
+
+        if plane_normal.shape != (3,):
+            msg = "Expected a 3D vector for the plane normal, got '{}'."
+            raise ValueError(msg.format(plane_normal.shape))
+
+        # Make sure that the plane normal is a unit vector.
+        plane_normal = plane_normal / jnp.linalg.norm(plane_normal)
+
+        return PlaneTerrain(
+            z=float(plane_height_over_origin),
+            plane_normal=tuple(plane_normal.tolist()),
+        )
+
+    def height(self, x: jtp.FloatLike, y: jtp.FloatLike) -> jtp.Float:
+        """
+        Compute the height of the terrain at a specific (x, y) location on a plane.
+
+        Args:
+            x: The x-coordinate of the location.
+            y: The y-coordinate of the location.
+
+        Returns:
+            The height of the terrain at the specified location on the plane.
+        """
+
+        # Equation of the plane:      A x + B y + C z + D = 0
+        # Normal vector coordinates:  (A, B, C)
+        # The height over the origin: -D/C
+
+        # Get the plane equation coefficients from the terrain normal.
+        A, B, C = self.plane_normal
+
+        # Compute the final coefficient D considering the terrain height.
+        D = -C * self.z
+
+        # Invert the plane equation to get the height at the given (x, y) coordinates.
+        return jnp.array(-(A * x + B * y + D) / C).astype(float)
+
+    def __hash__(self) -> int:
+
+        from jaxsim.utils.wrappers import HashedNumpyArray
+
+        return hash(
+            (
+                hash(self.z),
+                HashedNumpyArray.hash_of_array(
+                    array=jnp.array(self.plane_normal, dtype=float)
+                ),
+            )
+        )
+
+    def __eq__(self, other: PlaneTerrain) -> bool:
+
+        if not isinstance(other, PlaneTerrain):
+            return False
+
+        if not (
+            np.allclose(self.z, other.z)
+            and np.allclose(
+                np.array(self.plane_normal, dtype=float),
+                np.array(other.plane_normal, dtype=float),
+            )
+        ):
+            return False
+
+        return True
