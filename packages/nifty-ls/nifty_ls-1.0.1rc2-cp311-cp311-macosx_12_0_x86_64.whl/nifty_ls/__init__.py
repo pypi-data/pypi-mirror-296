17050e2b9c48a0c1ff5025c35044c55c2e03f31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 04:03:41 +0000

    Add package nifty-ls/1.0.1rc2/nifty_ls-1.0.1rc2-cp311-cp311-macosx_12_0_x86_64.whl

---
+++ packages/nifty-ls/nifty_ls-1.0.1rc2-cp311-cp311-macosx_12_0_x86_64.whl/nifty_ls/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+from .core import lombscargle
+from .core import NiftyResult
+
+from .version import __version__
+
+__all__ = ['lombscargle', 'NiftyResult', '__version__']
+
+
+# Make "fastnifty" available as a method for astropy's Lomb Scargle
+try:
+    import astropy.timeseries.periodograms.lombscargle.implementations.main as astropy_ls
+except ImportError:
+    logging.info('Astropy not found, fastnifty method will not be available')
+    astropy_ls = None
+
+if astropy_ls:
+    from .astropy import lombscargle_fastnifty
+
+    astropy_ls.METHODS['fastnifty'] = lombscargle_fastnifty
