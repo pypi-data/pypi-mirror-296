327c64e420b02afc3b7185a238521cc106216654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:15 +0000

    Add package MaaFw/2.0.0a1/MaaFw-2.0.0a1-py3-none-macosx_13_0_x86_64.whl

---
+++ packages/MaaFw/MaaFw-2.0.0a1-py3-none-macosx_13_0_x86_64.whl/maa/custom_recognizer.py
@@ -0,0 +1,88 @@
+import ctypes
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import numpy
+
+from .buffer import ImageBuffer, RectBuffer, StringBuffer
+from .context import Context
+from .define import *
+
+
+class CustomRecognizer(ABC):
+    _handle: MaaCustomRecognizerCallback
+
+    def __init__(self):
+        self._handle = self._c_analyze_agent
+
+    @dataclass
+    class AnalyzeArg:
+        task_detail: TaskDetail
+        current_task: str
+        custom_recognition_name: str
+        custom_recognition_param: str
+        image: numpy.ndarray
+        roi: Rect
+
+    @dataclass
+    class AnalyzeResult:
+        box: Optional[RectType]
+        detail: str
+
+    @abstractmethod
+    def analyze(
+        self,
+        context: Context,
+        argv: AnalyzeArg,
+    ) -> AnalyzeResult:
+        raise NotImplementedError
+
+    @property
+    def c_handle(self) -> MaaCustomRecognizerCallback:
+        return self._handle
+
+    @property
+    def c_arg(self) -> ctypes.c_void_p:
+        return ctypes.c_void_p.from_buffer(ctypes.py_object(self))
+
+    @MaaCustomRecognizerCallback
+    def _c_analyze_agent(
+        c_context: MaaContextHandle,
+        c_task_id: MaaTaskId,
+        c_task_name: ctypes.c_char_p,
+        c_custom_reco_name: ctypes.c_char_p,
+        c_custom_reco_param: ctypes.c_char_p,
+        c_image: MaaImageBufferHandle,
+        c_roi: MaaRectHandle,
+        c_transparent_arg: ctypes.c_void_p,
+        c_out_box: MaaRectHandle,
+        c_out_detail: MaaStringBufferHandle,
+    ) -> MaaBool:
+        if not c_transparent_arg:
+            return MaaBool(False)
+
+        self: CustomRecognizer = ctypes.cast(c_transparent_arg, ctypes.py_object).value
+
+        context = Context(c_context)
+        task_detail = context.tasker()._get_task_detail(c_task_id)
+
+        image = ImageBuffer(c_image).get()
+
+        result: CustomRecognizer.AnalyzeResult = self.analyze(
+            context,
+            CustomRecognizer.AnalyzeArg(
+                task_detail=task_detail,
+                current_task=c_task_name.decode(),
+                custom_recognition_name=c_custom_reco_name.decode(),
+                custom_recognition_param=c_custom_reco_param.decode(),
+                image=image,
+                roi=RectBuffer(c_roi).get(),
+            ),
+        )
+
+        if result.box:
+            RectBuffer(c_out_box).set(result.box)
+
+        StringBuffer(c_out_detail).set(result.detail)
+
+        return MaaBool(result.box is not None)
