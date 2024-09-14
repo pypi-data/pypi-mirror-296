2cc1f81a3292f7ce86b8af98b44db57e4655dcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:43 +0000

    Add package djing-admin/0.3.3/djing_admin-0.3.3.tar.gz

---
+++ packages/djing-admin/djing_admin-0.3.3.tar.gz/djing_admin-0.3.3/djing/core/decorators/ensure_published.pyi
@@ -0,0 +1,5 @@
+from _typeshed import Incomplete
+from django.http import HttpRequest as HttpRequest
+from djing.core.application import Application as Application
+
+def ensure_published(func: Incomplete | None = None, *, name: str = 'guest'): ...
