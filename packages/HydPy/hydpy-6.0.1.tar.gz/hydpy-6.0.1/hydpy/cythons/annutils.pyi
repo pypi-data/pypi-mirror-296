46008b43f11589d0a167376649c12806ea10a2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:04:06 +0000

    Add package HydPy/6.0.1/hydpy-6.0.1.tar.gz

---
+++ packages/HydPy/hydpy-6.0.1.tar.gz/hydpy-6.0.1/hydpy/cythons/annutils.pyi
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+
+from hydpy.core.typingtools import *
+
+class ANN:
+    # required for usage as an "algorithm" by interputils:
+    nmb_inputs: int
+    nmb_outputs: int
+    inputs: VectorFloat
+    outputs: VectorFloat
+    output_derivatives: VectorFloat
+    def calculate_values(self) -> None: ...
+    def calculate_derivatives(self, __idx_input: int) -> None: ...
+    # algorithm-specific requirements:
+    nmb_layers: int
+    nmb_neurons: VectorInt
+    weights_input: MatrixFloat
+    weights_output: MatrixFloat
+    weights_hidden: TensorFloat
+    intercepts_hidden: MatrixFloat
+    intercepts_output: VectorFloat
+    activation: MatrixInt
+    neurons: MatrixFloat
