9e46fb12ff1fd2bead1c13fdd18c7854d8e507d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:38 +0000

    Add package santali-textts/0.0.4/santali_textts-0.0.4.tar.gz

---
+++ packages/santali-textts/santali_textts-0.0.4.tar.gz/santali_textts-0.0.4/main.py
@@ -0,0 +1,5 @@
+from santali_textts import santali_audio
+
+text = "ᱥᱟᱠᱟᱢ ᱛᱟᱞᱟᱨᱮᱱ ᱛᱳᱵᱤ ᱵᱦᱮᱛᱨᱮ"
+output_filename = "output_audio.wav"
+santali_audio(text, output_filename)
---
+++ packages/santali-textts/santali_textts-0.0.4.tar.gz/santali_textts-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of the README file
+README = (Path(__file__).parent / 'README.md').read_text()
+
+setup(
+    name='santali_textts',
+    version='0.0.4',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'santali-audio=santali_audio:main'
+        ]
+    },
+    description='Convert Olchiki text into speech (Beta version)',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Shivnath Kisku',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
