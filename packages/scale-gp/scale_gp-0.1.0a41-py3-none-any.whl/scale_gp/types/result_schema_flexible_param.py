03912d22b9d2ea8ec5b3c3b07e2347844b828908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:44 +0000

    Add package scale-gp/0.1.0a41/scale_gp-0.1.0a41-py3-none-any.whl

---
+++ packages/scale-gp/scale_gp-0.1.0a41-py3-none-any.whl/scale_gp/types/result_schema_flexible_param.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Union, Iterable
+from typing_extensions import Required, TypeAlias, TypedDict
+
+from .shared_params.chunk_extra_info_schema import ChunkExtraInfoSchema
+from .shared_params.string_extra_info_schema import StringExtraInfoSchema
+from .evaluation_datasets.flexible_chunk_param import FlexibleChunkParam
+from .evaluation_datasets.flexible_message_param import FlexibleMessageParam
+
+__all__ = ["ResultSchemaFlexibleParam", "GenerationExtraInfo"]
+
+GenerationExtraInfo: TypeAlias = Union[ChunkExtraInfoSchema, StringExtraInfoSchema]
+
+
+class ResultSchemaFlexibleParam(TypedDict, total=False):
+    generation_output: Required[
+        Union[
+            str,
+            float,
+            Iterable[FlexibleChunkParam],
+            Iterable[FlexibleMessageParam],
+            Iterable[object],
+            Dict[
+                str,
+                Union[
+                    str, float, Iterable[FlexibleChunkParam], Iterable[FlexibleMessageParam], Iterable[object], object
+                ],
+            ],
+            object,
+        ]
+    ]
+
+    generation_extra_info: GenerationExtraInfo
