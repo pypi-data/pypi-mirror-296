2651730bee13f30e4ac06231d8021aa51f48602b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:04:29 +0000

    Add package loco-mujoco/0.4.1/loco-mujoco-0.4.1.tar.gz

---
+++ packages/loco-mujoco/loco-mujoco-0.4.1.tar.gz/loco-mujoco-0.4.1/loco_mujoco/environments/humanoids/__init__.py
@@ -0,0 +1,25 @@
+from .atlas import Atlas
+from .talos import Talos
+from .unitreeH1 import UnitreeH1
+from .unitreeG1 import UnitreeG1
+from .humanoids import HumanoidTorque, HumanoidMuscle, HumanoidTorque4Ages, HumanoidMuscle4Ages
+from .myoskeleton import MyoSkeleton
+
+
+# register environments in mushroom
+Atlas.register()
+Talos.register()
+UnitreeH1.register()
+UnitreeG1.register()
+HumanoidTorque.register()
+HumanoidMuscle.register()
+HumanoidTorque4Ages.register()
+HumanoidMuscle4Ages.register()
+MyoSkeleton.register()
+
+from gymnasium import register
+
+# register gymnasium wrapper environment
+register("LocoMujoco",
+         entry_point="loco_mujoco.environments.gymnasium:GymnasiumWrapper"
+         )
