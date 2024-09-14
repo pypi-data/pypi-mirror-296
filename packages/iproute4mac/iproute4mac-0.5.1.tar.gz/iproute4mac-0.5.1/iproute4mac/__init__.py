fca44933f1d9947a5b0d75e19d9cfa64b468182f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:38 +0000

    Add package iproute4mac/0.5.1/iproute4mac-0.5.1.tar.gz

---
+++ packages/iproute4mac/iproute4mac-0.5.1.tar.gz/iproute4mac-0.5.1/iproute4mac/__init__.py
@@ -0,0 +1,24 @@
+VERSION = "0.5.1"
+
+# global options
+OPTION = {
+    "preferred_family": 0,
+    "human_readable": False,
+    "use_iec": False,
+    "show_stats": False,
+    "show_details": False,
+    "oneline": False,
+    "brief": False,
+    "json": False,
+    "pretty": False,
+    "timestamp": False,
+    "timestamp_short": False,
+    "echo_request": False,
+    "force": False,
+    "max_flush_loops": 10,
+    "batch_mode": False,
+    "do_all": False,
+    "uid": -1,
+    "compress_vlans": False,
+    "verbose": 3,
+}
