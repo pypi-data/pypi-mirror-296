1ed607e7974dbc53aa3086c1f8829660cac3fe10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:41 +0000

    Add package physion/1.1/physion-1.1.tar.gz

---
+++ packages/physion/physion-1.1.tar.gz/physion-1.1/src/physion/__main__.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5 import QtWidgets
+
+from physion.gui.main import MainWindow
+app = QtWidgets.QApplication(sys.argv)
+
+try:
+    import qdarktheme # pip install pyqtdarktheme
+    app.setStyleSheet(qdarktheme.load_stylesheet('dark'))
+except ModuleNotFoundError:
+    pass
+
+GUI = MainWindow(app)
+
+sys.exit(app.exec_())
+
