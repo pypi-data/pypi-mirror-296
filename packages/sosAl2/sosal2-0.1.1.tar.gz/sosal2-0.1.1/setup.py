ad4296fdc004d1adb9d9ced5e416b3a1e9bd4355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:33 +0000

    Add package sosAl2/0.1.1/sosal2-0.1.1.tar.gz

---
+++ packages/sosAl2/sosal2-0.1.1.tar.gz/sosal2-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='sosAl2',
+  version='0.1.1',
+  author='feeland',
+  author_email='prokuronov.valera@mail.ru',
+  description='This is the simplest modu2le for quick work with files.',
+  long_description='ss',
+  packages=find_packages(),
+  keywords='files speedfiles2 ',
+  python_requires='>=3.6'
+)
