68c7294c47987e4119521dae5078b046218df1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:12 +0000

    Add package syncservers/0.0.7/syncservers-0.0.7-py3-none-any.whl

---
+++ packages/syncservers/syncservers-0.0.7-py3-none-any.whl/syncservers/app.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import asyncio
+import logging
+from easyschedule import EasyScheduler
+from syncservers.config import Config, ConfigSync
+from syncservers.async_task_queue import AsyncTaskQueue
+from syncservers.syncs import LiveSync, CronSync
+
+
+logger = logging.getLogger(__name__)
+
+
+class SyncServersApp:
+
+    def __init__(self, config, schedule) -> None:
+        self._config = config
+        self._schedule = schedule
+
+    def run(self):
+        logger.info(f"starting sync servers")
+
+        tasks = []
+
+        # async queue
+        async_queue = AsyncTaskQueue(self._config)
+
+        async_queue_tasks = async_queue.run()
+        tasks.extend(async_queue_tasks)
+
+        # live sync
+        if self._config.get_path_mappings(LiveSync.PATH_TYPE):
+            live_sync = LiveSync(self._config, async_queue)
+            live_sync.add_retry_task_to_schedule(self._schedule)
+            live_sync.add_to_startup_sync()
+            live_sync_tasks = live_sync.run()
+            tasks.extend(live_sync_tasks)
+
+        # cron sync
+        if self._config.get_path_mappings(CronSync.PATH_TYPE):
+            cron_sync = CronSync(self._config, async_queue)
+            cron_sync.add_retry_task_to_schedule(self._schedule)
+            cron_sync.add_to_startup_sync()
+            cron_sync.add_to_schedule(self._schedule)
+
+        return tasks
+
+
+async def run():
+    # init logging
+    from syncservers import config_logging
+    logs_folder_path = os.path.abspath("logs")
+    os.makedirs(logs_folder_path, exist_ok=True)
+    log_file_path = os.path.join(logs_folder_path, "syncservers.log")
+    config_logging(log_file_path)
+
+    tasks = []
+
+    # config
+    config_file_path = os.path.abspath(os.path.join("config", "syncservers.ini"))
+    config = Config(config_file_path)
+    if not config.load_config():
+        sys.exit(1)
+    config_sync = ConfigSync(config)
+    config_sync_task = config_sync.run()
+    tasks.append(config_sync_task)
+
+    # schedule
+    schedule = EasyScheduler()
+    schedule_task = asyncio.create_task(schedule.start())
+    tasks.append(schedule_task)
+
+    app = SyncServersApp(config, schedule)
+    tasks.extend(app.run())
+
+    # wait for tasks
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
