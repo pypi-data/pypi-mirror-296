8b06bd4a087dbc2a06859b100bc17ec3e6c1809f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:05:08 +0000

    Add package pytype/2024.9.13/pytype-2024.9.13-cp311-cp311-macosx_10_14_universal2.whl

---
+++ packages/pytype/pytype-2024.9.13-cp311-cp311-macosx_10_14_universal2.whl/pytype/tools/merge_pyi/test_data/partial.pep484.py
@@ -0,0 +1,15 @@
+def f1(a: e1, b: e2) -> r1:
+    pass
+
+def f2(a: e1, b: e2) -> r2:
+    pass
+
+def f3(a) -> r3:
+    pass
+
+def f4(a: e4, b):
+    pass
+
+def f5(a: e5) -> r5:
+    # type: (Any) -> Any
+    pass
