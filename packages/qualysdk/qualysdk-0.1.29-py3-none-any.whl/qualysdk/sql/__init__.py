8215caf672336f4fe8044957788d2787d87fe05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:25 +0000

    Add package qualysdk/0.1.29/qualysdk-0.1.29-py3-none-any.whl

---
+++ packages/qualysdk/qualysdk-0.1.29-py3-none-any.whl/qualysdk/sql/__init__.py
@@ -0,0 +1,33 @@
+"""
+SQL server integration for qualysdk
+"""
+
+from .base import db_connect
+from .vmdr import (
+    upload_vmdr_ags,
+    upload_vmdr_kb,
+    upload_vmdr_hosts,
+    upload_vmdr_ips,
+    upload_vmdr_hld,
+    upload_vmdr_scanners,
+    upload_vmdr_static_search_lists,
+    upload_vmdr_dynamic_search_lists,
+    upload_vmdr_users,
+    upload_vmdr_scan_list,
+    upload_vmdr_report_list,
+    upload_vmdr_scheduled_report_list,
+    upload_vmdr_template_list,
+    upload_vmdr_kb_qvs,
+    upload_vmdr_activity_log,
+)
+
+from .gav import upload_gav_hosts
+
+from .totalcloud import (
+    upload_totalcloud_aws_connectors,
+    upload_totalcloud_azure_connectors,
+    upload_totalcloud_gcp_connectors,
+    upload_totalcloud_control_metadata,
+)
+
+from .cloud_agent import upload_cloud_agents
