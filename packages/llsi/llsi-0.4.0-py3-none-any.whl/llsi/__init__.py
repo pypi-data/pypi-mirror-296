c0892feedbcdd4c2002d56493e01c67011da20d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:09 +0000

    Add package llsi/0.4.0/llsi-0.4.0-py3-none-any.whl

---
+++ packages/llsi/llsi-0.4.0-py3-none-any.whl/llsi/__init__.py
@@ -0,0 +1,6 @@
+from .polynomialmodel import PolynomialModel
+from .statespacemodel import StateSpaceModel
+from .sysidalg import sysid
+from .sysiddata import SysIdData
+from .utils import cv
+from .figure import Figure
