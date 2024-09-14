4e2399467248447403f0efe28040d106308d7e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:11 +0000

    Add package fetter/0.3.2/fetter-0.3.2.tar.gz

---
+++ packages/fetter/fetter-0.3.2.tar.gz/fetter-0.3.2/src/lib.rs
@@ -0,0 +1,28 @@
+// use std::env;
+use ::fetter::run_cli;
+use pyo3::prelude::*;
+use std::env;
+
+#[pyfunction]
+fn run(args: Vec<String>) -> PyResult<()> {
+    // let args: Vec<String> = py_args.iter().map(|arg| arg.to_string()).collect();
+    // env::set_var("RUST_BACKTRACE", "1");
+    run_cli(args);
+    Ok(())
+}
+
+#[pyfunction]
+fn run_with_argv() -> PyResult<()> {
+    // the first argument is Python, which must be removed
+    let args: Vec<String> = env::args().skip(1).collect();
+    // println!("args: {:?}", args);
+    run_cli(args);
+    Ok(())
+}
+
+#[pymodule]
+fn fetter(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run, m)?)?;
+    m.add_function(wrap_pyfunction!(run_with_argv, m)?)?;
+    Ok(())
+}
