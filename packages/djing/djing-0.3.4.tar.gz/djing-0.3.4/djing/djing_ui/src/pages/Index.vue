b955ab342d27ab54ed14b33653e32a03f59e0299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:03:54 +0000

    Add package djing/0.3.4/djing-0.3.4.tar.gz

---
+++ packages/djing/djing-0.3.4.tar.gz/djing-0.3.4/djing/djing_ui/src/pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    className="text-center h-screen flex justify-center flex-col items-center p-4"
+  >
+    <h1 className="text-3xl font-bold">Welcome, Djing Admin</h1>
+    <p className="text-green-600 mb-5">Your project is setup successfully!</p>
+    <h3 className="text-slate-500">Powered by:</h3>
+  </div>
+</template>
