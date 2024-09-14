84432fab11dd219ead01d14c78c33bd8d6e2af6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:36 +0000

    Add package gdmo/0.1.2/gdmo-0.1.2.tar.gz

---
+++ packages/gdmo/gdmo-0.1.2.tar.gz/gdmo-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='gdmo',
+    packages=find_packages(),
+    version='0.1.2',
+    description='GDMO Library for standardized actions and routines. Current options / functions are Forecast.Forecast, API.APIRequest, Tables.Landing, and Tables.Delta',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Stephan Kuiper',
+    install_requires=['prophet','matplotlib','statsmodels'],
+)
