78dc8bc26e7a00c15e96cede65f5f49fdd907f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:43 +0000

    Add package barkshark-sql/0.2.0/barkshark_sql-0.2.0.tar.gz

---
+++ packages/barkshark-sql/barkshark_sql-0.2.0.tar.gz/barkshark_sql-0.2.0/bsql/__init__.py
@@ -0,0 +1,48 @@
+__software__ = "Barkshark SQL"
+__version__ = "0.2.0"
+__author__ = "Zoey Mae"
+__homepage__ = "https://git.barkshark.xyz/barkshark/bsql"
+
+from .backends import Backend, PG8000, Sqlite3
+from .database import Connection, Cursor, Database
+from .enums import BackendType, Comparison, OrderDirection, StrEnum
+from .table import Tables, Table, Column, Row
+from .statement import Statement, Select, Insert, Update, Delete, Where, OrderBy
+
+from .misc import (
+	ColumnDescription,
+	ConnectionProto,
+	CursorProto,
+	StatementParsingError,
+	boolean
+)
+
+
+__all__ = [
+	"Backend",
+	"PG8000",
+	"Sqlite3",
+	"Connecction",
+	"Cursor",
+	"Database",
+	"BackendType",
+	"Comparison",
+	"OrderDirection",
+	"StrEnum",
+	"Tables",
+	"Table",
+	"Column",
+	"Statement",
+	"Select",
+	"Insert",
+	"Update",
+	"Delete",
+	"Where",
+	"OrderBy",
+	"ColumnDescription",
+	"ConnectionProto",
+	"CursorProto",
+	"Row",
+	"StatementParsingError",
+	"boolean"
+]
