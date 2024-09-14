4fc455ac66926c58cb878313470cce604166abb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:54 +0000

    Add package pyprogress-elunico/0.1.3/pyprogress_elunico-0.1.3.tar.gz

---
+++ packages/pyprogress-elunico/pyprogress_elunico-0.1.3.tar.gz/pyprogress_elunico-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyprogress-elunico",
+    version="0.1.3",
+    author="Thomas Povinelli",
+    author_email="tompov227@gmail.com",
+    description="A terminal progress printing utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/elunico/pyprogress",
+    keywords="progress bar terminal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
