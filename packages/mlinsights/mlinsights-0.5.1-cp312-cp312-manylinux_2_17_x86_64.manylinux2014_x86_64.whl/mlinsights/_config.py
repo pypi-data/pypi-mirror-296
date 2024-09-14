b916e43e30621c3a9ab5e481c6f7dc99723324c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:53 +0000

    Add package mlinsights/0.5.1/mlinsights-0.5.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/mlinsights/mlinsights-0.5.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mlinsights/_config.py
@@ -0,0 +1,11 @@
+HAS_CUDA = 0
+CXX_FLAGS = ' -Wall -Wno-unknown-pragmas -Wextra -mavx -fPIC'
+CMAKE_CXX_STANDARD_REQUIRED = 'ON'
+CMAKE_CXX_EXTENSIONS = 'OFF'
+CMAKE_CXX_STANDARD = 11
+
+# Was compiled with the following versions.
+CYTHON_VERSION = '3.0.11'
+SKLEARN_VERSION = '1.5.1'
+NUMPY_VERSION = '2.1.1'
+SCIPY_VERSION = '1.14.1'
