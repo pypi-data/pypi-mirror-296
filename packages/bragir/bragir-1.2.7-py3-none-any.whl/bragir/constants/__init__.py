aaf892366ebdf262e40cabd1fcc779d3f5656362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:31 +0000

    Add package bragir/1.2.7/bragir-1.2.7-py3-none-any.whl

---
+++ packages/bragir/bragir-1.2.7-py3-none-any.whl/bragir/constants/__init__.py
@@ -0,0 +1,5 @@
+from .files import BLACKLISTED_FILES
+from .format_specs import BITRATES_KBPS, DURATION_SECONDS_25MB
+from .ai import TOKEN_LIMIT
+
+__all__ = ["BLACKLISTED_FILES", "BITRATES_KBPS", "DURATION_SECONDS_25MB", "TOKEN_LIMIT"]
