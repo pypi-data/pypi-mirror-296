50edb00db2ecaf36569b949862ae077eef37ebfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:52 +0000

    Add package couchformation/4.0.0a390/couchformation-4.0.0a390.tar.gz

---
+++ packages/couchformation/couchformation-4.0.0a390.tar.gz/couchformation-4.0.0a390/couchformation/__init__.py
@@ -0,0 +1,10 @@
+import os
+from pkg_resources import parse_version
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+__version__ = "4.0.0a390"
+VERSION = parse_version(__version__)
+
+
+def get_data_dir():
+    return os.path.join(_ROOT, 'data')
