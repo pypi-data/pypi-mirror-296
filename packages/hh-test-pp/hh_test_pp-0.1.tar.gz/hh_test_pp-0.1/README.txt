7aef67642e77e46fc873b08eccb8aebdbf0d2629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:04:12 +0000

    Add package hh-test-pp/0.1/hh_test_pp-0.1.tar.gz

---
+++ packages/hh-test-pp/hh_test_pp-0.1.tar.gz/hh_test_pp-0.1/README.txt
@@ -0,0 +1,8 @@
+# My Popup Package
+
+A simple package to show popup messages on Windows.
+
+## Installation
+
+```bash
+pip install my_popup_package
---
+++ packages/hh-test-pp/hh_test_pp-0.1.tar.gz/hh_test_pp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hh_test_pp',
+    version='0.1',
+    description='A package to show popup messages on Windows',
+    author='hello_ev',
+    author_email='tzrjqddct0@zvvzuv.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
