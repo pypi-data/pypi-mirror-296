66db616cb58dd05e1540bfeb930e6925989166cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:45 +0000

    Add package addn-xiong3731/0.0.1/addn_xiong3731-0.0.1.tar.gz

---
+++ packages/addn-xiong3731/addn_xiong3731-0.0.1.tar.gz/addn_xiong3731-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+packages = ['addn']
+
+setup(
+    name='addn_xiong3731',          # 包名
+    version='0.0.1',      # 版本号
+    author='xiong3731',    # 作者名
+    packages=packages,    # 包名，用于 import
+)
+
