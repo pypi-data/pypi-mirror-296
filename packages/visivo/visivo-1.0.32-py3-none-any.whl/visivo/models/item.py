5fbde0e857bb939b71e2f149a5a5bf9e44e0f734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:53 +0000

    Add package visivo/1.0.32/visivo-1.0.32-py3-none-any.whl

---
+++ packages/visivo/visivo-1.0.32-py3-none-any.whl/visivo/models/item.py
@@ -0,0 +1,82 @@
+from visivo.models.selector import Selector
+from .base.base_model import BaseModel, REF_REGEX, generate_ref_field
+from .base.parent_model import ParentModel
+from pydantic import Field
+from typing import Optional, Union
+from .chart import Chart
+from .table import Table
+from pydantic import model_validator
+import uuid
+
+
+class Item(BaseModel, ParentModel):
+    """
+    The Item houses one chart, table or markdown object. It also informs the width that the chart, table or markdown should occupy within a row. Widths are evaluated for each item in the row by summing all of the widths and then using the relative weights.
+
+    In the example below, the markdown would take up 1/4th of the row and would be positioned on the left edge. The table would also take up 1/4th of the page and would sit to the right of the markdown. The chart would take up 1/2 of the page and would touch the right edge of the row.
+
+    ``` yaml
+    items:
+      - width: 1
+        markdown: "# Some inline **markdown**"
+      - width: 1
+        table: ref(table-name)
+      - width: 2
+        chart: ref(chart-name)
+      - width: 1
+        selector: ref(selector-name)
+    ```
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._id = kwargs["id"] if "id" in kwargs else uuid.uuid4().hex
+
+    def id(self):
+        return f"Item - {self._id}"
+
+    width: int = Field(
+        1,
+        description="The width of the Item determines is evaluated relative to the other items in a row.",
+    )
+    markdown: Optional[str] = Field(
+        None, description="Markdown text to include in the dashboard."
+    )
+    chart: Optional[generate_ref_field(Chart)] = Field(
+        None, description="A chart object defined inline or a ref() to a chart."
+    )
+    table: Optional[generate_ref_field(Table)] = Field(
+        None, description="A Table object defined inline or a ref() to a table"
+    )
+    selector: Optional[generate_ref_field(Selector)] = Field(
+        None, description="A Selector object defined inline or a ref() to a selector"
+    )
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_unique_item_types(cls, data: any):
+        markdown, chart, table, selector = (
+            data.get("markdown"),
+            data.get("chart"),
+            data.get("table"),
+            data.get("selector"),
+        )
+        items_set = [i for i in [markdown, chart, table, selector] if i is not None]
+        if len(items_set) > 1:
+            raise ValueError(
+                'only one of the "markdown", "chart", "table", or "selector" properties should be set on an item'
+            )
+        return data
+
+    def child_items(self):
+        return [self.__get_child()]
+
+    def __get_child(self):
+        if self.markdown is not None:
+            return self.markdown
+        if self.table is not None:
+            return self.table
+        if self.chart is not None:
+            return self.chart
+        if self.selector is not None:
+            return self.selector
---
+++ packages/visivo/visivo-1.0.32-py3-none-any.whl/visivo/models/row.py
@@ -0,0 +1,51 @@
+from .base.base_model import BaseModel
+from .base.parent_model import ParentModel
+from pydantic import Field
+from typing import List
+from enum import Enum
+from .item import Item
+
+
+class HeightEnum(str, Enum):
+    compact = "compact"
+    xsmall= "xsmall"
+    small = "small"
+    medium = "medium"
+    large = "large"
+    xlarge = "xlarge"
+    xxlarge = 'xxlarge'
+
+
+class Row(BaseModel, ParentModel):
+    """
+    Rows are the horizontal component of the dashboard grid and house 1 to many Items.
+
+    !!! tip
+        You can set the height of a row using the `height` attribute on a row
+        
+        ??? information
+
+            | Height | Pixels |
+            |------------|-------|
+            | compact | wrapped |
+            | xsmall | 128 |
+            | small | 256 |
+            | medium | 396 |
+            | large | 512 |
+            | xlarge | 768 |
+            | xxlarge | 1024 |
+    """
+
+    def id(self):
+        return f"Row - {','.join(list(map(lambda i: i.id(), self.items)))}"
+
+    height: HeightEnum = Field(
+        HeightEnum.medium, description="Sets the height of the row."
+    )
+    items: List[Item] = Field(
+        None,
+        description="A list of items containing tables, charts or markdown. Items are placed in the row in the order that they are listed from left to right.",
+    )
+
+    def child_items(self):
+        return self.items
