b32ff4d1f9769995c87145ed22696724a209e953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:02 +0000

    Add package envaml/0.1.2/envaml-0.1.2.tar.gz

---
+++ packages/envaml/envaml-0.1.2.tar.gz/envaml-0.1.2/src/envaml/__init__.py
@@ -0,0 +1,15 @@
+import click
+from .converter import convert_yaml_to_env
+
+
+@click.command()
+@click.argument('yaml_file')
+@click.argument('env_file')
+def cli(yaml_file, env_file):
+    """Convert YAML_FILE to ENV_FILE."""
+    convert_yaml_to_env(yaml_file, env_file)
+    click.echo(f"Converted {yaml_file} to {env_file}")
+
+
+# if __name__ == '__main__':
+#     cli()
