2aac2682e224bd5077b9af4c6debb5632275880d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:05:10 +0000

    Add package pybiolib/1.2.174/pybiolib-1.2.174.tar.gz

---
+++ packages/pybiolib/pybiolib-1.2.174.tar.gz/pybiolib-1.2.174/biolib/tables.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+from rich.console import Console
+from rich.table import Table, Column
+
+from biolib.typing_utils import List, Any
+
+
+class BioLibTable:
+    def __init__(self, columns_to_row_map: OrderedDict, rows: List[Any], title):
+        self.title = title
+        self.rows = rows
+        self.columns_to_row_map = columns_to_row_map
+        self.table = self._create_table()
+
+    def _create_table(self) -> Table:
+        columns = [Column(header=header, **meta['params']) for header, meta in self.columns_to_row_map.items()]
+        table = Table(*columns, title=self.title)
+        for row in self.rows:
+            table.add_row(
+                *[str(row[column['key']]) for column in self.columns_to_row_map.values()]
+            )
+        return table
+
+    def print_table(self):
+        console = Console()
+        console.print(self.table)
