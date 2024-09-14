ca7b37e99a73a37f8f1f9b40cb01baf043a2a943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:48 +0000

    Add package weavel/1.9.2/weavel-1.9.2-py3-none-any.whl

---
+++ packages/weavel/weavel-1.9.2-py3-none-any.whl/weavel/_constants.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if os.getenv("WEAVEL_TESTING") == "true":
+    BACKEND_SERVER_URL = "http://localhost:8000"
+else:
+    BACKEND_SERVER_URL = "https://api.weavel.ai"
