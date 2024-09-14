943a2dece9c7fbddb227d2eae91de574fa5988d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:04:32 +0000

    Add package craft-parts/1.33.1/craft_parts-1.33.1.tar.gz

---
+++ packages/craft-parts/craft_parts-1.33.1.tar.gz/craft_parts-1.33.1/craft_parts/sources/__init__.py
@@ -0,0 +1,22 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Source handler definitions and helpers."""
+
+from . import errors
+from .local_source import LocalSource
+from .snap_source import SnapSource
+from .sources import SourceHandler, get_source_handler, get_source_type_from_uri
