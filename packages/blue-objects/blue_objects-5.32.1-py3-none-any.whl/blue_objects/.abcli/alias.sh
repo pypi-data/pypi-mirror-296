3c54056d5c0692b1ab73ebc9de62491bd2ec5917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:03:48 +0000

    Add package blue-objects/5.32.1/blue_objects-5.32.1-py3-none-any.whl

---
+++ packages/blue-objects/blue_objects-5.32.1-py3-none-any.whl/blue_objects/.abcli/alias.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+alias @download=abcli_download
+
+alias @gif=abcli_gif
+
+alias @cache=abcli_cache
+alias @ref="abcli_cache read"
+alias @tag=abcli_tag
+alias @relation=abcli_relation
+
+alias @clone=abcli_clone
+alias @copy=abcli_copy
+alias @cp=abcli_cp
+
+alias @host=abcli_host
+
+alias @publish=abcli_publish
+
+alias @select=abcli_select
+
+alias @upload=abcli_upload
