41fcbad355e615cae652ea052e676d9af005f233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:03:54 +0000

    Add package blue-objects/5.33.1/blue_objects-5.33.1.tar.gz

---
+++ packages/blue-objects/blue_objects-5.33.1.tar.gz/blue_objects-5.33.1/setup.py
@@ -0,0 +1,25 @@
+from blue_objects import NAME, VERSION, DESCRIPTION, REPO_NAME
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.file",
+        f"{NAME}.graphics",
+        f"{NAME}.host",
+        f"{NAME}.storage",
+    ],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            "config.env",
+            "sample.env",
+            ".abcli/**/*.sh",
+        ],
+    },
+)
