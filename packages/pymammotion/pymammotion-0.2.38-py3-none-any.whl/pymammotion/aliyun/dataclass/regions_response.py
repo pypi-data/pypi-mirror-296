0a3d102b1268de6ffa06e95035ee89d7583e2f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:42 +0000

    Add package pymammotion/0.2.38/pymammotion-0.2.38-py3-none-any.whl

---
+++ packages/pymammotion/pymammotion-0.2.38-py3-none-any.whl/pymammotion/aliyun/dataclass/regions_response.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional, TypeVar
+
+from mashumaro import DataClassDictMixin
+from mashumaro.config import BaseConfig
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+DataT = TypeVar("DataT")
+
+
+@dataclass
+class RegionResponseData(DataClassORJSONMixin):
+    shortRegionId: str
+    oaApiGatewayEndpoint: str
+    regionId: str
+    mqttEndpoint: str
+    pushChannelEndpoint: str
+    regionEnglishName: str
+    apiGatewayEndpoint: str
+
+
+@dataclass
+class RegionResponse(DataClassDictMixin):
+    data: RegionResponseData
+    code: int
+    id: Optional[str] = None
+    msg: Optional[str] = None
+
+    class Config(BaseConfig):
+        omit_default = True
