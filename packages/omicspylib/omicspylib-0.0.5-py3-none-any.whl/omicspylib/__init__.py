e9b8f92e56f669343ecacc56f766241269943b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:53 +0000

    Add package omicspylib/0.0.5/omicspylib-0.0.5-py3-none-any.whl

---
+++ packages/omicspylib/omicspylib-0.0.5-py3-none-any.whl/omicspylib/__init__.py
@@ -0,0 +1,18 @@
+"""
+Starting point for importing libraries main objects.
+"""
+from omicspylib.datasets.proteins import ProteinsDataset
+from omicspylib.datasets.peptides import PeptidesDataset
+from omicspylib.comparisons.pairs.statistical import PairwiseComparisonTTestFC
+from omicspylib.comparisons.pairs.frequency_based import PairwiseUniqueEntryComparison
+
+
+__all__ = [
+    'PeptidesDataset',
+    'ProteinsDataset',
+    'PairwiseComparisonTTestFC',
+    'PairwiseUniqueEntryComparison'
+]
+
+
+__version__ = '0.0.5'
