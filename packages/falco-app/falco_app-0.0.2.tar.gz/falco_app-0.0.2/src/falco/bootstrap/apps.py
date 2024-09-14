2fdf1418286e1e09a1edcad5c7903be8a519a9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:03:49 +0000

    Add package falco-app/0.0.2/falco_app-0.0.2.tar.gz

---
+++ packages/falco-app/falco_app-0.0.2.tar.gz/falco_app-0.0.2/src/falco/bootstrap/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FalcoBootstrapConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "falco.bootstrap"
---
+++ packages/falco-app/falco_app-0.0.2.tar.gz/falco_app-0.0.2/src/falco/favicons/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FalcoFaviconsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "falco.favicons"
