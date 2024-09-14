9a18d781fc5c07fdcd4a67dcaf96c57d55a1bbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:18 +0000

    Add package micrograd_andrews/0.1.6/micrograd_andrews-0.1.6.tar.gz

---
+++ packages/micrograd_andrews/micrograd_andrews-0.1.6.tar.gz/micrograd_andrews-0.1.6/micrograd_andrews/__init__.py
@@ -0,0 +1,5 @@
+"""
+Micrograd module by Andrews Peter - A simple branch of Karpathy's micrograd project.
+Mainly branched off for logging purposes but who knows what I else I will do.
+"""
+__version__ = "0.1.6"  # Change this line to specify the version for flit publish
