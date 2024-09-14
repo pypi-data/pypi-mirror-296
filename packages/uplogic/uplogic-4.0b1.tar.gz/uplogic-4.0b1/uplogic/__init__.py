6b84d718f73761e17a9550d7d0610ddc11d407c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:05:21 +0000

    Add package uplogic/4.0b1/uplogic-4.0b1.tar.gz

---
+++ packages/uplogic/uplogic-4.0b1.tar.gz/uplogic-4.0b1/uplogic/__init__.py
@@ -0,0 +1,32 @@
+__version__ = '4.0b1'
+
+try:
+    from . import animation
+    from . import audio
+    from . import events
+    from . import input
+    from . import console
+    from . import network
+    from . import physics
+    from . import shaders
+    from . import ui
+    from . import utils
+except:
+    print('Not in runtime!')
+import bpy
+
+
+def check_version(version: str):
+    nums = version.split('.')
+    vnums = __version__.split('.')
+    
+    while len(vnums) < len(nums):
+        vnums.append(0)
+    
+    for i, num in enumerate(nums):
+        if int(num) < int(vnums[i]):
+            return False
+
+    return True
+
+
