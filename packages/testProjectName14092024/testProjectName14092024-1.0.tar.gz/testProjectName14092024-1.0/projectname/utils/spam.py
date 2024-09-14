58fba89406e2b336802bd73c4c1a71670b2ef25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:19 +0000

    Add package testProjectName14092024/1.0/testProjectName14092024-1.0.tar.gz

---
+++ packages/testProjectName14092024/testProjectName14092024-1.0.tar.gz/testProjectName14092024-1.0/projectname/utils/spam.py
@@ -0,0 +1,1 @@
+print("I'm spam under utils")
---
+++ packages/testProjectName14092024/testProjectName14092024-1.0.tar.gz/testProjectName14092024-1.0/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='testProjectName14092024', version='1.0', author='Tester', author_email='test@test.com', url='https://www.google.com', packages=['projectname', 'projectname.utils'])
