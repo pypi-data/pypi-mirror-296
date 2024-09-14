0ee7cd520b06476db215b8b51e6534f74abbe9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:41 +0000

    Add package akkpopo/0.4/akkpopo-0.4.tar.gz

---
+++ packages/akkpopo/akkpopo-0.4.tar.gz/akkpopo-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='akkpopo',
+    version='0.4',
+    description='A package to show popup messages on Windows',
+    author='hello_ev',
+    author_email='tzrjqddct0@zvvzuv.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
