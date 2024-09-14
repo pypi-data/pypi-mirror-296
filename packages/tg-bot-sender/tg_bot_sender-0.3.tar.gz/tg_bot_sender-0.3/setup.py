070b6a807c447bcedc31a9d87e91c55e39c7f2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:04:12 +0000

    Add package tg-bot-sender/0.3/tg_bot_sender-0.3.tar.gz

---
+++ packages/tg-bot-sender/tg_bot_sender-0.3.tar.gz/tg_bot_sender-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tg_bot_sender',
+      version='0.3',
+      description='This lib for sending message to tg bots',
+      packages=['tg_bot_sender'],
+      author_email='poznkirill3@gmail.com',
+      long_description="""[Документация/Documentation](https://magnificent-maamoul-2f7a55.netlify.app/)""",
+      long_description_content_type='text/markdown',
+      zip_safe=False)
