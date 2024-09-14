6e3663e5a13265f0d95c5be81d81e59914a26b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:10 +0000

    Add package exinc/1.0.3/exinc-1.0.3-py3-none-any.whl

---
+++ packages/exinc/exinc-1.0.3-py3-none-any.whl/exinc/default_config.py
@@ -0,0 +1,22 @@
+# this is a python configuration file for Exinc
+# DO NOT TOUCH THE NEXT LINE
+RELEASE = 1
+
+# put here your default include paths
+DEFAULT_PATHS = []
+
+# put here your default compilation flags in shlex array form
+# additional flags provided by command line/Exinc constructor
+# will be merged with this
+DEFAULT_FLAGS = [
+    "-std=c++17",
+    "-fdiagnostics-color=always",
+]
+
+# put here your default compilation line in shlex array form
+# DO NOT CHANGE THIS VALUE IF YOU DONT KNOW ITS GONNA WORK XD
+DEFAULT_COMPILER = ["g++"]
+
+# put here config which is specifically related to Caide
+CMD_PATH = ""
+CLANG_INCLUDES = ""
