51a8c00988d5e75ec78481b47e77f273d8bc740d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:01 +0000

    Add package controlflow/0.9.3/controlflow-0.9.3.tar.gz

---
+++ packages/controlflow/controlflow-0.9.3.tar.gz/controlflow-0.9.3/tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from prefect.testing.utilities import prefect_test_harness
+
+from controlflow.settings import temporary_settings
+
+from .fixtures import *
+
+
+@pytest.fixture(autouse=True, scope="session")
+def temp_controlflow_settings():
+    with temporary_settings(
+        pretty_print_agent_events=False,
+        log_all_messages=True,
+        log_level="DEBUG",
+        orchestrator_max_agent_turns=10,
+        orchestrator_max_llm_calls=10,
+    ):
+        yield
+
+
+@pytest.fixture(autouse=True)
+def reset_settings_after_each_test():
+    with temporary_settings():
+        yield
+
+
+@pytest.fixture(autouse=True, scope="session")
+def prefect_test_fixture():
+    """
+    Run Prefect against temporary sqlite database
+    """
+    with prefect_test_harness():
+        yield
