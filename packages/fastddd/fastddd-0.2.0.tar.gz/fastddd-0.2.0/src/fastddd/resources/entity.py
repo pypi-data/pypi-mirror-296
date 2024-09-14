b0775422be204fe4f3a2c9d86d8e091318eff792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:56 +0000

    Add package fastddd/0.2.0/fastddd-0.2.0.tar.gz

---
+++ packages/fastddd/fastddd-0.2.0.tar.gz/fastddd-0.2.0/src/fastddd/resources/entity.py
@@ -0,0 +1,35 @@
+import string
+from datetime import datetime, timezone
+import secrets
+from uuid import uuid4
+from pydantic import BaseModel, ConfigDict
+
+
+class Entity(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id_field: str = "id"
+
+    def set_id_field(self, id_field: str):
+        self.id_field = id_field
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Entity):
+            return False
+        return getattr(self, self.id_field) == getattr(value, self.id_field)
+
+    @classmethod
+    def generate_id(cls):
+        return str(uuid4())
+
+    @classmethod
+    def generate_secret(cls, size: int = 12, include_punctuation=False):
+        pool = string.ascii_letters + string.digits
+        if include_punctuation:
+            pool += string.punctuation
+        secret = "".join([secrets.choice(pool) for _ in range(size)])
+        return secret
+
+    @classmethod
+    def now(cls) -> datetime:
+        return datetime.now(timezone.utc)
