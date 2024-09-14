26f5e3cd7270b8ac2d078c04fdc85908b2c4ebef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:03:22 +0000

    Add package hiku/0.7.5/hiku-0.7.5.tar.gz

---
+++ packages/hiku/hiku-0.7.5.tar.gz/hiku-0.7.5/hiku/executors/sync.py
@@ -0,0 +1,36 @@
+from typing import (
+    TypeVar,
+    Callable,
+    TYPE_CHECKING,
+    Any,
+)
+
+from hiku.executors.base import BaseSyncExecutor
+from hiku.result import Proxy
+
+if TYPE_CHECKING:
+    from hiku.executors.queue import (
+        Queue,
+        Workflow,
+    )
+
+
+T = TypeVar("T")
+
+
+class FutureLike:
+    def __init__(self, result: T) -> None:
+        self._result = result
+
+    def result(self) -> T:  # type: ignore[type-var]
+        return self._result
+
+
+class SyncExecutor(BaseSyncExecutor):
+    def submit(self, fn: Callable, *args: Any, **kwargs: Any) -> FutureLike:
+        return FutureLike(fn(*args, **kwargs))
+
+    def process(self, queue: "Queue", workflow: "Workflow") -> Proxy:
+        while queue.__futures__:
+            queue.progress(queue.__futures__)
+        return workflow.result()
