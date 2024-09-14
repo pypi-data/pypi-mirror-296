315ca93f1ae7ffb88d66daff50517976f2dd29b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:04 +0000

    Add package tarsier/0.7.2/tarsier-0.7.2-py3-none-any.whl

---
+++ packages/tarsier/tarsier-0.7.2-py3-none-any.whl/tarsier/core.py
@@ -0,0 +1,112 @@
+from asyncio import Protocol
+from pathlib import Path
+from typing import Dict, Tuple
+
+from tarsier._utils import load_js
+from tarsier.adapter import AnyDriver, BrowserAdapter, adapter_factory
+from tarsier.ocr import OCRService
+from tarsier.text_format import format_text
+
+TagToXPath = Dict[int, str]
+
+
+class ITarsier(Protocol):
+    async def page_to_image(self, driver: AnyDriver) -> Tuple[bytes, Dict[int, str]]:
+        raise NotImplementedError()
+
+    async def page_to_text(self, driver: AnyDriver) -> Tuple[str, Dict[int, str]]:
+        raise NotImplementedError()
+
+
+class Tarsier(ITarsier):
+    _JS_TAG_UTILS = Path(__file__).parent / "tag_utils.min.js"
+
+    def __init__(self, ocr_service: OCRService):
+        self._ocr_service = ocr_service
+        self._js_utils = load_js(self._JS_TAG_UTILS)
+
+    async def page_to_image(
+        self,
+        driver: AnyDriver,
+        tag_text_elements: bool = False,
+        tagless: bool = False,
+        keep_tags_showing: bool = False,
+    ) -> Tuple[bytes, TagToXPath]:
+        adapter = adapter_factory(driver)
+        tag_to_xpath = (
+            await self._tag_page(adapter, tag_text_elements) if not tagless else {}
+        )
+        if tagless:
+            await self._remove_tags(adapter)
+
+        screenshot = await self._take_screenshot(adapter)
+
+        if not keep_tags_showing:
+            await self._remove_tags(adapter)
+
+        return screenshot, tag_to_xpath if not tagless else {}
+
+    async def page_to_text(
+        self,
+        driver: AnyDriver,
+        tag_text_elements: bool = False,
+        tagless: bool = False,
+        keep_tags_showing: bool = False,
+    ) -> Tuple[str, TagToXPath]:
+        image, tag_to_xpath = await self.page_to_image(
+            driver, tag_text_elements, tagless, keep_tags_showing
+        )
+        page_text = self._run_ocr(image)
+        return page_text, tag_to_xpath
+
+    async def page_to_image_and_text(
+        self,
+        driver: AnyDriver,
+        tag_text_elements: bool = False,
+        tagless: bool = False,
+        keep_tags_showing: bool = False,
+    ) -> Tuple[bytes, str, TagToXPath]:
+        image, tag_to_xpath = await self.page_to_image(
+            driver, tag_text_elements, tagless, keep_tags_showing
+        )
+        page_text = self._run_ocr(image)
+        return image, page_text, tag_to_xpath
+
+    @staticmethod
+    async def _take_screenshot(adapter: BrowserAdapter) -> bytes:
+        viewport = await adapter.get_viewport_size()
+        default_width = viewport["width"]
+
+        await adapter.set_viewport_size(default_width, viewport["content_height"])
+        screenshot = await adapter.take_screenshot()
+        await adapter.set_viewport_size(default_width, viewport["height"])
+
+        return screenshot
+
+    def _run_ocr(self, image: bytes) -> str:
+        ocr_text = self._ocr_service.annotate(image)
+        page_text = format_text(ocr_text)
+        return page_text
+
+    async def _tag_page(
+        self, adapter: BrowserAdapter, tag_text_elements: bool = False
+    ) -> Dict[int, str]:
+        await self._load_tarsier_utils(adapter)
+
+        script = f"return window.tagifyWebpage({str(tag_text_elements).lower()});"
+        tag_to_xpath = await adapter.run_js(script)
+
+        return {int(key): value for key, value in tag_to_xpath.items()}
+
+    async def _remove_tags(self, adapter: BrowserAdapter) -> None:
+        await self._load_tarsier_utils(adapter)
+        script = "return window.removeTags();"
+
+        await adapter.run_js(script)
+
+    async def remove_tags(self, driver: AnyDriver) -> None:
+        adapter = adapter_factory(driver)
+        await self._remove_tags(adapter)
+
+    async def _load_tarsier_utils(self, adapter: BrowserAdapter) -> None:
+        await adapter.run_js(self._js_utils)
