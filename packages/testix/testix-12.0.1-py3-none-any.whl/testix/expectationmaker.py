5f34bdb3b8eaca69d4b4eea423d8d0c38e2d34fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:45 +0000

    Add package testix/12.0.1/testix-12.0.1-py3-none-any.whl

---
+++ packages/testix/testix-12.0.1-py3-none-any.whl/testix/expectationmaker.py
@@ -0,0 +1,46 @@
+from . import call_character
+import testix.expectations.call
+from testix import expectations
+import testix.call_modifiers.synchronous
+import testix.call_modifiers.asynchronous
+import testix.call_modifiers.awaitable
+import testix.call_modifiers.trivial
+
+
+class ExpectationMaker:
+    def __init__(self, scenario, scenarioMocks, path, character: call_character.CallCharacter):
+        self.__scenario = scenario
+        self.__scenarioMocks = scenarioMocks
+        self.__path = path
+        self.__character = character
+
+    def __getattr__(self, name):
+        return self.__next_expectation_maker(name)
+
+    def __next_expectation_maker(self, name):
+        childPath = '{path}.{name}'.format(path=self.__path, name=name)
+        return ExpectationMaker(self.__scenario, self.__scenarioMocks, childPath, self.__character)
+
+    def __call__(self, *args, **kwargs):
+        call = self.__generate_expectation(*args, **kwargs)
+        self.__scenario.addEvent(call)
+        if call.extra_path is not None:
+            extra = expectations.call.Call(call.extra_path, testix.call_modifiers.trivial.Trivial)
+            self.__scenario.addEvent(extra)
+        return call
+
+    def __setitem__(self, key, value):
+        expectation_maker = self.__next_expectation_maker('__setitem__')
+        return expectation_maker(key, value)
+
+    def __generate_expectation(self, *args, **kwargs):
+        if self.__character.normal:
+            modifier = testix.call_modifiers.trivial.Trivial
+        if self.__character.awaitable:
+            modifier = testix.call_modifiers.awaitable.Awaitable
+        if self.__character.is_sync_context:
+            modifier = testix.call_modifiers.synchronous.Synchronous
+        if self.__character.is_async_context:
+            modifier = testix.call_modifiers.asynchronous.Asynchronous
+
+        return expectations.call.Call(self.__path, modifier, *args, **kwargs)
