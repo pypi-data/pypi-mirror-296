32f77a4cbcfa9f32be0b974cd96c927315658a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:02 +0000

    Add package pyroherd/0.0.6/pyroherd-0.0.6-py3-none-any.whl

---
+++ packages/pyroherd/pyroherd-0.0.6-py3-none-any.whl/pyroherd/raw/types/emoji_status_until.py
@@ -0,0 +1,111 @@
+#  <<<<<<< HEAD
+#  =======
+#  <<<<<<< HEAD
+#  >>>>>>> c723d0e (update)
+#  Pyroherd - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present OnTheHerd <https://github.com/OnTheHerd>
+#
+#  This file is part of Pyroherd.
+#
+#  Pyroherd is free software: you can redistribute it and/or modify
+#  <<<<<<< HEAD
+#  =======
+#  =======
+#  Pyroherd - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyroherd.
+#
+#  Pyroherd is free software: you can redistribute it and/or modify
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  <<<<<<< HEAD
+#  Pyroherd is distributed in the hope that it will be useful,
+#  =======
+#  <<<<<<< HEAD
+#  Pyroherd is distributed in the hope that it will be useful,
+#  =======
+#  Pyroherd is distributed in the hope that it will be useful,
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  <<<<<<< HEAD
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  =======
+#  <<<<<<< HEAD
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  =======
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+
+from io import BytesIO
+
+from pyroherd.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from pyroherd.raw.core import TLObject
+from pyroherd import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class EmojiStatusUntil(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~pyroherd.raw.base.EmojiStatus`.
+
+    Details:
+        - Layer: ``187``
+        - ID: ``FA30A8C7``
+
+    Parameters:
+        document_id (``int`` ``64-bit``):
+            N/A
+
+        until (``int`` ``32-bit``):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["document_id", "until"]
+
+    ID = 0xfa30a8c7
+    QUALNAME = "types.EmojiStatusUntil"
+
+    def __init__(self, *, document_id: int, until: int) -> None:
+        self.document_id = document_id  # long
+        self.until = until  # int
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "EmojiStatusUntil":
+        # No flags
+        
+        document_id = Long.read(b)
+        
+        until = Int.read(b)
+        
+        return EmojiStatusUntil(document_id=document_id, until=until)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(Long(self.document_id))
+        
+        b.write(Int(self.until))
+        
+        return b.getvalue()
---
+++ packages/pyroherd/pyroherd-0.0.6-py3-none-any.whl/pyroherd/raw/types/update_group_call.py
@@ -0,0 +1,111 @@
+#  <<<<<<< HEAD
+#  =======
+#  <<<<<<< HEAD
+#  >>>>>>> c723d0e (update)
+#  Pyroherd - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present OnTheHerd <https://github.com/OnTheHerd>
+#
+#  This file is part of Pyroherd.
+#
+#  Pyroherd is free software: you can redistribute it and/or modify
+#  <<<<<<< HEAD
+#  =======
+#  =======
+#  Pyroherd - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyroherd.
+#
+#  Pyroherd is free software: you can redistribute it and/or modify
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  <<<<<<< HEAD
+#  Pyroherd is distributed in the hope that it will be useful,
+#  =======
+#  <<<<<<< HEAD
+#  Pyroherd is distributed in the hope that it will be useful,
+#  =======
+#  Pyroherd is distributed in the hope that it will be useful,
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  <<<<<<< HEAD
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  =======
+#  <<<<<<< HEAD
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  =======
+#  along with Pyroherd.  If not, see <http://www.gnu.org/licenses/>.
+#  >>>>>>> 47ad949 (update)
+#  >>>>>>> c723d0e (update)
+
+from io import BytesIO
+
+from pyroherd.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from pyroherd.raw.core import TLObject
+from pyroherd import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class UpdateGroupCall(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~pyroherd.raw.base.Update`.
+
+    Details:
+        - Layer: ``187``
+        - ID: ``14B24500``
+
+    Parameters:
+        chat_id (``int`` ``64-bit``):
+            N/A
+
+        call (:obj:`GroupCall <pyroherd.raw.base.GroupCall>`):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["chat_id", "call"]
+
+    ID = 0x14b24500
+    QUALNAME = "types.UpdateGroupCall"
+
+    def __init__(self, *, chat_id: int, call: "raw.base.GroupCall") -> None:
+        self.chat_id = chat_id  # long
+        self.call = call  # GroupCall
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "UpdateGroupCall":
+        # No flags
+        
+        chat_id = Long.read(b)
+        
+        call = TLObject.read(b)
+        
+        return UpdateGroupCall(chat_id=chat_id, call=call)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(Long(self.chat_id))
+        
+        b.write(self.call.write())
+        
+        return b.getvalue()
