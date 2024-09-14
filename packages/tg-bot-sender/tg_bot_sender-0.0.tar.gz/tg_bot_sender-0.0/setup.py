619b28b7ce1cfd4b68c09496cce4ce737fc69841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:04:08 +0000

    Add package tg-bot-sender/0.0/tg_bot_sender-0.0.tar.gz

---
+++ packages/tg-bot-sender/tg_bot_sender-0.0.tar.gz/tg_bot_sender-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tg_bot_sender',
+      version='0.0',
+      description='This lib for sending message to tg bots',
+      packages=['tg_bot_sender'],
+      author_email='poznkirill3@gmail.com',
+      zip_safe=False)
