f68777b57bd83529251cf6f86ce89229d63111a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:03:27 +0000

    Add package yta-multimedia/0.0.82/yta_multimedia-0.0.82-py3-none-any.whl

---
+++ packages/yta-multimedia/yta_multimedia-0.0.82-py3-none-any.whl/yta_multimedia/video/edition/effect/moviepy/position_effects/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ScreenEdge(Enum):
+    BOTTOM_LEFT = 'bottom_left'
+    LEFT = 'left'
+    TOP_LEFT = 'top_left'
+    TOP = 'top'
+    TOP_RIGHT = 'top_right'
+    RIGHT = 'right'
+    BOTTOM_RIGHT = 'bottom_right'
+    BOTTOM = 'bottom'
