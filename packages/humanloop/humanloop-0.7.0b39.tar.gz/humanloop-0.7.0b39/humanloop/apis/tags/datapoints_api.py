8e49c7c329fb7eff71fc549c9a9293253370c014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:04 +0000

    Add package humanloop/0.7.0b39/humanloop-0.7.0b39.tar.gz

---
+++ packages/humanloop/humanloop-0.7.0b39.tar.gz/humanloop-0.7.0b39/humanloop/apis/tags/datapoints_api.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from humanloop.apis.tags.datapoints_api_generated import DatapointsApiGenerated
+
+class DatapointsApi(DatapointsApiGenerated):
+    pass
---
+++ packages/humanloop/humanloop-0.7.0b39.tar.gz/humanloop-0.7.0b39/humanloop/apis/tags/datasets_api.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from humanloop.apis.tags.datasets_api_generated import DatasetsApiGenerated
+
+class DatasetsApi(DatasetsApiGenerated):
+    pass
