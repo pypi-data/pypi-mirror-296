9518932f8cf33e63fcb26e8d9bd57a5cfe445e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:39 +0000

    Add package codern/4.4.8/codern-4.4.8.tar.gz

---
+++ packages/codern/codern-4.4.8.tar.gz/codern-4.4.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    with open(filename, encoding='utf-8') as f:return f.read()
+
+setup(
+    name='codern',
+    version='4.4.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'httpx',
+        'bs4',
+        'urllib3',
+        'pycryptodome',
+        'pillow',
+    ],
+    include_package_data=True,
+    description='Codern Team Library from api free O - > api-free.ir',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://api-free.ir',
+    author='Mahdi Ahmadi',
+    author_email='mahdiahmadi.1208@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
