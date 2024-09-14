3ca629b9fa73e126d07928e5ce88cfd6b363bc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:03:25 +0000

    Add package asymmetric-encryption/0.2.0/asymmetric-encryption-0.2.0.tar.gz

---
+++ packages/asymmetric-encryption/asymmetric-encryption-0.2.0.tar.gz/asymmetric-encryption-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+DESC = 'A simple asymmetric encryption module'
+with open("README.md", 'r') as f:
+    LONG_DESC = f.read()
+setup(
+    name="asymmetric-encryption",
+    version="0.2.0",
+    author="Roy Nisimov, jacebalaron (Daniel Gaisenberg)",
+    description=DESC,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    packages=find_packages(),
+    url='https://github.com/RoyNisimov/AsymmetricEncryption',
+    license='MIT',
+    install_requires=[],
+    keywords=['python', 'cipher', 'asymmetric encryptions', 'signing', 'verifying', 'protocols', 'encryption', 'decryption', "signature", "ring signature", "rsa", "dsa", "ecc"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
