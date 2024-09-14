8adaed739e3311a84fca6758c4e75b68fbc29a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:36 +0000

    Add package bigdata-client/1.6.1/bigdata_client-1.6.1-py3-none-any.whl

---
+++ packages/bigdata-client/bigdata_client-1.6.1-py3-none-any.whl/bigdata/__init__.py
@@ -0,0 +1,29 @@
+from bigdata.bigdata import Bigdata
+from bigdata.models.entities import (
+    Company,
+    Concept,
+    Facility,
+    Landmark,
+    Organization,
+    OrganizationType,
+    Person,
+    Place,
+    Product,
+    ProductType,
+)
+from bigdata.models.topics import Topic
+
+__all__ = [
+    "Bigdata",
+    "Company",
+    "Concept",
+    "Facility",
+    "Landmark",
+    "Organization",
+    "OrganizationType",
+    "Person",
+    "Place",
+    "Product",
+    "ProductType",
+    "Topic",
+]
---
+++ packages/bigdata-client/bigdata_client-1.6.1-py3-none-any.whl/bigdata/models/sharing.py
@@ -0,0 +1,6 @@
+from bigdata.enum_utils import StrEnum
+
+
+class SharePermission(StrEnum):
+    READ = "read"
+    UNDEFINED = "undefined"
