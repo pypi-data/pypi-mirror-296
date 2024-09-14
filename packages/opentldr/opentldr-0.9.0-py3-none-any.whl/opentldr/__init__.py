68feab94f94ae4895496130f59bacaae61cb830a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:24 +0000

    Add package opentldr/0.9.0/opentldr-0.9.0-py3-none-any.whl

---
+++ packages/opentldr/opentldr-0.9.0-py3-none-any.whl/opentldr/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.9.0"
+print("OpenTLDR version {version}".format(version=__version__))
+
+from .KnowledgeGraph import KnowledgeGraph
+from .Workflow import Workflow
+from .DataRepo import DataRepo
