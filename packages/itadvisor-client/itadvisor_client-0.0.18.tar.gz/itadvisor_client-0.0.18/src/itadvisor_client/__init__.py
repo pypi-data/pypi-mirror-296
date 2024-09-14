44a7028fc4d921688e7caa3b29a0b8908893dacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 06:04:44 +0000

    Add package itadvisor-client/0.0.18/itadvisor_client-0.0.18.tar.gz

---
+++ packages/itadvisor-client/itadvisor_client-0.0.18.tar.gz/itadvisor_client-0.0.18/src/itadvisor_client/__init__.py
@@ -0,0 +1,37 @@
+from . import(itadvisor_client)
+
+from .itadvisor_client import(ITAdvisor,
+        _Assets,
+        _AuditTrail,
+        _Authentication,
+        _AuthenticationServers,
+        _Backup,
+        _Certificates,
+        _ChangeRequest,
+        _ChangeRequestTemplate,
+        _Configuration,
+        _CustomProperties,
+        _Customers,
+        _CustomersCount,
+        _EquipmentBrowser,
+        _ETLConfiguration,
+        _Genomes,
+        _KPIS,
+        _Licenses,
+        _Mail,
+        _PlatformStatus,
+        _PowerCapacity,
+        _PowerPath,
+        _Routing,
+        _SensorMapping,
+        _StruxureOn,
+        _SVG,
+        _UserGroups,
+        _UserMessage,
+        _Users,
+         _WorkOrders)
+
+__all__ = ('ITAdvisor')
+
+if __name__ == "__main__":
+    pass
