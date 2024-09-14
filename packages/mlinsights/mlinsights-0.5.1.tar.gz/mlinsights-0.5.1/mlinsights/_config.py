9122fa6df764267d0ddc33a6242d95721944feda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:53 +0000

    Add package mlinsights/0.5.1/mlinsights-0.5.1.tar.gz

---
+++ packages/mlinsights/mlinsights-0.5.1.tar.gz/mlinsights-0.5.1/mlinsights/_config.py
@@ -0,0 +1,11 @@
+HAS_CUDA = 0
+CXX_FLAGS = ' -Wall -Wno-unknown-pragmas -Wextra -mavx -fPIC'
+CMAKE_CXX_STANDARD_REQUIRED = ''
+CMAKE_CXX_EXTENSIONS = ''
+CMAKE_CXX_STANDARD = 11
+
+# Was compiled with the following versions.
+CYTHON_VERSION = '3.0.11'
+SKLEARN_VERSION = '1.6.dev0'
+NUMPY_VERSION = '2.1.1'
+SCIPY_VERSION = '1.14.1'
