1bd5b283e6aa84aa25f6763d4cd41e548a105782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:45 +0000

    Add package advanced-automapper/0.1.11/advanced_automapper-0.1.11.tar.gz

---
+++ packages/advanced-automapper/advanced_automapper-0.1.11.tar.gz/advanced_automapper-0.1.11/src/automapper/sql_alchemy_mapper.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict, Type
+
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+from automapper.mapping_plugin import MappingPlugin
+from automapper.types import TSource, TTarget
+
+
+class SqlAlchemyMapper(MappingPlugin):
+    """SqlAlchemy plugin for the mapping
+
+    Args:
+        MappingPlugin (_type_): _description_
+    """
+
+    def can_handle(self, source: TSource, target: TTarget) -> bool:
+        return isinstance(source, DeclarativeMeta)
+
+    def get_source_fields(self, source: Any) -> Dict[str, Type]:
+        return {key: value for key, value in source.__mapper__.c.items()}
