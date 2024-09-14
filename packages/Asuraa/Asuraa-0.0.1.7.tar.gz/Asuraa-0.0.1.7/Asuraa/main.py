0b8d4d8788f969c3a70af289d4cbfde0ea971392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:04:09 +0000

    Add package Asuraa/0.0.1.7/Asuraa-0.0.1.7.tar.gz

---
+++ packages/Asuraa/Asuraa-0.0.1.7.tar.gz/Asuraa-0.0.1.7/Asuraa/main.py
@@ -0,0 +1,271 @@
+import random
+import requests
+import string,re,os,io
+import base64,json,time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib
+from requests_html import HTMLSession
+from PIL import Image, ImageDraw, ImageFont
+
+__all__ = ["api"]
+
+
+class Asuraa:
+    
+    def __init__(self)->None:
+        """Api for various purpose
+    support group : https://t.me/the_support_chat
+    owner : @AsuraaSupport
+        """
+        pass
+    
+    @staticmethod
+    def datagpt(args:str):
+        """
+        Sends a query to a specified datagpt API endpoint to retrieve a response based on the provided question.
+
+        Args:
+            args (str): The question or input for the datagpt.
+
+        Returns:
+            str: The response text from the datagpt API.
+
+        Example usage:
+        >>> from Asuraa import api
+        >>> response = api.datagpt("What are the latest trends in AI?")
+        >>> print(response)
+        """
+        url = "https://app.creator.io/api/chat=="
+        payload = {
+            "question": args,
+            "chatbotId": "712544d1-0c95-459e-ba22-45bae8905bed",
+            "session_id": "8a790e7f-ec7a-4834-be4a-40a78dfb525f",
+            "site": "datacareerjumpstart.mykajabi.com"
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            extracted_text = re.findall(r"\{(.*?)\}", response.text, re.DOTALL)
+            extracted_json = "{" + extracted_text[0] + "}]}"
+            json_text = extracted_json.replace('\n', ' ')
+
+            data = json.loads(json_text)
+            return {"results":data["text"],"join": "@AsuraaSupport", "success": True
+                    }
+        except Exception as e:
+            return e   
+
+    @staticmethod
+    def blackbox(args: str) -> requests.Response:
+        """
+        Interact with the Blackbox AI API for generating content. 🧠
+
+        Args:
+            args (str): The input text to interact with the Blackbox AI chat API.
+
+        Returns:
+            requests.Response: The response object from the API request.
+
+        Example usage:
+        >>> from Asuraa import api
+        >>> response = api.blackbox("Hello, how are you?")
+        >>> print(response.text)
+        {
+            "response": "Generated content response",
+            "status": 200
+        }
+        """
+
+        url = 'https://www.blackbox.ai/api/chat='
+        
+        payload = {
+            "agentMode": {},
+            "codeModelMode": True,
+            "id": "XM7KpOE",
+            "isMicMode": False,
+            "maxTokens": None,
+            "messages": [
+                {
+                    "id": "XM7KpOE",
+                    "content": urllib.parse.unquote(args),
+                    "role": "user"
+                }
+            ],
+            "previewToken": None,
+            "trendingAgentMode": {},
+            "userId": "87cdaa48-cdad-4dda-bef5-6087d6fc72f6",
+            "userSystemPrompt": None
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Cookie': 'sessionId=f77a91e1-cbe1-47d0-b138-c2e23eeb5dcf; intercom-id-jlmqxicb=4cf07dd8-742e-4e3f-81de-38669816d300; intercom-device-id-jlmqxicb=1eafaacb-f18d-402a-8255-b763cf390df6; intercom-session-jlmqxicb=',
+            'Origin': 'https://www.blackbox.ai=',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+        }
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            if response.status_code == 200:
+                return {"results": response.text, "join": "@AsuraaSupport", "success": True}
+        except Exception as e:
+            return e  
+    
+    @staticmethod
+    def gemini(args: str) -> dict:
+        """
+        Generate content using the Gemini API. ✨
+
+        Args:
+            args (str): The input text to generate content.
+
+        Returns:
+            dict: A dictionary containing the generated content with metadata.
+
+        Example usage:
+        >>> from Asuraa import api
+        >>> generated_content = api.gemini("Hello, how are you?")
+        >>> print(generated_content)
+        """
+        url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBQaoUF-KWjPV_4ABtSN0DQ0RPkNeCh4tM'
+        headers = {'Content-Type': 'application/json'}
+        payload = {
+            'contents': [
+                {'parts': [{'text': args}]}
+            ]
+        }
+
+        try:
+            response = requests.post(url, headers=headers, data=json.dumps(payload))
+            if response.status_code == 200:
+                generated_text = response.json()["candidates"][0]["content"]["parts"][0]["text"]
+                return {"results":generated_text,"join": "@AsuraaSupport", "success": True}
+        except Exception as e:
+            return e
+            
+
+    @staticmethod
+    def imdb(args: str) -> dict:
+        """
+        Retrieve information about a movie or TV show from IMDb based on the search query.
+
+        Args:
+        args (str): The movie or TV show to search for on IMDb.
+
+        Returns:
+        dict: A dictionary containing details about the movie or TV show, such as name, description, genre,
+            actors, trailer link, and more.
+
+        Example usage:
+        >>> from Asuraa import api
+        >>> movie_data = api.imdb("The Godfather")
+        >>> print(movie_data)
+        """
+
+        session = HTMLSession()
+
+        url = f"https://www.imdb.com/find?q={args}"
+        response = session.get(url)
+        results = response.html.xpath("//section[@data-testid='find-results-section-title']/div/ul/li")
+        urls = [result.find("a")[0].attrs["href"] for result in results][0]
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        response = requests.get(f"https://www.imdb.com/{urls}", headers=headers)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        movie_name = soup.title.text.strip()
+
+        meta_tags = soup.find_all("meta")
+        description = ""
+        keywords = ""
+
+        for tag in meta_tags:
+            if tag.get("name", "") == "description":
+                description = tag.get("content", "")
+            elif tag.get("name", "") == "keywords":
+                keywords = tag.get("content", "")
+
+        json_data = soup.find("script", type="application/ld+json").string
+        parsed_json = json.loads(json_data)
+
+        movie_url = parsed_json["url"]
+        movie_image = parsed_json["image"]
+        movie_description = parsed_json["description"]
+        movie_review_body = parsed_json["review"]["reviewBody"]
+        movie_review_rating = parsed_json["review"]["reviewRating"]["ratingValue"]
+        movie_genre = parsed_json["genre"]
+        movie_actors = [actor["name"] for actor in parsed_json["actor"]]
+        movie_trailer = parsed_json["trailer"]
+        
+        output = []
+        for result in results:
+            name = result.text.replace("\n", " ")
+            url = result.find("a")[0].attrs["href"]
+            if ("Podcast" not in name) and ("Music Video" not in name):
+                try:
+                    image = result.xpath("//img")[0].attrs["src"]
+                    file_id = url.split("/")[2]
+                    output.append({
+                        "movie_name": movie_name,
+                        "id": file_id,
+                        "poster": image,
+                        "description": description,
+                        "keywords": keywords,
+                        "movie_url": movie_url,
+                        "movie_image": movie_image,
+                        "movie_description": movie_description,
+                        "movie_review_body": movie_review_body,
+                        "movie_review_rating": movie_review_rating,
+                        "movie_genre": movie_genre,
+                        "movie_actors": movie_actors,
+                        "movie_trailer": movie_trailer,
+                        "join": "@AsuraaSupport",
+                        "success": True,
+                    })
+                    return {"results": output}
+                except:
+                    return {"Success": False}
+    
+    @staticmethod
+    def imagine(prompt: str) -> bytes:
+        """Generates an AI-generated image based on the provided prompt.
+
+        Args:
+            prompt (str): The input prompt for generating the image.
+
+        Returns:
+            bytes: The generated image in bytes format.
+            
+        Example usage:
+        >>> from Asuraa import api
+        >>> generated_image= api.imagine("boy image")
+        >>> print(generated_image)
+        """
+        url = 'https://ai-api.magicstudio.com/api/ai-art-generator'
+
+        form_data = {
+            'prompt': prompt,
+            'output_format': 'bytes',
+            'request_timestamp': str(int(time.time())),
+            'user_is_subscribed': 'false',
+        }
+
+        response = requests.post(url, data=form_data)
+        if response.status_code == 200:
+            try:
+                if response.content:
+                    return response.content
+                else:
+                    raise Exception("Failed to get image from the server.")
+            except Exception as e:
+                raise e
+        else:
+            raise Exception("Error:", response.status_code)
+
+        
+            
+api=Asuraa()
