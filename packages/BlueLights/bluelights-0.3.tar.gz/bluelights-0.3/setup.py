6de8077bf2009e9a7f56a700fb4f0a6ad4a47727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:42 +0000

    Add package BlueLights/0.3/bluelights-0.3.tar.gz

---
+++ packages/BlueLights/bluelights-0.3.tar.gz/bluelights-0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BlueLights',
+    version='0.3',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [ 
+        'bleak',
+        'qasync',
+        'PyQt6',
+        'python-dotenv',
+        'nest_asyncio',
+        'asyncio',
+        'colorsys'
+    ],
+    entry_points = {
+        'console_scripts': [ 
+            'bluelights=manager.main:main',
+        ],
+    },
+    author = 'Walkercito',
+    author_email = 'walkercitoliver@gmail.com',
+    description ='A library for controlling MohuanLED lights via Bluetooth',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3", 
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent", 
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.8'
+)
