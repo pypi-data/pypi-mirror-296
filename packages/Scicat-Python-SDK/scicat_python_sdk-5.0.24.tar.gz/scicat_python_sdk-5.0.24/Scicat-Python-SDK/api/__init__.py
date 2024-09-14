c92bc20af2033935881965fbd7f0919c81349f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:40 +0000

    Add package Scicat-Python-SDK/5.0.24/scicat_python_sdk-5.0.24.tar.gz

---
+++ packages/Scicat-Python-SDK/scicat_python_sdk-5.0.24.tar.gz/scicat_python_sdk-5.0.24/Scicat-Python-SDK/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from Scicat-Python-SDK.api.admin_api import AdminApi
+from Scicat-Python-SDK.api.auth_api import AuthApi
+from Scicat-Python-SDK.api.datasets_api import DatasetsApi
+from Scicat-Python-SDK.api.default_api import DefaultApi
+from Scicat-Python-SDK.api.elastic_search_api import ElasticSearchApi
+from Scicat-Python-SDK.api.instruments_api import InstrumentsApi
+from Scicat-Python-SDK.api.jobs_api import JobsApi
+from Scicat-Python-SDK.api.logbooks_api import LogbooksApi
+from Scicat-Python-SDK.api.origdatablocks_api import OrigdatablocksApi
+from Scicat-Python-SDK.api.policies_api import PoliciesApi
+from Scicat-Python-SDK.api.proposals_api import ProposalsApi
+from Scicat-Python-SDK.api.published_data_api import PublishedDataApi
+from Scicat-Python-SDK.api.samples_api import SamplesApi
+from Scicat-Python-SDK.api.user_identities_api import UserIdentitiesApi
+from Scicat-Python-SDK.api.users_api import UsersApi
+
