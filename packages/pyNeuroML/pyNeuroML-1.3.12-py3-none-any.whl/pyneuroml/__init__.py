977f0f625a11cf500c46e754a41e95896159cc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:01 +0000

    Add package pyNeuroML/1.3.12/pyNeuroML-1.3.12-py3-none-any.whl

---
+++ packages/pyNeuroML/pyNeuroML-1.3.12-py3-none-any.whl/pyneuroml/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import typing
+
+try:
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version("pyNeuroML")
+except ImportError:
+    import importlib_metadata
+
+    __version__ = importlib_metadata.version("pyNeuroML")
+
+
+JNEUROML_VERSION = "0.13.3"
+
+# Define a logger for the package
+logging.basicConfig(
+    format="pyNeuroML >>> %(levelname)s - %(message)s",
+    level=logging.WARN,
+)
+
+
+def print_v(msg):
+    """Print message to console.
+
+    This is to be used for printing out messages during ordinary usage of the
+    tool. For status monitoring, fault investigation etc., use the logger.
+
+    :param msg: string to print
+    :type msg: str
+    """
+    print("pyNeuroML >>> " + msg)
+
+
+# read from env variable if found
+try:
+    java_max_memory = os.environ["JNML_MAX_MEMORY_LOCAL"]
+except KeyError:
+    java_max_memory = "400M"
+
+DEFAULTS: typing.Dict[str, typing.Any] = {
+    "v": False,
+    "default_java_max_memory": java_max_memory,
+    "nogui": False,
+}
