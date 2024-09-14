befa96243f218e51e0fe1d0249287e2ec19c53a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 15:04:41 +0000

    Add package octopus-common-python/1.0.6b15/octopus_common_python-1.0.6b15-py3-none-any.whl

---
+++ packages/octopus-common-python/octopus_common_python-1.0.6b15-py3-none-any.whl/octopus_common/model/ResultDetail.py
@@ -0,0 +1,60 @@
+from octopus_common.exception.BizException import BizException
+from octopus_common.enums.ErrorCode import ErrorCode
+
+
+class ResultDetail:
+
+    def __init__(self, class_type):
+        if class_type is not None:
+            self.classType = class_type
+
+
+class DefaultResultDetail(ResultDetail):
+
+    def __init__(self, params: str, result: str):
+        super().__init__(None)
+        self.params = params
+        self.result = result
+
+
+class ArrayListResultDetail(ResultDetail):
+
+    def __init__(self, class_type):
+        super().__init__(None)
+        self.__list = ["com.ctrip.fx.octopus.model.ResultArrayList<{0}>".format(class_type), []]
+
+    def add(self, item: ResultDetail):
+        self.__list[1].append(item.__dict__)
+
+    def add_all(self, items: list[ResultDetail]):
+        for item in items:
+            self.add(item)
+
+    def get(self, index):
+        return self.__list[1][index]
+
+    def remove(self, index):
+        self.__list[1].pop(index)
+
+    def size(self):
+        return len(self.__list[1])
+
+    def is_empty(self):
+        return len(self.__list[1]) == 0
+
+    def clear(self):
+        self.__list[1].clear()
+
+    @property
+    def __dict__(self):
+        return self.__list
+
+
+class DefaultDBPersistenceResultDetail(ResultDetail):
+    def __init__(self, ds_type: str, replace: bool, result: list):
+        if ds_type is None or result is None:
+            raise BizException(ErrorCode.DEFAULT_DB_PERSISTENCE_RESULT_DETAIL_ERROR)
+        super().__init__("com.ctrip.fx.octopus.model.DefaultDBPersistenceResultDetail")
+        self.dsType = ds_type
+        self.replace = replace
+        self.result = result
