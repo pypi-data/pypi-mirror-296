04f49c141d4d2eb07dc046810061f1e40e6080aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:38 +0000

    Add package basic-calculator-for-trial/0.2.0/basic_calculator_for_trial-0.2.0.tar.gz

---
+++ packages/basic-calculator-for-trial/basic_calculator_for_trial-0.2.0.tar.gz/basic_calculator_for_trial-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="basic_calculator_for_trial",
+    version="0.2.0",
+    author="Pallavi Hoge",
+    author_email="pallavihoge47@gmail.com",
+    description="A simple Python calculator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pallavihoge1/basic_calculator_for_trial",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
