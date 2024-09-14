3248236d81d04c8e1ebf2d2fbdca99d7a60da2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:04:04 +0000

    Add package composio-core/0.5.23/composio_core-0.5.23-py3-none-any.whl

---
+++ packages/composio-core/composio_core-0.5.23-py3-none-any.whl/composio/tools/local/shelltool/shell_exec/tool.py
@@ -0,0 +1,23 @@
+"""Tool for executing shell commands."""
+
+import typing as t
+
+from composio.tools.base.local import LocalAction, LocalTool
+from composio.tools.local.shelltool.shell_exec.actions.exec import ExecCommand
+from composio.tools.local.shelltool.shell_exec.actions.new import CreateShell
+from composio.tools.local.shelltool.shell_exec.actions.spawn import SpawnProcess
+
+
+class Shelltool(LocalTool, autoload=True):
+    """Tool for executing shell commands."""
+
+    logo = "https://raw.githubusercontent.com/ComposioHQ/composio/master/python/docs/imgs/logos/shelltool.png"
+
+    @classmethod
+    def actions(cls) -> t.List[t.Type[LocalAction]]:
+        """Returns list of actions."""
+        return [
+            ExecCommand,
+            CreateShell,
+            SpawnProcess,
+        ]
