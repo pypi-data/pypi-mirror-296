1b87f098ed6a56f85dfb92ea15cb31aa2b02ca6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:03:40 +0000

    Add package PyOPIA/2.4.6/pyopia-2.4.6.tar.gz

---
+++ packages/PyOPIA/pyopia-2.4.6.tar.gz/pyopia-2.4.6/pyopia/instrument/silcam.py
@@ -0,0 +1,227 @@
+'''
+Module containing SilCam specific tools to enable compatability with the :mod:`pyopia.pipeline`
+'''
+
+import os
+import numpy as np
+import pandas as pd
+from skimage.exposure import rescale_intensity
+import skimage.io
+
+
+def timestamp_from_filename(filename):
+    '''get a pandas timestamp from a silcam filename
+
+    Parameters
+    ----------
+        filename (string): silcam filename (.silc)
+
+    Returns
+    -------
+        timestamp: timestamp from pandas.to_datetime()
+    '''
+
+    # get the timestamp of the image (in this case from the filename)
+    timestamp = pd.to_datetime(os.path.splitext(os.path.basename(filename))[0][1:])
+    return timestamp
+
+
+def load_mono8(filename):
+    '''load a mono8 .msilc file from disc
+
+    Assumes 8-bit mono image in range 0-255
+
+    Parameters
+    ----------
+    filename : string
+        filename to load
+
+    Returns
+    -------
+    array
+        raw image float between 0-1
+    '''
+    im_mono = np.load(filename, allow_pickle=False).astype(np.float64) / 255
+    image_shape = np.shape(im_mono)
+    if len(image_shape) > 2:
+        if image_shape[2] == 1:
+            img = im_mono[:, :, 0]
+        else:
+            raise RuntimeError('Invalid image dimension')
+    return img
+
+
+def load_rgb8(filename):
+    '''load an RGB .silc file from disc
+
+    Assumes 8-bit RGB image in range 0-255
+
+    Parameters
+    ----------
+    filename : string
+        filename to load
+
+    Returns
+    -------
+    array
+        raw image float between 0-1
+    '''
+    img = np.load(filename, allow_pickle=False).astype(np.float64) / 255
+    return img
+
+
+def load_image(filename):
+    '''.. deprecated:: 2.4.6
+        :func:`pyopia.instrument.silcam.load_image` will be removed in version 3.0.0, it is replaced by
+        :func:`pyopia.instrument.silcam.load_rgb8` because this is more explicit to that image type.
+
+    Load an RGB .silc file from disc
+
+    Parameters
+    ----------
+    filename : string
+        filename to load
+
+    Returns
+    -------
+    array
+        raw image float between 0-1
+    '''
+
+    return load_rgb8(filename)
+
+
+class SilCamLoad():
+    '''PyOpia pipline-compatible class for loading a single silcam image
+    and extracting the timestamp using
+    :func:`pyopia.instrument.silcam.timestamp_from_filename`
+
+    Required keys in :class:`pyopia.pipeline.Data`:
+        - :attr:`pyopia.pipeline.Data.filename`
+
+    Parameters
+    ----------
+    image_format : str, optional
+        Image file format. Can be either 'infer', 'rgb8' or 'mono8', by default 'infer'.
+
+    Note
+    ----
+        'infer' uses the file extension to determine the image format using the following convention:
+         - '.silc' for RGB8
+         - '.msilc' for MONO8
+         - '.bmp' for using skimage.io.imread
+
+    Returns
+    -------
+    data : :class:`pyopia.pipeline.Data`
+        containing the following new keys:
+
+        :attr:`pyopia.pipeline.Data.timestamp`
+
+        :attr:`pyopia.pipeline.Data.img`
+    '''
+
+    def __init__(self, image_format='infer'):
+        self.image_format = image_format
+        self.extension_load = {'.silc': load_rgb8,
+                               '.msilc': load_mono8,
+                               '.bmp': skimage.io.imread}
+        self.format_load = {'RGB8': load_rgb8,
+                            'MONO8': load_mono8}
+
+    def __call__(self, data):
+        data['timestamp'] = timestamp_from_filename(data['filename'])
+        data['imraw'] = self.load_image(data['filename'])
+        return data
+
+    def load_image(self, filename):
+        if self.image_format == 'infer':
+            file_extension = os.path.splitext(os.path.basename(filename))[-1]
+            load_function = self.extension_load[file_extension]
+        else:
+            load_function = self.format_load[self.image_format]
+        img = load_function(filename)
+        return img
+
+
+class ImagePrep():
+    '''PyOpia pipline-compatible class for preparing silcam images for further analysis
+
+    Required keys in :class:`pyopia.pipeline.Data`:
+        - :attr:`pyopia.pipeline.Data.img`
+
+    Returns
+    -------
+    data : :class:`pyopia.pipeline.Data`
+        containing the following new keys:
+
+        :attr:`pyopia.pipeline.Data.im_minimum`
+    '''
+    def __init__(self, image_level='im_corrected'):
+        self.image_level = image_level
+        pass
+
+    def __call__(self, data):
+        image = data[self.image_level]
+
+        # simplify processing by squeezing the image dimensions into a 2D array
+        # min is used for squeezing to represent the highest attenuation of all wavelengths
+        data['im_minimum'] = np.min(image, axis=2)
+
+        data['imref'] = rescale_intensity(image, out_range=(0, 1))
+        return data
+
+
+def generate_config(raw_files: str, model_path: str, outfolder: str, output_prefix: str):
+    '''Generate example silcam config.toml as a dict
+
+    Parameters
+    ----------
+    raw_files : str
+        raw_files
+    model_path : str
+        model_path
+    outfolder : str
+        outfolder
+    output_prefix : str
+        output_prefix
+
+    Returns
+    -------
+    dict
+        pipeline_config toml dict
+    '''
+    # define the configuration to use in the processing pipeline - given as a dictionary - with some values defined above
+    pipeline_config = {
+        'general': {
+            'raw_files': raw_files,
+            'pixel_size': 28  # pixel size in um
+        },
+        'steps': {
+            'classifier': {
+                'pipeline_class': 'pyopia.classify.Classify',
+                'model_path': model_path
+            },
+            'load': {
+                'pipeline_class': 'pyopia.instrument.silcam.SilCamLoad'
+            },
+            'imageprep': {
+                'pipeline_class': 'pyopia.instrument.silcam.ImagePrep',
+                'image_level': 'imraw'
+            },
+            'segmentation': {
+                'pipeline_class': 'pyopia.process.Segment',
+                'threshold': 0.85,
+                'segment_source': 'im_minimum'
+            },
+            'statextract': {
+                'pipeline_class': 'pyopia.process.CalculateStats',
+                'roi_source': 'imref'
+            },
+            'output': {
+                'pipeline_class': 'pyopia.io.StatsH5',
+                'output_datafile': os.path.join(outfolder, output_prefix)
+            }
+        }
+    }
+    return pipeline_config
---
+++ packages/PyOPIA/pyopia-2.4.6.tar.gz/pyopia-2.4.6/pyopia/io.py
@@ -0,0 +1,359 @@
+'''
+Module containing tools for datafile and metadata handling
+'''
+
+from datetime import datetime
+
+import h5py
+import pandas as pd
+import toml
+import xarray
+import os
+from glob import glob
+import xarray as xr
+
+from pyopia import __version__ as pyopia_version
+
+import logging
+logger = logging.getLogger()
+
+# The netcdf4 engine seems to produce errors with the stats dataset, so we use h5netcdf instead
+NETCDF_ENGINE = 'h5netcdf'
+
+
+def write_stats(stats,
+                datafilename,
+                settings=None,
+                export_name_len=40,
+                dataformat='nc',
+                append=True,
+                image_stats=None):
+    '''
+    Writes particle stats into the ouput file.
+    Appends if file already exists.
+
+    Parameters
+    ----------
+    datafilename : str
+        Filame prefix for -STATS.h5 file that may or may not include a path
+    stats : DataFrame or xr.Dataset
+        particle statistics
+    export_name_len : int
+        Max number of chars allowed for col 'export name'
+    append : bool
+        Append all processed data into one nc file.
+        Defaults to True.
+        If False, then one nc file will be generated per raw image,
+        which can be loaded using :func:`pyopia.io.combine_stats_netcdf_files`
+        This is useful for larger datasets,
+        where appending causes substantial slowdown
+        as the dataset gets larger.
+    image_stats : xr.Dataset
+        summary statistics of each raw image (including those with no particles)
+    '''
+
+    if len(stats) == 0:  # to avoid issue with wrong time datatypes in xarray
+        return
+
+    if 'export name' in stats.columns:
+        min_itemsize = {'export name': export_name_len}
+    else:
+        min_itemsize = None
+
+    if dataformat == 'h5':
+        with pd.HDFStore(datafilename + '-STATS.h5', 'a') as fh:
+            stats.to_hdf(
+                fh, 'ParticleStats/stats', append=True, format='t',
+                data_columns=True, min_itemsize=min_itemsize)
+
+        # metadata
+        with h5py.File(datafilename + '-STATS.h5', "a") as fh:
+            meta = fh.require_group('Meta')
+            meta.attrs['Modified'] = str(datetime.now())
+            meta.attrs['PyOpia version'] = pyopia_version
+            meta.attrs['Pipeline steps'] = settings
+    elif dataformat == 'nc':
+
+        if isinstance(stats, xr.Dataset):
+            xstats = stats
+        else:
+            xstats = make_xstats(stats, settings)
+
+        if append and os.path.isfile(datafilename + '-STATS.nc'):
+            existing_stats = load_stats(datafilename + '-STATS.nc')
+            xstats = xarray.concat([existing_stats, xstats], 'index')
+        elif not append:
+            datafilename += ('-Image-D' +
+                             str(xstats['timestamp'][0].values).replace('-', '').replace(':', '').replace('.', '-'))
+        encoding = {k: {'dtype': 'str'} for k in ['export name', 'holo_filename'] if k in xstats.data_vars}
+        xstats.to_netcdf(datafilename + '-STATS.nc', encoding=encoding, engine=NETCDF_ENGINE, format='NETCDF4')
+
+        # If we have image statistics, add this to a group
+        if image_stats is not None:
+            image_stats.to_xarray().to_netcdf(datafilename + '-STATS.nc', group='image_stats', mode='a', engine=NETCDF_ENGINE)
+
+
+def make_xstats(stats, toml_steps):
+    '''Converts a stats dataframe into xarray DataSet, with metadata
+
+    Parameters
+    ----------
+    stats : Pandas DataFrame
+        particle statistics
+    toml_steps : dict
+        TOML-based steps dictionary
+
+    Returns
+    -------
+    xstats : xarray.Dataset
+        Xarray version of stats dataframe, including metadata
+    '''
+    xstats = stats.to_xarray()
+    xstats.attrs["steps"] = toml.dumps(toml_steps)
+    xstats.attrs['Modified'] = str(datetime.now())
+    xstats.attrs['PyOpia version'] = pyopia_version
+    xstats = xstats.assign_coords(time=xstats.timestamp)
+    return xstats
+
+
+def load_image_stats(datafilename):
+    '''Load the summary stats and time information for each image
+
+    Parameters
+    ----------
+    datafilename : str
+        filename of -STATS.nc
+
+    Returns
+    -------
+    image_stats : xarray.Dataset
+        summary statistics of each raw image (including those with no particles)
+    '''
+    with xarray.open_dataset(datafilename, engine=NETCDF_ENGINE, group='image_stats') as image_stats:
+        image_stats.load()
+    return image_stats
+
+
+def load_stats(datafilename):
+    '''Load STATS file as a DataFrame
+
+    Parameters
+    ----------
+    datafilename : str
+        filename of -STATS.h5 or STATS.nc
+
+    Returns
+    -------
+    stats : DataFrame
+        STATS DataFrame  / xarray dataset
+    '''
+
+    if datafilename.endswith('.nc'):
+        with xarray.open_dataset(datafilename) as stats:
+            stats.load()
+    elif datafilename.endswith('.h5'):
+        stats = pd.read_hdf(datafilename, 'ParticleStats/stats')
+    else:
+        logger.warning('WARNING. File extension not specified.' +
+                       'Assuming prefix of -STATS.h5 for backwards compatability.' +
+                       'In future, this function will only take .nc files')
+        stats = pd.read_hdf(datafilename + '-STATS.h5', 'ParticleStats/stats')
+    return stats
+
+
+def combine_stats_netcdf_files(path_to_data, prefix='*'):
+    '''Combine a multi-file directory of STATS.nc files into a 'stats' xarray dataset created by :func:`pyopia.io.write_stats`
+    when using 'append = false'
+
+    Parameters
+    ----------
+    path_to_data : str
+        Folder name containing nc files with pattern '*Image-D*-STATS.nc'
+
+    prefix : str
+        Prefix to multi-file dataset (for replacing the wildcard in '*Image-D*-STATS.nc').
+        Defaults to '*'
+
+    Returns
+    -------
+    xstats : xarray.Dataset
+        Particle statistics and metatdata from processing steps
+    image_stats : xarray.Dataset
+        summary statistics of each raw image (including those with no particles)
+    '''
+
+    sorted_filelist = sorted(glob(os.path.join(path_to_data, prefix + 'Image-D*-STATS.nc')))
+    with xarray.open_mfdataset(sorted_filelist, combine='nested', concat_dim='index',
+                               decode_cf=True, parallel=False,
+                               coords='minimal', compat='override') as ds:
+        xstats = ds.load()
+
+    # Check if we have image statistics in the last file, if so, load it.
+    # The last file should contain the entire time series of processed images.
+    try:
+        ds = xarray.open_dataset(sorted_filelist[-1], group='image_stats')
+    except OSError:
+        image_stats = None
+    else:
+        image_stats = ds.load()
+    finally:
+        ds.close()
+
+    return xstats, image_stats
+
+
+def merge_and_save_mfdataset(path_to_data, prefix='*'):
+    '''Combine a multi-file directory of STATS.nc files into a single '-STATS.nc' file
+    that can then be loaded with {func}`pyopia.io.load_stats`
+
+    Parameters
+    ----------
+    path_to_data : str
+        Folder name containing nc files with pattern '*Image-D*-STATS.nc'
+
+    prefix : str
+        Prefix to multi-file dataset (for replacing the wildcard in '*Image-D*-STATS.nc').
+        Defaults to '*'
+    '''
+
+    logging.info(f'combine stats netcdf files from {path_to_data}')
+    xstats, image_stats = combine_stats_netcdf_files(path_to_data, prefix=prefix)
+
+    settings = steps_from_xstats(xstats)
+
+    prefix_out = os.path.basename(settings['steps']['output']['output_datafile'])
+    output_name = os.path.join(path_to_data, prefix_out)
+
+    logging.info(f'writing {output_name}')
+    write_stats(xstats,
+                output_name,
+                settings,
+                image_stats=image_stats)
+    logging.info(f'writing {output_name} done.')
+
+
+def steps_from_xstats(xstats):
+    '''Get the steps attribute from xarray version of the particle stats into a dictionary
+
+    Parameters
+    ----------
+    xstats : xarray.DataSet
+        xarray version of the particle stats dataframe, containing metadata
+
+    Returns
+    -------
+    steps : dict
+        TOML-formatted dictionary of pipeline steps
+    '''
+    steps = toml.loads(xstats.__getattr__('steps'))
+    return steps
+
+
+def load_stats_as_dataframe(stats_file):
+    '''A loading function for stats files that forces stats into a pandas DataFrame
+
+    Parameters
+    ----------
+    stats_file : str
+        filename of NetCDF of H5 -STATS file
+
+    Returns
+    -------
+    stats : DataFrame
+        stats pandas dataframe
+    '''
+    # obtain particle statistics from the stats file
+    stats = load_stats(stats_file)
+    try:
+        stats = stats.to_dataframe()
+    except AttributeError:
+        logger.info('STATS was likely loaded from an old h5 format, \
+                    which will be deprecated in future. Please use NetCDF in future.')
+        pass
+    return stats
+
+
+def show_h5_meta(h5file):
+    '''
+    prints metadata from an exported hdf5 file created from pyopia.process
+
+    Parameters
+    ----------
+    h5file : str
+        h5 filename from exported data from pyopia.process
+    '''
+
+    with h5py.File(h5file, 'r') as f:
+        keys = list(f['Meta'].attrs.keys())
+
+        for k in keys:
+            logger.info(k + ':')
+            logger.info('    ' + f['Meta'].attrs[k])
+
+
+class StatsToDisc():
+    '''PyOpia pipline-compatible class for calling write_stats() that created NetCDF files.
+
+    Replaces the old StatsH5 class
+
+    Parameters
+    ----------
+    output_datafile : str
+        prefix path for output nc file
+    dataformat : str
+        either 'nc' or 'h5
+    export_name_len : int
+        max number of chars allowed for col 'export name'. Defaults to 40
+    append : bool
+        Append all processed data into one nc file.
+        Defaults to True.
+        If False, then one nc file will be generated per raw image,
+        which can be loaded using :func:`pyopia.io.combine_stats_netcdf_files`
+        This is useful for larger datasets, where appending causes substantial slowdown
+        as the dataset gets larger.
+
+    Returns
+    -------
+    data : :class:`pyopia.pipeline.Data`
+        data from the pipeline
+
+    Example
+    -------
+    Example config for pipeline useage:
+
+    .. code-block:: toml
+
+        [steps.output]
+        pipeline_class = 'pyopia.io.StatsToDisc'
+        output_datafile = './test' # prefix path for output nc file
+        append = true
+    '''
+    def __init__(self,
+                 output_datafile='data',
+                 dataformat='nc',
+                 export_name_len=40,
+                 append=True):
+
+        self.output_datafile = output_datafile
+        self.dataformat = dataformat
+        self.export_name_len = export_name_len
+        self.append = append
+
+    def __call__(self, data):
+        write_stats(data['stats'], self.output_datafile,
+                    settings=data['settings'],
+                    dataformat=self.dataformat,
+                    export_name_len=self.export_name_len,
+                    append=self.append,
+                    image_stats=data['image_stats'])
+
+        return data
+
+
+StatsH5 = StatsToDisc
+
+
+def load_toml(toml_file):
+    with open(toml_file, 'r') as f:
+        settings = toml.load(f)
+    return settings
