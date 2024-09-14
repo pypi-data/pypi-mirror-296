5aef044c456bd0bb820c5b97743f511f3f01a26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:03 +0000

    Add package qcml/0.1.1.3/qcml-0.1.1.3.tar.gz

---
+++ packages/qcml/qcml-0.1.1.3.tar.gz/qcml-0.1.1.3/qcml/utils/exceptions.py
@@ -0,0 +1,19 @@
+class QuantumError(Exception):
+    """
+    Custom exception class for quantum-related errors.
+    """
+
+    def __init__(self, message="Quantum error"):
+        """
+        Initialize the QuantumError instance.
+
+        Parameters:
+        - message (str): Error message. Default is "Quantum error".
+        """
+        super().__init__(message)
+
+
+class InvalidGateError(QuantumError):
+    """Exception raised for invalid quantum gate operations."""
+
+    pass
