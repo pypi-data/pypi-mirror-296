80deabfaffa519d80eeb7a0cd2e7f71edb91f4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:05:09 +0000

    Add package pyside6-expand/1.0.1/pyside6_expand-1.0.1.tar.gz

---
+++ packages/pyside6-expand/pyside6_expand-1.0.1.tar.gz/pyside6_expand-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme:
+    README = readme.read()
+setup(
+    name="pyside6-expand",
+    version="1.0.1",
+    author="yjfw",
+    author_email="yjfw_developer@163.com",
+    description="A expand about pyside6",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yjfw-developer/pyside6-expand.git",
+    packages=find_packages(),
+    install_requires=[
+        'PySide6'
+    ],
+    python_requires='>=3.6',
+    license='Apache'
+)
