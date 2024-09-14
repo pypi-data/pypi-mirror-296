565297ae4637aa2254fd1fac62cb034d8acde0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 04:04:21 +0000

    Add package gepwc/0.0.10851302149/gepwc-0.0.10851302149-py3-none-any.whl

---
+++ packages/gepwc/gepwc-0.0.10851302149-py3-none-any.whl/tst/test.py
@@ -0,0 +1,38 @@
+from gepwc.core import pywc
+from click.testing import CliRunner
+
+def test_gepcat():
+    runner = CliRunner()
+    result = runner.invoke(pywc, ['assets/simple'])
+    assert result.exit_code == 0
+    assert result.output == ('file size in byte: 61\n'
+ 'number of lines: 1\n'
+ 'number of words: 1\n'
+ 'number of characters: 12\n')
+
+def test_byte_calculation():
+    runner = CliRunner()
+    result = runner.invoke(pywc, ['assets/simple', '-c'])
+    assert result.exit_code == 0
+    assert result.output == 'file size in byte: 61\n'
+
+
+def test_lines_calculation():
+    runner = CliRunner()
+    result = runner.invoke(pywc, ['assets/simple', '-l'])
+    assert result.exit_code == 0
+    assert result.output == "number of lines: 1\n"
+
+
+def test_word_count():
+    runner = CliRunner()
+    result = runner.invoke(pywc, ['assets/simple', '-w'])
+    assert result.exit_code == 0
+    assert result.output == "number of words: 1\n"
+
+
+def test_characters_count():
+    runner = CliRunner()
+    result = runner.invoke(pywc, ['assets/simple', '-m'])
+    assert result.exit_code == 0
+    assert result.output == "number of characters: 12\n"
