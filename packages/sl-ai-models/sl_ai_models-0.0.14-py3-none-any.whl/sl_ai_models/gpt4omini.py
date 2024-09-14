5bfe9679e41da38ef803e0e7e0f0e795a9eeb082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:43 +0000

    Add package sl-ai-models/0.0.14/sl_ai_models-0.0.14-py3-none-any.whl

---
+++ packages/sl-ai-models/sl_ai_models-0.0.14-py3-none-any.whl/sl_ai_models/gpt4omini.py
@@ -0,0 +1,15 @@
+from typing import Final
+from sl_ai_models.model_archetypes.openai_text_model import OpenAiTextToTextModel
+from sl_ai_models.utils.ai_misc import clean_indents  # Keep this import here for easier imports into other files so prompts can keep proper indentation levels in code # NOSONAR
+from typing import TypeVar
+T = TypeVar("T")
+
+class Gpt4oMini(OpenAiTextToTextModel):
+    # See OpenAI Limit on the account dashboard for most up-to-date limit
+    MODEL_NAME: Final[str] = "gpt-4o-mini"
+    REQUESTS_PER_PERIOD_LIMIT: Final[int] = 10000
+    REQUEST_PERIOD_IN_SECONDS: Final[int] = 60
+    TIMEOUT_TIME: Final[int] = 40
+    TOKENS_PER_PERIOD_LIMIT: Final[int] = 800000
+    TOKEN_PERIOD_IN_SECONDS: Final[int] = 60
+
