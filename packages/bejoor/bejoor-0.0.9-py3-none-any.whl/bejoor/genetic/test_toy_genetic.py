819d54ad124f587c046681f546e5a0fc0294cd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 03:04:36 +0000

    Add package bejoor/0.0.9/bejoor-0.0.9-py3-none-any.whl

---
+++ packages/bejoor/bejoor-0.0.9-py3-none-any.whl/bejoor/genetic/test_toy_genetic.py
@@ -0,0 +1,21 @@
+from bejoor.genetic import  ToyGeneticAlgorithm
+
+def func(sol):
+    return abs( (sol[0]**sol[3] + sol[1]**sol[3] - sol[2]**sol[3]))
+
+
+solution_vector =  [{"type": "integer", "lower_bound": 1, "upper_bound": 500}] * 3 + \
+                   [{"type": "integer", "lower_bound": 2, "upper_bound": 100}] + \
+                   [{"type": "float", "lower_bound": 0, "upper_bound": 1}] * 2 + \
+                   [{"type": "string", "possible_values": ["option1", "option2", "option3"]}]
+
+
+tga= ToyGeneticAlgorithm(objective_function=func, solution_vector_size=7,
+                         solution_vector=solution_vector, optimization_side="min",
+                         mutation_probability=0.4, population_size=500, epochs=200)
+
+tga.run()
+
+
+print(tga.best_solution)
+print(tga.best_objective_value)
