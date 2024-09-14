f1db09322fc0c81d1086dadd394b85422cbbf3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 05:03:22 +0000

    Add package msegat/1.0.0/msegat-1.0.0-py3-none-any.whl

---
+++ packages/msegat/msegat-1.0.0-py3-none-any.whl/msegat/http/auth/api_token.py
@@ -0,0 +1,10 @@
+class APITokenAuthentication(object):
+
+    @staticmethod
+    def apply(http_request):
+        """
+        Add token authentication to the header request.
+        Args:
+            http_request (HttpRequest): The HttpRequest object to which authentication will be added.
+        """
+        pass
