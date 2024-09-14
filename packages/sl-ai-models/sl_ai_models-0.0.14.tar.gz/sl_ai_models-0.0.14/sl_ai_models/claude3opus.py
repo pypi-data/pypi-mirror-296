78634746a230f5eaea3650c01e980ddba9e5e5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-14 14:03:43 +0000

    Add package sl-ai-models/0.0.14/sl_ai_models-0.0.14.tar.gz

---
+++ packages/sl-ai-models/sl_ai_models-0.0.14.tar.gz/sl_ai_models-0.0.14/sl_ai_models/claude3opus.py
@@ -0,0 +1,16 @@
+from typing import Final
+from sl_ai_models.model_archetypes.anthropic_text_model import AnthropicTextToTextModel
+from sl_ai_models.utils.ai_misc import clean_indents  # Keep this import here for easier imports into other files so prompts can keep proper indentation levels in code # NOSONAR
+import logging
+logger = logging.getLogger(__name__)
+
+class Claude3Opus(AnthropicTextToTextModel):
+    # See Anthropic Limit on the account dashboard for most up-to-date limit
+    MODEL_NAME: Final[str] = "claude-3-opus-20240229"
+    REQUESTS_PER_PERIOD_LIMIT: Final[int] = 3500
+    REQUEST_PERIOD_IN_SECONDS: Final[int] = 60
+    TIMEOUT_TIME: Final[int] = 40
+    TOKENS_PER_PERIOD_LIMIT: Final[int] = 350000
+    TOKEN_PERIOD_IN_SECONDS: Final[int] = 60
+
+
