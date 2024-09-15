3fdb54fdfb3ca9a95005425dc166ea69e4c3fba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:05 +0000

    Add package totallylegit-django-rest-framework-braces/0.4.0/totallylegit_django_rest_framework_braces-0.4.0.tar.gz

---
+++ packages/totallylegit-django-rest-framework-braces/totallylegit_django_rest_framework_braces-0.4.0.tar.gz/totallylegit_django_rest_framework_braces-0.4.0/drf_braces/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+__author__ = 'Original: Miroslav Shubernetskiy, Current: Holden Karau'
+__email__ = 'miroslav@miki725.com'
+__version__ = '0.4.0'
