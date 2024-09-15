d9362ad8f3222c1b21f088c10851eace4b6ca617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:09 +0000

    Add package twitchAPI/4.3.1/twitchAPI-4.3.1-py3-none-any.whl

---
+++ packages/twitchAPI/twitchAPI-4.3.1-py3-none-any.whl/twitchAPI/object/__init__.py
@@ -0,0 +1,23 @@
+#  Copyright (c) 2023. Lena "Teekeks" During <info@teawork.de>
+"""
+Objects used by this Library
+----------------------------
+
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   twitchAPI.object.base
+   twitchAPI.object.api
+   twitchAPI.object.eventsub
+
+.. autosummary::
+
+   twitchAPI.object.base
+   twitchAPI.object.api
+   twitchAPI.object.eventsub
+
+"""
+
+__all__ = []
+
