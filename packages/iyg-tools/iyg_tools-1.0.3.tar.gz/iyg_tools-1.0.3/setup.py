07bd53bc5f00867f5baa6f0268914945ae0c4d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:03:48 +0000

    Add package iyg-tools/1.0.3/iyg_tools-1.0.3.tar.gz

---
+++ packages/iyg-tools/iyg_tools-1.0.3.tar.gz/iyg_tools-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iyg_tools",
+    version="1.0.3",
+    description="",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="İlker Turan",
+    author_email="ilkerturan@outlook.com",
+    url="https://github.com/ilkeryolundagerek/iyg_tools",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
