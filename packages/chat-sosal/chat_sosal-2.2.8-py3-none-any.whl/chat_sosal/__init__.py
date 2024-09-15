3bfa7cdaba18923adf81a41bdf1530bae7b64830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:32 +0000

    Add package chat-sosal/2.2.8/chat_sosal-2.2.8-py3-none-any.whl

---
+++ packages/chat-sosal/chat_sosal-2.2.8-py3-none-any.whl/chat_sosal/__init__.py
@@ -0,0 +1,22 @@
+from chat_sosal.chat_sosal import (
+    export,
+    raw_export,
+    quick_export,
+    link,
+    quick_link,
+    AttachmentHandler,
+    AttachmentToLocalFileHostHandler,
+    AttachmentToDiscordChannelHandler)
+
+__version__ = "2.8.0"
+
+__all__ = (
+    export,
+    raw_export,
+    quick_export,
+    link,
+    quick_link,
+    AttachmentHandler,
+    AttachmentToLocalFileHostHandler,
+    AttachmentToDiscordChannelHandler,
+)
