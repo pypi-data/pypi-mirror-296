723981683731e1169bf52ea1c13c1c7e3751eec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:03:51 +0000

    Add package syncro-api-ctek/0.0.43/syncro_api_ctek-0.0.43-py3-none-any.whl

---
+++ packages/syncro-api-ctek/syncro_api_ctek-0.0.43-py3-none-any.whl/syncro_api_ctek/tickets.py
@@ -0,0 +1,31 @@
+import requests
+from datetime import datetime, timedelta
+
+def get_tickets_recently_updated(api_baseurl, api_key, days):
+    url = f'{api_baseurl}/tickets'
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+    }
+
+    query_days = (datetime.now() - timedelta(days=days)).strftime('%Y-%m-%d')
+    params = {'since_updated_at': query_days}
+
+    all_syncro_tickets = []
+
+    page = 1
+    while True:
+        response = requests.get(url, headers=headers, params={**params, 'page': page})
+        if response.status_code == 200:
+            tickets_data = response.json().get('tickets', [])
+            all_syncro_tickets.extend(tickets_data)
+
+            meta = response.json().get('meta', {})
+            total_pages = meta.get('total_pages', 0)
+            if page >= total_pages:
+                break
+            else:
+                page += 1
+        else:
+            raise Exception(f"Failed to fetch tickets from SyncroMSP API. Status code: {response.status_code}, Response: {response.text}")
+
+    return all_syncro_tickets
