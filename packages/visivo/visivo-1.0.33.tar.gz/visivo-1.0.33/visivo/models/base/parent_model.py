a9994c881a234cbb055ce125ee8d4fdab57659ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:10 +0000

    Add package visivo/1.0.33/visivo-1.0.33.tar.gz

---
+++ packages/visivo/visivo-1.0.33.tar.gz/visivo-1.0.33/visivo/models/base/parent_model.py
@@ -0,0 +1,181 @@
+import re
+import click
+from typing import List
+from abc import ABC, abstractmethod
+from visivo.models.base.base_model import BaseModel
+from visivo.models.base.named_model import NamedModel
+from pydantic_core import PydanticCustomError
+from visivo.models.sources.source import DefaultSource
+
+
+class ParentModel(ABC):
+    @abstractmethod
+    def child_items(self):
+        return []
+
+    def dag(self, node_permit_list=None):
+        from networkx import DiGraph
+
+        dag = DiGraph()
+        dag.add_node(self)
+        self.__build_dag(
+            items=self.child_items(),
+            parent_item=self,
+            dag=dag,
+            node_permit_list=node_permit_list,
+            root=self,
+        )
+        self.__dereference_items(
+            items=self.child_items(),
+            parent_item=self,
+            dag=dag,
+            node_permit_list=node_permit_list,
+            root=self,
+        )
+        return dag
+
+    def __build_dag(self, items: List, parent_item, dag, node_permit_list, root):
+        for item in items:
+            if node_permit_list is None or item in node_permit_list:
+                dag_item = item
+                if BaseModel.is_ref(item):
+                    continue
+                elif isinstance(item, DefaultSource):
+                    name = root.defaults.source_name
+                    dag_item = self.__get_dereferenced_item(
+                        name=name,
+                        dag=dag,
+                        root=root,
+                        item=item,
+                        parent_item=parent_item,
+                    )
+                dag.add_edge(parent_item, dag_item)
+                if isinstance(dag_item, ParentModel):
+                    self.__build_dag(
+                        items=dag_item.child_items(),
+                        parent_item=dag_item,
+                        dag=dag,
+                        node_permit_list=node_permit_list,
+                        root=root,
+                    )
+
+    def __dereference_items(
+        self, items: List, parent_item, dag, node_permit_list, root
+    ):
+        for item in items:
+            if node_permit_list is None or item in node_permit_list:
+                if BaseModel.is_ref(item):
+                    name = NamedModel.get_name(obj=item)
+                    dereferenced_item = self.__get_dereferenced_item(
+                        name=name,
+                        dag=dag,
+                        root=root,
+                        item=item,
+                        parent_item=parent_item,
+                    )
+                    dag.add_edge(parent_item, dereferenced_item)
+                if isinstance(item, ParentModel):
+                    self.__dereference_items(
+                        items=item.child_items(),
+                        parent_item=item,
+                        dag=dag,
+                        node_permit_list=node_permit_list,
+                        root=root,
+                    )
+
+    @staticmethod
+    def all_descendants(dag, from_node=None, depth=None):
+        import networkx.algorithms.traversal.depth_first_search as dfs
+
+        return dfs.dfs_tree(dag, from_node, depth_limit=depth)
+
+    def descendants(self):
+        return ParentModel.all_descendants(dag=self.dag(), from_node=self)
+
+    @staticmethod
+    def all_descendants_of_type(type, dag, from_node=None, depth=None):
+        if not depth:
+            depth = len(dag)
+
+        def find_type(item):
+            return isinstance(item, type)
+
+        return list(
+            filter(
+                find_type,
+                ParentModel.all_descendants(dag=dag, from_node=from_node, depth=depth),
+            )
+        )
+
+    def descendants_of_type(self, type):
+        return ParentModel.all_descendants_of_type(
+            type=type, dag=self.dag(), from_node=self
+        )
+
+    @staticmethod
+    def all_descendants_with_name(name: str, dag, from_node=None):
+        def find_name(item):
+            return hasattr(item, "name") and item.name == name
+
+        return list(
+            filter(find_name, ParentModel.all_descendants(dag=dag, from_node=from_node))
+        )
+
+    def descendants_with_name(self, name: str):
+        ParentModel.all_descendants_with_name(name=name, dag=self.dag(), from_node=self)
+
+    @staticmethod
+    def all_nodes_including_named_node_in_graph(name: str, dag):
+        from networkx import descendants, ancestors
+
+        item = ParentModel.all_descendants_with_name(name=name, dag=dag)
+
+        if len(item) == 1:
+            item = item[0]
+        else:
+            raise click.ClickException(f"No item found with name: '{name}'.")
+
+        d = descendants(dag, item)
+        a = ancestors(dag, item)
+        items = d.union(a)
+        items.add(item)
+        return items
+
+    def nodes_including_named_node_in_graph(self, name):
+        return ParentModel.all_nodes_including_named_node_in_graph(
+            name=name, dag=self.dag()
+        )
+
+    @staticmethod
+    def filtered(pattern, objects) -> List:
+        def name_match(trace):
+            return re.search(pattern, trace.name)
+
+        return list(filter(name_match, objects))
+
+    @staticmethod
+    def show_dag(dag):
+        import matplotlib.pyplot as pyplot
+        from networkx import spring_layout, draw_networkx
+
+        options = {}
+        pos = spring_layout(dag)
+        draw_networkx(dag, pos, **options)
+
+        ax = pyplot.gca()
+        ax.margins(0.20)
+        pyplot.axis("off")
+        pyplot.show()
+
+    def __get_dereferenced_item(self, name, dag, root, item, parent_item):
+        dereferenced_items = ParentModel.all_descendants_with_name(
+            name=name, dag=dag, from_node=root
+        )
+        if len(dereferenced_items) == 1:
+            return dereferenced_items[0]
+        else:
+            raise PydanticCustomError(
+                "bad_reference",
+                f'The reference "{item}" on item "{parent_item.id()}" does not point to an object.',
+                parent_item.model_dump(),
+            )
---
+++ packages/visivo/visivo-1.0.33.tar.gz/visivo-1.0.33/visivo/models/selector.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from typing import List
+from pydantic import ConfigDict, Field, PrivateAttr, model_serializer
+
+from visivo.models.base.base_model import BaseModel, RefString, generate_ref_field
+from visivo.models.base.named_model import NamedModel
+from visivo.models.base.parent_model import ParentModel
+
+
+class SelectorType(str, Enum):
+    single = "single"
+    multiple = "multiple"
+
+
+class Selector(ParentModel, NamedModel, BaseModel):
+    """
+    Selectors enable you to toggle between multiple different traces in your chart. 
+    
+    !!! tip
+        Selectors can also be used to add interactivity between charts different tables. You can read more about using [selectors to add interactivity here](../../../topics/interactivity).
+
+    You can configure selectors to be single select or multi-select. Single select is great if you only want to show a single trace at a time on the chart while the multi-select can be really useful for providing filtering capabilities. 
+
+    ### Example
+    Here's how you might use selectors to create interactivity between two different charts. 
+    ``` yaml
+    charts:
+        - name: Chart One
+          selector:
+              name: Common Selector
+              type: single
+              ...
+        - name: Chart Two
+          selector: ref(Common Selector)
+          ...
+    ```
+
+    """
+
+    model_config = ConfigDict(extra="ignore")
+
+    name: str = Field(description="The name of the selector")
+    type: SelectorType = Field(
+        SelectorType.multiple, description="Single or multiple selector"
+    )
+    options: List[RefString] = Field(
+        [], description="Optional to set the traces to create the choices list"
+    )
+
+    _parent_name: str = PrivateAttr()
+
+    def set_parent_name(self, value: str):
+        self._parent_name = value
+
+    @model_serializer()
+    def serialize_model(self):
+        model = {"name": self.name, "type": self.type, "options": self.options}
+        if hasattr(self, "_parent_name"):
+            model["parent_name"] = self._parent_name
+        else:
+            model["parent_name"] = self.name
+        return model
+
+    def child_items(self):
+        return self.options
