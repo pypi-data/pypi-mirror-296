fa139d3e29891ddbedb709c86c795e1c1f227db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:04:15 +0000

    Add package manim-devanagari/0.0.3/manim_devanagari-0.0.3.tar.gz

---
+++ packages/manim-devanagari/manim_devanagari-0.0.3.tar.gz/manim_devanagari-0.0.3/README.md
@@ -0,0 +1,208 @@
+# Manim Devangari
+
+Manim plugin for adding devanagari script.
+
+# Table of Contents:
+
+- [Installation](#installation)
+- [Usage Devanagari](#usage-devanagari)
+
+# Installation
+
+- Install `pip install manim`
+- Install font - `Noto Sans` and `Noto Sans Devanagari`
+
+# Usage Devanagari
+
+## Devanagari Template
+
+`manim_devanagari.devanagari` supported Hindi, English and Math
+
+### Example :
+
+```python
+from manim import *
+import manim_devanagari
+
+class DevanagariTex(Scene):
+    def construct(self):
+        dev_tex = VGroup(
+            Tex("धन्यावद", tex_template=manim_devanagari.devanagari),
+            Tex("धन्यावद (Thank you!)", tex_template=manim_devanagari.devanagari),
+            Tex(r"द्विघात सुत्र (Quadratic formula) \\ $x = \dfrac{-b \pm \sqrt{b^2 - 4ac}}{2a}$", tex_template=manim_devanagari.devanagari),
+            MathTex(r"\text{भिन्न} = \dfrac{\text{अंश}}{\text{हर}}", tex_template=manim_devanagari.devanagari)
+        ).arrange(DOWN)
+
+        self.add(dev_tex)
+```
+
+![DevanagariTex](https://raw.githubusercontent.com/avnlearn/manim-devanagari/beta/assets/images/1.png)
+
+## Devanagari Class
+
+- A string compiled with LaTeX in normal mode
+
+```python
+manim_devanagari.Deva_Tex(...)
+```
+
+- A string compiled with LaTeX in math mode.
+
+```python
+manim_devanagari.Deva_MathTex(...)
+```
+
+### Example :
+
+```python
+from manim import *
+import manim_devanagari
+
+class Devanagari_ClassTex(Scene):
+    def construct(self):
+        dev_tex = VGroup(
+            manim_devanagari.Deva_Tex("धन्यावद", font_size=DEFAULT_FONT_SIZE),
+            manim_devanagari.Deva_Tex("धन्यावद (Thank you!)", font_size=DEFAULT_FONT_SIZE),
+            manim_devanagari.Deva_Tex(r"द्विघात सुत्र (Quadratic formula) \\ $x = \dfrac{-b \pm \sqrt{b^2 - 4ac}}{2a}$", font_size=DEFAULT_FONT_SIZE),
+            manim_devanagari.Deva_MathTex(r"\text{भिन्न} = \dfrac{\text{अंश}}{\text{हर}}", font_size=DEFAULT_FONT_SIZE)
+        ).arrange(DOWN)
+
+        self.add(dev_tex)
+```
+
+![Devanagari_ClassTex](https://raw.githubusercontent.com/avnlearn/manim-devanagari/beta/assets/images/2.png)
+
+## Cancel Class
+
+```python
+manim_devanagari.Cancel(...)
+```
+
+### Example :
+
+```python
+from manim import *
+import manim_devanagari
+
+class Cancel_Math(Scene):
+    def construct(self):
+        cancel_tex = VGroup(
+            manim_devanagari.Deva_MathTex(r"{{(1 + x)",r"(2 - x^2)}",r"\over",r"{(1 + x)}}", font_size=DEFAULT_FONT_SIZE),
+            MathTex(r"{{(1 + x)",r"(2 - x^2)}",r"\over",r"{(2 - x^2)}}")
+        ).arrange(DOWN)
+
+        self.add(cancel_tex)
+        self.add(manim_devanagari.Cancel(cancel_tex[0][0]))
+        self.add(manim_devanagari.Cancel(cancel_tex[0][3]))
+
+        self.add(manim_devanagari.Cancel(cancel_tex[1][1]))
+        self.add(manim_devanagari.Cancel(cancel_tex[1][3]))
+```
+
+![Cancel_Math](https://raw.githubusercontent.com/avnlearn/manim-devanagari/beta/assets/images/3.png)
+
+## Addition Feature
+
+- Question Header
+
+```python
+manim_devanagari.Question_Header(question_no, ...)
+```
+
+- Solution Header
+
+```python
+manim_devanagari.Solution_Header() # उत्तर :
+```
+
+```python
+manim_devanagari.Solution_Header(ans=False) # हल :
+```
+
+### Example :
+
+```python
+from manim import *
+import manim_devanagari
+
+class Addition_feature(Scene):
+    def construct(self):
+        footer = Rectangle(
+        width=self.camera.frame_width,
+        height=0.8,
+        stroke_opacity=0,
+        fill_color=GREEN,
+        fill_opacity=1,
+        ).to_edge(DOWN / 10)
+
+        cancel_tex = VGroup(
+            manim_devanagari.Question_Header(1, font_size=DEFAULT_FONT_SIZE),
+            manim_devanagari.Solution_Header(font_size=DEFAULT_FONT_SIZE),
+            manim_devanagari.Solution_Header(ans=False, font_size=DEFAULT_FONT_SIZE)
+        ).arrange(DOWN)
+        self.add(cancel_tex, footer)
+```
+
+![Addition_feature](https://raw.githubusercontent.com/avnlearn/manim-devanagari/beta/assets/images/4.png)
+
+# Light Mode and Custom Font Size
+
+```python
+from manim_devanagari import *
+
+_LIGHT_MODE = True # Light Mode is true
+config.background_color = WHITE
+_SET_FONT_SIZE = 30
+```
+
+# Groups
+
+## Question Group
+
+```python
+Question_Group(question_number, item_1, item_2, ..., item_n)
+```
+
+```python
+Question_Group(question_number, item_1, item_2, ..., item_n)
+```
+
+## Solution Group
+
+```python
+Solution_Group((question_number, item_1), item_2, ..., item_n)
+```
+
+```python
+Solution_VGroup(question_number, item_1,(item_2, ..., item_n))
+```
+
+## Answer Group
+
+```python
+Answer_Group((question_number, item_1), item_2, ..., item_n)
+```
+
+```python
+Answer_VGroup(question_number, item_1,(item_2, ..., item_n))
+```
+
+# String
+
+## String Join
+
+Space or newline is defalult
+
+```python
+Str_Join(item_1,item_2, ..., item_n, space=False)
+```
+
+## Bookmark
+
+```python
+Bookmark(mark)
+```
+
+```
+<bookmark mark='{mark}'>
+```
---
+++ packages/manim-devanagari/manim_devanagari-0.0.3.tar.gz/manim_devanagari-0.0.3/manim_devanagari/__init__.py
@@ -0,0 +1,366 @@
+"""
+Devanagari plugin for manim
+
+Attribute
+---------
+
+* _FONT_NAME
+* devanagari
+* __version__
+
+Classes
+-------
+
+* `Deva_Tex(tex_string, ....)` is normal mode (`Tex`)
+* `Deva_MathTex(tex_string, ....)` is math mode (`MathTex`)
+* `Deva_Text(text_string, ...)` is non-LaTeX string (`Text`)
+* `Deva_MarkupText(text_string, ...)` is non-LaTeX string (`MarkupText`)
+* `Question_Header(question_no,...)` is Question Header
+* `Solution_Header(ans, ...)` is Solution
+* `Cancel(...)` is cancel mobject
+
+"""
+
+import manimpango
+from manim import *
+
+__version__ = "2.0.0"
+
+
+_Devanagari = TexTemplate(
+    tex_compiler="xelatex",
+    output_format=".xdv",
+    documentclass="\\documentclass[preview]{standalone}",
+    preamble="\\usepackage{fontspec}\n\\usepackage{polyglossia}\n\\usepackage{cancel}\n\\setmainlanguage{english}\n\\setotherlanguage{hindi}\\setmainfont[Script=Devanagari]{Noto Sans}\n\\usepackage{amsmath}\n\\usepackage{amssymb}",
+)
+
+
+_FONT_NAME = "sans-serif"
+# _FONT_NAME="Noto Sans"
+
+_SET_COLOR = BLACK
+_SET_QUESTION_COLOR = RED
+_SET_SOLUTION_COLOR = GREEN_E
+
+_SET_FONT_SIZE = 35
+
+
+_LIGHT_MODE = False
+if _LIGHT_MODE:
+    config.background_color = WHITE
+    _SET_COLOR = WHITE
+
+_SET_TEX_FONT_SIZE = _SET_FONT_SIZE - 5
+_SET_TEXT_FONT_SIZE = _SET_TEX_FONT_SIZE - 4
+_SET_MARKUPTEXT_FONT_SIZE = _SET_TEX_FONT_SIZE - 5
+
+
+class Tex(Tex):
+    def __init__(self, *args, font_size=_SET_TEX_FONT_SIZE, color=_SET_COLOR, **kwargs):
+        super().__init__(*args, color=color, font_size=font_size, **kwargs)
+
+
+class Deva_Tex(Tex):
+    """Devanagari and English string compiled with LaTeX in normal mode."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tex_template=_Devanagari, **kwargs)
+
+
+class MathTex(MathTex):
+    def __init__(self, *args, font_size=_SET_TEX_FONT_SIZE, color=_SET_COLOR, **kwargs):
+        super().__init__(*args, color=color, font_size=font_size, **kwargs)
+
+
+class Deva_MathTex(MathTex):
+    """Devanagari and English string compiled with LaTeX in math mode."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tex_template=_Devanagari, **kwargs)
+
+
+class Text(Text):
+    """Display (non-LaTeX) text rendered using Pango ."""
+
+    def __init__(
+        self, *args, font_size=_SET_TEXT_FONT_SIZE, color=_SET_COLOR, **kwargs
+    ):
+        super().__init__(*args, color=color, font_size=font_size, **kwargs)
+
+
+class Deva_Text(Text):
+    """Display (non-LaTeX) text rendered using Pango ."""
+
+    def __init__(self, *args, font=_FONT_NAME, **kwargs):
+        super().__init__(*args, font=font, **kwargs)
+
+
+class Deva_MarkupText(MarkupText):
+    """
+    Display (non-LaTeX) text rendered using Pango .
+
+    Text objects behave like a VGroup-like iterable of all characters in the given text. In particular, slicing is possible.
+
+    What is PangoMarkup?
+
+    PangoMarkup is a small markup language like html and it helps you avoid using "range of characters" while coloring or styling a piece a Text. You can use this language with ~.MarkupText.
+
+    A simple example of a marked-up string might be
+
+    <span foreground="blue" size="x-large">Blue text</span> is <i>cool</i>!"
+    and it can be used with ~.MarkupText as
+
+    .. manim:: MarkupExample :save_last_frame:
+    """
+
+    def __init__(
+        self, *args, color=_SET_COLOR, font_size=_SET_MARKUPTEXT_FONT_SIZE, **kwargs
+    ):
+        super().__init__(*args, color=color, font_size=font_size, **kwargs)
+
+
+class Deva_MarkupText(MarkupText):
+    """
+    Display (non-LaTeX) text rendered using Pango .
+
+    Text objects behave like a VGroup-like iterable of all characters in the given text. In particular, slicing is possible.
+
+    What is PangoMarkup?
+
+    PangoMarkup is a small markup language like html and it helps you avoid using "range of characters" while coloring or styling a piece a Text. You can use this language with ~.MarkupText.
+
+    A simple example of a marked-up string might be
+
+    <span foreground="blue" size="x-large">Blue text</span> is <i>cool</i>!"
+    and it can be used with ~.MarkupText as
+
+    .. manim:: MarkupExample :save_last_frame:
+    """
+
+    def __init__(self, *args, font=_FONT_NAME, **kwargs):
+        super().__init__(*args, font=font, **kwargs)
+
+
+class Question_Header(Text):
+    """Question Header
+    Args:
+        question_no (int): Question Number
+    """
+
+    def __init__(
+        self,
+        question_no,
+        font=_FONT_NAME,
+        font_size=30,
+        color=_SET_QUESTION_COLOR,
+        weight=BOLD,
+        **kwargs,
+    ):
+
+        super().__init__(
+            "प्रश्‍न {}:".format(question_no),
+            font=font,
+            font_size=font_size,
+            color=color,
+            weight=weight,
+            **kwargs,
+        )
+
+
+class Solution(Text):
+    """Question Header
+    Args:
+        ans (bool): Default True is 'उत्तर' and False is 'हल'
+
+    """
+
+    def __init__(
+        self,
+        font=_FONT_NAME,
+        font_size=25,
+        color=_SET_SOLUTION_COLOR,
+        weight=BOLD,
+        **kwargs,
+    ):
+
+        super().__init__(
+            text="हल :",
+            font=font,
+            font_size=font_size,
+            color=color,
+            weight=weight,
+            **kwargs,
+        )
+
+
+class Solution_Group(VGroup):
+    r"""
+
+    Solution_Group : Solution Group
+
+    Structure
+        [
+            Group(Solution, line),
+            *args
+        ]
+
+    """
+
+    def __init__(
+        self,
+        line: str = Text(""),
+        *args,
+        solution_header_kwargs: dict = {},
+        first_kwargs: dict = {},
+        **kwargs,
+    ):
+
+        super().__init__(
+            VGroup(Solution(**solution_header_kwargs), line, **first_kwargs).arrange(
+                RIGHT
+            ),
+            *args,
+            **kwargs,
+        )
+
+
+class Solution_VGroup(VGroup):
+    r"""
+
+    Solution_VGroup : Solution VGroup
+
+    Structure
+        [
+            Solution,
+            *args
+        ]
+
+    """
+
+    def __init__(self, *args, solution_header_kwargs: dict = {}, **kwargs):
+        super().__init__(Solution(**solution_header_kwargs), *args, **kwargs)
+
+
+class Answer(Text):
+    """Question Header
+    Args:
+        ans (bool): Default True is 'उत्तर' and False is 'हल'
+
+    """
+
+    def __init__(
+        self,
+        font=_FONT_NAME,
+        font_size=25,
+        color=_SET_SOLUTION_COLOR,
+        weight=BOLD,
+        **kwargs,
+    ):
+
+        super().__init__(
+            text="उत्तर :",
+            font=font,
+            font_size=font_size,
+            color=color,
+            weight=weight,
+            **kwargs,
+        )
+
+
+class Answer_Group(VGroup):
+    r"""
+
+    Solution_Group : Solution Group
+
+    Structure
+        [
+            Group(Solution, line),
+            *args
+        ]
+
+    """
+
+    def __init__(
+        self,
+        line: str = Text(""),
+        *args,
+        solution_header_kwargs: dict = {},
+        first_kwargs: dict = {},
+        **kwargs,
+    ):
+
+        super().__init__(
+            VGroup(Answer(**solution_header_kwargs), line, **first_kwargs).arrange(
+                RIGHT
+            ),
+            *args,
+            **kwargs,
+        )
+
+
+class Answer_VGroup(VGroup):
+    r"""
+
+    Solution_VGroup : Solution VGroup
+
+    Structure
+        [
+            Solution,
+            *args
+        ]
+
+    """
+
+    def __init__(self, *args, solution_header_kwargs: dict = {}, **kwargs):
+        super().__init__(Answer(**solution_header_kwargs), *args, **kwargs)
+
+
+class Cancel(VGroup):
+    r"""
+
+    Cancel is `\cancel` same worked
+
+    Args:
+        mobject (Mobject): Manim Object
+
+    """
+
+    def __init__(
+        self,
+        mobject: Mobject | None = None,
+        stroke_color: ParsableManimColor = RED,
+        stroke_width: float = 2.0,
+        scale_factor: float = 1.5,
+        **kwargs,
+    ) -> None:
+        super().__init__(Line(UP + RIGHT, DOWN + LEFT), **kwargs)
+        if mobject is not None:
+            self.replace(mobject, stretch=True)
+        self.scale(scale_factor)
+        self.set_stroke(color=stroke_color, width=stroke_width)
+
+
+class Question_Group(VGroup):
+    r"""
+    Question_Group : Question Group
+
+    Structure
+        [
+            Question(id),
+            *args
+        ]
+
+    """
+
+    def __init__(self, id=0, *args, question_header_kwargs: dict = {}, **kwargs):
+        super().__init__(
+            Question_Header(question_no=id, **question_header_kwargs), *args, **kwargs
+        )
+
+
+def Str_Join(*args: str, space=False, **kwargs):
+    args = [str(i) for i in args]
+    return (" " if space else "\n").join(args)
+
+
+Bookmark = lambda mark: "<bookmark mark='{}'/>".format(mark)
