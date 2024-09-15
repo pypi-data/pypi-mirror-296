702e4b37d43ca1ad715c01c0e9508903f2d42a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:31 +0000

    Add package djing/0.3.9/djing-0.3.9-py3-none-any.whl

---
+++ packages/djing/djing-0.3.9-py3-none-any.whl/djing/djing_ui/src/pages/Resources/Index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AuthLayout from "@/layouts/AuthLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <Head title="Dashboard" />
+
+  <AuthLayout>
+    <div
+      class="text-center h-full flex justify-center flex-col items-center p-4 bg-white rounded-lg shadow-lg"
+    >
+      <h1 class="text-4xl font-bold mb-4 text-gray-800">Resources</h1>
+
+      <div class="flex space-x-4">hello</div>
+    </div>
+  </AuthLayout>
+</template>
