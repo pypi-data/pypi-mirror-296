0a990c88826a7498f8595f0a88d92ef04f7449b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:03:49 +0000

    Add package syncro-api-ctek/0.0.41/syncro_api_ctek-0.0.41.tar.gz

---
+++ packages/syncro-api-ctek/syncro_api_ctek-0.0.41.tar.gz/syncro_api_ctek-0.0.41/src/syncro_api_ctek/__init__.py
@@ -0,0 +1,3 @@
+from .customers import get_customers_managed, get_customers_all
+from .contacts import get_contacts_by_customer, get_contacts_all_customers
+__all__ = ["get_customers_managed, get_customers_all, get_contacts_by_customer, get_contacts_all_customers"]
