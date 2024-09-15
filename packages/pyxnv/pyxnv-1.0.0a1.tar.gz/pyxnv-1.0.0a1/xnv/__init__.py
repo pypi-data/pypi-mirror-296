65e46683b27ac01d6c47fe78865808593c4bcf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:03:51 +0000

    Add package pyxnv/1.0.0a1/pyxnv-1.0.0a1.tar.gz

---
+++ packages/pyxnv/pyxnv-1.0.0a1.tar.gz/pyxnv-1.0.0a1/xnv/__init__.py
@@ -0,0 +1,18 @@
+from typing import NamedTuple, Literal
+
+from .daemon import *
+from .wallet import *
+from .utils import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release_level: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(
+    major=1, minor=0, micro=0, release_level="alpha", serial=1
+)
