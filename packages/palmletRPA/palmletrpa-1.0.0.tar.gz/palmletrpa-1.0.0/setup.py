a1d1d41e4ab948288081a62ce5b9aa7a232389e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:09:16 +0000

    Add package palmletRPA/1.0.0/palmletrpa-1.0.0.tar.gz

---
+++ packages/palmletRPA/palmletrpa-1.0.0.tar.gz/palmletrpa-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '1.0.0'
+DESCRIPTION = '这是个rpa工具'
+LONG_DESCRIPTION = 'palmlet-rpa是一个python版的rpa工具'
+
+setup(
+    name="palmletRPA",
+    version=VERSION,
+    author="yangsheng",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['rpa', 'palmletRPA', 'palmlet', 'palm'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
