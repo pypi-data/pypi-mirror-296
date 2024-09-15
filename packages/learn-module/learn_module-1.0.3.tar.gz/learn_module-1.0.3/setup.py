f5a608d9806c8f33903cd8e1dcf8ff739b5d8832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:03:48 +0000

    Add package learn-module/1.0.3/learn_module-1.0.3.tar.gz

---
+++ packages/learn-module/learn_module-1.0.3.tar.gz/learn_module-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="learn_module",
+    version="1.0.3",
+    description="",
+    long_description="",
+    author="İlker Turan",
+    author_email="ilkerturan@outlook.com",
+    url="https://github.com/ilkeryolundagerek/learn_module",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
