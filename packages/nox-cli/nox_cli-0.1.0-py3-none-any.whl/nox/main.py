a3d68985ee939f8094ba7994e7e4bfcc317904d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:45 +0000

    Add package nox-cli/0.1.0/nox_cli-0.1.0-py3-none-any.whl

---
+++ packages/nox-cli/nox_cli-0.1.0-py3-none-any.whl/nox/main.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import click
+
+from nox.commands import datetime_commands
+from nox.commands import db_commands
+from nox.commands import docker_commands
+from nox.commands import encrypt_commands
+from nox.commands import env_commands
+from nox.commands import hash_commands
+from nox.commands import init_command
+from nox.commands import jwt_commands
+from nox.commands import net_commands
+from nox.commands import redis_commands
+from nox.commands import s3_commands
+from nox.commands import secret_commands
+from nox.commands import uuid_commands
+
+
+@click.group()
+def cli():
+    """Nox CLI tool."""
+    pass
+
+
+# Register Init command
+cli.add_command(init_command.init)
+
+# Register built-in commands
+cli.add_command(encrypt_commands.encrypt)
+cli.add_command(uuid_commands.gen)
+cli.add_command(jwt_commands.jwt)
+cli.add_command(net_commands.net)
+cli.add_command(s3_commands.s3)
+cli.add_command(hash_commands.hash)
+cli.add_command(secret_commands.secrets)
+cli.add_command(docker_commands.docker)
+cli.add_command(db_commands.db)
+cli.add_command(env_commands.env)
+cli.add_command(datetime_commands.datetime)
+cli.add_command(redis_commands.redis)
+
+if __name__ == '__main__':
+    cli()
