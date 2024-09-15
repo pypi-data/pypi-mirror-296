aea4244e8eae302b21c2dc1ac9b46c175d66c444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:26 +0000

    Add package smart-tools/0.10.1/smart_tools-0.10.1-py3-none-any.whl

---
+++ packages/smart-tools/smart_tools-0.10.1-py3-none-any.whl/smart_tools/common/downloader.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_url(url, http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
+    proxy = {}
+
+    if http_proxy:
+        proxy['http'] = http_proxy
+    if https_proxy:
+        proxy['https'] = https_proxy
+
+    return requests.get(url, proxies= proxy)
+
+def download_url(url, filename, encoding ='utf-8', http_proxy ='http://proxy:8080', https_proxy ='http://username:pwd@proxy:8080'):
+    response = get_url(url, http_proxy, https_proxy)
+
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    return f
