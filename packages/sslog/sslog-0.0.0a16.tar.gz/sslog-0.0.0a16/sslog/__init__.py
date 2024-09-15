e1cff51b43abdf1e2907d7555b8374895287ed88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:04:28 +0000

    Add package sslog/0.0.0a16/sslog-0.0.0a16.tar.gz

---
+++ packages/sslog/sslog-0.0.0a16.tar.gz/sslog-0.0.0a16/sslog/__init__.py
@@ -0,0 +1,107 @@
+import contextlib
+import json
+import logging
+from typing import TYPE_CHECKING, Any, Protocol, Self
+
+import structlog
+from structlog.contextvars import bind_contextvars, reset_contextvars
+from structlog.stdlib import BoundLogger
+
+from . import _default
+from ._process import add_time
+
+
+__all__ = ["context", "logger"]
+
+
+class _ConsoleRender(structlog.dev.ConsoleRenderer):
+
+    def _repr(self, val: Any) -> str:
+        return repr(val)
+
+
+if _default.use_json:
+    structlog.configure(
+        processors=[
+            add_time,
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.CallsiteParameterAdder([
+                structlog.processors.CallsiteParameter.PATHNAME,
+                structlog.processors.CallsiteParameter.LINENO,
+                structlog.processors.CallsiteParameter.THREAD,
+                structlog.processors.CallsiteParameter.PROCESS,
+            ]),
+            structlog.dev.set_exc_info,
+            structlog.processors.ExceptionRenderer(),
+            structlog.processors.EventRenamer("msg"),
+            structlog.processors.JSONRenderer(json.dumps, default=str),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+        logger_factory=structlog.WriteLoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+else:
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f", utc=False, key="time"),
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.CallsiteParameterAdder([
+                structlog.processors.CallsiteParameter.MODULE,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+                structlog.processors.CallsiteParameter.THREAD,
+                structlog.processors.CallsiteParameter.PROCESS,
+            ]),
+            structlog.processors.add_log_level,
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.processors.EventRenamer("msg"),
+            _ConsoleRender(
+                timestamp_key="time",
+                event_key="msg",
+                pad_event=0,
+            )
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=True
+    )
+
+
+class _Logger(Protocol):
+    def bind(self, **kwargs) -> Self: ...
+    def unbind(self, *keys: str) -> Self: ...
+    def try_unbind(self, *keys: str) -> Self: ...
+    def new(self, **new_values: Any) -> Self: ...
+
+    def debug(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def info(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def warning(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def warn(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def error(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def exception(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+    def fatal(self, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+
+    def log(self, level: int, event: str | None = None, *args: Any, **kw: Any) -> Any: ...
+
+    def setLevel(self, level: int) -> None: ...
+
+    @property
+    def level(self) -> int: ...
+
+    @property
+    def name(self) -> str: ...
+
+    def isEnabledFor(self, level: int) -> bool: ...
+
+
+logger: _Logger = structlog.get_logger()
+
+
+@contextlib.contextmanager
+def context(**kwargs):
+    ctx = bind_contextvars(**kwargs)
+    yield
+    reset_contextvars(**ctx)
