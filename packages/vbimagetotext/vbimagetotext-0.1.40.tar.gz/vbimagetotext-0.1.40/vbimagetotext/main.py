88d0eeeb8653f2b56e6a54cc45cb4c47464e5b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:03:49 +0000

    Add package vbimagetotext/0.1.40/vbimagetotext-0.1.40.tar.gz

---
+++ packages/vbimagetotext/vbimagetotext-0.1.40.tar.gz/vbimagetotext-0.1.40/vbimagetotext/main.py
@@ -0,0 +1,27 @@
+import click
+from .gptvision import gptvision
+from .geminivision import geminivision
+from .copyprompt import copyprompt
+from .solution import solution
+from .gptloop import gptloop
+
+
+CONTEXT_SETTINGS = dict(
+    help_option_names=[
+        '-h',
+        '--help'
+    ],
+    auto_envvar_prefix='VBIMAGETOTEXT',
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def main():
+    pass
+
+
+main.add_command(gptvision)
+main.add_command(geminivision)
+main.add_command(copyprompt)
+main.add_command(solution)
+main.add_command(gptloop)
