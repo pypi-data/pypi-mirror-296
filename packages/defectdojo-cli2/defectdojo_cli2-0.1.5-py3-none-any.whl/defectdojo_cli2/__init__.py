42e0ee729646e9c85d5f7cafb2afc4e9d0cba89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:04:33 +0000

    Add package defectdojo-cli2/0.1.5/defectdojo_cli2-0.1.5-py3-none-any.whl

---
+++ packages/defectdojo-cli2/defectdojo_cli2-0.1.5-py3-none-any.whl/defectdojo_cli2/__init__.py
@@ -0,0 +1,9 @@
+from .util import Util
+from .findings import Findings
+from .engagements import Engagements
+from .tests import Tests
+from .Announcements import Announcements
+from .ApiToken import ApiToken
+import pkg_resources  # part of setuptools
+
+__version__ = pkg_resources.get_distribution("defectdojo_cli2").version
