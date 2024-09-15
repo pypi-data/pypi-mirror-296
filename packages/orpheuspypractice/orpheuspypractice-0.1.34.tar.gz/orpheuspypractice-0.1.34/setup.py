7d39afed2cb3b6e71bbe866d248b73dd0c1c5d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:16 +0000

    Add package orpheuspypractice/0.1.34/orpheuspypractice-0.1.34.tar.gz

---
+++ packages/orpheuspypractice/orpheuspypractice-0.1.34.tar.gz/orpheuspypractice-0.1.34/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='orpheuspypractice',
+    version='0.1.34',
+    author='JGWill',
+    author_email='jgi@jgwill.com',
+    description='A Practice Package to Experiment with Orpheus\'s goals',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jgwill/orpheuspypractice',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "tlid",
+        "requests",
+        "music21",
+        "ipython",
+        "jgcmlib>=1.0.43"
+    ],
+    entry_points={
+        'console_scripts': [
+            "oabc = orpheuspypractice:jgabcli_main",
+            "omid2score = orpheuspypractice:jgabcli_main_mid2score",
+            "say_hello_orpheuspypractice = orpheuspypractice:say_hello",
+            "odep = orpheuspypractice.dependency_action:main"
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
