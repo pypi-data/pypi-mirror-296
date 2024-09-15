ef12a29a8ccf12bb3c6c3c85160f4e33ffa3b27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:14 +0000

    Add package decentmesh/0.0.70/decentmesh-0.0.70.tar.gz

---
+++ packages/decentmesh/decentmesh-0.0.70.tar.gz/decentmesh-0.0.70/decentnet/modules/pow/pow_utils.py
@@ -0,0 +1,29 @@
+import pyximport
+import rich
+
+from decentnet.consensus.byte_conversion_constants import ENDIAN_TYPE
+from decentnet.modules.hash_type.hash_type import ShaHash, MemoryHash
+from decentnet.modules.pow.difficulty import Difficulty
+from decentnet.modules.pow.hashing import argon_hash_func, sha256_hash_func
+
+pyximport.install()
+try:
+    from decentnet.modules.pow.computation_fast import int_to_bytes  # noqa
+except ImportError as ex:
+    rich.print(f"[red]POW Operating in low hashrate mode, more instructions: {ex}[/red]")
+    from decentnet.modules.convert.byte_operations import int_to_bytes
+
+
+class PowUtils:
+    @staticmethod
+    def get_bit_length(i_hash: MemoryHash | ShaHash, nonce: int, diff: Difficulty) -> int:
+        if not diff.express:
+            return PowUtils.value_as_int(
+                argon_hash_func(int_to_bytes(i_hash.value_as_int() + nonce), diff)).bit_length()
+        else:
+            return PowUtils.value_as_int(
+                sha256_hash_func(int_to_bytes(i_hash.value_as_int() + nonce))).bit_length()
+
+    @staticmethod
+    def value_as_int(value) -> int:
+        return int.from_bytes(value, ENDIAN_TYPE)
---
+++ packages/decentmesh/decentmesh-0.0.70.tar.gz/decentmesh-0.0.70/decentnet/modules/tcp/client.py
@@ -0,0 +1,67 @@
+import logging
+import socket
+from typing import Optional
+
+import cbor2
+
+from decentnet.consensus.block_sizing import BLOCK_PREFIX_LENGTH_BYTES, MAXIMUM_DATA_SIZE
+from decentnet.consensus.byte_conversion_constants import ENDIAN_TYPE
+from decentnet.consensus.dev_constants import RUN_IN_DEBUG
+from decentnet.modules.logger.log import setup_logger
+from decentnet.modules.tcp.socket_functions import recv_all
+
+logger = logging.getLogger(__name__)
+
+setup_logger(RUN_IN_DEBUG, logger)
+
+
+class TCPClient:
+    def __init__(self, host: Optional[str] = None, port: Optional[str] = None,
+                 client_socket: Optional[socket.socket] = None, ip_type: Optional[int] = None):
+        if not client_socket:
+            self.host = host
+            self.port = port
+            if ip_type == 4:
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket.connect((self.host, self.port))
+            else:
+                self.socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+                self.socket.connect((self.host, self.port, 0, 0))
+            logger.debug(f"Connected on {self.socket.getsockname()}")
+        else:
+            peer_name = client_socket.getpeername()
+            self.host, self.port = peer_name[0], peer_name[1]
+            self.socket = client_socket
+
+    def send_message(self, data: bytearray | bytes, ack=True) -> Optional[dict]:
+        data_len = len(data)
+
+        if data_len > MAXIMUM_DATA_SIZE:
+            raise Exception("Too much data for one block, please split data to more blocks!")
+
+        length_prefix = int.to_bytes(data_len, length=BLOCK_PREFIX_LENGTH_BYTES,
+                                     byteorder=ENDIAN_TYPE, signed=False)
+        logger.debug(
+            f"Outgoing message prefix {length_prefix.hex()} | {data_len} bytes, with prefix {BLOCK_PREFIX_LENGTH_BYTES + data_len} B")
+        try:
+            self.socket.sendall(length_prefix + data)
+        except OSError as e:
+            logger.error(f"Socket error {e}")
+        if ack:
+            logger.debug("Waiting for acknowledgement...")
+            response = recv_all(self.socket, self.host, self.port)
+            if len(response) == 0:
+                return None
+            return cbor2.loads(response)
+
+    def receive_message(self, decode=True) -> bytes | bytearray | str:
+        response = recv_all(self.socket, self.host, self.port)
+        logger.debug(
+            f"Receiving message {cbor2.loads(response)} {len(response)} bytes | tcp_wrapped")
+        if decode:
+            return cbor2.loads(response)
+        else:
+            return response
+
+    def close(self):
+        self.socket.close()
