e2108f5057fbd24cb65eba0ecb0c704c527fab7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:26 +0000

    Add package MLscannerHEP/1/mlscannerhep-1.tar.gz

---
+++ packages/MLscannerHEP/mlscannerhep-1.tar.gz/mlscannerhep-1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MLscannerHEP",
+    version="1",
+    author="Ahmed Hammad",
+    author_email="your.email@example.com",
+    description="A simple package example",
+    py_modules=["my_module"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+)
