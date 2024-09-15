21bc3bccd6b8808265d580aa37b4ab1ade5293d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:04:08 +0000

    Add package pynmeagps/1.0.40/pynmeagps-1.0.40.tar.gz

---
+++ packages/pynmeagps/pynmeagps-1.0.40.tar.gz/pynmeagps-1.0.40/src/pynmeagps/nmeatypes_core.py
@@ -0,0 +1,376 @@
+# pylint: disable=line-too-long
+"""
+NMEA Protocol core globals and constants
+
+Created on 4 Mar 2021
+
+While the NMEA 0183 © protocol is proprietary, the information here
+has been collated from public domain sources.
+
+:author: semuadmin
+"""
+
+from datetime import datetime
+
+INPUT = 1
+"""Input message type"""
+OUTPUT = 0
+"""Output message type"""
+GET = 0
+"""GET (receive, response) message types"""
+SET = 1
+"""SET (command) message types"""
+POLL = 2
+"""POLL (query) message types"""
+SETPOLL = 3
+"""SETPOLL (SET or POLL) message types"""
+VALNONE = 0
+"""Do not validate checksum or msgid"""
+VALCKSUM = 1
+"""Validate checksum"""
+VALMSGID = 2
+"""Validate message id"""
+NMEA_PROTOCOL = 1
+"""NMEA Protocol"""
+UBX_PROTOCOL = 2
+"""UBX Protocol"""
+RTCM3_PROTOCOL = 4
+"""RTCM3 Protocol"""
+ERR_RAISE = 2
+"""Raise error and quit"""
+ERR_LOG = 1
+"""Log errors"""
+ERR_IGNORE = 0
+"""Ignore errors"""
+
+# proprietary messages where msgId is first element of payload:
+PROP_MSGIDS = ("FEC", "UBX", "TNL", "ASHR", "GPPADV")
+"""Proprietary message prefixes"""
+
+GNSSLIST = {
+    0: "GPS",
+    1: "SBAS",
+    2: "Galileo",
+    3: "BeiDou",
+    4: "IMES",
+    5: "QZSS",
+    6: "GLONASS",
+    7: "NAVIC",
+}
+"""GNSS code"""
+FIXTYPE_GGA = {
+    0: "NO FIX",
+    1: "3D",
+    2: "3D",
+    4: "RTK FIXED",
+    5: "RTK FLOAT",
+    6: "DR",
+}
+"""Fix type from GGA"""
+
+GPSEPOCH0 = datetime(1980, 1, 6)
+"""GPS epoch base date"""
+# Geodetic datum spheroid values:
+# WGS84, ETRS89, EPSG4326
+WGS84 = "WGS_84"
+"""WGS84 datum descriptor"""
+WGS84_SMAJ_AXIS = 6378137.0  # semi-major axis
+"""WGS84 semi-major axis"""
+WGS84_FLATTENING = 298.257223563  # flattening
+"""WGS84 flattening"""
+
+# ***************************************************
+# THESE ARE THE NMEA PROTOCOL PAYLOAD ATTRIBUTE TYPES
+# ***************************************************
+CH = "CH"  # Character
+DE = "DE"  # Decimal
+DT = "DT"  # Date ddmmyy
+DM = "DM"  # Date mmddyy
+HX = "HX"  # Hexadecimal Integer
+IN = "IN"  # Integer
+LA = "LA"  # Latitude value ddmm.mmmmm (ddmm.mmmmmmm in HP mode)
+LAD = "LAD"  # Latitude direction (N/S)
+LN = "LN"  # Longitude value dddmm.mmmmm (dddmm.mmmmmmm in HP mode)
+LND = "LND"  # Longitude direction (E/W)
+ST = "ST"  # String
+TM = "TM"  # Time hhmmss.ss
+
+VALID_TYPES = (CH, DE, DM, DT, HX, IN, LA, LAD, LN, LND, ST, TM)
+
+# *****************************************
+# THESE ARE THE NMEA V4 PROTOCOL TALKER IDS
+# *****************************************
+NMEA_TALKERS = {
+    # ***************************************************************
+    # Base Stations:
+    # ***************************************************************
+    "AB": "Independent AIS Base Station",
+    "AD": "Dependent AIS Base Station",
+    # ***************************************************************
+    # Heading Track Controllers:
+    # ***************************************************************
+    "AG": "Heading Track Controller (Autopilot): General",
+    "AI": "Mobile Class A or B AIS Station",
+    "AP": "Heading Track Controller (Autopilot): Magnetic",
+    "AN": "AIS Aids to Navigation Station",
+    "AR": "AIS Receiving Station",
+    "AS": "AIS Station (ITU_R M1371, (Limited Base Station))",
+    "AT": "AIS Transmitting Station",
+    "AX": "AIS Simplex Repeater Station",
+    "BI": "Bilge Systems",
+    "BN": "Bridge Navigational Watch Alarm System",
+    "CA": "Central Alarm Management",
+    # ***************************************************************
+    # Communications:
+    # ***************************************************************
+    "CD": "Digital Selective Calling (DSC)",
+    "CR": "Data Receiver",
+    "CS": "Satellite",
+    "CT": "Radio-Telephone (MF/HF)",
+    "CV": "Radio-Telephone (VHF)",
+    "CX": "Scanning Receiver",
+    "DE": "DECCA Navigator",
+    "DF": "Direction Finder",
+    "DP": "Dynamic Position",
+    "DU": "Duplex Repeater Station",
+    "EC": "Electronic Chart System (ECS)",
+    "EI": "Electronic Chart Display & Information System (ECDIS)",
+    "EP": "Emergency Position Indicating Beacon (EPIRB)",
+    "ER": "Engine Room Monitoring Systems",
+    "FD": "Fire Door Controller/Monitoring Point",
+    "FE": "Fire Extinguisher System",
+    "FR": "Fire Detection Point",
+    "FS": "Fire Sprinkler System",
+    # ***************************************************************
+    # Navigation System Satellite Receivers:
+    # ***************************************************************
+    "GA": "Galileo Positioning System",
+    "GB": "BDS (BeiDou System)",
+    "BD": "BDS (BeiDou System)",  # legacy, use GB instead
+    "GI": "NavIC (IRNSS)",
+    "GL": "GLONASS Receiver",
+    "GN": "Global Navigation Satellite System (GNSS)",
+    "GP": "Global Positioning System (GPS)",
+    "GQ": "QZSS",
+    "P": "Proprietary",
+    # ***************************************************************
+    # Heading Sensors:
+    # ***************************************************************
+    "HC": "Compass, Magnetic",
+    "HE": "Gyro, North Seeking",
+    "HF": "Fluxgate",
+    "HN": "Gyro, Non-North Seeking",
+    # ***************************************************************
+    # Other Maritime Instrumentation:
+    # ***************************************************************
+    "HD": "Hull Door Monitoring",
+    "HS": "Hull Stress Monitoring",
+    "II": "Integrated Instrumentation",
+    "IN": "Integrated Navigation",
+    "LC": "Loran C",
+    "MP": "Microprocessor  Controller",
+    "RA": "Radar and/or Radar Plotting",
+    "SA": "Physical Shore AIS Station",
+    "SC": "Steering Control System/Device",
+    "SD": "Sounder, Depth",
+    "SG": "Steering Gear/Steering Engine",
+    "SN": "Electronic Positioning System",
+    "SS": "Sounder, Scanning",
+    "TC": "Track Control System",
+    # ***************************************************************
+    # Velocity Sensors:
+    # ***************************************************************
+    "VD": "Doppler",
+    "VM": "Speed Log, Water, Magnetic",
+    "VR": "Voyage Data Recorder",
+    "VW": "Speed Log, Water, Mechanical",
+    "WD": "Watertight Door Controller/Monitoring Panel",
+    "WI": "Weather Instruments",
+    "WL": "Water Level Detection Systems",
+    "YX": "Transducer",
+    # ***************************************************************
+    # Timekeepers:
+    # ***************************************************************
+    "ZA": "Atomic Clock",
+    "ZC": "Chronometer",
+    "ZQ": "Quartz",
+    "ZV": "Radio Update",
+}
+
+# format list of permissible NMEA 2-byte header sequences
+NMEA_HDR = {b"\x24" + i[0:1].encode("utf-8") for i in NMEA_TALKERS}
+
+# ****************************************************************************
+# THESE ARE THE NMEA PROTOCOL CORE MESSAGE IDENTITIES
+# Payloads for each of these identities are defined in the nmeatypes_* modules
+# ****************************************************************************
+NMEA_MSGIDS = {
+    # ***************************************************************
+    # NMEA Standard message types
+    # ***************************************************************
+    "AAM": "Waypoint Arrival Alarm",
+    # "ALM": "Almanac Data",
+    "APA": "Auto Pilot A sentence",  # deprecated
+    "APB": "Auto Pilot B sentence",
+    "BOD": "Bearing Origin to Destination",
+    "BWC": "Bearing using Great Circle route",
+    "DTM": "Datum Reference",
+    "GAQ": "Poll Standard Message - Talker ID GA (Galileo)",
+    "GBQ": "Poll Standard Message - Talker ID GB (BeiDou)",
+    "GBS": "GNSS Satellite Fault Detection",
+    "GGA": "Global positioning system fix data",
+    "GLL": "Latitude and longitude, with time of position fix and status",
+    "GLQ": "Poll Standard Message - Talker ID GL (GLONASS)",
+    "GMP": "GNSS Map Projection Fix Data",
+    "GNQ": "Poll Standard Message - Talker ID GN (Any GNSS)",
+    "GNS": "GNSS Fix Data",
+    "GPQ": "Poll Standard Message - Talker ID GP (GPS, SBAS)",
+    "GQQ": "Poll Standard Message - Talker ID GQ (QZSS)",
+    "GRS": "GNSS Range Residuals",
+    "GSA": "GNSS DOP and Active Satellites",
+    "GST": "GNSS Pseudo Range Error Statistics",
+    "GSV": "GNSS Satellites in View",
+    "HDG": "Heading, Magnetic, Deviation, Variation",
+    "HDM": "Heading, Magnetic",  # deprecated
+    "HDT": "Heating, True",
+    "LLQ": "Leica local position and quality",
+    "MSK": "Send control for a beacon receiver",
+    "MSS": "Beacon receiver status information",
+    "RLM": "Return Link Message",
+    "RMA": "Recommended Loran data",
+    "RMB": "Recommended Navigation data for GPS",
+    "RMC": "Recommended Minimum data",
+    "ROT": "Rate and direction of turn",
+    "RTE": "Route message",
+    "STN": "Multiple Data ID",
+    "THS": "True Heading and Status",
+    "TRF": "Transit Fix Data",
+    "TXT": "Text Transmission",
+    "VBW": "Dual Ground / Water Speed",
+    "VLW": "Dual Ground Water Distance",
+    "VTG": "Course over ground and Groundspeed",
+    # "WCV": "Waypoint closure velocity (Velocity Made Good)",
+    "WPL": "Waypoint Location information",
+    # "XTC": "Cross track error",
+    "XTE": "Measured cross track error",
+    "ZDA": "Time and Date",
+    # "ZTG": "Zulu (UTC) time and time to go (to destination)",
+}
+NMEA_MSGIDS_PROP = {
+    # ***************************************************************
+    # NMEA Proprietary message types
+    # ***************************************************************
+    # ***************************************************************
+    # GARMIN Proprietary message types
+    # ***************************************************************
+    "GRMB": "DGPS Beacon Information",
+    "GRMC": "Set Sensor Configuration information",
+    "GRMC1": "Set Additional Sensor Configuration Information",
+    "GRME": "Estimated Error Information",
+    "GRMF": "GPS Fix Data sentence",
+    "GRMH": "Aviation Height and VNAV data",
+    "GRMI": "Set Sensor Initialisation Information",
+    "GRMM": "MapDatum",
+    "GRMO": "Set Output Sentence Enable",
+    "GRMT": "Sensor Status Information",
+    "GRMV": "3D Velocity Information",
+    "GRMW": "Set Additional Waypoint Information",
+    "GRMZ": "Altitude",
+    # ***************************************************************
+    # JVC Kenwood Proprietary message types
+    # ***************************************************************
+    "KLDS": "Position, Speed, Course",
+    "KLSH": "FleetSync GNSS sentence",
+    "KNDS": "Position, Speed, Course",
+    "KNSH": "Position",
+    "KWDWPL": "Waypoint Location",
+    # ***************************************************************
+    # Magellan Proprietary message types
+    # ***************************************************************
+    "MGNWPL": "Waypoint Location",
+    # ***************************************************************
+    # U-BLOX Proprietary message types
+    # ***************************************************************
+    "UBX00": "PUBX-POSITION Lat/Long Position Data",
+    "UBX03": "PUBX-SVSTATUS Satellite Status",
+    "UBX04": "PUBX-TIME Time of Day and Clock Information",
+    "UBX05": "Lat/Long Position Data",
+    "UBX06": "Lat/Long Position Data",
+    "UBX40": "Set NMEA message output rate",
+    "UBX41": "PUBX-CONFIG Set Protocols and Baudrate",
+    # ***************************************************************
+    # Trimble Proprietary message types
+    # ***************************************************************
+    "ASHR": "RT300 Roll and Pitch",
+    "ASHRALR": "Alarms",
+    "ASHRARA": "True Heading",
+    "ASHRARR": "Vector & Accuracy",
+    "ASHRATT": "True Heading",
+    "ASHRBTS": "Bluetooth Status",
+    "ASHRCAP": "Parameters of Antenna Used at Received Base",
+    "ASHRCPA": "Height of Antenna Used at Received Base",
+    "ASHRCPO": "Position of Received Base",
+    "ASHRDDM": "Differential Decoder Message",
+    "ASHRDDS": "Differential Decoder Status",
+    "ASHRHPR": "True Heading",
+    # "ASHRHR": "Proprietary Roll and Pitch",
+    "ASHRLTN": "Latency",
+    "ASHRMDM": "Modem State and Parameter",
+    "ASHRPOS": "Position",
+    "ASHRPBN": "Position and Velocity Information",
+    "ASHRPTT": "PPS Time Tag",
+    "ASHRPWR": "Power Status",
+    "ASHRRCS": "Recording Status",
+    "ASHRSBD": "BEIDOU Satellites Status",
+    "ASHRSGA": "GALILEO Satellites Status (E1,E5a,E5b)",
+    "ASHRSGL": "GLONASS Satellites Status",
+    "ASHRSGO": "GALILEO Satellites Status (E1,E5a,E5b,E6)",
+    "ASHRSGP": "GPS Satellites Status",
+    "ASHRSIR": "IRNSS Satellites Status",
+    "ASHRSLB": "L-Band Satellites Status",
+    "ASHRSQZ": "QZSS Satellites Status",
+    "ASHRSSB": "SBAS Satellites Status",
+    "ASHRTEM": "Receiver Temperature",
+    "ASHRTHS": "True Heading and Status",
+    "ASHRTTT": "Event Marker",
+    "ASHRVCR": "Vector and Accuracy",
+    "ASHRVCT": "Vector and Accuracy",
+    "ASHRVEL": "Velocity",
+    "FUGDP": "Fugro Dynamic Positioning",
+    "GPPADV110": "Position and satellite information for RTK network operations 110",
+    "GPPADV120": "Position and satellite information for RTK network operations 120",
+    "TNLAVR": "Time, yaw, tilt/roll, range for moving baseline RTK",
+    "TNLBPQ": "Base station position and quality indicator",
+    "TNLDG": "L-band corrections and beacon signal strength and related information",
+    "TNLEVT": "Event marker data",
+    "TNLGGK": "Time, position, position type, DOP",
+    "TNLGGKx": "GNSS Position Message",
+    "TNLPJK": "Local coordinate position output",
+    "TNLPJT": "Projection type",
+    "TNLREX": "Rover Extended Output",
+    "TNLVGK": "Vector information",
+    "TNLVHD": "Heading information",
+    # ***************************************************************
+    # Furuno
+    # ***************************************************************
+    "FECGPATT": "Attitude yaw, pitch, roll",
+    "FECGPHVE": "Heave",
+    # ***************************************************************
+    # Locosys Proprietary Messages (GET and SET)
+    # ***************************************************************
+    "INVCRES": "Clear the NVM data",
+    "INVCSTR": "Start session",
+    "INVMATTIT": "ATTIT information",
+    "INVMIMU": "MEMS RAW-DATA message information",
+    "INVMINR": "Calibration status",
+    "INVMSTR": "Session Status",
+    "INVMSLOPE": "SLOPE information",
+    "LSC": "Set status/poll version",
+    "LSR": "Set status response",
+    "LSVD": "Attitude yaw, pitch, roll",
+    # "MTKnnn": "Proprietary command sets - not implemented",
+    # ***************************************************************
+    # Dummy message for testing only
+    # ***************************************************************
+    "FOO": "Dummy message",
+}
