a63be5503b4b6adbef85598642b8d9e81f63d885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:34 +0000

    Add package MaxOS/1.0.4/maxos-1.0.4.tar.gz

---
+++ packages/MaxOS/maxos-1.0.4.tar.gz/maxos-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MaxOS',
+    version='1.0.4',
+    packages=find_packages(),
+    description='MaxOS library',
+    readme ='readme.md',
+    author='fhghfhfhfh',
+    author_email='maximdev@internet.ru',
+    url='',  # Укажите ссылку на ваш репозиторий
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
