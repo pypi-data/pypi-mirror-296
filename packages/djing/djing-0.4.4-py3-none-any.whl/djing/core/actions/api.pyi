78298e2d0483f9229aa4a2491efc1a1ed87d998f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:23 +0000

    Add package djing/0.4.4/djing-0.4.4-py3-none-any.whl

---
+++ packages/djing/djing-0.4.4-py3-none-any.whl/djing/core/actions/api.pyi
@@ -0,0 +1,7 @@
+from _typeshed import Incomplete
+from django.http import HttpRequest as HttpRequest
+
+api: Incomplete
+
+def process_signin(request: HttpRequest): ...
+def signout(request: HttpRequest): ...
