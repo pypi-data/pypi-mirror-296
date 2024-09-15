8a40525c7d1376a158c27679a7485423ef6faa20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:06 +0000

    Add package nonebot-plugin-wakatime/0.2.5/nonebot_plugin_wakatime-0.2.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-wakatime/nonebot_plugin_wakatime-0.2.5-py3-none-any.whl/nonebot_plugin_wakatime/schema/__init__.py
@@ -0,0 +1,11 @@
+from typing_extensions import TypedDict
+
+from .stats import Stats as Stats
+from .users import Users as Users
+
+
+class WakaTime(TypedDict):
+    user: Users
+    stats: Stats
+    all_time_since_today: str
+    background_image: str
