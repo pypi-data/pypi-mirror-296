9cbefd1e40e8e4bbfc71c2356e4c821e5890e190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:09:11 +0000

    Add package docker-recreate/1.4.5/docker_recreate-1.4.5.tar.gz

---
+++ packages/docker-recreate/docker_recreate-1.4.5.tar.gz/docker_recreate-1.4.5/docker_recreate/tests.py
@@ -0,0 +1,32 @@
+import json
+import unittest
+import os
+
+from docker_recreate import cli
+
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MainTest(unittest.TestCase):
+
+    def get_json_file(self, name):
+        path = os.path.join(CUR_DIR, 'fixtures', name)
+        with open(path, 'r') as f:
+            return json.loads(f.read())
+
+    def test_a(self):
+        container_json = self.get_json_file('grafana-container.json')
+        image_json = self.get_json_file('grafana-image.json')
+        conf = cli.get_conf(['grafana'])
+        container = cli.Container(conf, container_json, image_json)
+        cmds = container.get_cmds()
+        expected = [
+            'docker', 'run', '-d',
+            '--name', 'grafana',
+            '--net', 'host',
+            '--restart', 'always',
+            '-v', 'grafana:/var/lib/grafana:rw',
+            'grafana/grafana:8.0.3']
+        print(cmds)
+        self.assertEqual(cmds, expected)
