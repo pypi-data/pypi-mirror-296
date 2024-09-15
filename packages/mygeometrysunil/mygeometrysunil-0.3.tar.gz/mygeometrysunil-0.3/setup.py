4710771daa5ee6eaf34a9a4b0323855d040e428f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:04:02 +0000

    Add package mygeometrysunil/0.3/mygeometrysunil-0.3.tar.gz

---
+++ packages/mygeometrysunil/mygeometrysunil-0.3.tar.gz/mygeometrysunil-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mygeometrysunil',
+    version='0.3',
+    description='I am ludoo king, Please share OTP here',
+    author='Sunil Kumar',
+    packages=find_packages(),
+)
