c61c1e7c78fbb8467c848652862acf8284681ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:03:56 +0000

    Add package pkg-alm2/1.0.3/pkg_alm2-1.0.3.tar.gz

---
+++ packages/pkg-alm2/pkg_alm2-1.0.3.tar.gz/pkg_alm2-1.0.3/pkg_alm2/init_hor_shift.h
@@ -0,0 +1,17 @@
+/*
+###########################################
+#   Copyright (C) 2024
+###########################################
+#   Authors:
+#	- Sherenaz Al-Haj Baddar (s.baddar@ju.edu.jo)
+#	- Alessandro Languasco (alessandro.languasco@unipd.it)
+#	- Mauro Migliardi (mauro.migliardi@unipd.it)
+# 	This is a header file for function prototypes required for the LM_time_lib.c code
+###########################################
+#   This software is licensed under the GNU General Public License version 3 (GPL-3), as published by the Free Software Foundation.
+#   You can find a copy of the license at https://www.gnu.org/licenses/gpl-3.0.en.html
+########################################################
+*/
+void init_dataset(double pown);
+void initBern();
+void initBern_logL();
