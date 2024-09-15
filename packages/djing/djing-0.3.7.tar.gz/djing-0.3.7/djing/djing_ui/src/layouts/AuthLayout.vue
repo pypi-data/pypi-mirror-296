e91af02573a3ba56245d981e528438755b92cdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:09:55 +0000

    Add package djing/0.3.7/djing-0.3.7.tar.gz

---
+++ packages/djing/djing-0.3.7.tar.gz/djing-0.3.7/djing/djing_ui/src/layouts/AuthLayout.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { Link, usePage } from "@inertiajs/vue3";
+import { PageProps } from "@/libs/props";
+import { useDjingRoute } from "@/hooks/useDjingRoute";
+
+const user_id = 1;
+
+const page = usePage<PageProps>();
+
+const djing_route = useDjingRoute();
+
+const resource_classes = page.props.djing_config.resource_classes;
+</script>
+
+<template>
+  <div class="h-screen flex flex-col bg-gray-100">
+    <!-- Top Header -->
+    <header
+      class="bg-white shadow px-6 py-3 flex justify-between items-center w-full"
+    >
+      <h1 class="text-2xl font-semibold">Admin Dashboard</h1>
+      <nav>
+        <ul class="flex space-x-4">
+          <li v-if="user_id">
+            <a href="/genie/signout" class="text-red-500 hover:underline"
+              >Sign Out</a
+            >
+          </li>
+          <li v-else>
+            <a href="/genie/signin" class="text-green-500 hover:underline"
+              >Sign In</a
+            >
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Main content area -->
+    <div class="flex flex-1 overflow-hidden">
+      <!-- Sidebar -->
+      <aside class="w-64 bg-gray-800 text-white flex-shrink-0 p-4">
+        <h1 class="text-lg font-bold mb-4">Resources</h1>
+        <nav>
+          <div>
+            <div
+              v-for="(items, groupName) in resource_classes"
+              :key="groupName"
+              class="mb-6"
+            >
+              <h2
+                class="text-xl font-semibold text-gray-200 border-b border-gray-600 pb-2 mb-2"
+              >
+                {{ groupName }}
+              </h2>
+              <ul class="list-disc pl-5">
+                <!-- Loop through each item in the group -->
+                <li
+                  v-for="item in items"
+                  :key="item.resource_key"
+                  class="mb-1 hover:bg-gray-700 rounded p-1"
+                >
+                  <Link
+                    :href="
+                      djing_route('resources', { resource: item.resource_key })
+                    "
+                    class="text-gray-300"
+                    >{{ item.resource_plural_name }}</Link
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </aside>
+
+      <!-- Main content (Right side) -->
+      <main class="flex-1 p-6 overflow-auto bg-gray-50">
+        <!-- Slot for the page content -->
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
