4e7e20c10e916162cf2a7d4f082a05cda9ff9180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:10:33 +0000

    Add package fastapi_accelerator/0.0.5/fastapi_accelerator-0.0.5.tar.gz

---
+++ packages/fastapi_accelerator/fastapi_accelerator-0.0.5.tar.gz/fastapi_accelerator-0.0.5/fastapi_accelerator/__init__.py
@@ -0,0 +1,6 @@
+"""Пакет с утилитами для FatAPI проекта, которые ускоряет разработку"""
+
+from .appstate import *  # noqa F403
+from .pattern import *  # noqa F403
+from .testutils import *  # noqa F403
+from .utils import *  # noqa F403
