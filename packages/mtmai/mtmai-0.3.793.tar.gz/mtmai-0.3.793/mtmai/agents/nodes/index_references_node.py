a6e5f0a46c7606d65ac96c6a77562260dcb6453c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:18 +0000

    Add package mtmai/0.3.793/mtmai-0.3.793.tar.gz

---
+++ packages/mtmai/mtmai-0.3.793.tar.gz/mtmai-0.3.793/mtmai/agents/nodes/index_references_node.py
@@ -0,0 +1,42 @@
+from langchain_core.documents import Document
+
+from mtmai.agents.nodes.utils import get_vector_store
+from mtmai.agents.states.research_state import ResearchState
+from mtmai.agents.states.research_state import  ResearchState
+from mtmai.core.logging import get_logger
+from langchain_community.vectorstores import VectorStore
+from langchain_core.runnables import RunnableConfig
+from langchain_core.runnables import chain as as_runnable
+from langchain_core.runnables import Runnable, RunnableConfig
+
+from mtmai.core.logging import get_logger
+
+from mtmai.agents.states.research_state import  ResearchState
+
+logger = get_logger()
+
+
+
+class IndexReferencesNode:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def node_name(self):
+        return "index_references"
+
+    async def __call__(self, state: ResearchState, config: RunnableConfig):
+        logger.info(f"进入 index_references node: {state}")
+        # ctx = state.get("context")
+        vs  = get_vector_store(state)
+
+
+        all_docs = []
+        for interview_state in state["interview_results"]:
+            reference_docs = [
+                Document(page_content=v, metadata={"source": k})
+                for k, v in interview_state["references"].items()
+            ]
+            all_docs.extend(reference_docs)
+        # await vectorstore.aadd_documents(all_docs)
+        await vs.aadd_documents(all_docs)
+        return state
