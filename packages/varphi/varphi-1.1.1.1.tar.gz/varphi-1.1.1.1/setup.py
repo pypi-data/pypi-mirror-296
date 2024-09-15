4539db63af74b81f1620a9575f50c6efd0fe0839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:17 +0000

    Add package varphi/1.1.1.1/varphi-1.1.1.1.tar.gz

---
+++ packages/varphi/varphi-1.1.1.1.tar.gz/varphi-1.1.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="varphi",
+    version="1.1.1.1",
+    description="A package for parsing and evaluating Turing programs.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Hassan El-Sheikha",
+    author_email="hassan.elsheikha@utoronto.ca",
+    url="https://github.com/hassanelsheikha/varphi",
+    packages=find_packages(),
+    install_requires=[
+        "antlr4-python3-runtime",
+        "varphitape",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    include_package_data=True,
+)
