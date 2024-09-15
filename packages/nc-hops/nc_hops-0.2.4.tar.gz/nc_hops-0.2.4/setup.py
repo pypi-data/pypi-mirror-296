932c24f018f2039d7a4af071f2189c62c8529dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:13 +0000

    Add package nc-hops/0.2.4/nc_hops-0.2.4.tar.gz

---
+++ packages/nc-hops/nc_hops-0.2.4.tar.gz/nc_hops-0.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Hops',
+    version='0.2.4',
+    author='Nikola Brajic',
+    author_email='nikolabrajic@proton.me',
+    description='Hop file writer and reader',
+    url='https://gitlab.com/brejabrejic/Hops',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/nc-hops/nc_hops-0.2.4.tar.gz/nc_hops-0.2.4/src/ReadHop/__init__.py
@@ -0,0 +1,4 @@
+from .ReadHop import ExtractHopProcessing
+from .SMBReadHop import ExtractHopProcessing as SmbExtractHopProcessing
+from .SMBLabelParser import LabelParser as SmbLabelParser
+from .extract_all_processing import get_processing
