6dc89995c275ebdce591f29289e28a232f9e8fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:09:10 +0000

    Add package qualitative-coding/1.5.3/qualitative_coding-1.5.3.tar.gz

---
+++ packages/qualitative-coding/qualitative_coding-1.5.3.tar.gz/qualitative_coding-1.5.3/qualitative_coding/cli/init.py
@@ -0,0 +1,25 @@
+import click
+import os
+from qualitative_coding.cli.decorators import handle_qc_errors
+from qualitative_coding.logs import configure_logger
+from os import getcwd
+from pathlib import Path
+
+@click.command()
+@click.option("-s", "--settings", type=click.Path(), help="Settings file")
+@click.option("-y", "--accept-defaults", "accept_defaults", is_flag=True, 
+        help="Use default values")
+@click.option("-i", "--import", "_import", help="Import an existing qdpx project")
+@handle_qc_errors
+def init(settings, accept_defaults, _import):
+    "Initialize a qc project"
+    settings_path = settings or os.environ.get("QC_SETTINGS", "settings.yaml")
+    if _import:
+        from qualitative_coding.refi_qda.reader import REFIQDAReader
+        reader = REFIQDAReader(_import)
+        reader.unpack_project(Path.cwd())
+    else:
+        log = configure_logger(settings_path)
+        log.info("init", accept_defaults=accept_defaults)
+        from qualitative_coding.corpus import QCCorpus
+        QCCorpus.initialize(settings_path, accept_defaults)
