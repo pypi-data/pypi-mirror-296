6026e8f2298566d213144afe910bc90b7cafd683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:25 +0000

    Add package hvicorn/0.1.2/hvicorn-0.1.2.tar.gz

---
+++ packages/hvicorn/hvicorn-0.1.2.tar.gz/hvicorn-0.1.2/hvicorn/models/client/chat.py
@@ -0,0 +1,104 @@
+from pydantic import BaseModel
+from typing import Optional, Literal
+from hvicorn.models.client.update_message import UpdateMessageRequest
+from asyncio import run
+
+
+class Message:
+    def __init__(self, text: str, customId: Optional[str] = None) -> None:
+        self.text = text
+        self.customId = customId
+        self.editable = customId != None
+
+    def _generate_edit_request(
+        self, mode: Literal["overwrite", "prepend", "append", "complete"], text: str
+    ):
+        if not self.editable:
+            raise SyntaxError("This message isn't editable.")
+        if self.customId:
+            return UpdateMessageRequest(customId=self.customId, mode=mode, text=text)
+        else:
+            raise SyntaxError("Missing customId")
+
+    def _edit(
+        self, mode: Literal["overwrite", "prepend", "append", "complete"], text: str
+    ) -> None: ...
+
+    def edit(self, text):
+        self.text = text
+        return self._edit("overwrite", text)
+
+    def prepend(self, text):
+        self.text = text + self.text
+        return self._edit("prepend", text)
+
+    def append(self, text):
+        self.text += text
+        return self._edit("append", text)
+
+    def complete(self):
+        if not self.customId:
+            raise SyntaxError("Missing customId")
+        self.editable = False
+        return UpdateMessageRequest(customId=self.customId, mode="complete")
+
+    def __add__(self, string: str):
+        self.append(string)
+        return self
+
+    def __radd__(self, string: str):
+        self.prepend(string)
+        return self
+
+
+class AsyncMessage:
+    def __init__(self, text: str, customId: Optional[str] = None) -> None:
+        self.text = text
+        self.customId = customId
+        self.editable = customId != None
+
+    def _generate_edit_request(
+        self, mode: Literal["overwrite", "prepend", "append", "complete"], text: str
+    ):
+        if not self.editable:
+            raise SyntaxError("This message isn't editable.")
+        if self.customId:
+            return UpdateMessageRequest(customId=self.customId, mode=mode, text=text)
+        else:
+            raise SyntaxError("Missing customId")
+
+    async def _edit(
+        self, mode: Literal["overwrite", "prepend", "append", "complete"], text: str
+    ) -> None: ...
+
+    async def edit(self, text):
+        self.text = text
+        return await self._edit("overwrite", text)
+
+    async def prepend(self, text):
+        self.text = text + self.text
+        return await self._edit("prepend", text)
+
+    async def append(self, text):
+        self.text += text
+        return await self._edit("append", text)
+
+    async def complete(self):
+        self.editable = False
+        if not self.customId:
+            raise SyntaxError("Missing customId")
+        return UpdateMessageRequest(customId=self.customId, mode="complete")
+
+    def __add__(self, string: str):
+        run(self.append(string))
+        return self
+
+    def __radd__(self, string: str):
+        run(self.prepend(string))
+        return self
+
+
+class ChatRequest(BaseModel):
+    cmd: str = "chat"
+    text: str
+    customId: Optional[str] = None
