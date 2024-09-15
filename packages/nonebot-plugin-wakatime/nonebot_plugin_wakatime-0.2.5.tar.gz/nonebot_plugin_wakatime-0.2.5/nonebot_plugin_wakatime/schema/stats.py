c5d1d23e4ee18a35007dc578c4b9b23dd031acb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:06 +0000

    Add package nonebot-plugin-wakatime/0.2.5/nonebot_plugin_wakatime-0.2.5.tar.gz

---
+++ packages/nonebot-plugin-wakatime/nonebot_plugin_wakatime-0.2.5.tar.gz/nonebot_plugin_wakatime-0.2.5/nonebot_plugin_wakatime/schema/stats.py
@@ -0,0 +1,77 @@
+from typing_extensions import TypedDict
+
+
+class Categories(TypedDict):
+
+    name: str
+    total_seconds: float
+    percent: float
+    digital: str
+    decimal: str
+    text: str
+    hours: int
+    minutes: int
+
+
+class Project(TypedDict):
+
+    name: str
+    total_seconds: float
+    percent: float
+    digital: str
+    decimal: str
+    text: str
+    hours: int
+    minutes: int
+
+
+class Languages(TypedDict):
+    name: str
+    total_seconds: float
+    percent: float
+    digital: str
+    decimal: str
+    text: str
+    hours: int
+    minutes: int
+    seconds: int | None
+
+
+class Editors(TypedDict):
+    name: str
+    total_seconds: float
+    percent: float
+    digital: str
+    decimal: str
+    text: str
+    hours: int
+    minutes: int
+    seconds: int | None
+
+
+class OperatingSystems(TypedDict):
+    name: str
+    total_seconds: float
+    percent: float
+    digital: str
+    decimal: str
+    text: str
+    hours: int
+    minutes: int
+
+
+class Stats(TypedDict):
+
+    human_readable_total: str
+    human_readable_total_including_other_language: str
+    daily_average: float
+    daily_average_including_other_language: float
+    human_readable_daily_average: str
+    human_readable_daily_average_including_other_language: str
+    categories: list[Categories] | None
+    projects: list[Project] | None
+    languages: list[Languages] | None
+    editors: list[Editors] | None
+    operating_systems: list[OperatingSystems] | None
+    user_id: str
+    username: str
