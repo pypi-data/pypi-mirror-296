485f1af444f98275140f2dbc95059f45b6fb73fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:05 +0000

    Add package varphi/2.0/varphi-2.0-py3-none-any.whl

---
+++ packages/varphi/varphi-2.0-py3-none-any.whl/varphi/model/TuringMachine.py
@@ -0,0 +1,33 @@
+import random
+
+from .Head import Head
+from .types import State
+from varphi_tape.model import Tape, TapeCharacter
+
+class TuringMachine:
+    head: Head
+    state: State
+
+    def __init__(self, initialState: State) -> None:
+        self.head = Head()
+        self.state = initialState
+    
+    def execute(self, tape: Tape) -> None:
+        """
+        Run this Turing machine on the tape.
+        """
+        while True:
+            tally = False
+            if self.head.read(tape) == TapeCharacter.TALLY:
+                if len(self.state.onTally) == 0:
+                    return
+                tally = True
+            else:
+                if len(self.state.onBlank) == 0:
+                    return
+
+            # A non-deterministic choice is made to determine the execution path
+            # If the Turing program corresponds to a deterministic machine, this choice will be deterministic, since onTally and onBlank contain only one instruction
+            instruction = random.choice(self.state.onTally if tally else self.state.onBlank)
+            self.state = instruction.nextState
+            self.head.execute(instruction.characterToPlace, instruction.directionToMove, tape)
