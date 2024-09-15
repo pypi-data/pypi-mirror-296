38763ed7bf01f09d70eeec19e5ac11f61c8d7266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:04:08 +0000

    Add package icodetoyou/0.0.5/icodetoyou-0.0.5.tar.gz

---
+++ packages/icodetoyou/icodetoyou-0.0.5.tar.gz/icodetoyou-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+versionInt = [0,0,5]
+
+ve=f'{versionInt[0]}.{versionInt[1]}.{versionInt[2]}'
+
+
+setup(
+    name='icodetoyou',
+    version=ve,
+    description="I'am icecomet",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_library',  # หรือ URL ของโปรเจกต์
+    author='icecomet',
+    author_email='icecomet634@gmail.com',
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
+
+
+
