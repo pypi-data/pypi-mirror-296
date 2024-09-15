86fe97b7824cd0b25dc2fa75470f58f4b1bb9741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:03:50 +0000

    Add package mygeometrysunil/0.2/mygeometrysunil-0.2.tar.gz

---
+++ packages/mygeometrysunil/mygeometrysunil-0.2.tar.gz/mygeometrysunil-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mygeometrysunil',
+    version='0.2',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='Tushar Mahuri',
+    packages=find_packages(),
+)
