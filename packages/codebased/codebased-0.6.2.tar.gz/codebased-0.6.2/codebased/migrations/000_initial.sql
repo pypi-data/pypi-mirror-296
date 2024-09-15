ba67d258abbbe6b53f26ee00091cfdcba0473072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:04:36 +0000

    Add package codebased/0.6.2/codebased-0.6.2.tar.gz

---
+++ packages/codebased/codebased-0.6.2.tar.gz/codebased-0.6.2/codebased/migrations/000_initial.sql
@@ -0,0 +1,39 @@
+create table file
+(
+    -- Note: this is relative to the repository root.
+    path             text primary key,
+    size_bytes       integer,
+    -- st_mtime_ns
+    last_modified_ns integer,
+    -- sha256 digest bytes
+    sha256_digest    blob
+);
+
+create table object
+(
+    id             integer primary key,
+    path           text,
+    name           text,
+    language       text,
+    context_before text,
+    context_after  text,
+    kind           text,
+    byte_range     text,
+    coordinates    text,
+    foreign key (path) references file (path)
+);
+
+create index object_path_index on object (path);
+
+create table embedding
+(
+    object_id       integer primary key,
+    data       blob,
+    content_sha256 blob,
+    foreign key (object_id) references object (id)
+);
+
+create index embedding_content_sha256_index on embedding (content_sha256);
+
+-- rowid is object id
+create virtual table fts using fts5(path, name, content, tokenize="trigram");
---
+++ packages/codebased/codebased-0.6.2.tar.gz/codebased-0.6.2/codebased/models.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import dataclasses
+import typing as T
+from pathlib import Path
+
+
+@dataclasses.dataclass
+class Object:
+    path: Path
+    name: str
+    language: str
+    context_before: list[int]
+    context_after: list[int]
+    kind: str
+    # text: bytes  # This field is an excellent candidate for removal / using a memoryview.
+    byte_range: T.Tuple[int, int]  # [start, end)
+    coordinates: Coordinates
+    id: T.Union[int, None] = None
+
+    def __len__(self):
+        start, end = self.byte_range
+        return end - start
+
+    @property
+    def line_length(self) -> int:
+        return self.coordinates[1][0] - self.coordinates[0][0] + 1
+
+
+@dataclasses.dataclass
+class EmbeddingRequest:
+    object_id: int
+    content: str
+    content_hash: str
+
+
+@dataclasses.dataclass
+class Embedding:
+    object_id: int
+    data: list[float]
+    content_hash: str
+
+
+Coordinates = T.Tuple[T.Tuple[int, int], T.Tuple[int, int]]
