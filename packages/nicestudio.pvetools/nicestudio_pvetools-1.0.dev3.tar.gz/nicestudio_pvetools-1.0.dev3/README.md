339d617510cd4bbeae874c0f42b8079eeb361e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:25 +0000

    Add package nicestudio.pvetools/1.0.dev3/nicestudio_pvetools-1.0.dev3.tar.gz

---
+++ packages/nicestudio.pvetools/nicestudio_pvetools-1.0.dev3.tar.gz/nicestudio_pvetools-1.0.dev3/README.md
@@ -0,0 +1,14 @@
+PVETools
+===================
+
+This project provides some tools for Proxmox VE, includes:
+
+- ```pvetools```
+
+
+pvetools
+--------
+
+```pvetools``` has the following sub-commands:
+
+- ``vm``
---
+++ packages/nicestudio.pvetools/nicestudio_pvetools-1.0.dev3.tar.gz/nicestudio_pvetools-1.0.dev3/setup.py
@@ -0,0 +1,41 @@
+kwargs = {
+    'name': 'nicestudio.pvetools',
+    'version': '1.0.dev3',
+    'description': 'Python script for PVE.',
+    'long_description': open('README.md').read(),
+    'author': 'niceStudio, Inc',
+    'author_email': 'service@niceStudio.com.tw',
+    'license': 'BSD',
+    'url': 'https://github.com/niceStudio/nicestudio.pvetools',
+    'package_dir': {'': 'src'},
+    # 'packages': find_namespace_packages('src'),
+    # namespace_packages=['nicestudio', ],
+    'include_package_data': True,
+    'zip_safe': False,
+    'classifiers': [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    'install_requires': open('requirements.txt').read().split('\n'),
+    'entry_points': {'console_scripts': [
+        'pvetool = nicestudio.pvetools.main:main',
+    ]},
+}
+
+try:
+    from setuptools import find_namespace_packages
+
+    kwargs['packages'] = find_namespace_packages(where='src')
+except ImportError:
+    from setuptools import find_packages
+
+    kwargs['packages'] = find_packages(where='src')
+    kwargs['namespace_packages'] = ['nicestudio', ]
+
+from setuptools import setup
+
+setup(**kwargs)
