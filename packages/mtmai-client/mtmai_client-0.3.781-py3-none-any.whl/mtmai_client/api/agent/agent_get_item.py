b003dc74cbd787f20e7a4c7cf67bfdea024da219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:05 +0000

    Add package mtmai-client/0.3.781/mtmai_client-0.3.781-py3-none-any.whl

---
+++ packages/mtmai-client/mtmai_client-0.3.781-py3-none-any.whl/mtmai_client/api/agent/agent_get_item.py
@@ -0,0 +1,150 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.http_validation_error import HTTPValidationError
+from ...types import Response
+
+
+def _get_kwargs(
+    agent_id: str,
+) -> Dict[str, Any]:
+    _kwargs: Dict[str, Any] = {
+        "method": "get",
+        "url": f"/api/v1/agent/{agent_id}",
+    }
+
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[HTTPValidationError]:
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        response_422 = HTTPValidationError.from_dict(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[HTTPValidationError]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    agent_id: str,
+    *,
+    client: AuthenticatedClient,
+) -> Response[HTTPValidationError]:
+    """Get Item
+
+    Args:
+        agent_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[HTTPValidationError]
+    """
+
+    kwargs = _get_kwargs(
+        agent_id=agent_id,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    agent_id: str,
+    *,
+    client: AuthenticatedClient,
+) -> Optional[HTTPValidationError]:
+    """Get Item
+
+    Args:
+        agent_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        HTTPValidationError
+    """
+
+    return sync_detailed(
+        agent_id=agent_id,
+        client=client,
+    ).parsed
+
+
+async def asyncio_detailed(
+    agent_id: str,
+    *,
+    client: AuthenticatedClient,
+) -> Response[HTTPValidationError]:
+    """Get Item
+
+    Args:
+        agent_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[HTTPValidationError]
+    """
+
+    kwargs = _get_kwargs(
+        agent_id=agent_id,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    agent_id: str,
+    *,
+    client: AuthenticatedClient,
+) -> Optional[HTTPValidationError]:
+    """Get Item
+
+    Args:
+        agent_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        HTTPValidationError
+    """
+
+    return (
+        await asyncio_detailed(
+            agent_id=agent_id,
+            client=client,
+        )
+    ).parsed
