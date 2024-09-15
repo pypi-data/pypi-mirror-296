806cea9e24c413b59acb5d19cb9775d8bfc4aaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 15:04:10 +0000

    Add package realtime-crypto/0.0.1/realtime_crypto-0.0.1-py3-none-any.whl

---
+++ packages/realtime-crypto/realtime_crypto-0.0.1-py3-none-any.whl/realtime_crypto/models/coin.py
@@ -0,0 +1,88 @@
+from typing import Optional
+import httpx
+from realtime_crypto.models.crypto_websocket import CryptoWebsocket
+from realtime_crypto.models.types import HistoryPoint
+from realtime_crypto.urls import format_history_url, get_token_data_url
+from realtime_crypto.models.statistic import Statistic
+
+
+class Coin:
+    def __init__(
+        self, id: int, symbol: str, slug: str | int, desc: str, price_stats: Statistic
+    ) -> None:
+        self.id = id
+        self.symbol = symbol
+        self.slug = slug
+        self.description = desc
+        self.price_stats = price_stats
+        self.price = price_stats.get_price()
+
+    def get_id(self) -> int:
+        return self.id
+
+    def get_slug(self) -> str | int:
+        return self.slug
+
+    def get_symbol(self) -> str:
+        return self.symbol
+
+    def get_description(self) -> str:
+        return self.description
+
+    def get_statistics(self) -> Statistic:
+        """
+        Get statistics for this coin.
+        """
+        return self.price_stats
+
+    @classmethod
+    def get_current_price(self, slug: str):
+        """
+        Fetch the current price of a cryptocurrency by its slug.
+        Example usage: Coin.get_current_price('bitcoin')
+        """
+
+        res = httpx.get(get_token_data_url(slug))
+        if res.status_code != 200:
+            raise Exception(f"Failed to fetch data for {slug}: {res.text}")
+
+        json_data = res.json()
+        if json_data["status"]["error_code"] != "0":
+            raise Exception(
+                f"Error in response: {json_data['status']['error_message']}"
+            )
+
+        new_price = json_data["data"]["statistics"]["price"]
+        self.price = new_price
+        return new_price
+
+    def get_price(self):
+        return self.price
+
+    async def get_realtime_price(self, callback):
+        crypto_ws = CryptoWebsocket([self.get_slug()], callback)
+        await crypto_ws.websocket_init()
+
+    def get_history(self, range: str) -> Optional[list[HistoryPoint]]:
+        """
+        Get the price history.
+        Example range inputs: 1h, 1d, 7d, 1m, 3m, 1y
+
+        Return example [{'id': '1', 'timestamp': '1723334400', 'price': 60944}]
+        """
+
+        history = httpx.get(format_history_url(self.id, range))
+        history_json = history.json()
+
+        if history_json["status"]["error_code"] != "0":
+            return None
+
+        history_data = history_json["data"]["points"]
+
+        return [
+            {"id": self.id, "timestamp": point, "price": history_data[point]["v"][0]}
+            for point in history_data
+        ]
+
+    def __repr__(self) -> str:
+        return f"this is {self.symbol}"
---
+++ packages/realtime-crypto/realtime_crypto-0.0.1-py3-none-any.whl/realtime_crypto/models/crypto_websocket.py
@@ -0,0 +1,63 @@
+from typing import Callable
+import websockets
+import json
+import requests
+from realtime_crypto.models.websocket_details import WebsocketDetails
+from realtime_crypto.urls import get_token_data_url
+
+
+class CryptoWebsocket:
+    def __init__(
+        self,
+        cryptocurrencies: list[str],
+        callback: Callable,
+    ):
+        self.uri = "wss://push.coinmarketcap.com/ws"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"
+        }
+        self.cryptocurrencies = cryptocurrencies
+        self.callback = callback
+
+    async def websocket_init(self):
+        crypto_ids = {}
+        for name in self.cryptocurrencies:
+            res = requests.get(get_token_data_url(name))
+            json_res = res.json()
+            if "data" in json_res:
+                id = str(json_res["data"]["id"])
+                crypto_ids[id] = name
+            else:
+                print(
+                    f"{name} not found, please make sure the name is correct in the coinmarketcap url"
+                )
+        websockets
+        async with websockets.connect(self.uri, extra_headers=self.headers) as ws:
+            msg = {
+                "method": "RSUBSCRIPTION",
+                "params": [
+                    "main-site@crypto_price_5s@{}@normal",
+                    ",".join(list(crypto_ids.keys())),
+                ],
+            }
+            await ws.send(json.dumps(msg))
+
+            # Listen for messages
+            async for message in ws:
+                msg = json.loads(message)
+                if "d" in msg:
+                    msg_data = msg["d"]
+                    ws_detail = WebsocketDetails(
+                        id=msg_data["id"],
+                        crypto=crypto_ids[str(msg_data["id"])],
+                        price=msg_data["p"],
+                        p24=msg_data["p24h"],
+                        p7d=msg_data["p7d"],
+                        p30d=msg_data["p30d"],
+                        p3m=msg_data["p3m"],
+                        p1y=msg_data["p1y"],
+                        pytd=msg_data["pytd"],
+                        p_all=msg_data["pall"],
+                        mc=msg_data["mc"],
+                    )
+                    await self.callback(ws_detail)
