2334557c5b1039e73d56d9f01664c8bfa3f1080d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 14:04:18 +0000

    Add package varphitape/1.0/varphitape-1.0-py3-none-any.whl

---
+++ packages/varphitape/varphitape-1.0-py3-none-any.whl/varphitape/antlr/VarphiTapeLexer.py
@@ -0,0 +1,50 @@
+# Generated from VarphiTape.g4 by ANTLR 4.13.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+def serializedATN():
+    return [
+        4,0,2,9,6,-1,2,0,7,0,2,1,7,1,1,0,1,0,1,1,1,1,0,0,2,1,1,3,2,1,0,2,
+        4,0,49,49,73,73,105,105,124,124,10,0,9,10,13,13,32,32,45,45,48,48,
+        66,66,79,79,95,95,98,98,111,111,8,0,1,1,0,0,0,0,3,1,0,0,0,1,5,1,
+        0,0,0,3,7,1,0,0,0,5,6,7,0,0,0,6,2,1,0,0,0,7,8,7,1,0,0,8,4,1,0,0,
+        0,1,0,0
+    ]
+
+class VarphiTapeLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    TALLY = 1
+    BLANK = 2
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+ ]
+
+    symbolicNames = [ "<INVALID>",
+            "TALLY", "BLANK" ]
+
+    ruleNames = [ "TALLY", "BLANK" ]
+
+    grammarFileName = "VarphiTape.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.13.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
