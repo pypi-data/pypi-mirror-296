c1fa52065ea96e2c35ec5759e964bf9dfed2a6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 04:03:52 +0000

    Add package orpheuspypractice/0.1.20/orpheuspypractice-0.1.20-py3-none-any.whl

---
+++ packages/orpheuspypractice/orpheuspypractice-0.1.20-py3-none-any.whl/orpheuspypractice/__init__.py
@@ -0,0 +1,16 @@
+from jgcmlib.jgabcli import main as jgabcli_main
+
+from jgcmlib.jgabcli import main_mid2score as jgabcli_main_mid2score
+
+
+def say_hello():
+    print("Hello, World!")
+    
+
+def __help__():
+    print("This is a practice package to experiment with Orpheus's goals.")
+    print("The following commands are available:")
+    print("oabc")
+    print("omid2score")
+    print("say_hello_orpheuspypractice")
+    
