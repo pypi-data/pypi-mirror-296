73fe287f021d40b3511f48c15d5c94f469776680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-15 03:12:06 +0000

    Add package python-killbill-client/0.0.6/python_killbill_client-0.0.6.tar.gz

---
+++ packages/python-killbill-client/python_killbill_client-0.0.6.tar.gz/python_killbill_client-0.0.6/src/killbill/clients/subscription.py
@@ -0,0 +1,94 @@
+from killbill.clients.base import BaseClient
+from killbill.header import Header
+from killbill.enums import (
+    Audit,
+    EntitlementPolicy,
+    BillingPolicy,
+    ProductCategory,
+    BillingPeriod,
+)
+
+
+class SubscriptionClient(BaseClient):
+
+    def create(
+        self,
+        header: Header,
+        account_id: str,
+        plan_name: str,
+        start_date: str = None,
+        external_key: str = None,
+        product_name: str = None,
+        product_category: ProductCategory = None,
+        billing_period: BillingPeriod = None,
+        price_list: str = None,
+    ):
+        """Create an subscription
+
+        Returns:
+            str or None: The subscription's ID or None if the request failed.
+        """
+
+        payload = {
+            "accountId": account_id,
+            "planName": plan_name,
+            "startDate": start_date,
+            "externalKey": external_key,
+            "productName": product_name,
+            "productCategory": str(product_category) if product_category else None,
+            "billingPeriod": str(billing_period) if billing_period else None,
+            "priceList": price_list,
+        }
+
+        response = self._post("subscriptions", headers=header.dict(), payload=payload)
+
+        self._raise_for_status(response)
+
+        return self._get_uuid(response.headers.get("Location"))
+
+    def retrieve(self, header: Header, subscription_id: str, audit: Audit = Audit.NONE):
+        """Retrieve a subscription by id"""
+
+        response = self._get(
+            f"subscriptions/{subscription_id}",
+            headers=header.dict(),
+            params={"audit": str(audit)},
+        )
+
+        self._raise_for_status(response)
+
+        return response.json()
+
+    def cancel(
+        self,
+        header: Header,
+        subscription_id: str,
+        requested_date: str = None,
+        use_requested_date_for_billing: bool = False,
+        entitlement_policy: EntitlementPolicy = None,
+        billing_policy: BillingPolicy = None,
+    ):
+        """Cancel an entitlement plan"""
+
+        response = self._delete(
+            f"subscriptions/{subscription_id}",
+            headers=header.dict(),
+            params={
+                "requestedDate": requested_date,
+                "entitlementPolicy": str(entitlement_policy),
+                "billingPolicy": str(billing_policy),
+                "useRequestedDateForBilling": use_requested_date_for_billing,
+            },
+        )
+
+        self._raise_for_status(response)
+
+    def uncancel(self, header: Header, subscription_id: str):
+        """Un-cancel an entitlement"""
+
+        response = self._put(
+            f"subscriptions/{subscription_id}/uncancel",
+            headers=header.dict(),
+        )
+
+        self._raise_for_status(response)
