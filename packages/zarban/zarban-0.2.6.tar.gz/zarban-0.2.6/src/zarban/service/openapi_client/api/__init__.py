593d587684f6aa7e6616dc4877a32d984738b695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:36 +0000

    Add package zarban/0.2.6/zarban-0.2.6.tar.gz

---
+++ packages/zarban/zarban-0.2.6.tar.gz/zarban-0.2.6/src/zarban/service/openapi_client/api/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.accounts_api import AccountsApi
+from openapi_client.api.addresses_api import AddressesApi
+from openapi_client.api.borrows_api import BorrowsApi
+from openapi_client.api.collector_api import CollectorApi
+from openapi_client.api.deposits_api import DepositsApi
+from openapi_client.api.events_api import EventsApi
+from openapi_client.api.ilks_api import IlksApi
+from openapi_client.api.lending_pool_api import LendingPoolApi
+from openapi_client.api.orders_api import OrdersApi
+from openapi_client.api.permissions_api import PermissionsApi
+from openapi_client.api.permit_api import PermitApi
+from openapi_client.api.prices_api import PricesApi
+from openapi_client.api.proxies_api import ProxiesApi
+from openapi_client.api.stable_coin_system_api import StableCoinSystemApi
+from openapi_client.api.swap_api import SwapApi
+from openapi_client.api.vaults_api import VaultsApi
+from openapi_client.api.websocket_api import WebsocketApi
