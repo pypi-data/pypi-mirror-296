2c49c067d2f202bce0e0dff751c4c6cd20cd0814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:24 +0000

    Add package ffxiv_stats/0.5.6/ffxiv_stats-0.5.6-py3-none-any.whl

---
+++ packages/ffxiv_stats/ffxiv_stats-0.5.6-py3-none-any.whl/ffxiv_stats/__init__.py
@@ -0,0 +1,13 @@
+"""
+Compute damage variability in the critically acclaimed MMORPG Final Fantasy XIV.
+
+Account for variability from hit types, random damage rolls, and rotations.
+This module computes exact damage DPS distributions or the first three moments (mean, variance, skewness).
+
+"""
+
+__version__ = "0.5.6"
+
+from ffxiv_stats import moments, rate, jobs, modifiers
+from ffxiv_stats.moments import Rotation
+from ffxiv_stats.rate import Rate
