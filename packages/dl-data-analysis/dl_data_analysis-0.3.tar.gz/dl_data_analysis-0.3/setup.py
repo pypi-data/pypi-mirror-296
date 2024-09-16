6fbe0ceef56c4f4fc2f7c1390b294cdd0a96010e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:15 +0000

    Add package dl-data-analysis/0.3/dl_data_analysis-0.3.tar.gz

---
+++ packages/dl-data-analysis/dl_data_analysis-0.3.tar.gz/dl_data_analysis-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+with open("readme.md", "r") as f:
+	description = f.read()
+
+setup(
+	name="dl_data_analysis",
+	version="0.3",
+	packages = find_packages(),
+	install_requires=[
+		"numpy", 
+		"matplotlib",
+		"scikit-learn",
+		"pandas",
+		"imblearn",
+	],
+	long_description = description,
+    long_description_content_type = 'text/markdown'
+	)
