73010cc9001f6b3f97a1711b8c4abc94d52c2739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:11:16 +0000

    Add package my-python-package24/1.0.1/my_python_package24-1.0.1-py3-none-any.whl

---
+++ packages/my-python-package24/my_python_package24-1.0.1-py3-none-any.whl/my_python_package24/__init__.py
@@ -0,0 +1,1 @@
+from .helloworld import greet_me
---
+++ packages/my-python-package24/my_python_package24-1.0.1-py3-none-any.whl/my_python_package24/helloworld.py
@@ -0,0 +1,8 @@
+def greet_me(name):
+    """
+    Prints a greeting message with the provided name.
+    """ 
+    greet = f"Hello, {name}!"
+    return greet  
+
+
