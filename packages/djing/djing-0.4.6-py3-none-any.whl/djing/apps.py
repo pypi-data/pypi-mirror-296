e8b585b5cdbb2effbe458aa6b770729e93042d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:05:10 +0000

    Add package djing/0.4.6/djing-0.4.6-py3-none-any.whl

---
+++ packages/djing/djing-0.4.6-py3-none-any.whl/djing/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+from djing.core.application import Application
+from djing.core.inertia_application import InertiaApplication
+
+
+class DjingAdminConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+
+    name = "djing"
+
+    def ready(self):
+        inertia_application = InertiaApplication()
+
+        inertia_application.boot()
+
+        application = Application()
+
+        application.run()
