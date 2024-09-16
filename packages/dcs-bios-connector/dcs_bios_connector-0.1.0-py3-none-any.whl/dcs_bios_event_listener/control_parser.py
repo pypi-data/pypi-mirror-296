4a82c7f75f1153b6530103da6f422e1d90f57e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:50 +0000

    Add package dcs-bios-connector/0.1.0/dcs_bios_connector-0.1.0-py3-none-any.whl

---
+++ packages/dcs-bios-connector/dcs_bios_connector-0.1.0-py3-none-any.whl/dcs_bios_event_listener/control_parser.py
@@ -0,0 +1,57 @@
+
+import struct
+from pyee import EventEmitter
+from aircraft_json_parser import AircraftJsonParser
+from event_constants import EVENTS
+from parser import ProtocolParser
+
+
+class ControlParser(EventEmitter):
+    def __init__(self):
+        self.aircraft_data, self.controls, self.address_lookup = AircraftJsonParser().get_aircraft_controls()
+        self.emit_queue = []
+        self.data_array =[0] * 65536
+        self.parser = ProtocolParser(self.handle_data_change_for_address, self.handle_sync_complete)
+
+        # When we get data from the network, handle in dcs bios parser
+        self.on(EVENTS.NETWORK_DATA_MESSAGE, self.handle_incoming_dcs_bios_message)
+
+    def handle_incoming_dcs_bios_message(self, message):
+        for byte in bytearray(message):
+            self.parser.process_byte(byte)
+
+    def parse_address_data(self,address, data):
+        if address not in self.address_lookup:
+            return
+        
+        for control in self.address_lookup[address]:
+            for output in control['outputs']:
+                if output['address'] == address:
+                    if output['type'] == 'string':
+                        data_values = self.data_array[address:address + output['max_length']]
+
+                        byte_data = bytes(data_values)
+                        utf8_string = byte_data.decode('utf-8')
+                        control_value = utf8_string
+                    else:
+                        control_value = (data & output['mask']) >> output['shift_by']
+                    
+                    value_has_changed = output.get('value') != control_value
+                    is_already_added = any(item['control'] == control and item['output'] == output for item in self.emit_queue)
+
+                    if value_has_changed and not is_already_added:
+                        output['value'] = control_value
+                        self.emit_queue.append({'output': output, 'control': control})
+
+    def handle_data_change_for_address(self, address, data):
+        bytes_data = struct.pack('<H', data)
+        self.data_array[address] = bytes_data[0]
+        self.data_array[address + 1] =  bytes_data[1]
+        self.parse_address_data(address, data)
+
+    def handle_sync_complete(self):
+        for item in self.emit_queue:
+            if 'address_identifier' in item['output']:
+                print(item['output']['address_identifier'], item['output']['value'])
+        
+        self.emit_queue = []
