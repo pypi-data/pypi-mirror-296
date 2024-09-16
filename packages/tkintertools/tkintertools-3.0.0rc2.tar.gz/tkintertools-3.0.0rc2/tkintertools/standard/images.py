5bb898d6debeb433c76fe37a32a54b6d3535fe78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:23 +0000

    Add package tkintertools/3.0.0rc2/tkintertools-3.0.0rc2.tar.gz

---
+++ packages/tkintertools/tkintertools-3.0.0rc2.tar.gz/tkintertools-3.0.0rc2/tkintertools/standard/images.py
@@ -0,0 +1,26 @@
+"""All standard `Image` classes"""
+
+from ..core import virtual
+
+__all__ = [
+    "StillImage",
+]
+
+
+class StillImage(virtual.Image):
+    """A simple still image"""
+
+    # @typing.override
+    def display(self) -> None:
+        self.items = [self.widget.master.create_image(
+            0, 0, image=self.image, **self.kwargs)]
+
+    # @typing.override
+    def coords(
+        self,
+        size: tuple[float, float] | None = None,
+        position: tuple[float, float] | None = None,
+    ) -> None:
+        super().coords(size, position)
+
+        self.widget.master.coords(self.items[0], *self.center())
