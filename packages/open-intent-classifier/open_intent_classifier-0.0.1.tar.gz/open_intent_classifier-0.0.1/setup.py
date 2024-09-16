920ed6a37d77fe2848afe156ab04debcf4558a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:57 +0000

    Add package open-intent-classifier/0.0.1/open_intent_classifier-0.0.1.tar.gz

---
+++ packages/open-intent-classifier/open_intent_classifier-0.0.1.tar.gz/open_intent_classifier-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from open_intent_classifier import VERSION
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="open_intent_classifier",
+    version=VERSION,
+    packages=["open_intent_classifier"],
+    author="Serj Smorodinsky",
+    author_email="serjsmor@gmail.com",
+    description="This library has two purposes: 1. allow to easily test semantic classification with open labels (not pre defined) for intent recognition. "
+                "2. allow to experiment with different n-shot classification components.",
+    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
