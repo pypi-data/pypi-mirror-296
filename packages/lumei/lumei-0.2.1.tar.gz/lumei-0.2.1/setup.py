8cf3c471ab589cbf9d465a36273e431fc47d6104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:04:48 +0000

    Add package lumei/0.2.1/lumei-0.2.1.tar.gz

---
+++ packages/lumei/lumei-0.2.1.tar.gz/lumei-0.2.1/setup.py
@@ -0,0 +1,36 @@
+import atexit
+
+from setuptools import find_packages, setup
+
+
+def print_message():
+    print('Type `lm` in the CLI to start searching file content.')
+
+
+atexit.register(print_message)
+
+with open('requirements.txt') as f:
+    requirements = [line for line in f.read().splitlines() if line]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lumei',
+    version="0.2.1",
+    description='File search tool using OpenAI assistant.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hansololz/lumei',
+    keywords=["lumei", 'openai', 'file search'],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.12',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'lm = lumei.lumei:main',
+        ]
+    },
+    license='GPL',
+)
