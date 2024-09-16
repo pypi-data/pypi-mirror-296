f58bc9757a7dc2ebee51cf2447461a1513cb58cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:05:03 +0000

    Add package codegenai/5.4/codegenai-5.4.tar.gz

---
+++ packages/codegenai/codegenai-5.4.tar.gz/codegenai-5.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'codegenai',
+      version = '5.4',
+      description = "AI Algorithms",
+      author = 'Anonymus',
+      install_requires = ['networkx','matplotlib','tqdm'],
+      packages = ['codegenai'],
+      zip_safe = False)
