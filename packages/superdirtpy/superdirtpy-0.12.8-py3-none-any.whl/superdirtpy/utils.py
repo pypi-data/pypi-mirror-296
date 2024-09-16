657002cde210e14d3a7489c92ccb5195a8ecfdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:11:15 +0000

    Add package superdirtpy/0.12.8/superdirtpy-0.12.8-py3-none-any.whl

---
+++ packages/superdirtpy/superdirtpy-0.12.8-py3-none-any.whl/superdirtpy/utils.py
@@ -0,0 +1,19 @@
+def zmap(
+    v: float, low_in: float, high_in: float, low_out: float, high_out: float
+) -> float:
+    v = min(max(v, low_in), high_in)
+    percent = (v - low_in) / (high_in - low_in)
+    return percent * (high_out - low_out) + low_out
+
+
+def bind_method(roots: list[int], methods: list[int], method_map: dict) -> list:
+    ret: list = []
+    for i in range(len(roots)):
+        method = method_map.get(methods[i % len(methods)])
+        if method is None:
+            ret.append(None)
+        elif not isinstance(method, list):
+            ret.append(method + roots[i])
+        else:
+            ret.append([m + roots[i] for m in method])
+    return ret
