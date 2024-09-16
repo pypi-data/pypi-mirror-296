86127c1be324ae1aff7d0678c7def036950f61da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:46 +0000

    Add package zarban/0.2.7/zarban-0.2.7-py3-none-any.whl

---
+++ packages/zarban/zarban-0.2.7-py3-none-any.whl/zarban/service/openapi_client/api/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .accounts_api import AccountsApi
+from .addresses_api import AddressesApi
+from .borrows_api import BorrowsApi
+from .collector_api import CollectorApi
+from .deposits_api import DepositsApi
+from .events_api import EventsApi
+from .ilks_api import IlksApi
+from .lending_pool_api import LendingPoolApi
+from .orders_api import OrdersApi
+from .permissions_api import PermissionsApi
+from .permit_api import PermitApi
+from .prices_api import PricesApi
+from .proxies_api import ProxiesApi
+from .stable_coin_system_api import StableCoinSystemApi
+from .swap_api import SwapApi
+from .vaults_api import VaultsApi
+from .websocket_api import WebsocketApi
