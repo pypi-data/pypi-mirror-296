04532d5255d1961d839d2fd7faa67727fa768578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:05:06 +0000

    Add package betterprints/0.1.2/betterprints-0.1.2.tar.gz

---
+++ packages/betterprints/betterprints-0.1.2.tar.gz/betterprints-0.1.2/betterprints/__init__.py
@@ -0,0 +1,7 @@
+from .functions import betterprint, debugprint, classprint, styledprint, hello
+from .configclass import BetterPrintClass
+from .colors import Colors as BetterPrintColors
+from .config import betterprintconfig as betterprintsconfig
+
+__all__ = ['betterprint', 'debugprint', 'classprint', 'styledprint', 'hello', 'BetterPrintClass', 'betterprintsconfig']
+
