52689dc4467a88725e7ff687750032ba27588e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:05:12 +0000

    Add package HarborTools/0.0.1/harbortools-0.0.1.tar.gz

---
+++ packages/HarborTools/harbortools-0.0.1.tar.gz/harbortools-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HarborTools",
+    version="0.0.1",
+    packages=find_packages(),
+    description="HarborTools Package",
+    author="HarborTools",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
