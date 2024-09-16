c9e49b5f80b8b9ea928b37e0f5563358a85b6924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:54 +0000

    Add package wkmigrate/0.1.3.post1/wkmigrate-0.1.3.post1-py3-none-any.whl

---
+++ packages/wkmigrate/wkmigrate-0.1.3.post1-py3-none-any.whl/wkmigrate/activity_translators/spark_jar_activity_translator.py
@@ -0,0 +1,17 @@
+""" This module defines methods for translating Databricks Spark jar activities."""
+from wkmigrate.utils import identity, translate
+
+
+mapping = {
+    'main_class_name': {'key': 'main_class_name', 'parser': identity},
+    'parameters': {'key': 'parameters', 'parser': identity}
+}
+
+
+def translate_spark_jar_activity(activity: dict) -> dict:
+    """ Translates a Databricks Spark jar activity definition in Data Factory's object model to a Databricks Spark jar
+        task in the Databricks SDK object model.
+        :parameter activity: Databricks Spark jar activity definition as a ``dict``
+        :return: Databricks Spark jar task properties as a ``dict``
+    """
+    return translate(activity, mapping)
