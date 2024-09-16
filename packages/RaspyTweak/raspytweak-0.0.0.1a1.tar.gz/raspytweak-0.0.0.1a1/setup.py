30235a53bacb5581b573f6e7ef67b019f98fc11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:29 +0000

    Add package RaspyTweak/0.0.0.1a1/raspytweak-0.0.0.1a1.tar.gz

---
+++ packages/RaspyTweak/raspytweak-0.0.0.1a1.tar.gz/raspytweak-0.0.0.1a1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+__package_name__ = 'RaspyTweak'
+__author__ = 'Voyager'
+__version__ = '0.0.0.1a1'
+__version_prefix__ = 'pre-alpha'
+__doc__  = 'A library for raspberry pi'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name=__package_name__,
+    version=__version__,
+    description=__doc__,
+    package_dir=dict({"": __package_name__}),
+    packages=find_packages(where=__package_name__),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=__author__,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Utilities"
+    ],
+    python_requires=">=3.12",
+    license='MIT',
+    url="https://github.com/voyager-2021/RaspyTweak",
+    download_url=f"https://github.com/voyager-2021/RaspyTweak/releases/tag/{__version__}",
+    keywords=["Raspberry Pi", "GPIO", "GPIO Library"],
+    project_urls={
+        "Issues": "https://github.com/voyager-2021/RaspyTweak/issues"
+    }
+)
