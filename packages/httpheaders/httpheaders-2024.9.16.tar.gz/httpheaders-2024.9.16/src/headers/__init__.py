877d8fa8d0876991f184447b1f45c1fc00c2f00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:20 +0000

    Add package httpheaders/2024.9.16/httpheaders-2024.9.16.tar.gz

---
+++ packages/httpheaders/httpheaders-2024.9.16.tar.gz/httpheaders-2024.9.16/src/headers/__init__.py
@@ -0,0 +1,8 @@
+"""HTTP Headers
+
+see: https://www.iana.org/assignments/http-fields/http-fields.xhtml
+generated at: 2024-09-16T02:56:30.550932
+"""
+from .string import *
+
+__version__ = "2024.09.16"
