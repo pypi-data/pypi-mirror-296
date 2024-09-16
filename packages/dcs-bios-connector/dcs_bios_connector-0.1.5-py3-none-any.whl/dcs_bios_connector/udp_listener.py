f7d2caef2f8066b91886923cea958aa28a83623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:05:04 +0000

    Add package dcs-bios-connector/0.1.5/dcs_bios_connector-0.1.5-py3-none-any.whl

---
+++ packages/dcs-bios-connector/dcs_bios_connector-0.1.5-py3-none-any.whl/dcs_bios_connector/udp_listener.py
@@ -0,0 +1,31 @@
+from pyee import EventEmitter
+
+import socket
+import struct
+import threading
+
+MULTICAST_GROUP = '239.255.50.10'
+PORT = 5010
+LOOPBACK_INTERFACE  = ""
+
+class UdpListener(EventEmitter):
+    def __init__(self):
+        super().__init__()
+
+    def udp_listener(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((LOOPBACK_INTERFACE, PORT))
+
+        mreq = struct.pack('4sl', socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        print(f"Listening for data on {MULTICAST_GROUP}:{PORT}...")
+
+        while True:
+            data, addr = sock.recvfrom(65536)  # Buffer size of 65536 bytes
+            self.emit('data_message', data)
+
+    def start(self):
+        self.thread = threading.Thread(target=self.udp_listener)
+        self.thread.daemon = True  # Makes sure the thread exits when the main program does
+        self.thread.start()
