32327eff864549887a00607a04a7000ca68a508a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:21 +0000

    Add package dbt-af/0.7.3/dbt_af-0.7.3-py3-none-any.whl

---
+++ packages/dbt-af/dbt_af-0.7.3-py3-none-any.whl/dbt_af/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    'dags',
+    'conf',
+]
+
+__version__ = '0.7.3'
+
+from . import conf, dags  # noqa
---
+++ packages/dbt-af/dbt_af-0.7.3-py3-none-any.whl/dbt_af/conf/__init__.py
@@ -0,0 +1,19 @@
+from dbt_af.conf.config import (
+    Config,
+    CustomAfCallbacksConfig,
+    DbtDefaultTargetsConfig,
+    DbtProjectConfig,
+    K8sConfig,
+    MCDIntegrationConfig,
+    TableauIntegrationConfig,
+)
+
+__all__ = [
+    'Config',
+    'DbtDefaultTargetsConfig',
+    'DbtProjectConfig',
+    'K8sConfig',
+    'MCDIntegrationConfig',
+    'TableauIntegrationConfig',
+    'CustomAfCallbacksConfig',
+]
