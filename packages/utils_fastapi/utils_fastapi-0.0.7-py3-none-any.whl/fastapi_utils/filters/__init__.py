d2d7172f98a6cd2a0678315b164e21bff21531d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:37 +0000

    Add package utils_fastapi/0.0.7/utils_fastapi-0.0.7-py3-none-any.whl

---
+++ packages/utils_fastapi/utils_fastapi-0.0.7-py3-none-any.whl/fastapi_utils/filters/__init__.py
@@ -0,0 +1,4 @@
+from fastapi_utils.filters.ordering_filter import OrderingFilter
+from fastapi_utils.filters.search_filter import SearchFilter, DictSearchFilter
+
+__all__ = ['OrderingFilter', "SearchFilter", "DictSearchFilter"]
