c12dd68bd23e90ebf8f3e85ef56edc68dfbdf5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:05:12 +0000

    Add package vancouver-watching/3.429.1/vancouver_watching-3.429.1.tar.gz

---
+++ packages/vancouver-watching/vancouver_watching-3.429.1.tar.gz/vancouver_watching-3.429.1/vancouver_watching/ingest/__main__.py
@@ -0,0 +1,40 @@
+import argparse
+from blueness import module
+from vancouver_watching import NAME, VERSION
+from vancouver_watching.area import Area
+from vancouver_watching.logger import logger
+from blueness.argparse.generic import sys_exit
+
+NAME = module.name(__file__, NAME)
+
+
+parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+parser.add_argument(
+    "--geojson",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--count",
+    type=int,
+    default=-1,
+)
+parser.add_argument(
+    "--do_dryrun",
+    type=int,
+    default=0,
+    help="0|1",
+)
+args = parser.parse_args()
+
+area = Area(
+    args.geojson,
+    do_dryrun=args.do_dryrun,
+)
+success = area.valid
+
+if success:
+    success = area.ingest(count=args.count)
+
+
+sys_exit(logger, NAME, "discover", success)
