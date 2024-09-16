40ceb5dc623aeaa62de1405d1e7fd7e5fba76a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:26 +0000

    Add package vinces_selenium_extension/0.1.0/vinces_selenium_extension-0.1.0-py3-none-any.whl

---
+++ packages/vinces_selenium_extension/vinces_selenium_extension-0.1.0-py3-none-any.whl/vinces_selenium_extension/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
+
+print('helloo world')
