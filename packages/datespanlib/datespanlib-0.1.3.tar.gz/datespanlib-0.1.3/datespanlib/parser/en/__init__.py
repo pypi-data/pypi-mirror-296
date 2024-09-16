0b1f911d7e68c4cb5b65a56fa4f26c674cc04973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:37 +0000

    Add package datespanlib/0.1.3/datespanlib-0.1.3.tar.gz

---
+++ packages/datespanlib/datespanlib-0.1.3.tar.gz/datespanlib-0.1.3/datespanlib/parser/en/__init__.py
@@ -0,0 +1,16 @@
+# DateSpanLib - Copyright (c)2024, Thomas Zeutschler, MIT license
+
+from datespanlib.parser.en.parser import DateTextParser
+
+__all__ = [
+    "DateTextParser"
+]
+
+
+
+
+
+
+
+
+
