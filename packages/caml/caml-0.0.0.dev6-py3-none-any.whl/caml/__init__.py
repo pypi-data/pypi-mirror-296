98c9af3c4559c0bd0da9b4e4396d52a71af0400e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:11:12 +0000

    Add package caml/0.0.0.dev6/caml-0.0.0.dev6-py3-none-any.whl

---
+++ packages/caml/caml-0.0.0.dev6-py3-none-any.whl/caml/__init__.py
@@ -0,0 +1,6 @@
+from .core._base import CamlBase
+from .core.cate import CamlCATE
+from .core.dml import CamlDML
+from .core.dynamic import CamlDynamic
+from .core.policy import CamlPolicy
+from .logging import setup_logging
