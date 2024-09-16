ebd1bd3e0214e50cebaf0355646c5636cf383842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:52 +0000

    Add package mtmai-client/0.3.808/mtmai_client-0.3.808.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.808.tar.gz/mtmai_client-0.3.808/mtmai_client/api/agent/agent_items.py
@@ -0,0 +1,187 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union, cast
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.http_validation_error import HTTPValidationError
+from ...types import UNSET, Response, Unset
+
+
+def _get_kwargs(
+    *,
+    skip: Union[Unset, int] = 0,
+    limit: Union[Unset, int] = 100,
+) -> Dict[str, Any]:
+    params: Dict[str, Any] = {}
+
+    params["skip"] = skip
+
+    params["limit"] = limit
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "get",
+        "url": "/api/v1/agent",
+        "params": params,
+    }
+
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[Any, HTTPValidationError]]:
+    if response.status_code == HTTPStatus.UNAUTHORIZED:
+        response_401 = cast(Any, None)
+        return response_401
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        response_500 = cast(Any, None)
+        return response_500
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        response_422 = HTTPValidationError.from_dict(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[Any, HTTPValidationError]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: AuthenticatedClient,
+    skip: Union[Unset, int] = 0,
+    limit: Union[Unset, int] = 100,
+) -> Response[Union[Any, HTTPValidationError]]:
+    """获取 Agent 列表
+
+     此端点用于获取 agent 列表。支持分页功能可以通过 `skip` 和 `limit` 参数控制返回的 agent 数量。
+
+    Args:
+        skip (Union[Unset, int]):  Default: 0.
+        limit (Union[Unset, int]):  Default: 100.
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[Any, HTTPValidationError]]
+    """
+
+    kwargs = _get_kwargs(
+        skip=skip,
+        limit=limit,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    *,
+    client: AuthenticatedClient,
+    skip: Union[Unset, int] = 0,
+    limit: Union[Unset, int] = 100,
+) -> Optional[Union[Any, HTTPValidationError]]:
+    """获取 Agent 列表
+
+     此端点用于获取 agent 列表。支持分页功能可以通过 `skip` 和 `limit` 参数控制返回的 agent 数量。
+
+    Args:
+        skip (Union[Unset, int]):  Default: 0.
+        limit (Union[Unset, int]):  Default: 100.
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[Any, HTTPValidationError]
+    """
+
+    return sync_detailed(
+        client=client,
+        skip=skip,
+        limit=limit,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: AuthenticatedClient,
+    skip: Union[Unset, int] = 0,
+    limit: Union[Unset, int] = 100,
+) -> Response[Union[Any, HTTPValidationError]]:
+    """获取 Agent 列表
+
+     此端点用于获取 agent 列表。支持分页功能可以通过 `skip` 和 `limit` 参数控制返回的 agent 数量。
+
+    Args:
+        skip (Union[Unset, int]):  Default: 0.
+        limit (Union[Unset, int]):  Default: 100.
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[Any, HTTPValidationError]]
+    """
+
+    kwargs = _get_kwargs(
+        skip=skip,
+        limit=limit,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    *,
+    client: AuthenticatedClient,
+    skip: Union[Unset, int] = 0,
+    limit: Union[Unset, int] = 100,
+) -> Optional[Union[Any, HTTPValidationError]]:
+    """获取 Agent 列表
+
+     此端点用于获取 agent 列表。支持分页功能可以通过 `skip` 和 `limit` 参数控制返回的 agent 数量。
+
+    Args:
+        skip (Union[Unset, int]):  Default: 0.
+        limit (Union[Unset, int]):  Default: 100.
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[Any, HTTPValidationError]
+    """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            skip=skip,
+            limit=limit,
+        )
+    ).parsed
