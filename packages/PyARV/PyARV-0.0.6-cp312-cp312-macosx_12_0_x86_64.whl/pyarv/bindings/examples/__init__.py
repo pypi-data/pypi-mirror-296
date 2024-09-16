3bc0b4f07ea188486f35a4d6562018dbf97671d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:57 +0000

    Add package PyARV/0.0.6/PyARV-0.0.6-cp312-cp312-macosx_12_0_x86_64.whl

---
+++ packages/PyARV/PyARV-0.0.6-cp312-cp312-macosx_12_0_x86_64.whl/pyarv/bindings/examples/__init__.py
@@ -0,0 +1,2 @@
+from pyarv.bindings.examples.simple_examples_bindings import hello_world, foo, fatal_failure, non_fatal_failure, set_at_exit
+from pyarv.bindings.examples.numpy_examples_bindings import multiply_into
