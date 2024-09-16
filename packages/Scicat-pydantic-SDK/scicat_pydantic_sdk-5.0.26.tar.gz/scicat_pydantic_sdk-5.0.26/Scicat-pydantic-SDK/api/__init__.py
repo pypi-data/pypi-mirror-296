9abc05d74bb4172a00579fec9d676959279ad36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:45 +0000

    Add package Scicat-pydantic-SDK/5.0.26/scicat_pydantic_sdk-5.0.26.tar.gz

---
+++ packages/Scicat-pydantic-SDK/scicat_pydantic_sdk-5.0.26.tar.gz/scicat_pydantic_sdk-5.0.26/Scicat-pydantic-SDK/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from Scicat-pydantic-SDK.api.admin_api import AdminApi
+from Scicat-pydantic-SDK.api.auth_api import AuthApi
+from Scicat-pydantic-SDK.api.datasets_api import DatasetsApi
+from Scicat-pydantic-SDK.api.default_api import DefaultApi
+from Scicat-pydantic-SDK.api.elastic_search_api import ElasticSearchApi
+from Scicat-pydantic-SDK.api.instruments_api import InstrumentsApi
+from Scicat-pydantic-SDK.api.jobs_api import JobsApi
+from Scicat-pydantic-SDK.api.logbooks_api import LogbooksApi
+from Scicat-pydantic-SDK.api.origdatablocks_api import OrigdatablocksApi
+from Scicat-pydantic-SDK.api.policies_api import PoliciesApi
+from Scicat-pydantic-SDK.api.proposals_api import ProposalsApi
+from Scicat-pydantic-SDK.api.published_data_api import PublishedDataApi
+from Scicat-pydantic-SDK.api.samples_api import SamplesApi
+from Scicat-pydantic-SDK.api.user_identities_api import UserIdentitiesApi
+from Scicat-pydantic-SDK.api.users_api import UsersApi
+
