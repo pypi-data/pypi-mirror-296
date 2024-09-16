f9fa41379a54ea693d7df9aa205323a3b99e6534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:12 +0000

    Add package cythonsendevent/0.10/cythonsendevent-0.10-py3-none-any.whl

---
+++ packages/cythonsendevent/cythonsendevent-0.10-py3-none-any.whl/cythonsendevent/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+import numpy as np
+
+try:
+    from .cysend import *
+
+except Exception as e:
+    import Cython, setuptools, platform, subprocess, os, sys, time, exceptdrucker, numpy,regex
+
+    iswindows = "win" in platform.platform().lower()
+    if iswindows:
+        addtolist = []
+    else:
+        addtolist = ["&"]
+
+    olddict = os.getcwd()
+    dirname = os.path.dirname(__file__)
+    os.chdir(dirname)
+    compile_file = os.path.join(dirname, "cysend_compile.py")
+    subprocess._USE_VFORK = False
+    subprocess._USE_POSIX_SPAWN = False
+    subprocess.run(
+        " ".join([sys.executable, compile_file, "build_ext", "--inplace"] + addtolist),
+        shell=True,
+        env=os.environ,
+        preexec_fn=None
+        if iswindows
+        else os.setpgrp
+        if hasattr(os, "setpgrp")
+        else None,
+    )
+    if not iswindows:
+        time.sleep(120)
+    from .cysend import *
+
+    os.chdir(olddict)
