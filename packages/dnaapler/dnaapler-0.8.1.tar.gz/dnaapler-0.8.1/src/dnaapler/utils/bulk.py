27b51efe896e2dc9c18e54b4c4a80cdc238f044d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:05:01 +0000

    Add package dnaapler/0.8.1/dnaapler-0.8.1.tar.gz

---
+++ packages/dnaapler/dnaapler-0.8.1.tar.gz/dnaapler-0.8.1/src/dnaapler/utils/bulk.py
@@ -0,0 +1,329 @@
+import os
+import shutil
+from pathlib import Path
+
+import pandas as pd
+from Bio import SeqIO
+from loguru import logger
+
+from dnaapler.utils.constants import DNAAPLER_DB
+from dnaapler.utils.external_tools import ExternalTool
+from dnaapler.utils.processing import reorient_single_record_bulk
+from dnaapler.utils.validation import validate_custom_db_fasta
+
+
+def run_bulk_blast(
+    ctx,
+    input: Path,
+    output: Path,
+    prefix: str,
+    gene: str,
+    evalue: float,
+    threads: int,
+    custom_db: Path,
+) -> bool:
+    """
+    Run the BLAST part for dnaapler bulk (not processing).
+
+    Args:
+        ctx (): The context manager for managing the execution context.
+        input (Path): Path to the input data for BLAST.
+        output (Path): Directory where BLAST results and logs will be saved.
+        prefix (str): Prefix for output files.
+        gene (str): Gene name to specify the BLAST database ("dnaA", "repA", "terL", "all", "cog1474", or "custom").
+        evalue (float): The E-value threshold for BLAST.
+        threads (int): The number of threads to use for BLAST.
+        custom_db (Path): Path to a custom database file (amino acid Fasta format) if 'gene' is set to 'custom'.
+
+    Yields:
+        bool: True if the BLAST process succeeded, False otherwise.
+    """
+
+    # sets DB directory based of the gene name
+
+    # defines db name
+    db_name = "dnaA_db"
+    if gene == "dnaA":
+        db_name = "dnaA_db"
+    elif gene == "repA":
+        db_name = "repA_db"
+    elif gene == "terL":
+        db_name = "terL_db"
+    elif gene == "all":
+        db_name = "all_db"
+    elif gene == "dnaA,repA":
+        db_name = "dnaA_repA_db"
+    elif gene == "dnaA,terL":
+        db_name = "dnaA_terL_db"
+    elif gene == "repA,terL":
+        db_name = "repA_terL_db"
+    elif gene == "cog1474":
+        db_name = "cog1474_db"
+
+    # for chromosome, plasmid or phage or all
+    # runs blast
+    if gene != "custom":
+        # blast
+        logdir = Path(f"{output}/logs")
+        blast_output = os.path.join(output, f"{prefix}_blast_output.txt")
+
+        db = os.path.join(DNAAPLER_DB, db_name)
+        blast = ExternalTool(
+            tool="blastx",
+            input=f"-query {input}",
+            output=f"-out {blast_output}",
+            params=f'-db {db} -evalue  {evalue} -num_threads {threads} -outfmt " 6 qseqid qlen sseqid slen length qstart qend sstart send pident nident gaps mismatch evalue bitscore qseq sseq "',
+            logdir=logdir,
+        )
+
+        ExternalTool.run_tool(blast, ctx)
+
+    # if gene == custom
+    # maikes db then runs blast
+    elif gene == "custom":
+        # validates custom fasta input for database
+        validate_custom_db_fasta(Path(custom_db))
+
+        # make db
+        db_dir = os.path.join(output, "custom_db")
+        Path(db_dir).mkdir(parents=True, exist_ok=True)
+        custom_db_fasta = os.path.join(db_dir, "custom_db.faa")
+        shutil.copy2(custom_db, custom_db_fasta)
+
+        logdir = Path(f"{output}/logs")
+        # custom db
+        # make custom db
+        custom_database = os.path.join(db_dir, "custom_db")
+        makeblastdb = ExternalTool(
+            tool="makeblastdb",
+            input=f"-in {custom_db_fasta}",
+            output=f"-out {custom_database}",
+            params="-dbtype prot ",
+            logdir=logdir,
+        )
+
+        # chromosome path
+        # blast
+        logdir = Path(f"{output}/logs")
+        blast_output = os.path.join(output, f"{prefix}_blast_output.txt")
+        # db
+        db = os.path.join(db_dir, "custom_db")
+        blast = ExternalTool(
+            tool="blastx",
+            input=f"-query {input}",
+            output=f"-out {blast_output}",
+            params=f'-db {db} -evalue  {evalue} -num_threads {threads} -outfmt " 6 qseqid qlen sseqid slen length qstart qend sstart send pident nident gaps mismatch evalue bitscore qseq sseq "',
+            logdir=logdir,
+        )
+
+        tools_to_run = (makeblastdb, blast)
+        ExternalTool.run_tools(tools_to_run, ctx)
+
+
+def bulk_process_blast_output_and_reorient(
+    input: Path, blast_file: Path, output: Path, prefix: str
+) -> None:
+    """
+    Processes the BLAST output and saves reoriented and failed-to-reorient contigs.
+
+    For dnaapler bulk
+
+    Reoriented contigs are saved to 'output/prefix_reoriented.fasta'.
+    Failed-to-reorient contigs are saved to 'output/prefix_failed_to_reorient.fasta'.
+
+    Args:
+        input (Path): Input file containing nucleotide sequences.
+        blast_file (Path): BLAST output file.
+        output (Path): Output directory for saving files.
+        prefix (str): Prefix for output files.
+
+    Returns:
+        None
+    """
+
+    # define colnames
+    col_list = [
+        "qseqid",
+        "qlen",
+        "sseqid",
+        "slen",
+        "length",
+        "qstart",
+        "qend",
+        "sstart",
+        "send",
+        "pident",
+        "nident",
+        "gaps",
+        "mismatch",
+        "evalue",
+        "bitscore",
+        "qseq",
+        "sseq",
+    ]
+
+    # read in the dataframe from BLAST
+    try:
+        blast_df = pd.read_csv(
+            blast_file, delimiter="\t", index_col=False, names=col_list
+        )
+
+    except Exception:
+        logger.error("There was an issue with parsing the BLAST output file.")
+
+    if isinstance(blast_df, pd.DataFrame) and blast_df.empty:
+        logger.error(
+            "There were 0 BLAST hits. Please check your input file or try dnaapler custom. If you have assembled an understudied species, this may also be the cause."
+        )
+
+    # Initialize the list to store the IDs
+    contigs = []
+    starts = []
+    strands = []
+    top_hits = []
+    top_hit_lengths = []
+    covered_lens = []
+    coverages = []
+    idents = []
+    identitys = []
+
+    reoriented_output_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+    fail_reoriented_output_file = os.path.join(
+        output, f"{prefix}_failed_to_reorient.fasta"
+    )
+
+    # Read the FASTA file and extract the IDs
+    for record in SeqIO.parse(input, "fasta"):
+        contig = record.description
+        # need this to match for BLAST
+        short_contig = record.id
+
+        contigs.append(contig)
+
+        # Filter the DataFrame where 'qseqid' matches 'contig'
+        filtered_df = blast_df[blast_df["qseqid"] == short_contig]
+
+        length_of_df = len(filtered_df)
+
+        # no hits at all then write to the not-reoriented file
+        if length_of_df == 0:
+            with open(fail_reoriented_output_file, "a") as out_fa:
+                SeqIO.write(record, out_fa, "fasta")
+
+            # no hit save for the output DF
+            message = "No_BLAST_hits"
+            starts.append(message)
+            strands.append(message)
+            top_hits.append(message)
+            top_hit_lengths.append(message)
+            covered_lens.append(message)
+            coverages.append(message)
+            idents.append(message)
+            identitys.append(message)
+
+        else:
+            # reset the index if you want to re-index the filtered DataFrame
+            filtered_df.reset_index(drop=True, inplace=True)
+
+            # top hit has a start of 1 ########
+            # take the top hit - blast sorts by bitscore
+            # if the start is 1 of the top hit
+            if filtered_df["qstart"][0] == 1:
+                # writes to file
+                with open(reoriented_output_file, "a") as out_fa:
+                    SeqIO.write(record, out_fa, "fasta")
+
+                # no hit save for the output DF
+                message = "Contig_already_reoriented"
+
+                starts.append(message)
+                strands.append(message)
+                top_hits.append(message)
+                top_hit_lengths.append(message)
+                covered_lens.append(message)
+                coverages.append(message)
+                idents.append(message)
+                identitys.append(message)
+
+            # top hit
+            # prokaryotes can use AUG M, GUG V or UUG L as start codons - e.g. for Pseudomonas aeruginosa PA01  dnaA actually starts with V
+            # Therefore, I will require the start codon to be the 1st in the searched sequence match - unlikely to not be but nonetheless
+            # Sometimes, the top hit might not be the actual gene of interest (e.g. in phages if the terL is disrupted - like SAOMS1)
+            # so in these cases, go down the list and check there is a hit with a legitimate start codon
+            # I anticipate this will be rare usually, the first will be it :)
+            else:
+                if filtered_df["qseq"][0][0] in ["M", "V", "L"] and (
+                    filtered_df["sstart"][0] == 1
+                ):
+                    # starts with valid start codon but needs orientation
+                    (
+                        start,
+                        strand,
+                        top_hit,
+                        top_hit_length,
+                        covered_len,
+                        coverage,
+                        ident,
+                        identity,
+                    ) = reorient_single_record_bulk(
+                        filtered_df,
+                        reoriented_output_file,
+                        record,
+                        overlapping_orf=False,
+                    )
+
+                else:  # top hit does not start with a valid start codon
+                    (
+                        start,
+                        strand,
+                        top_hit,
+                        top_hit_length,
+                        covered_len,
+                        coverage,
+                        ident,
+                        identity,
+                    ) = reorient_single_record_bulk(
+                        filtered_df,
+                        reoriented_output_file,
+                        record,
+                        overlapping_orf=True,
+                    )
+
+                    # because pyrodigal
+                    if strand == -1:
+                        strand = "reverse"
+                    else:
+                        strand = "forward"
+
+                # save all the stats
+                starts.append(start)
+                strands.append(strand)
+                top_hits.append(top_hit)
+                top_hit_lengths.append(top_hit_length)
+                covered_lens.append(covered_len)
+                coverages.append(coverage)
+                idents.append(ident)
+                identitys.append(identity)
+
+    # write the example info to file
+    #
+    # Create a dictionary from the lists
+    data = {
+        "Contig": contigs,
+        "Start": starts,
+        "Strand": strands,
+        "Top_Hit": top_hits,
+        "Top_Hit_Length": top_hit_lengths,
+        "Covered_Length": covered_lens,
+        "Coverage": coverages,
+        "Identical_AAs": idents,
+        "Identity_Percentage": identitys,
+    }
+
+    # Convert the dictionary to a DataFrame and save output
+    bulk_summary_df = pd.DataFrame(data)
+    bulk_summary_df.to_csv(
+        os.path.join(output, prefix + "_bulk_reorientation_summary.tsv"),
+        sep="\t",
+        index=False,
+    )
---
+++ packages/dnaapler/dnaapler-0.8.1.tar.gz/dnaapler-0.8.1/src/dnaapler/utils/cds_methods.py
@@ -0,0 +1,310 @@
+import os
+import random
+from pathlib import Path
+
+import pyrodigal
+from Bio import SeqIO
+from loguru import logger
+
+from dnaapler.utils.constants import DNAAPLER_DB
+from dnaapler.utils.external_tools import ExternalTool
+from dnaapler.utils.processing import (
+    process_blast_output_and_reorient,
+    reorient_sequence_random,
+)
+
+
+def run_mystery(ctx, input: Path, seed_value: int, output: Path, prefix: str) -> None:
+    """
+    Reorients a DNA sequence with a random Coding DNA Sequence (CDS) from the input file.
+
+    Args:
+        ctx (object): A context or environment object, possibly from a command-line tool.
+        input (str): Path to the input DNA sequence file in FASTA format.
+        seed_value (int): Seed value for randomization.
+        output (str): Path to the output directory where the reoriented sequence will be saved.
+        prefix (str): Prefix for the output file name.
+
+    Returns:
+        None
+
+    This function searches for CDS using Pyrodigal in the input DNA sequence, then randomly selects
+    one CDS (not the first or last) and reorients the DNA sequence with respect to that CDS.
+    It saves the reoriented sequence in the specified output directory with the given prefix.
+    """
+
+    # get number of records of input
+    logger.info("Searching for CDS with pyrodigal")
+    orf_finder = pyrodigal.GeneFinder(meta=True)
+
+    # set seed
+    random.seed(int(seed_value))
+
+    # there will only be 1 record
+    for i, record in enumerate(SeqIO.parse(input, "fasta")):
+        genes = orf_finder.find_genes(str(record.seq))
+        # get number of genes
+        gene_count = len(genes)
+
+        # ensure has > 3 genes
+        if gene_count < 4:
+            logger.error(
+                f"{input} has less than 4 CDS. You probably shouldn't be using dnaapler mystery!"
+            )
+            ctx.exit(2)
+
+        logger.info("Reorienting with a random CDS (that is not the first or last).")
+
+        # ensure not first or last gene
+        reorient_gene_number = random.randint(2, gene_count - 1)
+
+        logger.info(f"Gene number {reorient_gene_number} was selected.")
+        start = genes[reorient_gene_number].begin
+        strand = genes[reorient_gene_number].strand
+
+        if strand == 1:
+            strand_eng = "forward"
+        else:
+            strand_eng = "negative"
+
+        logger.info(f"Your random CDS has a start coordinate of {start}.")
+        logger.info(f"Your random CDS is on the {strand_eng} strand.")
+
+        output_processed_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+        reorient_sequence_random(input, output_processed_file, start, strand)
+
+
+def run_nearest(ctx, input: Path, output: Path, prefix: str) -> None:
+    """
+    Reorients a DNA sequence to begin with the first Coding DNA Sequence (CDS) found in the input file.
+
+    Args:
+        ctx (object): A context or environment object, possibly from a command-line tool.
+        input (str): Path to the input DNA sequence file in FASTA format.
+        output (str): Path to the output directory where the reoriented sequence will be saved.
+        prefix (str): Prefix for the output file name.
+
+    Returns:
+        None
+
+    This function searches for CDS using Pyrodigal in the input DNA sequence, then reorients the DNA
+    sequence to begin with the first CDS found. The reoriented sequence is saved in the specified
+    output directory with the given prefix.
+    """
+    # get number of records of input
+    logger.info("Searching for CDS with pyrodigal")
+    orf_finder = pyrodigal.GeneFinder(meta=True)
+
+    # there will only be 1 record
+    for i, record in enumerate(SeqIO.parse(input, "fasta")):
+        genes = orf_finder.find_genes(str(record.seq))
+        # get number of genes
+        gene_count = len(genes)
+
+        # ensure has > 1 genes
+        if gene_count < 2:
+            logger.error(
+                f"{input} has less than 2 CDS. You probably shouldn't be using dnaapler nearest!"
+            )
+            ctx.exit(2)
+
+        logger.info("Reorienting to begin with the first CDS.")
+
+        reorient_gene_number = 1
+
+        start = genes[reorient_gene_number].begin
+        strand = genes[reorient_gene_number].strand
+
+        if strand == 1:
+            strand_eng = "forward"
+        else:
+            strand_eng = "negative"
+
+        logger.info(f"Your first CDS has a start coordinate of {start}.")
+        logger.info(f"Your first CDS is on the {strand_eng} strand.")
+
+        output_processed_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+        reorient_sequence_random(input, output_processed_file, start, strand)
+
+
+def run_largest(ctx, input: Path, output: Path, prefix: str) -> None:
+    """
+    Reorients a DNA sequence to begin with the largest Coding DNA Sequence (CDS) found in the input file.
+
+    Args:
+        ctx (object): A context or environment object, possibly from a command-line tool.
+        input (Path): Path to the input DNA sequence file in FASTA format.
+        seed_value (int): Seed value for randomization.
+        output (Path): Path to the output directory where the reoriented sequence will be saved.
+        prefix (str): Prefix for the output file name.
+
+    Returns:
+        None
+
+    This function searches for CDS using Pyrodigal in the input DNA sequence, then reorients the DNA
+    sequence to begin with the largest CDS found based on the size of the coding region. The reoriented
+    sequence is saved in the specified output directory with the given prefix.
+    """
+    # get number of records of input
+    logger.info("Searching for CDS with pyrodigal")
+    orf_finder = pyrodigal.GeneFinder(meta=True)
+
+    # there will only be 1 record
+    for i, record in enumerate(SeqIO.parse(input, "fasta")):
+        genes = orf_finder.find_genes(str(record.seq))
+        # get number of genes
+        gene_count = len(genes)
+
+        # ensure has > 3 genes
+        if gene_count < 4:
+            logger.error(
+                f"{input} has less than 4 CDS. You probably shouldn't be using dnaapler mystery!"
+            )
+
+        logger.info("Reorienting with the largest CDS.")
+
+        # iterate over dict
+
+        size_dict = {}
+        gene_index = 0
+
+        for gene in genes:
+            size = abs(gene.end - gene.begin)
+            size_dict[gene_index] = size
+            gene_index += 1
+
+        # Find the gene with the max overlap
+        largest_gene_index = max(size_dict, key=lambda key: size_dict[key])
+
+        start = genes[largest_gene_index].begin
+        strand = genes[largest_gene_index].strand
+        max_size = size_dict[largest_gene_index] / 3
+
+        if strand == 1:
+            strand_eng = "forward"
+        else:
+            strand_eng = "negative"
+
+        logger.info(
+            f"Your largest CDS has a start coordinate of {start} and has a size of {max_size} AAs."
+        )
+        logger.info(f"Your largest CDS is on the {strand_eng} strand.")
+
+        output_processed_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+        reorient_sequence_random(input, output_processed_file, start, strand)
+
+
+def run_blast_based_method(
+    ctx, input: Path, output: Path, prefix: str, gene: str, evalue: float, threads: int
+) -> bool:
+    """
+    Run a BLAST-based approach to reorient a DNA sequence based on a specific gene.
+
+    Args:
+        ctx (object): A context or environment object, possibly from a command-line tool.
+        input (Path): Path to the input DNA sequence file in FASTA format.
+        output (Path): Path to the output directory where the reoriented sequence will be saved.
+        prefix (str): Prefix for the output file name.
+        gene (str): Name of the gene used for BLAST search (options: 'dnaA', 'repA', 'terL', 'custom', 'cog1474').
+        evalue (float): E-value threshold for BLAST search.
+        threads (int): Number of threads for BLAST search.
+
+    Returns:
+        bool: True if the BLAST-based approach succeeded, False otherwise.
+    """
+
+    # sets DB directory based of the gene name
+
+    # defines db name
+    db_name = "dnaA_db"
+    if gene == "dnaA":
+        db_name = "dnaA_db"
+    elif gene == "repA":
+        db_name = "repA_db"
+    elif gene == "terL":
+        db_name = "terL_db"
+    elif gene == "cog1474":
+        db_name = "cog1474_db"
+
+    # chromosome path
+    # blast
+    logdir = Path(f"{output}/logs")
+    blast_output = os.path.join(output, f"{prefix}_blast_output.txt")
+
+    db = os.path.join(DNAAPLER_DB, db_name)
+    if gene == "custom":
+        db = os.path.join(output, "custom_db", "custom_db")
+    blast = ExternalTool(
+        tool="blastx",
+        input=f"-query {input}",
+        output=f"-out {blast_output}",
+        params=f'-db {db} -evalue  {evalue} -num_threads {str(threads)} -outfmt " 6 qseqid qlen sseqid slen length qstart qend sstart send pident nident gaps mismatch evalue bitscore qseq sseq "',
+        logdir=logdir,
+    )
+
+    ExternalTool.run_tool(blast, ctx)
+
+    # reorient the genome based on the BLAST hit
+    output_processed_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+    blast_success = process_blast_output_and_reorient(
+        input, blast_output, output_processed_file, gene
+    )
+
+    return blast_success
+
+
+def run_blast_based_method_bulk(
+    ctx, input: Path, output: Path, prefix: str, gene: str, evalue: float, threads: int
+) -> bool:
+    """
+    Run a bulk BLAST-based approach to reorient multiple DNA sequences based on a specific gene.
+
+    Args:
+        ctx (object): A context or environment object, possibly from a command-line tool.
+        input (Path): Path to the directory containing input DNA sequence files in FASTA format.
+        output (Path): Path to the output directory where the reoriented sequences will be saved.
+        prefix (str): Prefix for the output file names.
+        gene (str): Name of the gene used for BLAST search (options: 'dnaA', 'repA', 'terL', 'custom').
+        evalue (float): E-value threshold for BLAST search.
+        threads (int): Number of threads for BLAST search.
+
+    Returns:
+        bool: True if the bulk BLAST-based approach succeeded for all sequences, False otherwise.
+    """
+
+    # sets DB directory based of the gene name
+
+    # defines db name
+    db_name = "dnaA_db"
+    if gene == "dnaA":
+        db_name = "dnaA_db"
+    elif gene == "repA":
+        db_name = "repA_db"
+    elif gene == "terL":
+        db_name = "terL_db"
+
+    # chromosome path
+    # blast
+    logdir = Path(f"{output}/logs")
+    blast_output = os.path.join(output, f"{prefix}_blast_output.txt")
+
+    db = os.path.join(DNAAPLER_DB, db_name)
+    if gene == "custom":
+        db = os.path.join(output, "custom_db", "custom_db")
+    blast = ExternalTool(
+        tool="blastx",
+        input=f"-query {input}",
+        output=f"-out {blast_output}",
+        params=f'-db {db} -evalue  {evalue} -num_threads {str(threads)} -outfmt " 6 qseqid qlen sseqid slen length qstart qend sstart send pident nident gaps mismatch evalue bitscore qseq sseq "',
+        logdir=logdir,
+    )
+
+    ExternalTool.run_tool(blast, ctx)
+
+    # reorient the genome based on the BLAST hit
+    output_processed_file = os.path.join(output, f"{prefix}_reoriented.fasta")
+    blast_success = process_blast_output_and_reorient(
+        input, blast_output, output_processed_file, gene
+    )
+
+    return blast_success
