969ba3197df6970821746825d627b13320b10b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 06:05:11 +0000

    Add package bhakti/0.2.13/bhakti-0.2.13.tar.gz

---
+++ packages/bhakti/bhakti-0.2.13.tar.gz/bhakti-0.2.13/bhakti/handler/exception_notifier.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+
+from bhakti.server.pipeline import PipelineStage
+from bhakti.const import EMPTY_STR
+
+log = logging.getLogger("bhakti")
+
+
+class ExceptionNotifier(PipelineStage):
+    def __init__(self, name: str = 'exception_notifier'):
+        super().__init__(name)
+
+    async def do(
+            self,
+            data: bytes | str,
+            fire: bool,
+            errors: list[Exception],
+            io_context: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None,
+            eof: bytes,
+            extra_context: any
+    ) -> tuple[any, any, list[Exception], bool]:
+        if isinstance(errors, list):
+            if len(errors) > 0:
+                err_log = EMPTY_STR()
+                for err in errors:
+                    err_log += f'\n{str(type(err).__name__)}: {str(err)}'
+                log.error(err_log)
+        return data, extra_context, errors, False
---
+++ packages/bhakti/bhakti-0.2.13.tar.gz/bhakti-0.2.13/test/test_bootstrap.py
@@ -0,0 +1,12 @@
+from bhakti import BhaktiServer
+from bhakti.database import DBEngine
+
+if __name__ == '__main__':
+    BhaktiServer(
+        eof=b'_wzh_',
+        dimension=4096,
+        db_path=r"E:\Python\bhakti\test_archive",
+        db_engine=DBEngine.DIPAMKARA,
+        cached=False,
+        verbose=False
+    ).run()
