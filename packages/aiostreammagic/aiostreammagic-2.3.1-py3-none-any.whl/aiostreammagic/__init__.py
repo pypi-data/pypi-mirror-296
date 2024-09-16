8dc0fe800edbaa4f31c9c70bec51fa15cb26c9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:55 +0000

    Add package aiostreammagic/2.3.1/aiostreammagic-2.3.1-py3-none-any.whl

---
+++ packages/aiostreammagic/aiostreammagic-2.3.1-py3-none-any.whl/aiostreammagic/__init__.py
@@ -0,0 +1,30 @@
+"""Asynchronous Python client for StreamMagic API."""
+
+from .exceptions import StreamMagicError, StreamMagicConnectionError
+from .models import (
+    Info,
+    PlayStateMetadata,
+    PlayState,
+    State,
+    Source,
+    NowPlaying,
+    TransportControl,
+    RepeatMode,
+    ShuffleMode,
+)
+from .stream_magic import StreamMagicClient
+
+__all__ = [
+    "StreamMagicClient",
+    "StreamMagicError",
+    "StreamMagicConnectionError",
+    "Info",
+    "Source",
+    "State",
+    "PlayState",
+    "PlayStateMetadata",
+    "NowPlaying",
+    "TransportControl",
+    "ShuffleMode",
+    "RepeatMode",
+]
