e467e202e6bddc8815339c7637c543bb435f0ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:25 +0000

    Add package eegsynth/0.8.0/eegsynth-0.8.0.tar.gz

---
+++ packages/eegsynth/eegsynth-0.8.0.tar.gz/eegsynth-0.8.0/src/module/plotspectral/__init__.py
@@ -0,0 +1,18 @@
+import sys
+
+from .plotspectral import _setup, _start, _loop_once, _loop_forever, _stop
+
+class _executable:
+    # start this module as a stand-alone executable
+    def __init__(self, args=None):
+        if args!=None:
+            # override the command line arguments
+            sys.argv = [sys.argv[0]] + args
+
+        # the Qt application does not restart upon a raised exception
+        _setup()
+        _start()
+        _loop_forever()
+
+    def __del__(self):
+        _stop()
