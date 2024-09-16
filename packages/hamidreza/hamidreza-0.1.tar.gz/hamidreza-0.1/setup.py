d7b07f795e023be39a8fb8fc9a4cb00a387e7e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:57 +0000

    Add package hamidreza/0.1/hamidreza-0.1.tar.gz

---
+++ packages/hamidreza/hamidreza-0.1.tar.gz/hamidreza-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hamidreza',
+    version='0.1',
+    description='A simple package with personal info about Hamidreza',
+    author='Hamidreza',
+    author_email='your-email@example.com',
+    packages=find_packages(),
+    install_requires=[],  # اگر پکیج‌های اضافی لازم دارید، اینجا اضافه کنید
+)
