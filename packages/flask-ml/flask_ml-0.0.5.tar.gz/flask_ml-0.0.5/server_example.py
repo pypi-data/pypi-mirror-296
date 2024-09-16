3f86f70140644f6392eb56d33133ea4d1fa43efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:23 +0000

    Add package flask-ml/0.0.5/flask_ml-0.0.5.tar.gz

---
+++ packages/flask-ml/flask_ml-0.0.5.tar.gz/flask_ml-0.0.5/server_example.py
@@ -0,0 +1,39 @@
+from flask_ml.flask_ml_server import MLServer
+from flask_ml.flask_ml_server.constants import DataTypes
+from flask_ml.flask_ml_server.models import ResponseModel, TextResult
+
+
+# Create a dummy ML model
+class DummyModel:
+    def predict(self, data: list) -> list:
+        return [str(e) for e in range(len(data))]  # Return 0 to len(data) - 1
+
+
+# create an instance of the model
+model = DummyModel()
+
+# Create a server
+server = MLServer(__name__)
+
+
+# Create an endpoint
+@server.route("/dummymodel", DataTypes.TEXT)
+def process_text(inputs: list, parameters: dict) -> dict:
+    results = model.predict(inputs)
+    results = [TextResult(text=e.text, result=r) for e, r in zip(inputs, results)]
+    response = ResponseModel(results=results)
+    return response.get_response()
+
+
+# Run the server (optional. You can also run the server using the command line)
+server.run()
+
+# Expected request json format:
+# {
+#     "inputs": [
+#         {"text": "Text to be classified"},
+#         {"text": "Another text to be classified"}
+#     ],
+#     "data_type": "TEXT",
+#     "parameters": {}
+# }
