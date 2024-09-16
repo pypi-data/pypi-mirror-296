c00d4243086b7792a712b27493ff4f3e1fb45c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:51 +0000

    Add package TelSDK/0.1.2/telsdk-0.1.2.tar.gz

---
+++ packages/TelSDK/telsdk-0.1.2.tar.gz/telsdk-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+setup(
+    name="TelSDK",
+    version="v0.1.2",
+    description="Simple Telegram bot library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="guangrei",
+    author_email="myawn@pm.me",
+    url="https://github.com/cirebon-dev/TelegramSDK",
+    packages=["TelegramSDK"],
+    license="MIT",
+    platforms="any",
+    install_requires=["requests", "zcache"],
+)
