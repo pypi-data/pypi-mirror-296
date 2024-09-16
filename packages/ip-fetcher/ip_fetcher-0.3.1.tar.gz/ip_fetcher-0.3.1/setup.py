7e0648afe1ff5b65089d56f9d7552bfb1893d55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:11:19 +0000

    Add package ip-fetcher/0.3.1/ip_fetcher-0.3.1.tar.gz

---
+++ packages/ip-fetcher/ip_fetcher-0.3.1.tar.gz/ip_fetcher-0.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ip_fetcher",
+    version="0.3.1",  # Incremented version for updates
+    packages=find_packages(),
+    description="A Python library to fetch public and private IP addresses.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    install_requires=[
+        'requests',  # Only external dependencies should be listed here
+    ],
+)
