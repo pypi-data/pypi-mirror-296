5c47aacbc0ea2fc3ed0c0fa9e4ea07dbba996aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:33 +0000

    Add package TraceNinja/1.0.5/TraceNinja-1.0.5-py3-none-any.whl

---
+++ packages/TraceNinja/TraceNinja-1.0.5-py3-none-any.whl/TraceNinja/modules/rapidapi.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+from rich import print as rprint
+
+env_file = find_dotenv(f'.env.{os.getenv("ENV", "example")}')
+
+load_dotenv(env_file)
+
+def fetch(target):
+    api_key = os.getenv('RAPIDAPI_API_KEY')
+
+    if not api_key:
+        return []
+
+    url = f"https://subdomain-finder3.p.rapidapi.com/v1/subdomain-finder/"
+
+    querystring = {"domain":{target}}
+
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": "subdomain-finder3.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    if response.status_code != 200:
+        rprint("[red1][ERROR] [RapidApi]","You do not have permission to access this resource. Check your API key.")
+        return []
+
+    data = response.json()["subdomains"]
+
+    subdomains = []
+    for item in data:
+        subdomains.append(item["subdomain"])
+
+    return list(subdomains)
+
+if __name__ == '__main__':
+    fetch()
