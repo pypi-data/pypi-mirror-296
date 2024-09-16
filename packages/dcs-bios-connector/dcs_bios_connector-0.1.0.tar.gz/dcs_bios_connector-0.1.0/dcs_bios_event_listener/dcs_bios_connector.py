4941db143edba706911cb3f11be47d3787d8e2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:50 +0000

    Add package dcs-bios-connector/0.1.0/dcs_bios_connector-0.1.0.tar.gz

---
+++ packages/dcs-bios-connector/dcs_bios_connector-0.1.0.tar.gz/dcs_bios_connector-0.1.0/dcs_bios_event_listener/dcs_bios_connector.py
@@ -0,0 +1,15 @@
+
+
+from pyee import EventEmitter
+from control_parser import ControlParser
+from udp_listener import UdpListener
+
+
+class DcsBiosEventListener(EventEmitter):
+
+    def __init__(self):
+        self.updListener = UdpListener()
+        self.control_parser = ControlParser()
+    
+    def connect(self):
+        self.updListener.start()
