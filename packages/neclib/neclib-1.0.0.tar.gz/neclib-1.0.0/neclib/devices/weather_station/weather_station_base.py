29b944ad8c35d5e56074072bf3b1aeb725c09492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:05:10 +0000

    Add package neclib/1.0.0/neclib-1.0.0.tar.gz

---
+++ packages/neclib/neclib-1.0.0.tar.gz/neclib-1.0.0/neclib/devices/weather_station/weather_station_base.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod
+
+import astropy.units as u
+
+from ..device_base import DeviceBase
+
+
+class WeatherStation(DeviceBase):
+
+    Manufacturer: str = ""
+    Model: str
+
+    @abstractmethod
+    def get_temperature(self) -> u.Quantity: ...
+
+    @abstractmethod
+    def get_humidity(self) -> float: ...
+
+    @abstractmethod
+    def get_pressure(self) -> u.Quantity: ...
+
+    @abstractmethod
+    def finalize(self) -> None: ...
