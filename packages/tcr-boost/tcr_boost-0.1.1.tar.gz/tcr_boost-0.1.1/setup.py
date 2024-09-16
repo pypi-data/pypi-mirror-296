6e5bde18f18cc75c1873b736b0a8c856ebe83534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:11:21 +0000

    Add package tcr-boost/0.1.1/tcr_boost-0.1.1.tar.gz

---
+++ packages/tcr-boost/tcr_boost-0.1.1.tar.gz/tcr_boost-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tcr_boost',
+    version='0.1.1',
+    description='Minimal setup for the tcr_boost package',
+    packages=find_packages(),
+    license='MIT',
+)
