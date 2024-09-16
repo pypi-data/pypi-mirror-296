6b7de6c9658fdb3832c096caea0a485505487b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:08 +0000

    Add package Jord/0.7.6/Jord-0.7.6-py36-none-any.whl

---
+++ packages/Jord/Jord-0.7.6-py36-none-any.whl/jord/qgis_utilities/conversion/parsing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import Collection
+
+
+def sanitise_wkb() -> str: ...
+
+
+def sanitise_wkt() -> str: ...
+
+
+def explode_geometry_collection() -> Collection[str]: ...
