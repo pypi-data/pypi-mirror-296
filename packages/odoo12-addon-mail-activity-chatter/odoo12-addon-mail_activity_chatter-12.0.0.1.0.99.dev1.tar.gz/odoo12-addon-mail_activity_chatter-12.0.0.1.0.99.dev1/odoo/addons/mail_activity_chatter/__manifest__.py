5a556ea7ac0500139c8ccfb1ba35930e53681edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:41 +0000

    Add package odoo12-addon-mail-activity-chatter/12.0.0.1.0.99.dev1/odoo12-addon-mail_activity_chatter-12.0.0.1.0.99.dev1.tar.gz

---
+++ packages/odoo12-addon-mail-activity-chatter/odoo12-addon-mail_activity_chatter-12.0.0.1.0.99.dev1.tar.gz/odoo12-addon-mail_activity_chatter-12.0.0.1.0.99.dev1/odoo/addons/mail_activity_chatter/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2024-SomItCoop SCCL(<https://gitlab.com/somitcoop>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail activities with chatter",
+    "version": "12.0.0.1.0",
+    "depends": ["mail_activity_board"],
+    "author": """
+        Som It Cooperatiu SCCL,
+        Som Connexió SCCL,
+        Odoo Community Association (OCA)",
+    """,
+    "category": "Customer Relationship Management",
+    "website": "https://gitlab.com/somitcoop/erp-research/odoo-helpdesk",
+    "license": "AGPL-3",
+    "summary": """
+        Adds the chatter functionality in mail activities so notes can be left on them.
+    """,
+    "demo": [],
+    "data": [
+        "views/mail_activity_views.xml",
+    ],
+    "qweb": [
+        "static/src/xml/mail_chatter_buttons.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
