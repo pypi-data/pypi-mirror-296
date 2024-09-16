df16bc8488fb874881ae416e779819d13dfc10cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:29 +0000

    Add package johnlock/0.1/johnlock-0.1.tar.gz

---
+++ packages/johnlock/johnlock-0.1.tar.gz/johnlock-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="johnlock",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple package that says hello",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnlock/johnlock",
+    author="John Lock",
+    author_email="john.lock@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
