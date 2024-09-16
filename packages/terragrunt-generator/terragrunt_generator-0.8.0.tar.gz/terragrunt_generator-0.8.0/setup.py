d2f472cf80ed81654d1d03b70dc9b083e66ba857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 03:10:50 +0000

    Add package terragrunt-generator/0.8.0/terragrunt_generator-0.8.0.tar.gz

---
+++ packages/terragrunt-generator/terragrunt_generator-0.8.0.tar.gz/terragrunt_generator-0.8.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+import generator
+
+setup(
+    name=generator.__name__,
+    version=generator.__version__,
+    author='Chris',
+    author_email='goabonga@pm.me',
+    description='generate terragrunt manifest from terraform module.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'python-hcl2==3.0.5',
+        'GitPython==3.1.41',
+    ],
+    extras_require={
+        'dev': [
+            'pytest==7.1.3',
+            'pytest-cov==4.1.0',
+            'black==23.3.0',
+            'isort==5.12.0',
+            'commitizen==2.42.1',
+            'flake8==6.0.0',
+            'pre-commit==3.2.2',
+        ]
+    },
+    entry_points={'console_scripts': [f'{generator.__name__}=generator.main:main']},
+)
