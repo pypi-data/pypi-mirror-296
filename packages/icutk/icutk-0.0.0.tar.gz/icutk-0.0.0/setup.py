c76115745a3ba55d6642cc9f3fb2d18ef4a2e252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:26 +0000

    Add package icutk/0.0.0/icutk-0.0.0.tar.gz

---
+++ packages/icutk/icutk-0.0.0.tar.gz/icutk-0.0.0/setup.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+import sys
+
+sys.path.insert(0, ".")
+
+import icutk
+
+VERSION = icutk.__version__
+DESCRIPTION = "test for name"
+PROJECT = "icutk"
+AUTHOR = "YEUNGCHIE"
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR.lower()}/{PROJECT}",
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3.7",
+    install_requires=[],
+    keywords=[
+        PROJECT,
+        "linux",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: Unix",
+    ],
+)
