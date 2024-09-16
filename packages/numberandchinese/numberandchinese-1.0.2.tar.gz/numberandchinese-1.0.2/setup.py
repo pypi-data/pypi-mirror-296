d9bb722e5bba8f35b9d8cdc51ff96f4f10b0a536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:38 +0000

    Add package numberandchinese/1.0.2/numberandchinese-1.0.2.tar.gz

---
+++ packages/numberandchinese/numberandchinese-1.0.2.tar.gz/numberandchinese-1.0.2/setup.py
@@ -0,0 +1,24 @@
+# coding:UTF-8
+import setuptools
+
+setuptools.setup(
+    name="numberandchinese",
+    version="1.0.0",
+    author="Chay",
+    author_email="lichenyi_2020@qq.com",
+    url="https://github.com/lichenyichay/numberandchinese-Including/",
+    description="numberandchinese",
+    long_description="数字中文转换器",
+    python_requires=">=3.5",
+    packages_dir={"": "src"},
+    packages_data={"": ["*.txt", "*.info", "*.properties"], "": ["data/*.*"]},
+    exclude=["*.test", "*.test.*", "test.*", "test"],
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2",
+                 "Programming Language :: Python :: 3"
+                 ]
+)
