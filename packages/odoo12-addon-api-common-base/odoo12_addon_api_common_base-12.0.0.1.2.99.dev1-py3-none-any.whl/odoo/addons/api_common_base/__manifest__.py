717980f1cc54c06a087aa169d70dfc9ca5abb3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:41 +0000

    Add package odoo12-addon-api-common-base/12.0.0.1.2.99.dev1/odoo12_addon_api_common_base-12.0.0.1.2.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-api-common-base/odoo12_addon_api_common_base-12.0.0.1.2.99.dev1-py3-none-any.whl/odoo/addons/api_common_base/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024-SomItCoop SCCL(<https://gitlab.com/somitcoop>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "API Common Base",
+    "version": "12.0.0.1.2",
+    "depends": ["base", "base_rest", "auth_api_key"],
+    "author": """
+        Som It Cooperatiu SCCL,
+        Som Connexió SCCL,
+        Odoo Community Association (OCA)
+    """,
+    "website": "https://gitlab.com/somitcoop/erp-research/odoo-helpdesk",
+    "license": "AGPL-3",
+    "summary": """
+        REST API Common Controller
+    """,
+    "data": [],
+    "demo": ["demo/api_key.xml"],
+    "application": False,
+    "installable": True,
+}
