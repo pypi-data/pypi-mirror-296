a93ff777ecac6777be18a47dc03c8cf288851fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:04:25 +0000

    Add package dcs-bios-connector/1.1/dcs_bios_connector-1.1.tar.gz

---
+++ packages/dcs-bios-connector/dcs_bios_connector-1.1.tar.gz/dcs_bios_connector-1.1/dcs_bios_connector/__init__.py
@@ -0,0 +1,3 @@
+from .dcs_bios_connector import DcsBiosConnector
+
+__all__ = ['DcsBiosConnector']
---
+++ packages/dcs-bios-connector/dcs_bios_connector-1.1.tar.gz/dcs_bios_connector-1.1/dcs_bios_connector/dcs_bios_connector.py
@@ -0,0 +1,20 @@
+from pyee import EventEmitter
+from .control_parser import ControlParser
+from .udp_connector import UDPConnector
+from .event_constants import NETWORK_DATA_MESSAGE
+
+
+class DcsBiosConnector(EventEmitter):
+
+    def __init__(self):
+        super().__init__()
+        self.client = UDPConnector()
+        self.control_parser = ControlParser(self)
+    
+        self.client.on(NETWORK_DATA_MESSAGE, self.control_parser.handle_incoming_dcs_bios_message)
+
+    def connect(self):
+        self.client.start()
+    
+    def send(self, message):
+        self.client.send(message.trim() + '\n')
