c7e1d78d7e3b9dd59aea378a7684cf09a67270ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:57 +0000

    Add package changpan/0.0.1.3/changpan-0.0.1.3-py3-none-any.whl

---
+++ packages/changpan/changpan-0.0.1.3-py3-none-any.whl/changpan/__init__.py
@@ -0,0 +1,5 @@
+print('MyWhl module loaded2')
+from changpan import imgProcess
+from changpan import example
+from changpan import submodule
+# import imgProcess
---
+++ packages/changpan/changpan-0.0.1.3-py3-none-any.whl/changpan/imgProcess.py
@@ -0,0 +1,7 @@
+print("imgProcess.py is loaded - 2")
+def add(x, y):
+    print(f"add function is called, with {x} and {y}")
+    return x+y
+    
+
+    
