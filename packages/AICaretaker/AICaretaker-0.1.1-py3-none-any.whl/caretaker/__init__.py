268e3db430e5ff37bdeaa8479bad4fd3b7fa4b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 05:04:11 +0000

    Add package AICaretaker/0.1.1/AICaretaker-0.1.1-py3-none-any.whl

---
+++ packages/AICaretaker/AICaretaker-0.1.1-py3-none-any.whl/caretaker/__init__.py
@@ -0,0 +1,13 @@
+# 
+# Caretaker LLM rewriter
+# Dynamically fixes and manages the application
+#
+
+import caretaker.config as config
+import caretaker.prompts.prompts as prompts
+import caretaker.necromancer.master 
+
+#from caretaker.decorators.decorators import ExceptionHandler
+#import sys
+
+
---
+++ packages/AICaretaker/AICaretaker-0.1.1-py3-none-any.whl/caretaker/prompts/prompts.py
@@ -0,0 +1,50 @@
+
+# Right now we are going to just have multiple prompts available.
+# Choose one, pass in the values and hope we get an answer.
+
+# In the future need a system of, hopefully selecting the appropriate prompt
+# for each model. 
+
+# Thinking logically:
+# Each prompt is likely to have following components. Which components 
+# get added to a prompt can be dependent on parameters? Context length, etc?
+# 1. Instruction
+# 2. Context
+# 3. Constraints (list of requirements, formatting rules, etc.)
+# 4. Examples
+# 5. Expected output format. 
+# 6. Tone and style
+# 
+
+
+
+
+
+import logging
+import pprint
+logger = logging.getLogger("prompts")
+
+prompts=[]
+
+prompts.append("""### Problem:
+A Python function has encountered an error. Below is the source code of the function and the error message generated.
+
+### Source Code:
+{source_code}
+
+### Error Message:
+{error_message}
+
+### Task:
+Please analyze the source code and the error message. Identify the root cause of the error and suggest a revised version of the function that fixes the issue. Ensure the revised function handles this and other common error conditions gracefully.
+Provide the complete fixed function. Do not include sample code. Do not change the name or signature of this function please.
+
+### Response:
+""")
+
+
+def get_prompt(dct, logger=logger):
+    """This will needs to be made more intelligent. We are just passing in a dictionary.
+    Based on the values in the dict we should select the best prompt."""
+    logger.debug(f"get_prompt called with {pprint.pformat(dct)}")
+    return prompts[0].format(**dct)
