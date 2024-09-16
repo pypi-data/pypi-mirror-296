fb3bff7b89cc81cde94e73bfe4a890162a48dfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 15:04:57 +0000

    Add package nbmetaclean/0.1.2/nbmetaclean-0.1.2.tar.gz

---
+++ packages/nbmetaclean/nbmetaclean-0.1.2.tar.gz/nbmetaclean-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+REQUIREMENTS_TEST_FILENAME = "requirements_test.txt"
+REQUIREMENTS_DEV_FILENAME = "requirements_dev.txt"
+
+
+def load_requirements(filename: str) -> list[str]:
+    """Load requirements from file"""
+    try:
+        with open(filename, encoding="utf-8") as fh:
+            return fh.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+
+REQUIRED = []
+TEST_REQUIRED = load_requirements(REQUIREMENTS_TEST_FILENAME)
+DEV_REQUIRED = load_requirements(REQUIREMENTS_DEV_FILENAME)
+
+
+# What packages are optional?
+EXTRAS = {
+    "test": TEST_REQUIRED,
+    "dev": DEV_REQUIRED + TEST_REQUIRED,
+}
+
+
+setup(
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+)
