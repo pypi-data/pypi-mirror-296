b3304288c740e4f977b181c354114a2d9907624c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-16 14:05:02 +0000

    Add package littlelog/0.1.1/littlelog-0.1.1.tar.gz

---
+++ packages/littlelog/littlelog-0.1.1.tar.gz/littlelog-0.1.1/README.md
@@ -0,0 +1,3 @@
+# LittleLog
+Minimalism python logger. Import it directly in one python file,
+or create a configurable instance as the logger of your whole project.
---
+++ packages/littlelog/littlelog-0.1.1.tar.gz/littlelog-0.1.1/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name="littlelog",
+    version="0.1.1",
+    author="buggist",
+    author_email="316114933@qq.com",
+    description="Minimalism python logger. ",
+    long_description=open('README.md', "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Buggist/LittleLog",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
+
