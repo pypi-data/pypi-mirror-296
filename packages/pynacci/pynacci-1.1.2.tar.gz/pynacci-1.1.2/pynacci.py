e33911ce853b07fefa75bc043de61f04a256495f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:23 +0000

    Add package pynacci/1.1.2/pynacci-1.1.2.tar.gz

---
+++ packages/pynacci/pynacci-1.1.2.tar.gz/pynacci-1.1.2/pynacci.py
@@ -0,0 +1,14 @@
+# pynacci.py
+
+__version__ = "1.0.0"  
+
+def fibonacci(iter = 10):
+    """Devuelve una lista con los primeros 'iter' números de la serie de Fibonacci."""
+    sequence = []
+    a, b = 0, 1
+    for _ in range(iter):
+        sequence.append(b)
+        a, b = b, a + b
+    return sequence
+
+__all__ = ['fibonacci']
