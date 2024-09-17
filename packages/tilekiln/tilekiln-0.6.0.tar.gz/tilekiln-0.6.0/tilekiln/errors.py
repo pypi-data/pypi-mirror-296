70e9dc1509e3b1db4e422ad89cde17c4b2654003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:56 +0000

    Add package tilekiln/0.6.0/tilekiln-0.6.0.tar.gz

---
+++ packages/tilekiln/tilekiln-0.6.0.tar.gz/tilekiln-0.6.0/tilekiln/errors.py
@@ -0,0 +1,28 @@
+'''
+Custom exceptions used by tilekiln
+
+Exception (base)
+|_ Error
+   |_ConfigError
+'''
+
+
+class Error(Exception):
+    pass
+
+
+class ConfigError(Error):
+    pass
+
+
+class ConfigYAMLError(ConfigError):
+    ''' Errors where YAML is invalid, missing, or types are wrong'''
+    pass
+
+
+class ConfigLayerError(ConfigError):
+    pass
+
+
+class DefinitionError(ConfigLayerError):
+    pass
