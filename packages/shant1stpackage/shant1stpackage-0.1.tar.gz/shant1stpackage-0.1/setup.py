06f287520be3856ae9fd0b2ce8ba558e3edb9a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:43 +0000

    Add package shant1stpackage/0.1/shant1stpackage-0.1.tar.gz

---
+++ packages/shant1stpackage/shant1stpackage-0.1.tar.gz/shant1stpackage-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="shant1stpackage",          # Package name
+    version="0.1",             # Version
+    packages=["shant1stmodule"],     # Python module directories to include
+)
