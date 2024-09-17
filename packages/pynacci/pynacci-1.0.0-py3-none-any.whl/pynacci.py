33d167b714d91f2b480788f1a0f0236f92c8855f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:25 +0000

    Add package pynacci/1.0.0/pynacci-1.0.0-py3-none-any.whl

---
+++ packages/pynacci/pynacci-1.0.0-py3-none-any.whl/pynacci.py
@@ -0,0 +1,19 @@
+# pynacci.py
+
+def is_in_fibonacci(number):
+    """Devuelve True si el número está en la serie de Fibonacci, False si no."""
+    a, b = 0, 1
+    while b <= number:
+        if b == number:
+            return True
+        a, b = b, a + b
+    return False
+
+def fibonacci(iter):
+    """Devuelve una lista con los primeros 'iter' números de la serie de Fibonacci."""
+    sequence = []
+    a, b = 0, 1
+    for _ in range(iter):
+        sequence.append(b)
+        a, b = b, a + b
+    return sequence
