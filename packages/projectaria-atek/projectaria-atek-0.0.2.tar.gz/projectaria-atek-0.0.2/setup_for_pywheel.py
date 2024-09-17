0a3e2038f58762bc72e4ddd39385c3e79ad88c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:40 +0000

    Add package projectaria-atek/0.0.2/projectaria-atek-0.0.2.tar.gz

---
+++ packages/projectaria-atek/projectaria-atek-0.0.2.tar.gz/projectaria-atek-0.0.2/setup_for_pywheel.py
@@ -0,0 +1,46 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+
+def main():
+    setup(
+        name="projectaria-atek",
+        version="0.0.2",
+        description="Aria train and evaluation kits",
+        author="Meta Reality Labs Research",
+        packages=find_packages(),  # automatically discover all packages and subpackages
+        install_requires=[
+            "torch==2.4.1",  # Assuming 'pytorch=2' corresponds to this version
+            "torchvision",
+            "torchmetrics==0.10.1",
+            "fvcore",
+            "fsspec",
+            "iopath",
+            "tqdm",
+            "scipy",
+            "webdataset",
+            "trimesh",
+            "pybind11",
+            "toolz",
+            "opencv-python",
+            "projectaria-tools",
+            # Add other dependencies that can be resolved by pip here
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
