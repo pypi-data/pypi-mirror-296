263e47d01d1fdc5f0776df34b1d1542fd47488b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:04:35 +0000

    Add package kweather-cord/0.1.2/kweather_cord-0.1.2-py3-none-any.whl

---
+++ packages/kweather-cord/kweather_cord-0.1.2-py3-none-any.whl/kweathercord/__init__.py
@@ -0,0 +1,41 @@
+"""
+Korean Weather Tool For Discord Bot
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2024-present Gooraeng
+:license: MIT, see LICENSE.txt for more datils.
+
+"""
+
+
+__title__ = 'kweathercord'
+__author__ = 'Gooraeng'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2024-present Gooraeng'
+__version__ = '0.1.2'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+import logging
+from typing import NamedTuple, Literal
+
+from .client import *
+from .enums import *
+from .exception import *
+from .model import *
+from .utils import *
+from .view import *
+
+
+class Version(NamedTuple):
+    major : int
+    minor : int
+    micro : int
+    release : Literal['alpha', 'beta', 'candidate', 'final']
+    serial : int
+
+version_info = Version(major=0, minor=1, micro=2, release='final', serial=0)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+del logging, NamedTuple, Literal, Version
