faa500849d7023a155bbad0700ab8455cf403be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:54 +0000

    Add package bio-compose/0.6.6/bio_compose-0.6.6.tar.gz

---
+++ packages/bio-compose/bio_compose-0.6.6.tar.gz/bio_compose-0.6.6/bio_compose/__init__.py
@@ -0,0 +1,25 @@
+import os
+import toml
+
+from bio_compose.api import (
+    get_output,
+    get_compatible_verification_simulators,
+    verify,
+    run_simulation
+)
+
+
+pyproject_file_path = os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml')
+try:
+    __version__ = toml.load(pyproject_file_path)['tool']['poetry']['version']
+except:
+    __version__ = ' '
+
+
+__all__ = [
+    '__version__',
+    'get_output',
+    'get_compatible_verification_simulators',
+    'verify',
+    'run_simulation'
+]
