3a3833384cce3b50f6e20d3a639163b9828116b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:05:32 +0000

    Add package osintr/0.3.0/osintr-0.3.0.tar.gz

---
+++ packages/osintr/osintr-0.3.0.tar.gz/osintr-0.3.0/osintr/__init__.py
@@ -0,0 +1,10 @@
+'''0SINTr Project'''
+import os
+import sys
+
+# Get the absolute path to the package's directory
+package_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Check if the package's directory is already in sys.path, if not, add it
+if package_dir not in sys.path:
+    sys.path.append(package_dir)
