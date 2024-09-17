2d35c62376a4fb64ab72475a812c0009f79f7074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:05:02 +0000

    Add package intuned-sdk/0.0.1/intuned_sdk-0.0.1-py3-none-any.whl

---
+++ packages/intuned-sdk/intuned_sdk-0.0.1-py3-none-any.whl/sdk/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+# Import everything from your module so it can be accessed directly from the package
+from .module import say_hello, add_numbers
+
+__all__ = ["say_hello", "add_numbers"]
