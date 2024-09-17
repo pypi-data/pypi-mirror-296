83ad464a8c9cd0c094e47d0a9365404851544952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:37 +0000

    Add package digitalhub-runtime-container/0.6.1/digitalhub_runtime_container-0.6.1.tar.gz

---
+++ packages/digitalhub-runtime-container/digitalhub_runtime_container-0.6.1.tar.gz/digitalhub_runtime_container-0.6.1/digitalhub_runtime_container/entities/runs/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub_core.entities.runs.status import RunStatus
+
+
+class RunStatusContainer(RunStatus):
+    """
+    Run Container status.
+    """
