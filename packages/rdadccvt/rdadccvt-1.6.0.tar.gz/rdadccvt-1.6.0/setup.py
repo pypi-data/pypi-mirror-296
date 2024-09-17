abd71e3b68a072a8379cc6ccb7f533d1929f50cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:54 +0000

    Add package rdadccvt/1.6.0/rdadccvt-1.6.0.tar.gz

---
+++ packages/rdadccvt/rdadccvt-1.6.0.tar.gz/rdadccvt-1.6.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdadccvt",
+    version="1.6.0",
+    description="Balzer's algorithm (DCCVT) to find a maximally population compact map",
+    url="https://github.com/rdatools/rdadccvt",
+    author="alecramsay",
+    author_email="a73cram5ay@gmail.com",
+    license="MIT",
+    packages=[
+        "rdadccvt",
+    ],
+    install_requires=["rdabase", "scipy"],
+    zip_safe=False,
+)
