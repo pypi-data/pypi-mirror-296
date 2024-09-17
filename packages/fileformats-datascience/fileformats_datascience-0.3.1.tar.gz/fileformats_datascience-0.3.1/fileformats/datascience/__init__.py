a09d92401792aeba72186c950baca242c34d99fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:22 +0000

    Add package fileformats-datascience/0.3.1/fileformats_datascience-0.3.1.tar.gz

---
+++ packages/fileformats-datascience/fileformats_datascience-0.3.1.tar.gz/fileformats_datascience-0.3.1/fileformats/datascience/__init__.py
@@ -0,0 +1,25 @@
+from ._version import __version__
+from .data import (
+    TextMatrix,
+    MatFile,
+    RData,
+    DatFile,
+    Hdf5,
+)
+from .scripts import RFile
+from .object_serialization import (
+    Pickle,
+    Pickle__Gzip,
+)
+
+__all__ = [
+    "__version__",
+    "TextMatrix",
+    "MatFile",
+    "RData",
+    "DatFile",
+    "Hdf5",
+    "RFile",
+    "Pickle",
+    "Pickle__Gzip",
+]
