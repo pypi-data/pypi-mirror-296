499f3b7b8d86cd1400873f2d87c15460868d9f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 06:06:11 +0000

    Add package kafi/0.0.22/kafi-0.0.22.tar.gz

---
+++ packages/kafi/kafi-0.0.22.tar.gz/kafi-0.0.22/kafi/kafka/cluster/cluster.py
@@ -0,0 +1,32 @@
+from kafi.kafka.cluster.cluster_admin import ClusterAdmin
+from kafi.kafka.cluster.cluster_consumer import ClusterConsumer
+from kafi.kafka.cluster.cluster_producer import ClusterProducer
+from kafi.kafka.kafka import Kafka
+
+# Cluster class
+
+class Cluster(Kafka):
+    def __init__(self, config_str):
+        super().__init__("clusters", config_str, ["kafka"], ["schema_registry"])
+        #
+        self.admin = self.get_admin()
+
+    #
+
+    def get_admin(self):
+        admin = ClusterAdmin(self)
+        #
+        return admin
+
+    #
+    def get_consumer(self, topics, **kwargs):
+        consumer = ClusterConsumer(self, topics, **kwargs)
+        #
+        return consumer
+
+    #
+
+    def get_producer(self, topic, **kwargs):
+        producer = ClusterProducer(self, topic, **kwargs)
+        #
+        return producer
