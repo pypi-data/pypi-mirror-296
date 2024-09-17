1f793d79abd7312dffbdc6674686d95248856ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:34 +0000

    Add package freeweather/0.1.0/freeweather-0.1.0.tar.gz

---
+++ packages/freeweather/freeweather-0.1.0.tar.gz/freeweather-0.1.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text(encoding="utf-8")
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+VERSION = "0.1.0"
+
+setup(
+    name="freeweather",
+    version=VERSION,
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Python library for fetching weather data using Open-Meteo API.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ohswedd/freeweather",  
+    packages=find_packages(exclude=["tests*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.7',
+    install_requires=REQUIREMENTS,
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-asyncio",
+            "Faker",
+            "black",
+            "flake8",
+        ],
+    },
+    include_package_data=True,  
+    license="MIT", 
+    keywords="weather open-meteo API",
+)
