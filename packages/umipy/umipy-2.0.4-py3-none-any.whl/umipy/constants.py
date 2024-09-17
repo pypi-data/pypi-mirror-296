79546b7a9ededbcd6725708872122c904a97e309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:31 +0000

    Add package umipy/2.0.4/umipy-2.0.4-py3-none-any.whl

---
+++ packages/umipy/umipy-2.0.4-py3-none-any.whl/umipy/constants.py
@@ -0,0 +1,13 @@
+BASE_URL_ORIGINAL_MAINNET = "https://mainnet.umi.top"  # mainnet for legacy rod
+BASE_URL_LEGEND_MAINNET = "https://legend.umi.top"  # mainnet for umi legend
+
+BASE_URL_ORIGINAL_TESTNET = "https://testnet-original.umi.top"  # testnet for legacy rod
+BASE_URL_LEGEND_TESTNET = "https://testnet.umi.top"  # testnet for umi legend
+
+
+BASE_STATS_URL_ORIGINAL_MAINNET = "https://stats.umi.top"
+BASE_STATS_URL_ORIGINAL_TESTNET = "https://stats-testnet.umi.top"
+
+
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+BECH32M_CONST = 0x2BC830A3
