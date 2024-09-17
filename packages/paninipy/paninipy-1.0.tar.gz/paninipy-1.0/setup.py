3adc04848f9e6328ed65aaf9e4a208b83e3fb9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:05:01 +0000

    Add package paninipy/1.0/paninipy-1.0.tar.gz

---
+++ packages/paninipy/paninipy-1.0.tar.gz/paninipy-1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name='paninipy',
+    version='1.0',
+    description='Package of Algorithms for Nonparametric Inference with Networks in Python',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://paninipy.readthedocs.io/en/latest/index.html',
+    author='Baiyue He',
+    author_email='baiyueh@hku.hk',
+    license='The Unlicense',
+    projects_urls={
+        "Documentation": "https://paninipy.readthedocs.io/en/latest/index.html",
+        "Source": "https://paninipy.readthedocs.io/en/latest/index.html"
+    },
+    python_requires=">=3.9, <3.12",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["paper = paper.cli:main"]},
+)
