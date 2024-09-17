e8968482509687428b19d43ff3875084b4648c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:40 +0000

    Add package datamule/0.26/datamule-0.26.tar.gz

---
+++ packages/datamule/datamule-0.26.tar.gz/datamule-0.26/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+long_description = Path("../readme.md").read_text()
+
+setup(
+    name="datamule",
+    author="John Friedman",
+    version="0.26",
+    description = "Making it easier to use SEC filings.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url="https://github.com/john-friedman/datamule-python",
+    install_requires=[
+        'aiohttp',
+        'aiolimiter',
+        'polars',
+        'tqdm',
+        'requests',
+        'nest_asyncio'
+    ],
+    extras_require={
+        "filingsviewer": ["some-filingsviewer-dependency"],
+    },
+)
