c1f530dab94cace171fb280fc9652312002d5f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:06 +0000

    Add package syrkis/0.1.0/syrkis-0.1.0-py3-none-any.whl

---
+++ packages/syrkis/syrkis-0.1.0-py3-none-any.whl/syrkis/__init__.py
@@ -0,0 +1,5 @@
+from . import train
+from . import data
+from . import plot
+
+__all__ = ['train', 'data', 'plot']
---
+++ packages/syrkis/syrkis-0.1.0-py3-none-any.whl/syrkis/train/__init__.py
@@ -0,0 +1,16 @@
+from .utils import save_params, load_params, n_params, load_config, cross_entropy, mean_squared_error, print_model, glorot_init, conv2d, deconv2d, conv1d
+
+__all__ = [
+    "save_params",
+    "load_params",
+    "n_params",
+    "plot_multiples",
+    "load_config",
+    "cross_entropy",
+    "mean_squared_error",
+    "print_model",
+    "glorot_init",
+    "conv2d",
+    "deconv2d",
+    "conv1d",
+    ]
