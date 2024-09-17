733cd3e5906fd939367df368e7898bc04adefcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:42 +0000

    Add package django-zanwango/0.1.0/django_zanwango-0.1.0-py3-none-any.whl

---
+++ packages/django-zanwango/django_zanwango-0.1.0-py3-none-any.whl/zanwango/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class ZanwangoConfig(AppConfig):
+    name = "zanwango"
+    verbose_name = _("Zanwango")
