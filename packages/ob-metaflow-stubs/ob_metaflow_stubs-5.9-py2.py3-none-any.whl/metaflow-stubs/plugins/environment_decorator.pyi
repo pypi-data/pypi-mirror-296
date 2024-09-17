e5756389ddab33eba26fa70cea87a31bf0efb639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 06:06:00 +0000

    Add package ob-metaflow-stubs/5.9/ob_metaflow_stubs-5.9-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-5.9-py2.py3-none-any.whl/metaflow-stubs/plugins/environment_decorator.pyi
@@ -0,0 +1,17 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.20.1+ob(v1)                                                   #
+# Generated on 2024-09-16T17:51:44.739421                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class EnvironmentDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    def runtime_step_cli(self, cli_args, retry_count, max_user_code_retries, ubf_context):
+        ...
+    ...
+
