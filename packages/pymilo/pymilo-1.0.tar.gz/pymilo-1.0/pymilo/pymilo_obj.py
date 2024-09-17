9b55deb3d0dbb6e64ee0abd995732e8ce0292f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:20 +0000

    Add package pymilo/1.0/pymilo-1.0.tar.gz

---
+++ packages/pymilo/pymilo-1.0.tar.gz/pymilo-1.0/pymilo/pymilo_obj.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""PyMilo modules."""
+import json
+from copy import deepcopy
+from warnings import warn
+from traceback import format_exc
+from .utils.util import get_sklearn_type, download_model
+from .pymilo_func import get_sklearn_data, get_sklearn_version, to_sklearn_model
+from .exceptions.serialize_exception import PymiloSerializationException, SerializationErrorTypes
+from .exceptions.deserialize_exception import PymiloDeserializationException, DeserializationErrorTypes
+from .pymilo_param import PYMILO_VERSION, UNEQUAL_PYMILO_VERSIONS, UNEQUAL_SKLEARN_VERSIONS, INVALID_IMPORT_INIT_PARAMS
+
+
+class Export:
+    """
+    The Pymilo Export class facilitates exporting of models to json files.
+
+    >>> exported_model = Export(model) # the model could be any sklearn linear model.
+    >>> exported_model_serialized_path = os.path.join(os.getcwd(), "MODEL_NAME.json")
+    >>> exported_model.save(exported_model_serialized_path)
+    """
+
+    def __init__(self, model):
+        """
+        Initialize the Pymilo Export instance.
+
+        :param model: given model(any sklearn linear model)
+        :type model: any class of the sklearn's linear models
+        :return: an instance of the Pymilo Export class
+        """
+        self.data = get_sklearn_data(deepcopy(model))
+        self.version = get_sklearn_version()
+        self.type = get_sklearn_type(model)
+
+    def save(self, file_adr):
+        """
+        Save model in a file.
+
+        :param file_adr: file address
+        :type file_adr: str
+        :return: None
+        """
+        with open(file_adr, 'w') as fp:
+            fp.write(self.to_json())
+
+    def to_json(self):
+        """
+        Return a json-like representation of model.
+
+        :return: model's representation as str
+        """
+        try:
+            return json.dumps(
+                {
+                    "data": self.data,
+                    "sklearn_version": self.version,
+                    "pymilo_version": PYMILO_VERSION,
+                    "model_type": self.type
+                },
+                indent=4
+            )
+        except Exception as e:
+            raise PymiloSerializationException(
+                {
+                    'error_type': SerializationErrorTypes.VALID_MODEL_INVALID_INTERNAL_STRUCTURE,
+                    'error': {
+                        'Exception': repr(e),
+                        'Traceback': format_exc()},
+                    'object': {
+                        "data": self.data,
+                        "sklearn_version": self.version,
+                        "pymilo_version": PYMILO_VERSION,
+                        "model_type": self.type},
+                })
+
+
+class Import:
+    """
+    The Pymilo Import class facilitates importing of serialized models from either a designated file path or a JSON string dump.
+
+    >>> imported_model = Import(exported_model_serialized_path)
+    >>> imported_sklearn_model = imported_model.to_model()
+    >>> imported_sklearn_model.predict(x_test)
+    """
+
+    def __init__(self, file_adr=None, json_dump=None, url=None):
+        """
+        Initialize the Pymilo Import instance.
+
+        :param file_adr: the file path where the serialized model's JSON file is located.
+        :type file_adr: str or None
+        :param json_dump: the json dump of the associated model, it can be None(reading from the file_adr)
+        :type json_dump: str or None
+        :param url: url to exported JSON file
+        :type: str or None
+        :return: an instance of the Pymilo Import class
+        """
+        serialized_model_obj = None
+        if url is not None:
+            serialized_model_obj = download_model(url)
+        elif json_dump is not None and isinstance(json_dump, str):
+            serialized_model_obj = json.loads(json_dump)
+        elif file_adr is not None:
+            with open(file_adr, 'r') as fp:
+                serialized_model_obj = json.load(fp)
+        else:
+            raise Exception(INVALID_IMPORT_INIT_PARAMS)
+        try:
+            if not serialized_model_obj["pymilo_version"] == PYMILO_VERSION:
+                warn(UNEQUAL_PYMILO_VERSIONS, category=Warning)
+            if not serialized_model_obj["sklearn_version"] == get_sklearn_version():
+                warn(UNEQUAL_SKLEARN_VERSIONS, category=Warning)
+            self.data = serialized_model_obj["data"]
+            self.version = serialized_model_obj["sklearn_version"]
+            self.type = serialized_model_obj["model_type"]
+        except Exception as e:
+            json_content = None
+            if json_dump and isinstance(json_dump, str):
+                json_content = json_dump
+            elif file_adr is not None:
+                with open(file_adr) as f:
+                    json_content = f.readlines()
+            else:
+                json_content = serialized_model_obj
+            raise PymiloDeserializationException(
+                {
+                    'json_file': json_content,
+                    'error_type': DeserializationErrorTypes.CORRUPTED_JSON_FILE,
+                    'error': {
+                        'Exception': repr(e),
+                        'Traceback': format_exc()},
+                    'object': ""})
+
+    def to_model(self):
+        """
+        Convert imported model to sklearn model.
+
+        :return: sklearn model
+        """
+        return to_sklearn_model(self)
---
+++ packages/pymilo/pymilo-1.0.tar.gz/pymilo-1.0/pymilo/streaming/pymilo_server.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""PyMiloServer for RESTFull protocol."""
+from ..pymilo_obj import Export, Import
+from .compressor import Compression
+from .encryptor import DummyEncryptor
+from .communicator import RESTServerCommunicator
+from .param import PYMILO_SERVER_NON_EXISTENT_ATTRIBUTE
+from ..transporters.general_data_structure_transporter import GeneralDataStructureTransporter
+
+
+class PymiloServer:
+    """Facilitate streaming the ML models."""
+
+    def __init__(self, model=None, port=8000, compressor=Compression.NULL):
+        """
+        Initialize the Pymilo PymiloServer instance.
+
+        :param model: the ML model which will be streamed
+        :type model: any
+        :param port: the port to which PyMiloServer listens
+        :type port: int
+        :param compressor: the compression method to be used in client-server communications
+        :type compressor: pymilo.streaming.compressor.Compression
+        :return: an instance of the PymiloServer class
+        """
+        self._model = model
+        self._compressor = compressor.value
+        self._encryptor = DummyEncryptor()
+        self.communicator = RESTServerCommunicator(ps=self, port=port)
+
+    def export_model(self):
+        """
+        Export the ML model to string json dump using PyMilo Export class.
+
+        :return: str
+        """
+        return Export(self._model).to_json()
+
+    def update_model(self, serialized_model):
+        """
+        Update the PyMilo Server's ML model.
+
+        :param serialized_model: the json dump of a pymilo export ml model
+        :type serialized_model: str
+        :return: None
+        """
+        self._model = Import(file_adr=None, json_dump=serialized_model).to_model()
+
+    def execute_model(self, request):
+        """
+        Execute the request attribute call from PyMilo Client.
+
+        :param request: request obj containing requested attribute to call with the associated args and kwargs
+        :type request: obj
+        :return: str | dict
+        """
+        gdst = GeneralDataStructureTransporter()
+        attribute = request.attribute
+        retrieved_attribute = getattr(self._model, attribute, None)
+        if retrieved_attribute is None:
+            raise Exception(PYMILO_SERVER_NON_EXISTENT_ATTRIBUTE)
+        arguments = {
+            'args': request.args,
+            'kwargs': request.kwargs
+        }
+        args = gdst.deserialize(arguments, 'args', None)
+        kwargs = gdst.deserialize(arguments, 'kwargs', None)
+        output = retrieved_attribute(*args, **kwargs)
+        if isinstance(output, type(self._model)):
+            self._model = output
+            return None
+        return gdst.serialize({'output': output}, 'output', None)
+
+    def is_callable_attribute(self, request):
+        """
+        Check whether the requested attribute is callable or not.
+
+        :param request: request obj containing requested attribute to check it's type
+        :type request: obj
+        :return: True if it is callable False otherwise
+        """
+        attribute = request.attribute
+        retrieved_attribute = getattr(self._model, attribute, None)
+        if callable(retrieved_attribute):
+            return True, None
+        else:
+            return False, GeneralDataStructureTransporter().serialize({'output': retrieved_attribute}, 'output', None)
