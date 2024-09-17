d8b0fe214e71d82eda6f2c41822e0e47efc696c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:05:33 +0000

    Add package subtitle-alchemy/0.1.7/subtitle_alchemy-0.1.7.tar.gz

---
+++ packages/subtitle-alchemy/subtitle_alchemy-0.1.7.tar.gz/subtitle_alchemy-0.1.7/src/subtitle_alchemy/cli/__main__.py
@@ -0,0 +1,16 @@
+"""All CLI commands here."""
+
+import click
+from subtitle_alchemy.cli.align import align
+from subtitle_alchemy.cli.generate import generate
+from subtitle_alchemy.cli.transcribe import transcribe
+
+
+@click.group()
+def cli() -> None:
+    """Subalch CLI."""
+
+
+cli.add_command(align)
+cli.add_command(generate)
+cli.add_command(transcribe)
---
+++ packages/subtitle-alchemy/subtitle_alchemy-0.1.7.tar.gz/subtitle_alchemy-0.1.7/src/subtitle_alchemy/forge/__init__.py
@@ -0,0 +1,7 @@
+"""Subtitle Builder Module."""
+
+from subtitle_alchemy.forge._srt import srt
+
+__all__ = [
+    "srt",
+]
