eda3e66cafef2c686bc0ab2195a5dc7551735b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:53 +0000

    Add package mongol-register-parser/0.1.3/mongol_register_parser-0.1.3.tar.gz

---
+++ packages/mongol-register-parser/mongol_register_parser-0.1.3.tar.gz/mongol_register_parser-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = "mongol_register_parser",
+    version = "0.1.3",
+    author = "Ganbaatar Bold",
+    author_email = "elmerganbaa@gmail.com",
+    description = "mongol register parser",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ganbaaelmer/mongol_register_parser.git",
+    project_urls = {
+        "Bug Tracker": "https://github.com/ganbaaelmer/mongol_register_parser.git",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
