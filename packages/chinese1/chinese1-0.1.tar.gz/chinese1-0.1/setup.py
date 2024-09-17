3ac4fdad3608c51df29b587f98929d564259b0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:48 +0000

    Add package chinese1/0.1/chinese1-0.1.tar.gz

---
+++ packages/chinese1/chinese1-0.1.tar.gz/chinese1-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chinese1',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出依赖的库
+    ],
+    description='A brief description of your module',
+    author='xiaotbl',
+    author_email='monios114514@outlook.com',
+    url='https://github.com/yourusername/yourrepository',  # 可选
+)
