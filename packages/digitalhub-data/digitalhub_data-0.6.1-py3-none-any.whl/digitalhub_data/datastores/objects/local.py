7c69c053c4d673cd62e2689f57b2700bad127540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:36 +0000

    Add package digitalhub-data/0.6.1/digitalhub_data-0.6.1-py3-none-any.whl

---
+++ packages/digitalhub-data/digitalhub_data-0.6.1-py3-none-any.whl/digitalhub_data/datastores/objects/local.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+from digitalhub_core.entities._builders.uuid import build_uuid
+from digitalhub_data.datastores.objects.base import Datastore
+from digitalhub_data.readers.builder import get_reader_by_object
+
+
+class LocalDatastore(Datastore):
+    """
+    Local Datastore class.
+    """
+
+    def write_df(self, df: Any, dst: str, extension: str | None = None, **kwargs) -> str:
+        """
+        Method to write a dataframe to a file. Kwargs are passed to df.to_parquet().
+        If destination is not provided, the dataframe is written to the default
+        store path with generated name.
+
+        Parameters
+        ----------
+        df : Any
+            The dataframe to write.
+        dst : str
+            The destination of the dataframe.
+        **kwargs : dict
+            Keyword arguments.
+
+        Returns
+        -------
+        str
+            Path of written dataframe.
+        """
+        # Check/set destination
+        if dst is None:
+            dst = str(Path(self.store.config.path) / f"{build_uuid()}.parquet")
+        self._check_local_dst(dst)
+        self._validate_extension(Path(dst).suffix.removeprefix("."))
+
+        # Write dataframe
+        reader = get_reader_by_object(df)
+        reader.write_df(df, dst, extension=extension, **kwargs)
+
+        return dst
---
+++ packages/digitalhub-data/digitalhub_data-0.6.1-py3-none-any.whl/digitalhub_data/entities/dataitems/entity/table.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+from typing import Any
+
+from digitalhub_core.stores.builder import get_store
+from digitalhub_data.datastores.builder import get_datastore
+from digitalhub_data.entities.dataitems.entity._base import Dataitem
+
+
+class DataitemTable(Dataitem):
+
+    """
+    Table dataitem.
+    """
+
+    def as_df(self, file_format: str | None = None, **kwargs) -> Any:
+        """
+        Read dataitem as a DataFrame. If the dataitem is not local, it will be downloaded
+        to a temporary folder and deleted after the method is executed. If no file_format is passed,
+        the function will try to infer it from the dataitem.spec.path attribute.
+        The path of the dataitem is specified in the spec attribute, and must be a store aware path.
+        If the dataitem is stored on s3 bucket, the path must be s3://<bucket>/<path_to_dataitem>.
+        If the dataitem is stored on database (Postgres is the only one supported), the path must
+        be sql://postgres/<database>/<schema>/<table/view>.
+
+        Parameters
+        ----------
+        file_format : str
+            Format of the file. (Supported csv and parquet).
+        **kwargs : dict
+            Keyword arguments passed to the read_df function.
+
+        Returns
+        -------
+        Any
+            DataFrame.
+        """
+        datastore = get_datastore(self.spec.path)
+        tmp_path = False
+
+        # Download dataitem if not local
+        if not self._check_local(self.spec.path):
+            path = datastore.download(self.spec.path)
+            tmp_path = True
+        else:
+            path = self.spec.path
+
+        # Get file info
+        store = get_store(path)
+        file_info = store.get_file_info(self.spec.path, path)
+        if file_info is not None:
+            self.refresh()
+            self.status.add_file(file_info)
+            self.save(update=True)
+
+        # Check file format and get dataitem as DataFrame
+        extension = self._get_extension(self.spec.path, file_format)
+        df = datastore.read_df(path, extension, **kwargs)
+
+        # Delete tmp folder
+        if tmp_path:
+            pth = Path(path)
+            if pth.is_file():
+                pth = pth.parent
+            shutil.rmtree(pth)
+
+        return df
+
+    def write_df(
+        self,
+        df: Any,
+        extension: str | None = None,
+        **kwargs,
+    ) -> str:
+        """
+        Write DataFrame as parquet/csv/table into dataitem path.
+
+        Parameters
+        ----------
+        df : Any
+            DataFrame to write.
+        extension : str
+            Extension of the file.
+        **kwargs : dict
+            Keyword arguments passed to the write_df function.
+
+        Returns
+        -------
+        str
+            Path to the written dataframe.
+        """
+        datastore = get_datastore(self.spec.path)
+        target = datastore.write_df(df, self.spec.path, extension=extension, **kwargs)
+
+        # Get file info
+        store = get_store(target)
+        file_info = store.get_file_info(target, self.spec.path)
+        if file_info is not None:
+            self.status.add_file(file_info)
+        return target
