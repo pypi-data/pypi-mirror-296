94987176372399daa86fdbb09fd7120d76258889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 16:04:33 +0000

    Add package DGTL-db-connector/0.1.0/dgtl_db_connector-0.1.0.tar.gz

---
+++ packages/DGTL-db-connector/dgtl_db_connector-0.1.0.tar.gz/dgtl_db_connector-0.1.0/dgtl_db_connector/user.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from .client import Client
+from .customer import Customer
+from .model import Model
+from .role import Role
+
+
+class User(Model):
+    table_name = "users"
+    primary_key = "username"
+    auto_id = False
+    client = Client(table_name=table_name, index_name=primary_key)
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.username: str = kwargs.get('username')
+        self.role_ids: list = self.convert_json(kwargs.get('role_ids'))
+        self.customer_ids: list = self.convert_json(kwargs.get('customer_ids'))
+        self.grader_pool_ids: list = self.convert_json(kwargs.get('grader_pool_ids'))
+
+    def customers(self) -> list[Customer]:
+        return [Customer(uuid=customer) for customer in self.customer_ids]
+
+    def roles(self) -> list[Role]:
+        return [Role(uuid=role) for role in self.role_ids]
+
+
+
