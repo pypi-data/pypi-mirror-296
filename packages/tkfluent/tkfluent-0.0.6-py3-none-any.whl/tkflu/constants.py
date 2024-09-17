761f6f57b11a59f97428b84b8d9cdc088831e0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:28 +0000

    Add package tkfluent/0.0.6/tkfluent-0.0.6-py3-none-any.whl

---
+++ packages/tkfluent/tkfluent-0.0.6-py3-none-any.whl/tkflu/constants.py
@@ -0,0 +1,15 @@
+NO = FALSE = OFF = 0
+YES = TRUE = ON = 1
+
+# Modes
+LIGHT = 'light'
+DARK = 'dark'
+
+# States
+NORMAL = 'normal'
+DISABLED = 'disabled'
+
+# FluButton Styles
+STANDARD = 'standard'
+ACCENT = 'accent'
+MENU = 'menu'
