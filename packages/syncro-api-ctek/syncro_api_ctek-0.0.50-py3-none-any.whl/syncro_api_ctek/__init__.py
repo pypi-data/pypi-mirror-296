4af36fdc22ece67809cf21cca9a2d9e87fdccf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:05:43 +0000

    Add package syncro-api-ctek/0.0.50/syncro_api_ctek-0.0.50-py3-none-any.whl

---
+++ packages/syncro-api-ctek/syncro_api_ctek-0.0.50-py3-none-any.whl/syncro_api_ctek/__init__.py
@@ -0,0 +1,15 @@
+from .customers import get_customers_managed, get_customers_all
+from .contacts import get_contacts_by_customer, get_contacts_all_customers
+from .tickets import get_tickets_all, get_tickets_recently_updated, get_ticket
+from .ticket_timers import get_ticket_timers
+
+__all__ = [
+    "get_customers_managed",
+    "get_customers_all",
+    "get_contacts_by_customer",
+    "get_contacts_all_customers",
+    "get_tickets_all",
+    "get_tickets_recently_updated",
+    "get_ticket",
+    "get_ticket_timers"
+]
