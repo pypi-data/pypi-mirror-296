e8343a34a6c554714eaa11f0db155d6e141fe95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:04:56 +0000

    Add package camegame/0.0.61/camegame-0.0.61.tar.gz

---
+++ packages/camegame/camegame-0.0.61.tar.gz/camegame-0.0.61/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='camegame',
+    version='0.0.61',
+    description='권남서버에서 유래된 똥 코드 작성함',
+    author='ohhi3368',
+    author_email='gamechobo11@gmail.com',
+    url='',
+    keywords=['abusing', 'camegame'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=['Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',],
+    packages=find_packages(),
+)
