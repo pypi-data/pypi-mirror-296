5fadfdb8562032763e95d692d887b62bfae386c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 06:05:46 +0000

    Add package mkid-detect/0.0.1/mkid_detect-0.0.1.tar.gz

---
+++ packages/mkid-detect/mkid_detect-0.0.1.tar.gz/mkid_detect-0.0.1/src/mkid_detect/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def remove_deadtime(t_list, dead_time=10):
+    # TODO rewrite in C to speed up
+    keep_times = []
+    if len(t_list) == 0:
+        return t_list
+
+    for i, t in enumerate(t_list[:-1]):
+        if (t_list[i+1] - t) > dead_time:
+            keep_times.append(int(t))
+        else:
+            pass
+
+    keep_times.append(int(t_list[-1]))
+
+    return np.array(keep_times, dtype=int)
+
+
+def generate_pixel_mask(fraction_masked, dims):
+    mask = np.zeros(dims).flatten()
+    num_masked = int(mask.flatten().shape[0] * fraction_masked)
+    mask[:num_masked] = 1
+    np.random.shuffle(mask)
+    return mask.reshape(dims).astype('bool')
+
