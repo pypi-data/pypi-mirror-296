c6d60c00a939a0587fc8b33e0ce0fdff39d8c298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 06:06:15 +0000

    Add package cv2ext/0.0.17/cv2ext-0.0.17-py3-none-any.whl

---
+++ packages/cv2ext/cv2ext-0.0.17-py3-none-any.whl/cv2ext/bboxes/__init__.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2024 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+"""
+Subpackage containing tools for working with simple bounding boxes.
+
+Functions
+---------
+bounding
+    Get a bounding box which encloses all the given bounding boxes.
+constrain
+    Constrain a bounding box to be within the bounds of an image.
+draw_bboxes
+    Draw bounding boxes on an image.
+euclidean
+    Compute the euclidean distance between two bounding boxes.
+iou
+    Calculate the intersection over union of two bounding boxes.
+ious
+    Calculate the intersection over union of a set of bounding boxes.
+manhattan
+    Compute the manhattan distance between two bounding boxes.
+mean_ap
+    Calculate the mean average precision of a set of bounding boxes.
+nms
+    Perform non-maximum suppression on a set of bounding boxes.
+resize
+    Resize a bounding box based on one image size to another.
+score_bbox
+    Score a bounding box relative to a target bbox.
+score_bboxes
+    Score a set of bounding boxes relative to a target bbox.
+valid
+    Check if a bounding box is valid.
+xywh_to_xyxy
+    Convert bounding boxes from `(x, y, w, h)` to `(x1, y1, x2, y2)`.
+xyxy_to_xywh
+    Convert bounding boxes from `(x1, y1, x2, y2)` to `(x, y, w, h)`.
+xyxy_to_yolo
+    Convert bounding boxes from `(x1, y1, x2, y2)` to `(x, y, w, h)` in YOLO format.
+xywh_to_yolo
+    Convert bounding boxes from `(x, y, w, h)` to `(x, y, w, h)` in YOLO format.
+yolo_to_xyxy
+    Convert bounding boxes from `(x, y, w, h)` in YOLO format to `(x1, y1, x2, y2)`.
+yolo_to_xywh
+    Convert bounding boxes from `(x, y, w, h)` in YOLO format to `(x, y, w, h)`.
+
+"""
+
+from __future__ import annotations
+
+from ._bounding import bounding
+from ._constrain import constrain
+from ._convert import (
+    xywh_to_xyxy,
+    xywh_to_yolo,
+    xyxy_to_xywh,
+    xyxy_to_yolo,
+    yolo_to_xywh,
+    yolo_to_xyxy,
+)
+from ._distance import euclidean, manhattan
+from ._draw import draw_bboxes
+from ._iou import iou, ious
+from ._mean_ap import mean_ap
+from ._nms import nms
+from ._resize import resize
+from ._score import score_bbox, score_bboxes
+from ._valid import valid
+
+__all__ = [
+    "bounding",
+    "constrain",
+    "draw_bboxes",
+    "euclidean",
+    "iou",
+    "ious",
+    "manhattan",
+    "mean_ap",
+    "nms",
+    "resize",
+    "score_bbox",
+    "score_bboxes",
+    "valid",
+    "xywh_to_xyxy",
+    "xywh_to_yolo",
+    "xyxy_to_xywh",
+    "xyxy_to_yolo",
+    "yolo_to_xywh",
+    "yolo_to_xyxy",
+]
