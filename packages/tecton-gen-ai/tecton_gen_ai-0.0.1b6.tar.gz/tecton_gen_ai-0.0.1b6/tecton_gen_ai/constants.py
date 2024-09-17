ed9d0312f4f0ae9454413ee13b9189ebee250f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:25 +0000

    Add package tecton-gen-ai/0.0.1b6/tecton_gen_ai-0.0.1b6.tar.gz

---
+++ packages/tecton-gen-ai/tecton_gen_ai-0.0.1b6.tar.gz/tecton_gen_ai-0.0.1b6/tecton_gen_ai/constants.py
@@ -0,0 +1,4 @@
+_TECTON_CHECKPOINT_ATTR = "__tecton_checkpoint__"
+_TECTON_MOCK_OBJECT_ATTR = "_tecton_mock_object_attr"
+# TODO(brian): update to the official environment once that is published
+_DEFAULT_BATCH_FEATURE_VIEW_ENVIRONMENT = "han-ai-6_6"
