3cc65109a97e34e4845f858ecd2b9fe861d2fc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:06 +0000

    Add package syrkis/0.1.0/syrkis-0.1.0.tar.gz

---
+++ packages/syrkis/syrkis-0.1.0.tar.gz/syrkis-0.1.0/README.md
@@ -0,0 +1,12 @@
+<p align="center">Rolling seven day <a href="https://wakatime.com/@syrkis"/>WakaTime</a> coding stats:</p>
+<!--START_SECTION:waka-->
+
+```txt
+Python     3 hrs 13 mins   >>>>>>>>-----------------   33.64 %
+Nix        1 hr 55 mins    >>>>>--------------------   20.09 %
+JSON       1 hr 38 mins    >>>>---------------------   17.09 %
+Assembly   49 mins         >>-----------------------   08.54 %
+Markdown   38 mins         >>-----------------------   06.68 %
+```
+
+<!--END_SECTION:waka-->
---
+++ packages/syrkis/syrkis-0.1.0.tar.gz/syrkis-0.1.0/syrkis/plot/plot.py
@@ -0,0 +1,55 @@
+# plot.py
+#    syrkis plots
+# by: Noah Syrkis
+
+# imports
+import numpy as np
+from IPython.display import display, clear_output
+from IPython.display import display, HTML
+import numpy as np
+import base64
+from PIL import Image as PILImage
+from io import BytesIO
+from jinja2 import Environment, FileSystemLoader
+
+
+# globals
+TEMPLATE_DIR = "/Users/syrkis/code/syrkis/syrkis/templates"
+env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+
+
+# functions
+def multiples(imgs, info={}, figsize=(6, 3)):
+    top, bottom = info.get("top"), info.get("bottom")
+    if len(imgs[0].shape) == 2:
+        imgs = [np.expand_dims(img, axis=2) for img in imgs]  # add channel dim
+    invertable = imgs[0].shape[-1] == 1
+    n_rows, n_cols = figsize
+    imgs = np.array(imgs[: n_rows * n_cols])
+    if invertable and not darkdetect.isDark():
+        imgs = np.abs(1 - imgs)
+    template = env.get_template("images.html")
+    imgs = [matrix_to_image(pred) for pred in imgs]
+    background = "black" if darkdetect.isDark() else "white"
+    html = template.render(
+        images=imgs,
+        n_cols=n_cols,
+        low_bar=bottom if bottom else [""],
+        top_bar=top if top else [""],
+        background=background,
+    )
+
+    clear_output(wait=True)
+    display(HTML(html))
+
+
+def matrix_to_image(matrix):
+    # ensure all values are [0; 1]
+    matrix = np.clip(matrix, 0, 1)
+    # if matrix is 128 x 128 x 1, convert to 128 x 128 x 3
+    matrix = np.repeat(matrix, 3, axis=2) if matrix.shape[2] == 1 else matrix
+    image = PILImage.fromarray((matrix * 255).astype(np.uint8))
+    image_bytes = BytesIO()
+    image.save(image_bytes, format="png")
+    encoded_image = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
+    return encoded_image
