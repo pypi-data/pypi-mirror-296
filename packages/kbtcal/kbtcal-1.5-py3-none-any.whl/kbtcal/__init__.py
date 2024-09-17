b5becda7cb89fcb9cc5b2c7d07c91f5d63e57ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:51 +0000

    Add package kbtcal/1.5/kbtcal-1.5-py3-none-any.whl

---
+++ packages/kbtcal/kbtcal-1.5-py3-none-any.whl/kbtcal/__init__.py
@@ -0,0 +1,33 @@
+def add_numbers(num1, num2):
+    results = num1 + num2
+    return f'Sum is: {results}'
+
+
+def subtract_numbers(num1, num2):
+    results = num1 - num2
+    return f'Difference is: {results}'
+
+
+def multiply_numbers(num1, num2):
+    results = num1 * num2
+    return f'Product is: {results}'
+
+
+def divide_numbers_float(num1, num2):
+    results = num1 / num2
+    return f'Quotient Float is: {results}'
+
+
+def divide_numbers_floor(num1, num2):
+    results = num1 // num2
+    return f'Quotient Floor is: {results}'
+
+
+def power_numbers(num1, num2):
+    results = num1 ** num2
+    return f'Power is: {results}'
+
+
+def mode_numbers(num1, num2):
+    results = num1 % num2
+    return f'Mode is: {results}'
