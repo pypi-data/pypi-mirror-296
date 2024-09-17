9b186f8ab8a00a972e1fcee0841590d6a2324fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:00 +0000

    Add package byte-gg-wrapper/1.0.2/byte_gg_wrapper-1.0.2.tar.gz

---
+++ packages/byte-gg-wrapper/byte_gg_wrapper-1.0.2.tar.gz/byte_gg_wrapper-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='byte-gg-wrapper',
+    version='1.0.2',
+    author='icmedev', 
+    author_email='icmecodes@kys.gay', 
+    packages=find_packages(), 
+    install_requires=['requests'], 
+    python_requires='>=3.7', 
+)
