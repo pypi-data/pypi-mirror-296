64e530a06617d286ccc2db4951335cb0a6055228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:03 +0000

    Add package MEGABYTE-pytorch/0.3.4/megabyte_pytorch-0.3.4.tar.gz

---
+++ packages/MEGABYTE-pytorch/megabyte_pytorch-0.3.4.tar.gz/megabyte_pytorch-0.3.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'MEGABYTE-pytorch',
+  packages = find_packages(),
+  version = '0.3.4',
+  license='MIT',
+  description = 'MEGABYTE - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/MEGABYTE-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers'
+  ],
+  install_requires=[
+    'beartype',
+    'einops>=0.6.1',
+    'torch>=2.3',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
