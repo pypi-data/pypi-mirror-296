8adcb55be2b8839abb6718d05ff046389834eaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:27 +0000

    Add package myskoda/0.2.0/myskoda-0.2.0-py3-none-any.whl

---
+++ packages/myskoda/myskoda-0.2.0-py3-none-any.whl/myskoda/__init__.py
@@ -0,0 +1,27 @@
+"""A library for interacting with the MySkoda APIs."""
+
+from .authorization import (
+    AuthorizationError,
+    IDKAuthorizationCode,
+    IDKCredentials,
+    IDKSession,
+    idk_authorize,
+)
+from .models import air_conditioning, charging, common, health, info, position, status
+from .rest_api import RestApi
+
+__all__ = [
+    "AuthorizationError",
+    "IDKAuthorizationCode",
+    "IDKCredentials",
+    "IDKSession",
+    "idk_authorize",
+    "air_conditioning",
+    "charging",
+    "common",
+    "health",
+    "info",
+    "position",
+    "status",
+    "RestApi",
+]
