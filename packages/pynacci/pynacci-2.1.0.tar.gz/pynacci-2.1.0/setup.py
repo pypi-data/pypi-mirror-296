a88975a4d90f66b1d42c48c72f26d1806d0e6ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:42 +0000

    Add package pynacci/2.1.0/pynacci-2.1.0.tar.gz

---
+++ packages/pynacci/pynacci-2.1.0.tar.gz/pynacci-2.1.0/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='pynacci',  
+    version='2.1.0',  
+    description='Una librería para trabajar con la serie de Fibonacci',
+    long_description=open('README.txt', encoding='utf-8').read(),  
+    long_description_content_type='text/plain',  
+    author='Pablo Álvaro',  
+    author_email='palvaroh2000@gmail.com',  
+    license='MIT',  
+    py_modules=['pynacci'], 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent', 
+    ],
+    python_requires='>=3.6',  
+    keywords='fibonacci mathematics generator', 
+)
