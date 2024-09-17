8becf1aab79395109967161a90fcfdf7094f89cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:18 +0000

    Add package PolicyEngine-UK/1.8.0/PolicyEngine_UK-1.8.0-py3-none-any.whl

---
+++ packages/PolicyEngine-UK/PolicyEngine_UK-1.8.0-py3-none-any.whl/policyengine_uk/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import os
+from policyengine_uk import entities
+from policyengine_uk.system import (
+    CountryTaxBenefitSystem,
+    Microsimulation,
+    Simulation,
+    COUNTRY_DIR,
+    parameters,
+    variables,
+)
+from pathlib import Path
+import os
+from policyengine_core.taxbenefitsystems import TaxBenefitSystem
+from policyengine_uk.data import *
+from policyengine_uk.data import DATASETS
+
+REPO = Path(__file__).parent
