ab5d53bd6cb7f9dc4a63f7900721cf6f2621b1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:05:00 +0000

    Add package bleujs-utils/1.0.1/bleujs_utils-1.0.1.tar.gz

---
+++ packages/bleujs-utils/bleujs_utils-1.0.1.tar.gz/bleujs_utils-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bleujs-utils',
+    version='1.0.1',
+    description='A utility package for Bleu.js project',
+    author='Pejman Haghighatnia',
+    author_email='pejmanhnia@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'some-dependency',
+    ],
+)
+
