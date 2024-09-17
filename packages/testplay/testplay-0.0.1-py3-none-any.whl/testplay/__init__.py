63a31b4175e8ddf4cd56977b0a3ee31f6f102a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:05:17 +0000

    Add package testplay/0.0.1/testplay-0.0.1-py3-none-any.whl

---
+++ packages/testplay/testplay-0.0.1-py3-none-any.whl/testplay/__init__.py
@@ -0,0 +1,2 @@
+from functionality import *
+from other_functionality import *
---
+++ packages/testplay/testplay-0.0.1-py3-none-any.whl/testplay/functionality.py
@@ -0,0 +1,9 @@
+
+def greeting(you:str):
+    print(f"Welcome {you}")
+    return 1
+
+def something(you:str):
+    print(f"I don't know what to type.")
+    return f"{you}"
+
