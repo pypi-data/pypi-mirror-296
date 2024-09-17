4d4690e4fa2491dfffe44f518426180361a3ead4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 14:04:52 +0000

    Add package package-auto-assembler/0.5.1/package_auto_assembler-0.5.1.tar.gz

---
+++ packages/package-auto-assembler/package_auto_assembler-0.5.1.tar.gz/package_auto_assembler-0.5.1/package_auto_assembler/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+path_to_readme = os.path.join(here, "README.md")
+
+long_description = """# Package auto assembler
+
+Package auto assembler is a tool that meant to streamline creation of single module packages.
+Its purpose is to automate as many aspects of python package creation as possible,
+to shorten a development cycle of reusable components, maintain certain standard of quality
+for reusable code. It provides tool to simplify the process of package creatrion
+to a point that it can be triggered automatically within ci/cd pipelines,
+with minimal preparations and requirements for new modules.
+
+"""
+
+if os.path.exists(path_to_readme):
+  with codecs.open(path_to_readme, encoding="utf-8") as fh:
+      long_description += fh.read()
+
+setup(
+    name="package_auto_assembler",
+    packages=["package_auto_assembler"],
+    install_requires=['nbconvert==7.16.4', 'attrs>=22.2.0', 'click==8.1.7', 'uvicorn', 'fastapi', 'pandas==2.1.1', 'packaging', 'requests', 'nbformat', 'mkdocs==1.6.0', 'pyyaml', 'stdlib-list', 'numpy==1.26.0', 'pip_audit==2.7.3'],
+    classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'Intended Audience :: Science/Research', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.9', 'Programming Language :: Python :: 3.10', 'Programming Language :: Python :: 3.11', 'License :: OSI Approved :: MIT License', 'Topic :: Scientific/Engineering'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Kyrylo Mordan",
+    author_email="parachute.repo@gmail.com",
+    description="A tool to automate package creation within ci based on just .py and optionally .ipynb file.",
+    keywords="['python', 'packaging', 'aa-paa-tool']",
+    license="mit",
+    version="0.5.1",
+    entry_points = {'console_scripts': ['paa = package_auto_assembler.cli:cli']},
+
+)
