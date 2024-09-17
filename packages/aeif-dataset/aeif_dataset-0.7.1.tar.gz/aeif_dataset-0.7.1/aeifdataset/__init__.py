9d8bb5ec5e1c259b0c9383882a7d8d1fd25987d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:05:12 +0000

    Add package aeif-dataset/0.7.1/aeif_dataset-0.7.1.tar.gz

---
+++ packages/aeif-dataset/aeif_dataset-0.7.1.tar.gz/aeif_dataset-0.7.1/aeifdataset/__init__.py
@@ -0,0 +1,24 @@
+from .core import Dataloader, DataRecord
+from .data import *
+from .utils import *
+
+import re
+import pathlib
+
+# Path to setup.py
+setup_path = pathlib.Path(__file__).parent.parent / 'setup.py'
+
+
+# Function to extract version
+def get_version():
+    version_pattern = re.compile(r"version=['\"]([^'\"]*)['\"]")
+    with open(setup_path, 'r') as file:
+        setup_content = file.read()
+        match = version_pattern.search(setup_content)
+        if match:
+            return match.group(1)
+        return 'unknown'
+
+
+# Set the version attribute
+__version__ = get_version()
