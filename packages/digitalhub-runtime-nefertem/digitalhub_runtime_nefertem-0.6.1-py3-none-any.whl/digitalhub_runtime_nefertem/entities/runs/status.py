faf08c2f32dba067499f6b5c0a67dfb261c55cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:44 +0000

    Add package digitalhub-runtime-nefertem/0.6.1/digitalhub_runtime_nefertem-0.6.1-py3-none-any.whl

---
+++ packages/digitalhub-runtime-nefertem/digitalhub_runtime_nefertem-0.6.1-py3-none-any.whl/digitalhub_runtime_nefertem/entities/runs/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub_data.entities.runs.status import RunStatusData
+
+
+class RunStatusNefertem(RunStatusData):
+    """
+    Run Nefertem status.
+    """
