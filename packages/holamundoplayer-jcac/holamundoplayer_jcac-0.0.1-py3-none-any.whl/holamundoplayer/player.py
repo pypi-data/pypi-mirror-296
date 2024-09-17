9af2ca858a62f5c58995e7c845cc41f323f04826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 15:04:14 +0000

    Add package holamundoplayer-jcac/0.0.1/holamundoplayer_jcac-0.0.1-py3-none-any.whl

---
+++ packages/holamundoplayer-jcac/holamundoplayer_jcac-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,25 @@
+"""
+Esta es la clase que incluye la clase reporductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reporductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio como paramero
+
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 si reproduce con exito, en caso de fracaso devuelve 0
+        """
+        print("Reporducioendo cancion")
+
+    def stop(self):
+
+        print("stopping")
