465e4aea65830bde031be121d9e3cfc7c1081e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:23 +0000

    Add package DiscoEPG/0.0.11/DiscoEPG-0.0.11.tar.gz

---
+++ packages/DiscoEPG/DiscoEPG-0.0.11.tar.gz/DiscoEPG-0.0.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup 
+
+with open('package/Introduction.md', 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = 'DiscoEPG',
+    version = '0.0.11',
+    description = "Open-source package for insects EPG data analysis",
+    package_dir = {"":"package"},
+    packages= find_packages(where = "package"),
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/HySonLab/ML4Insects",
+    author = "DINHQuangDung1999",
+    author_email = "dqdung999@gmail.com",
+    license="MIT",
+    classifiers= [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent" 
+    ],
+    extra_requires = {
+        "twine >= 4.0.2"
+    },
+    python_requires = ">=3.10"
+)
