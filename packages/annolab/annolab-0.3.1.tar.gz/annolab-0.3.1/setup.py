5ce3503ce4f823ff73c6b934470412d59157f8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 07:04:56 +0000

    Add package annolab/0.3.1/annolab-0.3.1.tar.gz

---
+++ packages/annolab/annolab-0.3.1.tar.gz/annolab-0.3.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+  name = 'annolab',
+  version='0.3.1',
+  packages = find_packages(),
+  author='AnnoLab',
+  maintainer='Luke Simkins; Grant DeLozier',
+  author_email='luke@annolab.ai',
+  url='https://github.com/lsimkins/annolab-sdk',
+  license='Apache',
+  description='Official SDK for the AnnoLab Platform',
+  install_requires=[
+    'requests>=2.25.1',
+    'polling2>=0.5.0',
+    'jsonlines>=2.0.0'
+  ],
+  long_description=open('README.rst').read(),
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
