269d493a175664c2f6373247c4f3a4d90ebfc416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:29 +0000

    Add package timespent/1.0.0/timespent-1.0.0-py3-none-any.whl

---
+++ packages/timespent/timespent-1.0.0-py3-none-any.whl/timespent/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'WorkSession',
+    'Event',
+    'calculate_timespent',
+    'calculate_timespent_as_hours',
+    'map_dicts_to_work_sessions',
+    'unique_days_worked',
+    'too_long_after_last_event'
+]
+
+from .timespent import WorkSession, Event, calculate_timespent, calculate_timespent_as_hours, map_dicts_to_work_sessions, unique_days_worked, too_long_after_last_event
+
