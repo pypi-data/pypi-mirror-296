1d25105e2a91adab2ad839e59f1e5cdf5c94ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 06:06:27 +0000

    Add package codi_requests_oauthlib/0.0.2/codi_requests_oauthlib-0.0.2-py2.py3-none-any.whl

---
+++ packages/codi_requests_oauthlib/codi_requests_oauthlib-0.0.2-py2.py3-none-any.whl/codi_requests_oauthlib/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module represents the initialization file for the `codi_requests_oauthlib` package.
+
+Attributes:
+    __version__ (str): The version of the package.
+"""
+
+__version__ = '0.0.2'
