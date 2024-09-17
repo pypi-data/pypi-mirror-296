6204650576ecb330c67c3686574a2652545c91dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 04:04:30 +0000

    Add package ucaptcha/1.1.1/ucaptcha-1.1.1.tar.gz

---
+++ packages/ucaptcha/ucaptcha-1.1.1.tar.gz/ucaptcha-1.1.1/ucaptcha/exceptions.py
@@ -0,0 +1,18 @@
+class UCaptchaError(Exception):
+    pass
+
+
+class WrongUserKeyError(UCaptchaError):
+    pass
+
+
+class ZeroBalanceError(UCaptchaError):
+    pass
+
+
+class KeyDoesNotExistError(UCaptchaError):
+    pass
+
+
+class ProxyFormatError(UCaptchaError):
+    pass
