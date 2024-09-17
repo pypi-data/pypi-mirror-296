ae162c8a96519af9f7f09e5455e87fc8c15a2d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-17 03:04:53 +0000

    Add package satori-python/0.15.0rc1/satori_python-0.15.0rc1.tar.gz

---
+++ packages/satori-python/satori_python-0.15.0rc1.tar.gz/satori_python-0.15.0rc1/src/satori/server/adapter.py
@@ -0,0 +1,52 @@
+from abc import abstractmethod
+from collections.abc import AsyncIterator
+from typing import TYPE_CHECKING, Optional
+
+from launart import Service
+
+from ..model import Event, LoginType
+from .route import RouterMixin
+
+if TYPE_CHECKING:
+    from . import Server
+
+
+class Adapter(Service, RouterMixin):
+    server: "Server"
+
+    @abstractmethod
+    def get_platform(self) -> str: ...
+
+    @abstractmethod
+    def publisher(self) -> AsyncIterator[Event]: ...
+
+    @abstractmethod
+    def ensure(self, platform: str, self_id: str) -> bool: ...
+
+    @abstractmethod
+    def authenticate(self, token: Optional[str]) -> bool: ...
+
+    @staticmethod
+    def proxy_urls() -> list[str]:
+        return []
+
+    @abstractmethod
+    async def download_uploaded(self, platform: str, self_id: str, path: str) -> bytes: ...
+
+    async def download_proxied(self, prefix: str, url: str) -> Optional[bytes]:
+        async with self.server.session.get(url) as resp:
+            return await resp.read()
+
+    @abstractmethod
+    async def get_logins(self) -> list[LoginType]: ...
+
+    def __init__(self):
+        super().__init__()
+        self.routes = {}
+
+    @property
+    def id(self):
+        return f"satori-python.adapter.{self.get_platform()}#{id(self)}"
+
+    def ensure_server(self, server: "Server"):
+        self.server = server
