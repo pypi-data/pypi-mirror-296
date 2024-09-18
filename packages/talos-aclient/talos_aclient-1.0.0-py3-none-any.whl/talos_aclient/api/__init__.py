1aacff8c55b150ec94286612eb3d6d5d14554e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:06:09 +0000

    Add package talos-aclient/1.0.0/talos_aclient-1.0.0-py3-none-any.whl

---
+++ packages/talos-aclient/talos_aclient-1.0.0-py3-none-any.whl/talos_aclient/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from talos_aclient.api.dataset_api import DatasetApi
+from talos_aclient.api.default_api import DefaultApi
+from talos_aclient.api.resource_api import ResourceApi
+from talos_aclient.api.segment_api import SegmentApi
+from talos_aclient.api.vector_api import VectorApi
+
