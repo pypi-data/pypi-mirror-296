5b7b70c5fff257837e0b472bed84b3ee67f3e4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:30 +0000

    Add package pyx-core/1.18.2/pyx_core-1.18.2.tar.gz

---
+++ packages/pyx-core/pyx_core-1.18.2.tar.gz/pyx_core-1.18.2/src/PyXMake/Build/cmd/windows/iniCompiler.bat
@@ -0,0 +1,31 @@
+@REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+@REM %                             Initialization of Intel FORTRAN compiling environment (x64) on Windows                   %
+@REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+@REM Intel Fortran Win64 Initialization allowing access via PyDev - Eclipse
+@REM Created on 20.03.2018	
+@REM 
+@REM ------------------------------------------------------------------------------------------------------------------------
+@REM    Requirements and dependencies:
+@REM        - 
+@REM
+@REM     Changelog:
+@REM        - Commented out:                                                                                                                                  // mg 26.03.2018 
+@REM 		@REM Set C++ Compiler Environment
+@REM 		CALL "%CPPPATH%\vcvarsall.bat" %pyx_proc%
+@REM 		FOR /f "skip=21 delims=" %%G IN (%LogPath%Paths.log) DO IF NOT DEFINED CPPPATH SET CPPPATH=%%G
+@REM
+@REM     @author: garb_ma                                                                                                                 [DLR-SY,STM Braunschweig]
+@REM ------------------------------------------------------------------------------------------------------------------------
+@ECHO OFF
+@SET CURRENTPATH=%cd%
+@CD /d %~dp0..
+@CD ..\..
+@REM Get paths from Paths.log 
+@SET LogPath=%cd%\
+@FOR /f "skip=20 delims=" %%G IN (%LogPath:~0,-1%\Paths.log) DO IF NOT DEFINED IFORTPATH SET IFORTPATH=%%G
+@CD /d %CURRENTPATH%
+@REM Set C++ & Intel Fortran Compiler Environment on Windows
+IF NOT DEFINED pyx_environment SET pyx_environment=%IFORTPATH%\bin\ifortvars.bat
+IF EXIST "%pyx_environment%" @CALL "%pyx_environment%" %pyx_intel% %pyx_msvsc% 
+@REM Execute build command
+@CALL %* 
