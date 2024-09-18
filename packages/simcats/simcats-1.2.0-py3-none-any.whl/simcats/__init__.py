22b8da6a8bc91177292deb17eaf751e8c090644b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:26 +0000

    Add package simcats/1.2.0/simcats-1.2.0-py3-none-any.whl

---
+++ packages/simcats/simcats-1.2.0-py3-none-any.whl/simcats/__init__.py
@@ -0,0 +1,9 @@
+"""SimCATS is a python framework for simulating charge stability diagrams (CSDs) typically measured during the tuning
+process of qubits.
+"""
+
+from ._simulation import Simulation
+from ._default_configs import default_configs
+
+__all__ = ["Simulation", "default_configs"]
+__version__ = "1.2.0"
