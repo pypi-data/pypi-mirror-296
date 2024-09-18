f82245e5453d8a3e8b8647665eb522c567e69cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:05:17 +0000

    Add package zhisquant/0.1.0/zhisquant-0.1.0.tar.gz

---
+++ packages/zhisquant/zhisquant-0.1.0.tar.gz/zhisquant-0.1.0/zhisquant/__init__.py
@@ -0,0 +1,4 @@
+from .client import ZhiSQuant_SDK
+from .exceptions import APIError
+
+__all__ = ['ZhiSQuant_SDK', 'APIError']
---
+++ packages/zhisquant/zhisquant-0.1.0.tar.gz/zhisquant-0.1.0/zhisquant/exceptions.py
@@ -0,0 +1,3 @@
+class APIError(Exception):
+    """自定义API异常类，用于处理API请求错误"""
+    pass
