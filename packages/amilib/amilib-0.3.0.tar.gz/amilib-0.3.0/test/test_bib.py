87577d095a243b88b8bcd9cd3d68b4a954a913a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:25 +0000

    Add package amilib/0.3.0/amilib-0.3.0.tar.gz

---
+++ packages/amilib/amilib-0.3.0.tar.gz/amilib-0.3.0/test/test_bib.py
@@ -0,0 +1,14 @@
+from amilib.file_lib import FileLib
+from amilib.util import Util
+from test.test_all import AmiAnyTest
+
+logger = Util.get_logger(__name__)
+class AmiBibliographyTest(AmiAnyTest):
+    """
+
+    """
+    @classmethod
+    def convert_csv_to_bib(cls):
+        """
+
+        """
