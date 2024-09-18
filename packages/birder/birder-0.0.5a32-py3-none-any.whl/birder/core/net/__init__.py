fb32cec5532d94c1e23f5b293a2ee0df80ade794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:05:18 +0000

    Add package birder/0.0.5a32/birder-0.0.5a32-py3-none-any.whl

---
+++ packages/birder/birder-0.0.5a32-py3-none-any.whl/birder/core/net/__init__.py
@@ -0,0 +1,115 @@
+from birder.core.net.alexnet import AlexNet
+from birder.core.net.cait import CaiT
+from birder.core.net.convnext_v1 import ConvNeXt_v1
+from birder.core.net.convnext_v2 import ConvNeXt_v2
+from birder.core.net.crossvit import CrossViT
+from birder.core.net.deit import DeiT
+from birder.core.net.deit3 import DeiT3
+from birder.core.net.densenet import DenseNet
+from birder.core.net.edgenext import EdgeNeXt
+from birder.core.net.edgevit import EdgeViT
+from birder.core.net.efficientnet_v1 import EfficientNet_v1
+from birder.core.net.efficientnet_v2 import EfficientNet_v2
+from birder.core.net.focalnet import FocalNet
+from birder.core.net.ghostnet_v1 import GhostNet_v1
+from birder.core.net.ghostnet_v2 import GhostNet_v2
+from birder.core.net.inception_next import Inception_NeXt
+from birder.core.net.inception_resnet_v2 import Inception_ResNet_v2
+from birder.core.net.inception_v3 import Inception_v3
+from birder.core.net.inception_v4 import Inception_v4
+from birder.core.net.maxvit import MaxViT
+from birder.core.net.metaformer import MetaFormer
+from birder.core.net.mnasnet import MNASNet
+from birder.core.net.mobilenet_v1 import MobileNet_v1
+from birder.core.net.mobilenet_v2 import MobileNet_v2
+from birder.core.net.mobilenet_v3_large import MobileNet_v3_Large
+from birder.core.net.mobilenet_v3_small import MobileNet_v3_Small
+from birder.core.net.mobilenet_v4 import MobileNet_v4
+from birder.core.net.mobilenet_v4_hybrid import MobileNet_v4_Hybrid
+from birder.core.net.mobileone import MobileOne
+from birder.core.net.mobilevit_v1 import MobileViT_v1
+from birder.core.net.mobilevit_v2 import MobileViT_v2
+from birder.core.net.nextvit import NextViT
+from birder.core.net.rdnet import RDNet
+from birder.core.net.regnet import RegNet
+from birder.core.net.repvgg import RepVgg
+from birder.core.net.resnest import ResNeSt
+from birder.core.net.resnet_v2 import ResNet_v2
+from birder.core.net.resnext import ResNeXt
+from birder.core.net.se_resnet_v2 import SE_ResNet_v2
+from birder.core.net.se_resnext import SE_ResNeXt
+from birder.core.net.sequencer2d import Sequencer2d
+from birder.core.net.shufflenet_v1 import ShuffleNet_v1
+from birder.core.net.shufflenet_v2 import ShuffleNet_v2
+from birder.core.net.simple_vit import Simple_ViT
+from birder.core.net.squeezenet import SqueezeNet
+from birder.core.net.squeezenext import SqueezeNext
+from birder.core.net.swin_transformer_v1 import Swin_Transformer_v1
+from birder.core.net.swin_transformer_v2 import Swin_Transformer_v2
+from birder.core.net.swin_transformer_v2_w2 import Swin_Transformer_v2_w2
+from birder.core.net.vgg import Vgg
+from birder.core.net.vgg_reduced import Vgg_Reduced
+from birder.core.net.vit import ViT
+from birder.core.net.vitreg import ViTReg4
+from birder.core.net.wide_resnet import Wide_ResNet
+from birder.core.net.xception import Xception
+from birder.core.net.xcit import XCiT
+
+__all__ = [
+    "AlexNet",
+    "CaiT",
+    "ConvNeXt_v1",
+    "ConvNeXt_v2",
+    "CrossViT",
+    "DeiT",
+    "DeiT3",
+    "DenseNet",
+    "EdgeNeXt",
+    "EdgeViT",
+    "EfficientNet_v1",
+    "EfficientNet_v2",
+    "FocalNet",
+    "GhostNet_v1",
+    "GhostNet_v2",
+    "Inception_NeXt",
+    "Inception_ResNet_v2",
+    "Inception_v3",
+    "Inception_v4",
+    "MaxViT",
+    "MetaFormer",
+    "MNASNet",
+    "MobileNet_v1",
+    "MobileNet_v2",
+    "MobileNet_v3_Large",
+    "MobileNet_v3_Small",
+    "MobileNet_v4",
+    "MobileNet_v4_Hybrid",
+    "MobileOne",
+    "MobileViT_v1",
+    "MobileViT_v2",
+    "NextViT",
+    "RDNet",
+    "RegNet",
+    "RepVgg",
+    "ResNeSt",
+    "ResNet_v2",
+    "ResNeXt",
+    "SE_ResNet_v2",
+    "SE_ResNeXt",
+    "Sequencer2d",
+    "ShuffleNet_v1",
+    "ShuffleNet_v2",
+    "Simple_ViT",
+    "SqueezeNet",
+    "SqueezeNext",
+    "Swin_Transformer_v1",
+    "Swin_Transformer_v2",
+    "Swin_Transformer_v2_w2",
+    "Vgg",
+    "Vgg_Reduced",
+    "ViT",
+    "ViTReg4",
+    "Wide_ResNet",
+    "Xception",
+    "XCiT",
+]
