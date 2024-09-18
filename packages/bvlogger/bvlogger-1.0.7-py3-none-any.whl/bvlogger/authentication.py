c59a4d2802652e537b8bd5f05299d45dd515ca9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:25 +0000

    Add package bvlogger/1.0.7/bvlogger-1.0.7-py3-none-any.whl

---
+++ packages/bvlogger/bvlogger-1.0.7-py3-none-any.whl/bvlogger/authentication.py
@@ -0,0 +1,19 @@
+import requests
+
+class Auth:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def login(self, username, password):
+        response = requests.post(
+            f"{self.base_url}/auth",
+            json={
+                'username': username,
+                'password': password
+            }
+        )
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()  # Parse the JSON response
+        token = data.get('token')
+        project_id = data.get('projectId')
+        return token, project_id
