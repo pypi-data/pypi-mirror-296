988953ecbb073f501bbfb37b63e5a112333b3f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:06:12 +0000

    Add package PyTorchLabFlow/0.1.6.2/pytorchlabflow-0.1.6.2.tar.gz

---
+++ packages/PyTorchLabFlow/pytorchlabflow-0.1.6.2.tar.gz/pytorchlabflow-0.1.6.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyTorchLabFlow',
+    version='0.1.6.2',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'matplotlib',
+        'pandas',
+        'tqdm'
+    ],
+    author='BBEK-Anand',
+    author_email='',
+    description='A lightweight module to manage all components during experiments on a AI project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BBEK-Anand/PyTorchLabFlow',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
