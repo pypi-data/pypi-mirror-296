fab83e5a1343b3218f0dd422723614154aaf0cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:04:59 +0000

    Add package sda-cool-package-pg/0.1.0/sda_cool_package_pg-0.1.0.tar.gz

---
+++ packages/sda-cool-package-pg/sda_cool_package_pg-0.1.0.tar.gz/sda_cool_package_pg-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Library: sda_cool_library_pg
+
+## Introduction
+
+This is my first published library in Python course.
+
+## Usage
+
+Just run this script.
+
---
+++ packages/sda-cool-package-pg/sda_cool_package_pg-0.1.0.tar.gz/sda_cool_package_pg-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='sda_cool_package_pg',
+    version='0.1.0',
+    author='Petr Gregor (SDA)',
+    author_email='pe.gregor@gmail.com',
+    packages=find_packages(),
+    license='GNU GPL',
+    include_package_data=True,
+    description='Super useful library'
+)
