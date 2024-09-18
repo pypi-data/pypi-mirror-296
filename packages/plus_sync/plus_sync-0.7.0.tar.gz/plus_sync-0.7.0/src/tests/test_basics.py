638267d3626e03e91e8ab358f3871758c998b0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:07:00 +0000

    Add package plus_sync/0.7.0/plus_sync-0.7.0.tar.gz

---
+++ packages/plus_sync/plus_sync-0.7.0.tar.gz/plus_sync-0.7.0/src/tests/test_basics.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import plus_sync.cmd
+import plus_sync.config
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_init(plus_sync_to_temp_folder):
+    result = runner.invoke(plus_sync.cmd.app, ['init'], input='test\n')
+    assert result.exit_code == 0
+    assert 'Done' in result.stdout
+    assert Path('plus_sync.toml').exists()
+
+
+def test_init_fixture(plus_sync_initialized):
+    assert Path('plus_sync.toml').exists()
+    assert 'project_name = "test"' in Path('plus_sync.toml').read_text()
