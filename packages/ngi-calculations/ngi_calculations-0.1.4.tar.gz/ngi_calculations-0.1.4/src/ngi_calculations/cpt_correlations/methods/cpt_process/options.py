af2fc94ed10ac6f8cc660a069a44f0fac28d1b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:22 +0000

    Add package ngi-calculations/0.1.4/ngi_calculations-0.1.4.tar.gz

---
+++ packages/ngi-calculations/ngi_calculations-0.1.4.tar.gz/ngi_calculations-0.1.4/src/ngi_calculations/cpt_correlations/methods/cpt_process/options.py
@@ -0,0 +1,8 @@
+from pydantic.main import BaseModel
+
+
+class CptProcessOptions(BaseModel):
+    elevation: float = 0
+    shift_depth: float = 0.0  # Shift CPT depth (in m, negative value shifts upwards)
+    adjust_depth_tilt: bool = False  # Adjust depth due to tilt
+    compensate_atm_pressure: bool = False
