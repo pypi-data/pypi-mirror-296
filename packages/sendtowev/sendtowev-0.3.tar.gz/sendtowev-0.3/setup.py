8560a827a9704cf354f60c11c1ce7e4a9ad17c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:07:21 +0000

    Add package sendtowev/0.3/sendtowev-0.3.tar.gz

---
+++ packages/sendtowev/sendtowev-0.3.tar.gz/sendtowev-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sendtowev",
+    version="0.3",
+    description="Some package",
+    author="",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[
+        "requests", 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
