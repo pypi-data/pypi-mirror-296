e9718d725a8a84b050c1fd871b304253c428af7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:06:25 +0000

    Add package gitbase/0.0.6/gitbase-0.0.6.tar.gz

---
+++ packages/gitbase/gitbase-0.0.6.tar.gz/gitbase-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitbase",
+    version="0.0.6",
+    author="Taireru LLC",
+    author_email="tairerullc@gmail.com",
+    description="A GitHub based databse system.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaireruLLC/gitbase",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "cryptography"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
