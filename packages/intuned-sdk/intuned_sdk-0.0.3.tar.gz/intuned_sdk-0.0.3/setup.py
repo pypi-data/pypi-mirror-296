8c9ac8000ce0cd48b7ad08e83d130938f366fa38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:29 +0000

    Add package intuned-sdk/0.0.3/intuned_sdk-0.0.3.tar.gz

---
+++ packages/intuned-sdk/intuned_sdk-0.0.3.tar.gz/intuned_sdk-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="intuned_sdk",
+    version="0.0.3",
+    description="Intuned Python SDK",
+    author="Intuned",
+    author_email="infra@intuned.com",
+    url="https://github.com/intuned/intuned-python-sdk",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "boto3",
+        "botocore",
+        "playwright==1.44.0",
+    ],
+)
