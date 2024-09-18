ed907854722027eea9795c11a01d475f21d54fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:06:36 +0000

    Add package django-github-sso/3.0.0/django_github_sso-3.0.0.tar.gz

---
+++ packages/django-github-sso/django_github_sso-3.0.0.tar.gz/django_github_sso-3.0.0/django_github_sso/utils.py
@@ -0,0 +1,13 @@
+from django.contrib import messages
+from django_microsoft_sso import conf
+from loguru import logger
+
+
+def send_message(request, message, level: str = "error"):
+    getattr(logger, level.lower())(message)
+    if conf.MICROSOFT_SSO_ENABLE_MESSAGES:
+        messages.add_message(request, getattr(messages, level.upper()), message)
+
+
+def show_credential(credential):
+    return f"{credential[:5]}...{credential[-5:]}"
