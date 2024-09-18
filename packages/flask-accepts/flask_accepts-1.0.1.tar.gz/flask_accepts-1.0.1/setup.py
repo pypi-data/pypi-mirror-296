f5d578813edd7f96412586ac9614dd03b979a5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:19 +0000

    Add package flask-accepts/1.0.1/flask_accepts-1.0.1.tar.gz

---
+++ packages/flask-accepts/flask_accepts-1.0.1.tar.gz/flask_accepts-1.0.1/setup.py
@@ -0,0 +1,20 @@
+# Copyright Alan (AJ) Pryor, Jr. 2018
+
+from setuptools import setup, find_packages
+
+setup(
+    name="flask_accepts",
+    author='Alan "AJ" Pryor, Jr.',
+    author_email="apryor6@gmail.com",
+    version="1.0.1",
+    description="Easy, opinionated Flask input/output handling with Flask-restx and Marshmallow",
+    ext_modules=[],
+    packages=find_packages(),
+    install_requires=[
+        "marshmallow>=3.17.0",
+        "flask-restx==1.1.0; python_version < '3.8'",
+        "flask-restx>=1.2.0; python_version >= '3.8'",
+        "werkzeug>=2,<3; python_version < '3.8'",
+        "werkzeug>=3,<4; python_version >= '3.8'",
+    ],
+)
