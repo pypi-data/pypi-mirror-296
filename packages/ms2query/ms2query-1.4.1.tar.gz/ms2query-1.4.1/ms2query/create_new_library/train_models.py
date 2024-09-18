96327d422c7e76dd46565ab94bc0ad8b1957ea45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:44 +0000

    Add package ms2query/1.4.1/ms2query-1.4.1.tar.gz

---
+++ packages/ms2query/ms2query-1.4.1.tar.gz/ms2query-1.4.1/ms2query/create_new_library/train_models.py
@@ -0,0 +1,120 @@
+"""
+This script is not needed for normally running MS2Query, it is only needed to to train
+new models
+"""
+
+import os
+from spec2vec.model_building import train_new_word2vec_model
+from ms2query.clean_and_filter_spectra import (
+    clean_normalize_and_split_annotated_spectra, create_spectrum_documents)
+from ms2query.create_new_library.library_files_creator import \
+    LibraryFilesCreator
+from ms2query.create_new_library.train_ms2deepscore import \
+    train_ms2deepscore_wrapper
+from ms2query.create_new_library.train_ms2query_model import (
+    convert_to_onnx_model, train_ms2query_model)
+from ms2query.utils import load_matchms_spectrum_objects_from_file
+
+
+class SettingsTrainingModels:
+    def __init__(self,
+                 settings: dict = None):
+        default_settings = {"ms2ds_fraction_validation_spectra": 30,
+                            "ms2ds_epochs": 150,
+                            "spec2vec_iterations": 30,
+                            "ms2query_fraction_for_making_pairs": 40,
+                            "add_compound_classes": True}
+        if settings:
+            for setting in settings:
+                assert setting in default_settings, \
+                    f"Available settings are {default_settings.keys()}"
+                default_settings[setting] = settings[setting]
+        self.ms2ds_fraction_validation_spectra: float = default_settings["ms2ds_fraction_validation_spectra"]
+        self.ms2ds_epochs: int = default_settings["ms2ds_epochs"]
+        self.ms2query_fraction_for_making_pairs: int = default_settings["ms2query_fraction_for_making_pairs"]
+        self.spec2vec_iterations = default_settings["spec2vec_iterations"]
+        self.add_compound_classes: bool = default_settings["add_compound_classes"]
+
+
+def train_all_models(annotated_training_spectra,
+                     unannotated_training_spectra,
+                     output_folder,
+                     settings: SettingsTrainingModels):
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    # set file names of new generated files
+    ms2deepscore_model_file_name = os.path.join(output_folder, "ms2deepscore_model.hdf5")
+    spec2vec_model_file_name = os.path.join(output_folder, "spec2vec_model.model")
+    ms2query_model_file_name = os.path.join(output_folder, "ms2query_model.onnx")
+    ms2ds_history_figure_file_name = os.path.join(output_folder, "ms2deepscore_training_history.svg")
+
+    # Train MS2Deepscore model
+    train_ms2deepscore_wrapper(annotated_training_spectra,
+                               ms2deepscore_model_file_name,
+                               settings.ms2ds_fraction_validation_spectra,
+                               settings.ms2ds_epochs,
+                               ms2ds_history_figure_file_name
+                               )
+
+    # Train Spec2Vec model
+    spectrum_documents = create_spectrum_documents(annotated_training_spectra + unannotated_training_spectra)
+
+    train_new_word2vec_model(spectrum_documents,
+                             iterations=settings.spec2vec_iterations,
+                             filename=spec2vec_model_file_name,
+                             workers=4,
+                             progress_logger=True)
+
+    # Train MS2Query model
+    ms2query_model = train_ms2query_model(annotated_training_spectra,
+                                          os.path.join(output_folder, "library_for_training_ms2query"),
+                                          ms2deepscore_model_file_name,
+                                          spec2vec_model_file_name,
+                                          fraction_for_training=settings.ms2query_fraction_for_making_pairs)
+    convert_to_onnx_model(ms2query_model, ms2query_model_file_name)
+
+    # Create library with all training spectra
+    library_files_creator = LibraryFilesCreator(annotated_training_spectra,
+                                                output_folder,
+                                                spec2vec_model_file_name,
+                                                ms2deepscore_model_file_name,
+                                                add_compound_classes=settings.add_compound_classes)
+    library_files_creator.create_all_library_files()
+
+
+def clean_and_train_models(spectrum_file: str,
+                           ion_mode: str,
+                           output_folder,
+                           model_train_settings = None,
+                           do_pubchem_lookup = True):
+    """Trains a new MS2Deepscore, Spec2Vec and MS2Query model and creates all needed library files
+
+    :param spectrum_file:
+        The file name of the library spectra
+    :param ion_mode:
+        The ion mode of the spectra you want to use for training the models, choose from "positive" or "negative"
+    :param output_folder:
+        The folder in which the models and library files are stored.
+    :param model_train_settings:
+        The settings used for training the models, options can be found in SettingsTrainingModels. If None is given
+        all the default settings are used. The options and default settings are:
+        {"ms2ds_fraction_validation_spectra": 30, "ms2ds_epochs": 150, "spec2vec_iterations": 30,
+        "ms2query_fraction_for_making_pairs": 40, "add_compound_classes": True}
+    :param do_pubchem_lookup:
+        If True, the spectra are annotated with PubChem metadata before training the models.
+    """
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    assert os.path.isdir(output_folder), "The specified folder is not a folder"
+    assert ion_mode in {"positive", "negative"}, "ion_mode should be set to 'positive' or 'negative'"
+
+    settings = SettingsTrainingModels(model_train_settings)
+
+    spectra = load_matchms_spectrum_objects_from_file(spectrum_file)
+    annotated_spectra, unnnotated_spectra = clean_normalize_and_split_annotated_spectra(spectra,
+                                                                                        ion_mode,
+                                                                                        do_pubchem_lookup = do_pubchem_lookup)
+    train_all_models(annotated_spectra,
+                     unnnotated_spectra,
+                     output_folder,
+                     settings)
---
+++ packages/ms2query/ms2query-1.4.1.tar.gz/ms2query-1.4.1/tests/test_utils.py
@@ -0,0 +1,65 @@
+import os
+from io import StringIO
+from typing import List
+import pandas as pd
+import pytest
+from matchms import Spectrum
+from ms2query.utils import (add_unknown_charges_to_spectra,
+                            load_matchms_spectrum_objects_from_file)
+
+
+def test_convert_files_to_matchms_spectrum_objects_unknown_file(tmp_path):
+    """Tests if unknown file raises an Assertion error"""
+    with pytest.raises(AssertionError):
+        load_matchms_spectrum_objects_from_file(os.path.join(tmp_path, "file_that_does_not_exist.json"))
+
+
+def test_add_unknown_charges_to_spectra(hundred_test_spectra):
+    spectra = hundred_test_spectra
+    # Set charges to predefined values
+    for spectrum in spectra[:10]:
+        spectrum.set("charge", None)
+    for spectrum in spectra[10:20]:
+        spectrum.set("charge", 1)
+    for spectrum in spectra[20:30]:
+        spectrum.set("charge", -1)
+    for spectrum in spectra[30:]:
+        spectrum.set("charge", 2)
+
+    spectra_with_charge = add_unknown_charges_to_spectra(spectra)
+    # Test if charges are set correctly
+    for spectrum in spectra_with_charge[:20]:
+        assert spectrum.get("charge") == 1, "The charge is expected to be 1"
+    for spectrum in spectra_with_charge[20:30]:
+        assert spectrum.get("charge") == -1, "The charge is expected to be -1"
+    for spectrum in spectra_with_charge[30:]:
+        assert spectrum.get("charge") == 2, "The charge is expected to be 2"
+
+
+def check_correct_results_csv_file(dataframe_found: pd.DataFrame,
+                                   expected_headers: List[str],
+                                   nr_of_rows_to_check=2):
+    # Define expected results
+    csv_format_expected_results ="""query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrum_id,analog_compound_name,charge,s2v_score,ms2ds_score,retention_time,retention_index,smiles,cf_kingdom,cf_superclass,cf_class,cf_subclass,cf_direct_parent,npc_class_results,npc_superclass_results,npc_pathway_results\n
+        1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001760,Hoiamide B,1,0.9996,0.9232,,,CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O,b,c,d,e,f,g,h,i\n
+        2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001761,Halovir A,0,0.9621,0.4600,,,CCCCCCCCCCCCCC(=O)NC(C)(C)C(=O)N1C[C@H](O)C[C@H]1C(=O)NC(CC(C)C)C(=O)N[C@@H](C(C)C)C(=O)N[C@@H](CCC(N)=O)C(=O)N[C@H](CO)CC(C)C,b,c,d,e,f,g,h,i\n"""
+    dataframe_expected_results = pd.read_csv(StringIO(csv_format_expected_results),
+                                             sep=",", header=0)
+
+    # convert csv rows to dataframe
+    check_expected_headers(dataframe_found, expected_headers)
+
+    # Select only the matching columns
+    selection_of_matching_headers = dataframe_expected_results[dataframe_found.columns]
+    pd.testing.assert_frame_equal(dataframe_found.iloc[:nr_of_rows_to_check, :],
+                                  selection_of_matching_headers.iloc[:nr_of_rows_to_check, :],
+                                  check_dtype=False,
+                                  rtol=1.0e-4)
+
+
+def check_expected_headers(dataframe_found: pd.DataFrame,
+                           expected_headers: List[str]):
+    found_headers = list(dataframe_found.columns)
+    assert len(found_headers) == len(found_headers)
+    for i, header in enumerate(expected_headers):
+        assert header == found_headers[i]
