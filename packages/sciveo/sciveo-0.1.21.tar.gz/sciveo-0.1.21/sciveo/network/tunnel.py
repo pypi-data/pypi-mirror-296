bf97e0454c04df2fc27abbc971483098249785a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:03 +0000

    Add package sciveo/0.1.21/sciveo-0.1.21.tar.gz

---
+++ packages/sciveo/sciveo-0.1.21.tar.gz/sciveo-0.1.21/sciveo/network/tunnel.py
@@ -0,0 +1,17 @@
+#
+# Pavlin Georgiev, Softel Labs
+#
+# This is a proprietary file and may not be copied,
+# distributed, or modified without express permission
+# from the owner. For licensing inquiries, please
+# contact pavlin@softel.bg.
+#
+# 2024
+#
+
+
+import os
+import subprocess
+
+from sciveo.common.tools.logger import *
+
