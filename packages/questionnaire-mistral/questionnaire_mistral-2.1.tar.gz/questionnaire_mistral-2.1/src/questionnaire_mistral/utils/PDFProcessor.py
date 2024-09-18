01f8232f14f26117d09a262641deab6dc75f7796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:21 +0000

    Add package questionnaire-mistral/2.1/questionnaire_mistral-2.1.tar.gz

---
+++ packages/questionnaire-mistral/questionnaire_mistral-2.1.tar.gz/questionnaire_mistral-2.1/src/questionnaire_mistral/utils/PDFProcessor.py
@@ -0,0 +1,27 @@
+import PyPDF2
+from pdfminer.high_level import extract_text
+
+class PDFProcessor:
+    def __init__(self, pdf_file):
+        self.pdf_file = pdf_file
+        self.num_pages = self.get_pages()
+        self.text_content = self._extract_text()
+
+    def get_pages(self):
+        try:
+            with open(self.pdf_file, 'rb') as f:
+                pdf_reader = PyPDF2.PdfReader(f)
+                return len(pdf_reader.pages)
+        except (FileNotFoundError, Exception) as e:
+            print(f'Помилка при читані файлу: {e}')
+
+
+    def _extract_text(self):
+        try:
+            return extract_text(self.pdf_file)
+        except (FileNotFoundError, Exception) as e:
+            print(f'Failed to extract text from PDF file: {e}')
+
+
+    def get_text(self):
+        return self.text_content
