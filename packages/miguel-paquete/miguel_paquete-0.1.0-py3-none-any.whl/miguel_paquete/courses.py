a308904f99c9e3c3f916bce0e1b34e5a925b0b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:28 +0000

    Add package miguel-paquete/0.1.0/miguel_paquete-0.1.0-py3-none-any.whl

---
+++ packages/miguel-paquete/miguel_paquete-0.1.0-py3-none-any.whl/miguel_paquete/courses.py
@@ -0,0 +1,26 @@
+class Course:
+
+    def __init__(self, name, duration, link):
+        self.name = name
+        self.duration = duration
+        self.link = link
+
+    def __repr__(self):
+        return f"{self.name} [{self.duration} horas] {self.link}"
+
+courses = [
+    Course("Introduccion a Linux", 15, "aa"),
+    Course("Personalizacion de Linux", 3, "bb"),
+    Course("introduccion al hacking", 53, "cc")
+]
+
+def list_courses():
+    for course in courses:
+        print(course)
+
+def find_course(name):
+    for course in courses:
+        if course.name == name:
+            return(course)
+        
+    return None
