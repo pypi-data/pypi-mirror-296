6dc95eee11d38fd2e755ef6d013f376cf428bc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:34 +0000

    Add package Hardiksonware/0.0.2/hardiksonware-0.0.2.tar.gz

---
+++ packages/Hardiksonware/hardiksonware-0.0.2.tar.gz/hardiksonware-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Randsome ware for Fun'
+
+# Setting up
+setup(
+    name="Hardiksonware",
+    version=VERSION,
+    author="Hardik Kansara",
+    author_email="<h.kansara106@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['fernet', 'cryptography'],
+    keywords=['python', 'randsomeware'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
