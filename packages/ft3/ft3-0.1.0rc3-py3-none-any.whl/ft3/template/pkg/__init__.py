e628ce3a63c84ab9ff2cc7d75163cfed5f9e3f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:47 +0000

    Add package ft3/0.1.0rc3/ft3-0.1.0rc3-py3-none-any.whl

---
+++ packages/ft3/ft3-0.1.0rc3-py3-none-any.whl/ft3/template/pkg/__init__.py
@@ -0,0 +1,21 @@
+"""
+Summary
+-------
+
+A simple python package that can be copy / pasted / replaced.
+
+"""
+
+__all__ = (
+    'clients',
+    'enm',
+    'lib',
+    'obj',
+    'typ'
+    )
+
+from . import clients
+from . import enm
+from . import lib
+from . import obj
+from . import typ
