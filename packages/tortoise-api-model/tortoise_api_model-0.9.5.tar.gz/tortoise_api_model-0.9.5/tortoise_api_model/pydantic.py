e179d43f0d8765831326b6ef6e7e8af47712bd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:05:02 +0000

    Add package tortoise-api-model/0.9.5/tortoise_api_model-0.9.5.tar.gz

---
+++ packages/tortoise-api-model/tortoise_api_model-0.9.5.tar.gz/tortoise_api_model-0.9.5/tortoise_api_model/pydantic.py
@@ -0,0 +1,52 @@
+from typing import TypeVar, Generic
+from pydantic import BaseModel, ConfigDict
+
+from tortoise_api_model.enum import UserStatus, UserRole
+
+
+RootModelType = TypeVar("RootModelType")
+
+
+class PydList(BaseModel, Generic[RootModelType]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    data: list[RootModelType]
+    total: int
+
+
+class UserPwd(BaseModel):
+    password: str
+
+
+class UserReg(UserPwd):
+    username: str
+    email: str | None = None
+    phone: int | None = None
+
+
+class UserUpdate(BaseModel):
+    username: str
+    status: UserStatus
+    email: str | None
+    phone: int | None
+    role: UserRole
+
+
+class UserSchema(UserUpdate):
+    id: int
+
+
+# models for name endpoint for select2 inputs
+class Name(BaseModel):
+    id: int
+    text: str
+    logo: str | None = None
+    selected: bool | None = None
+
+
+class Pagination(BaseModel):
+    more: bool
+
+
+class Names(BaseModel):
+    results: list[Name]
+    pagination: Pagination
