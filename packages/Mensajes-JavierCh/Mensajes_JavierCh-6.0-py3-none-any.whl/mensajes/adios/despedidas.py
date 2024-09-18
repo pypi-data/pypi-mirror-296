d6467332cf7c7ca4e8c00d27a47ee9045f1ae5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:39 +0000

    Add package Mensajes-JavierCh/6.0/Mensajes_JavierCh-6.0-py3-none-any.whl

---
+++ packages/Mensajes-JavierCh/Mensajes_JavierCh-6.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,7 @@
+def despedir():
+    print("¡Hasta luego, me despido desde despedidas.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Adios, me despido desde Despedida.__init__()")
+        
