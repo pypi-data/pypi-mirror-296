c726c023de47aee0818fb3dff431b651762209a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:14 +0000

    Add package drmatlantis/0.5/drmatlantis-0.5.tar.gz

---
+++ packages/drmatlantis/drmatlantis-0.5.tar.gz/drmatlantis-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drmatlantis',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'anywidget',
+        'traitlets',
+        'ipylab',
+        'IPython'
+    ],
+    description='Dr. MATLANTIS server tool that connects local chatbot to MATLANTIS server',
+    author='Hristo Todorov',
+    author_email='httodoroff@gmail.com',
+    license='MIT',
+)
