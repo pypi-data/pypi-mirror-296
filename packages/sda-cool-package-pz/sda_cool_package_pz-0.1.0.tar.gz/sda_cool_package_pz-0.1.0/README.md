1930fe565c711eeb23a14296a0d2b988a81dbdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:04:55 +0000

    Add package sda-cool-package-pz/0.1.0/sda_cool_package_pz-0.1.0.tar.gz

---
+++ packages/sda-cool-package-pz/sda_cool_package_pz-0.1.0.tar.gz/sda_cool_package_pz-0.1.0/README.md
@@ -0,0 +1,9 @@
+# Library: sda_cool_package_pz
+
+## Introduction
+
+This is my test library package in Python course.
+
+## Usage
+
+Just run this script.
---
+++ packages/sda-cool-package-pz/sda_cool_package_pz-0.1.0.tar.gz/sda_cool_package_pz-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="sda_cool_package_pz",
+    version="0.1.0",
+    author="Patrik Závodník",
+    author_email="pilovinas@seznam.cz",
+    packages=find_packages(),
+    license="GNU GPL",
+    include_package_data=True,
+    description="test library"
+)
