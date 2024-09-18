d42a7d804abd359dab73bc343d9489fe77bea125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 07:04:55 +0000

    Add package django-csp/4.0b2/django_csp-4.0b2.tar.gz

---
+++ packages/django-csp/django_csp-4.0b2.tar.gz/django_csp-4.0b2/csp/tests/test_context_processors.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.test import RequestFactory
+
+from csp.context_processors import nonce
+from csp.middleware import CSPMiddleware
+from csp.tests.utils import response
+
+rf = RequestFactory()
+mw = CSPMiddleware(response())
+
+
+def test_nonce_context_processor() -> None:
+    request = rf.get("/")
+    mw.process_request(request)
+    context = nonce(request)
+
+    response = HttpResponse()
+    mw.process_response(request, response)
+
+    assert context["CSP_NONCE"] == getattr(request, "csp_nonce")
+
+
+def test_nonce_context_processor_with_middleware_disabled() -> None:
+    request = rf.get("/")
+    context = nonce(request)
+
+    assert context["CSP_NONCE"] == ""
