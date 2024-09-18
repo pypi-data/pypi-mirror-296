bff6837337ad5cfd2d83d0270ef87ec1d52326cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:07:40 +0000

    Add package jaraco.test/5.5.0/jaraco_test-5.5.0.tar.gz

---
+++ packages/jaraco.test/jaraco_test-5.5.0.tar.gz/jaraco_test-5.5.0/tests/test_http.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+import pytest
+
+
+@pytest.mark.usefixtures("needs_internet")
+def test_needs_internet() -> None:
+    """
+    This test should always succeed or be skipped.
+    """
+    urllib.request.urlopen('http://pypi.org/')
+
+
+@pytest.mark.network
+def test_network_marker() -> None:
+    """
+    This test should always succeed or be skipped.
+    """
+    urllib.request.urlopen('http://pypi.org/')
