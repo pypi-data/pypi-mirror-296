2356294ed57ba9237a1924f0395d159f4700f974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:11 +0000

    Add package toadr3/0.4.0/toadr3-0.4.0.tar.gz

---
+++ packages/toadr3/toadr3-0.4.0.tar.gz/toadr3-0.4.0/toadr3/exceptions.py
@@ -0,0 +1,53 @@
+class ToadrException(Exception):
+    def __init__(
+        self,
+        message: str,
+        status_code: int | str,
+        reason: str = None,
+        headers: dict = None,
+        json_response: dict = None,
+    ):
+        self._message = message
+
+        if isinstance(status_code, str):
+            status_code = int(status_code)
+
+        self._status_code: int = status_code
+        self._reason = reason
+        self._headers = headers
+        self._json_response = json_response
+
+    @property
+    def message(self) -> str:
+        """A short description of the exception."""
+        return self._message
+
+    @property
+    def status_code(self) -> int:
+        """The HTTP status code of the response."""
+        return self._status_code
+
+    @property
+    def reason(self) -> str:
+        """The reason of the exception (message from aiohttp.ClientResponseError)."""
+        return self._reason
+
+    @property
+    def headers(self) -> dict:
+        """The headers of the response."""
+        return self._headers
+
+    @property
+    def json_response(self) -> dict:
+        """The JSON response from the request (full JSON error response, if available)."""
+        return self._json_response
+
+
+class SchemaException(Exception):
+    def __init__(self, message: str):
+        self._message = message
+
+    @property
+    def message(self) -> str:
+        """A short description of the exception."""
+        return self._message
