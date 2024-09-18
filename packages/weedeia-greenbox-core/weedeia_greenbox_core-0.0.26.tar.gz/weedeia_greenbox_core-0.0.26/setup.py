c678a44f3db4b72b54c58057f5190493cc0c4588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:05:39 +0000

    Add package weedeia-greenbox-core/0.0.26/weedeia_greenbox_core-0.0.26.tar.gz

---
+++ packages/weedeia-greenbox-core/weedeia_greenbox_core-0.0.26.tar.gz/weedeia_greenbox_core-0.0.26/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weedeia-greenbox-core",
+    version="0.0.26",
+    author="Paulo Porto",
+    author_email="cesarpaulomp@gmail.com",
+    description="API for GPIO admnistration",
+    packages=[
+      "src",
+      "src.pin",
+      "src.util"
+    ],
+    entry_points={
+      "console_scripts": [
+          "weedeia-greenbox-core=src.main:main",
+      ],
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'wheel',
+        'fastapi',
+        'uvicorn',
+        'rpi.gpio',
+        'adafruit-circuitpython-dht'
+    ]
+)
---
+++ packages/weedeia-greenbox-core/weedeia_greenbox_core-0.0.26.tar.gz/weedeia_greenbox_core-0.0.26/src/util/errors.py
@@ -0,0 +1,8 @@
+class ComponentError(Exception):
+  def __init__(self, httpCode, errorMessage):
+    self.httpCode = httpCode
+    self.errorMessage = errorMessage
+
+class LowWaterError(ComponentError):
+  def __init__(self):
+    super().__init__(400, "low_water_detected")
