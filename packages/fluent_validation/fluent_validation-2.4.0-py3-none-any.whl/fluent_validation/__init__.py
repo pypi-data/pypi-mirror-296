67acb62ef94b15c1e4b6eba8024dee2622974fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:08:42 +0000

    Add package fluent_validation/2.4.0/fluent_validation-2.4.0-py3-none-any.whl

---
+++ packages/fluent_validation/fluent_validation-2.4.0-py3-none-any.whl/fluent_validation/__init__.py
@@ -0,0 +1,10 @@
+from fluent_validation.enums import (  # noqa: F401
+    CascadeMode,
+    ApplyConditionTo,
+    Severity,
+    StringComparer,
+)
+from fluent_validation.IValidationContext import ValidationContext  # noqa: F401
+from fluent_validation.abstract_validator import AbstractValidator  # noqa: F401
+
+from fluent_validation.ValidatorOptions import ValidatorOptions  # noqa: F401
