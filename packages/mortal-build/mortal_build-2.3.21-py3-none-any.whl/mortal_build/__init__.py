254666e4b6beffe3d8b364f0be75a7737b4e47ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:39 +0000

    Add package mortal-build/2.3.21/mortal_build-2.3.21-py3-none-any.whl

---
+++ packages/mortal-build/mortal_build-2.3.21-py3-none-any.whl/mortal_build/__init__.py
@@ -0,0 +1,30 @@
+# cython: language_level=3
+# #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: MaJian
+@Time: 2024/1/16 17:08
+@SoftWare: PyCharm
+@Project: mortal
+@File: __init__.py
+"""
+import os.path
+
+from .build_main import MortalBuildMain
+
+
+class MortalBuild(MortalBuildMain):
+    def build_config(self):
+        return self._build_config()
+
+    def build_ext_wheel_pypi(self, config):
+        return self._build_ext_wheel_pypi(config)
+
+    def build_ext(self, config):
+        self._build_ext(config)
+
+    def build_ext_wheel(self, config):
+        return self._build_ext_wheel(config)
+
+    def build_wheel(self, config):
+        self._build_wheel(config)
