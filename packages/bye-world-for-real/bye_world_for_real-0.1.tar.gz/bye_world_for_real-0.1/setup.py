cf935be2b2679018d44bb2bd88d5b51e840baaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:28 +0000

    Add package bye-world-for-real/0.1/bye_world_for_real-0.1.tar.gz

---
+++ packages/bye-world-for-real/bye_world_for_real-0.1.tar.gz/bye_world_for_real-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bye_world_for_real",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple bye_world package",
+    author="vasanth",
+    author_email="gskillsforce@gmail.com"
+)
