c36d8c27ae2c5bd5a1605dd80d5ea012d5160dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:26 +0000

    Add package simcats/1.2.0/simcats-1.2.0.tar.gz

---
+++ packages/simcats/simcats-1.2.0.tar.gz/simcats-1.2.0/README.md
@@ -0,0 +1,185 @@
+<h1 align="center">
+  <img src="https://raw.githubusercontent.com/f-hader/SimCATS/main/SimCATS_symbol.svg" alt="SimCATS logo">
+  <br>
+</h1>
+
+<div align="center">
+  <a href="https://github.com/f-hader/SimCATS/blob/main/LICENSE">
+    <img src="https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg" alt="License: CC BY-NC-SA 4.0"/>
+  </a>
+  <a href="https://pypi.org/project/simcats/">
+    <img src="https://img.shields.io/pypi/v/simcats.svg" alt="PyPi Latest Release"/>
+  </a>
+  <a href="https://simcats.readthedocs.io/en/latest/">
+    <img src="https://img.shields.io/readthedocs/simcats" alt="Read the Docs"/>
+  </a>
+  <a href="https://doi.org/10.1109/TQE.2024.3445967">
+    <img src="https://img.shields.io/badge/DOI-10.1109/TQE.2024.3445967-007ec6.svg" alt="DOI Publication"/>
+  </a>
+</div>
+
+# SimCATS
+
+Simulation of CSDs for Automated Tuning Solutions (`SimCATS`) is a Python framework for simulating charge stability 
+diagrams (CSDs) typically measured during the tuning process of qubits.
+
+## Installation
+
+The framework supports Python versions 3.7 - 3.11 and installs via pip:
+```
+pip install simcats
+```
+
+Alternatively, the `SimCATS` package can be installed by cloning the GitHub repository, navigating to the folder 
+containing the `setup.py` file and executing
+
+```
+pip install .
+```
+
+For the installation in development/editable mode, use the option `-e`.
+
+## Examples / Tutorials
+After installing the package, a good starting point is a look into the Jupyter Notebook 
+`example_SimCATS_simulation_class.ipynb`, which provides an overview of the usage of the simulation class offered by 
+the framework. 
+For more detailed examples and explanations of the geometric ideal CSD simulation using Total Charge Transitions (TCTs), look at the Jupyter Notebook `example_SimCATS_IdealCSDGeometric.ipynb`. This notebook also includes a hint
+regarding the generation of required labels for training algorithms that might need line labels defined as start and
+end points or require semantic information about particular transitions.
+
+## Tests
+
+The tests are written for the `PyTest` framework but should also work with the `unittest` framework.
+
+To run the tests, install the packages `pytest`, `pytest-cov`, and `pytest-xdist` with
+
+```
+pip install pytest pytest-cov pytest-xdist
+```
+
+and run the following command:
+
+```
+pytest --cov=simcats -n auto --dist loadfile .\tests\
+```
+
+The argument 
+- `--cov=simcats` enables a coverage summary of the `SimCATS` package,
+- `-n auto` enables the test to run with multiple threads (auto will choose as many threads as possible, but can be replaced with a specific number of threads to use), and
+- `--dist loadfile` specifies that each file should be executed only by one thread.
+
+<!-- start sec:documentation -->
+## Documentation
+
+The official documentation is hosted on [ReadtheDocs](https://simcats.readthedocs.io), but can also be built locally.
+To do this, first install the packages `sphinx`, `sphinx-rtd-theme`, `sphinx-autoapi`, `myst-nb `, and `jupytext` with
+
+```
+pip install sphinx sphinx-rtd-theme sphinx-autoapi myst-nb jupytext
+```
+
+and then, in the `docs` folder, execute the following command:
+
+```
+.\make html
+```
+
+To view the generated HTML documentation, open the file `docs\build\html\index.html`.
+<!-- end sec:documentation -->
+
+## Structure of SimCATS
+
+The primary user interface for `SimCATS` is the class `Simulation`, which combines all the necessary functionalities to
+measure (simulate) a CSD and adjust the parameters for the simulated measurement. The class `Simulation` and default
+configurations for the simulation (`default_configs`) can be imported directly from `simcats`. Aside from that,
+`SimCATS` contains the subpackages `ideal_csd`, `sensor`, `distortions`, and `support_functions`, described in
+the following sections.
+
+### Module `simulation`
+
+An instance of the simulation class requires
+
+-   an implementation of the `IdealCSDInterface` for the simulation of ideal CSD data,
+-   an implementation of the `SensorInterface` for the simulation of the sensor (dot) reaction based on the ideal CSD
+data, and
+-   (optionally) implementations of the desired types of distortions, which can be implementations from `OccupationDistortionInterface`, `SensorPotentialDistortionInterface`, or `SensorResponseDistortionInterface`.
+
+With an initialized instance of the `Simulation` class, it is possible to run simulations using the `measure` function
+(see `example_SimCATS_simulation_class.ipynb`).
+
+### Subpackage `ideal_csd`
+
+This subpackage contains the `IdealCSDInterface` used by the `Simulation` class  and an implementation of
+the `IdealCSDInterface` (`IdealCSDGeometric`) based on our geometric simulation approach.
+Additionally, it contains in the subpackage `geometric` the functions used by `IdealCSDGeometric`, including the
+implementation of the total charge transition (TCT) definition and functions for calculating the occupations using TCTs.
+
+### Subpackage `distortions`
+
+The distortions subpackage contains the `DistortionInterface` from which the `OccupationDistortionInterface`, the 
+`SensorPotentialDistortionInterface`, and the `SensorResponseDistortionInterface` are derived. Distortion functions used
+in the `Simulation` class have to implement these specific interfaces. Implemented distortions included in the
+subpackage are:
+
+-   white noise, generated by sampling from a normal distribution,
+-   pink noise, generated using the package colorednoise ([https://github.com/felixpatzelt/colorednoise](https://github.com/felixpatzelt/colorednoise)),
+-   random telegraph noise (RTN), generated using the algorithm described in ["Toward Robust Autotuning of Noisy Quantum Dot Devices" by Ziegler et al.](https://doi.org/10.1103/PhysRevApplied.17.024069) (RTN is called sensor jumps there),
+-   dot jumps, simulated using the algorithm described in ["Toward Robust Autotuning of Noisy Quantum Dot Devices" by Ziegler et al.](https://doi.org/10.1103/PhysRevApplied.17.024069) (In the `Simulation` class, this is applied to a whole block of rows or columns, but there is also a function for applying it linewise.), and
+-   lead transition blurring, simulated using Gaussian or Fermi-Dirac blurring.
+
+The implementations also offer the option to set ratios (parameter `ratio`) for the occurrence of the distortion (e.g. dot jumps may only happen sometimes and not in every measurement). Moreover, it is also possible to sample the
+noise parameters from a given sampling range using an object of type `ParameterSamplingInterface`.
+Classes for randomly sampling from a normal distribution or a uniform distribution within a given range are available in
+the subpackage `support_functions`.
+In this case, the strength is randomly chosen from the given range for every measurement.
+Additionally, it is possible to specify that this range should be a smaller subrange of the provided range.
+This allows restricting distortion fluctuations during a simulation while enabling a large variety of different strengths
+for the initialization of the objects. <br>
+RTN, dot jumps, and lead transition blurring are applied in the pixel domain. However, the jump length or the blurring strength should be consistent in the voltage domain even if the resolution changes. Therefore, the parameters
+are given in the voltage domain and adjusted according to the resolution in terms of pixel per voltage. <br>
+For a simulated measurement with a continuous voltage sweep involving an averaging for each pixel, the noise strength of the
+white and pink noise should be adjusted if the resolution (volt per pixel) changes, due to smoothing out the noise. This smoothing depends on the type of averaging used and is not incorporated in the default implementation.
+
+### Subpackage `sensor`
+
+This subpackage contains the `SensorInterface` that defines how a sensor simulation must be implemented to be used by the `Simulation` class. The `SensorPeakInterface` provides the desired representation for the definition of the Coulomb peaks the sensor uses. `SensorGeneric` implements the `SensorInterface` and offers functions for simulating the sensor response and potential. It offers the possibility to simulate with a single peak or multiple sensor peaks. Current implementations of the `SensorPeakInterface` are `SensorPeakGaussian` and `SensorPeakLorentzian`.
+
+### Subpackage `support_functions`
+
+This subpackage contains support functions, which are used by the end user and by different functions of the framework.  
+- `fermi_filter1d` is an implementation of a one-dimensional Fermi-Dirac filter.
+- `plot_csd` plots one and two-dimensional CSDs. The function can also plot ground truth data (see `example_SimCATS_simulation_class.ipynb` for examples).  
+- `rotate_points` simply rotates coordinates (stored in a (n, 2) shaped array) by a given angle. It is especially used during the generation of the ideal data.
+- `ParameterSamplingInterface` defines an interface for randomly sampled (fluctuated) strengths of distortions.
+  - `NormalSamplingRange` and `UniformSamplingRange` are implementations of the `ParameterSamplingInterface`.
+
+## Citations
+
+```bibtex
+@article{hader2024simcats,
+  author={Hader, Fabian and Fleitmann, Sarah and Vogelbruch, Jan and Geck, Lotte and Waasen, Stefan van},
+  journal={IEEE Transactions on Quantum Engineering}, 
+  title={Simulation of Charge Stability Diagrams for Automated Tuning Solutions (SimCATS)}, 
+  year={2024},
+  volume={5},
+  pages={1-14},
+  doi={10.1109/TQE.2024.3445967}
+}
+```
+
+## License, CLA, and Copyright
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa]
+
+This work is licensed under a
+[Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License][cc-by-nc-sa].
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-image]][cc-by-nc-sa]
+
+[cc-by-nc-sa]: http://creativecommons.org/licenses/by-nc-sa/4.0/
+[cc-by-nc-sa-image]: https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png
+[cc-by-nc-sa-shield]: https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg
+
+Contributions must follow the Contributor License Agreement. For more information, see the CONTRIBUTING.md file at the top of the GitHub repository.
+
+Copyright © 2024 Forschungszentrum Jülich GmbH - Central Institute of Engineering, Electronics and Analytics (ZEA) - Electronic Systems (ZEA-2)
---
+++ packages/simcats/simcats-1.2.0.tar.gz/simcats-1.2.0/simcats/support_functions/_parameter_sampling.py
@@ -0,0 +1,324 @@
+"""This module contains functionality for sampling a specific parameter from a parameter range.
+
+The contained functions can be used for example for the parameter sampling in the distortions module.
+
+@author: s.fleitmann, f.fuchs
+"""
+
+import warnings
+from abc import ABC, abstractmethod
+from typing import Tuple, Union
+
+import numpy as np
+
+__all__ = []
+
+
+class ParameterSamplingInterface(ABC):
+    """Interface for parameter sampling.
+
+    This is used for example for the fluctuation of the strengths of distortions during the simulation of CSDs.
+    """
+
+    @abstractmethod
+    def sample_parameter(self):
+        """This method is used to sample a parameter for example from a given range with a given distribution.
+
+        Returns:
+            Sampled parameter
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def last_sample(self):
+        """This method is used to get the last sampled parameter.
+
+        Which can be used to check which parameter exactly was used to simulate the last CSD.
+
+        Returns:
+            Last sampled parameter
+        """
+        raise NotImplementedError()
+
+
+class NormalSamplingRange(ParameterSamplingInterface):
+    """Normal sampling range implementation of ParameterSamplingInterface."""
+    def __init__(
+        self,
+        total_range: Tuple,
+        std: float,
+        mean: Union[float, None] = None,
+        sampling_range: Union[float, None] = None,
+        rng: Union[np.random.Generator, None] = None,
+    ) -> None:
+        """This class can be used to generate randomly normal sampled parameters within a given range.
+
+        For example, for the distortions used in the simulation of CSDs.
+
+        Args:
+            total_range (Tuple): The total range in which the parameters can be sampled. This can be narrowed down
+                randomly with the help of sampling_range. If the normal distribution generates a sample outside this
+                range, a new sample is drawn until a sample inside the sampling_range/total_range was generated,
+                leading to a truncated normal distribution.
+            std (float): The standard deviation of the sampled elements, which is used in the normal distribution.
+            mean (Union[float, None]): The mean to be used for the normal distribution. If None, the center of the
+                total range will be used. Defaults to None.
+            sampling_range (Union[float, None]): The maximum range in which the parameter is allowed to change during
+                the simulation. The explicit range is set up during the initialization, narrowing down the
+                supplied total_range. Default is None, which leads to no narrowing of the given total_range.
+            rng (np.random.Generator): random number generator used for the sampling of random numbers. If None,  the
+                default generator of numpy (np.random.default_rng()) is used. Default is None.
+        """
+        if rng:
+            self.__rng = rng
+        else:
+            self.__rng = np.random.default_rng()
+        if sampling_range is None:
+            self.__range = total_range
+        else:
+            if np.greater_equal(sampling_range, total_range[1] - total_range[0]):
+                warnings.warn(
+                    "The given reduced sampling range is equal or larger than the given total range. As "
+                    "default the given total sampling range is taken.",
+                    stacklevel=2,
+                )
+                self.__range = total_range
+            else:
+                sampled = self.__rng.uniform(
+                    total_range[0] + 0.5 * sampling_range, total_range[1] - 0.5 * sampling_range
+                )
+                self.__range = (sampled - 0.5 * sampling_range, sampled + 0.5 * sampling_range)
+        self.__std = std
+        if mean is not None:
+            self.__mean = mean
+        else:
+            self.__mean = np.mean(self.__range)
+        self.__last_sample = None
+
+    def sample_parameter(self):
+        sampled = self.__rng.normal(loc=self.__mean, scale=self.__std)
+        # repeat sampling until the sampled value is in self.__range
+        while sampled < self.__range[0] or sampled > self.__range[1]:
+            sampled = self.__rng.normal(loc=self.__mean, scale=self.__std)
+        self.__last_sample = sampled
+        return sampled
+
+    def last_sample(self):
+        return self.__last_sample
+
+    def __repr__(self) -> str:
+        return (
+            self.__class__.__name__
+            + f"(last_sample={self.last_sample()}, range={self.__range}, mean={self.__mean}, std={self.__std}, rng={self.__rng})"
+        )
+
+
+class UniformSamplingRange(ParameterSamplingInterface):
+    """Uniform sampling range implementation of ParameterSamplingInterface."""
+    def __init__(
+        self,
+        total_range: Tuple,
+        sampling_range: Union[float, None] = None,
+        rng: Union[np.random.Generator, None] = None,
+    ) -> None:
+        """This class can be used to generate randomly uniform sampled parameters within a given range.
+
+        For example, for the distortions used in the simulation of CSDs.
+
+        Args:
+            total_range (Tuple): The total range in which the parameters can be sampled. This can be narrowed down
+                randomly with the help of sampling_range.
+            sampling_range (Union[float, None]): The maximum range in which the parameter is allowed to change during
+                the simulation. The explicit range is set up during the initialization, narrowing down the supplied
+                total_range. Default is None, which leads to no narrowing of the given total_range.
+            rng (np.random.Generator): random number generator used for the sampling of random numbers. If None,  the
+                default generator of numpy (np.random.default_rng()) is used. Default is None.
+        """
+        if rng:
+            self.__rng = rng
+        else:
+            self.__rng = np.random.default_rng()
+        if sampling_range is None:
+            self.__range = total_range
+        else:
+            if np.greater_equal(sampling_range, total_range[1] - total_range[0]):
+                warnings.warn(
+                    "The given reduced sampling range is equal or larger than the given total range. As "
+                    "default the given total sampling range is taken.",
+                    stacklevel=2,
+                )
+                self.__range = total_range
+            else:
+                sampled = self.__rng.uniform(
+                    total_range[0] + 0.5 * sampling_range, total_range[1] - 0.5 * sampling_range
+                )
+                self.__range = (sampled - 0.5 * sampling_range, sampled + 0.5 * sampling_range)
+        self.__last_sample = None
+
+    def sample_parameter(self):
+        sampled = self.__rng.uniform(self.__range[0], self.__range[1])
+        self.__last_sample = sampled
+        return sampled
+
+    def last_sample(self):
+        return self.__last_sample
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + f"(last_sample={self.last_sample()}, range={self.__range}, rng={self.__rng})"
+
+
+class LogNormalSamplingRange(ParameterSamplingInterface):
+    """Logarithmic normal sampling range implementation of ParameterSamplingInterface."""
+    def __init__(
+        self,
+        total_range: Tuple,
+        sampling_range: Union[float, None] = None,
+        rng: Union[np.random.Generator, None] = None,
+        mean: float = 0,
+        sigma: float = 1,
+    ) -> None:
+        """This class can be used to generate randomly log-normal sampled parameters within a given range.
+
+        For example, for the distortions used in the simulation of CSDs.
+
+        Args:
+            total_range (Tuple): The total range in which the parameters can be sampled. This can be narrowed down
+                randomly with the help of sampling_range. If the log-normal distribution generates a sample outside this
+                range, a new sample is drawn until a sample inside the sampling_range/total_range was generated, leading
+                toa truncated log-normal distribution.
+            sampling_range (Union[float, None]): The maximum range in which the parameter is allowed to change during
+                the simulation. The explicit range is set up during the initialization, narrowing down the supplied
+                total_range. Default is None, which leads to no narrowing of the given total_range.
+            rng (np.random.Generator): random number generator used for the sampling of random numbers. If None,  the
+                default generator of numpy (np.random.default_rng()) is used. Default is None.
+            mean (float): Mean value of the underlying normal distribution. Default is 0.
+            sigma (float): Standard deviation of the underlying normal distribution. Must be non-negative. Default is 1.
+        """
+        if rng:
+            self.__rng = rng
+        else:
+            self.__rng = np.random.default_rng()
+        self.__mean: float = mean
+        self.__sigma: float = sigma
+        if sampling_range is None:
+            self.__range = total_range
+        else:
+            if np.greater_equal(sampling_range, total_range[1] - total_range[0]):
+                warnings.warn(
+                    "The given reduced sampling range is equal or larger than the given total range. As "
+                    "default the given total sampling range is taken.",
+                    stacklevel=2,
+                )
+                self.__range = total_range
+            else:
+                sampled = self.__rng.uniform(
+                    total_range[0] + 0.5 * sampling_range,
+                    total_range[1] - 0.5 * sampling_range,
+                )
+                self.__range = (
+                    sampled - 0.5 * sampling_range,
+                    sampled + 0.5 * sampling_range,
+                )
+        self.__last_sample = None
+
+    def sample_parameter(self):
+        sampled = self.__rng.lognormal(mean=self.__mean, sigma=self.__sigma)
+        # repeat sampling until the sampled value is in self.__range
+        while sampled < self.__range[0] or sampled > self.__range[1]:
+            sampled = self.__rng.lognormal(mean=self.__mean, sigma=self.__sigma)
+        self.__last_sample = sampled
+        return sampled
+
+    def last_sample(self):
+        return self.__last_sample
+
+    def __repr__(self) -> str:
+        return (
+            self.__class__.__name__
+            + f"(last_sample={self.last_sample()}, range={self.__range}, mean={self.__mean}, sigma={self.__sigma}"
+            + f", rng={self.__rng})"
+        )
+
+
+class ExponentialSamplingRange(ParameterSamplingInterface):
+    """Exponential distribution sampling range implementation of ParameterSamplingInterface."""
+
+    def __init__(
+            self,
+            total_range: Tuple,
+            scale: float,
+            sampling_range: Union[float, None] = None,
+            rng: Union[np.random.Generator, None] = None,
+    ) -> None:
+        """This class can be used to generate randomly sampled parameters from an exponential distribution within a given range.
+
+        The samples are calculated as follows:\n
+        min(sampling_range) + exponential_distribution_sample * (max(sampling_range) - min(Sampling_range))
+
+        To select the correct scale factor (1 / λ), take the following into consideration:\n
+        To have the p percent quantile at position q, the following must be valid:\n
+        q = ln( 1 / (1-p) ) / λ \n
+        with 1 / λ = scale \n
+        So in general the scale is calculated as: \n
+        scale = q / ln( 1 / (1-p) ) \n
+        For example: If it is desired to have 90% of the values in 50% of the sampling range, we get:\n
+        scale = 0.5 / ln( 1 / (1-0.9) ) = 0.21715
+
+        Further reading: https://en.wikipedia.org/wiki/Exponential_distribution#properties
+
+        Used for example for the distortions during the simulation of CSDs.
+
+        Args:
+            total_range (Tuple): The total range in which the parameters can be sampled. This can be narrowed down
+                randomly with the help of the parameter sampling_range. If the exponential distribution generates a
+                sample outside this range, a new sample is drawn until a sample inside the sampling_range/total_range
+                was generated, leading to a truncated exponential distribution.
+            scale (float): The scale of the exponential distribution. See __init__ docstring for more detailed
+                information.
+            sampling_range (Union[float, None]): The maximum range in which the parameter is allowed to change during
+                the simulation. The explicit range is set up during the initialization, narrowing down the
+                supplied total_range. Default is None, which leads to no narrowing of the given total_range.
+            rng (np.random.Generator): random number generator used for the sampling of random numbers. If None, the
+                default generator of numpy (np.random.default_rng()) is used. Default is None.
+        """
+        if rng:
+            self.__rng = rng
+        else:
+            self.__rng = np.random.default_rng()
+        if sampling_range is None:
+            self.__range = total_range
+        else:
+            if np.greater_equal(sampling_range, np.max(total_range) - np.min(total_range)):
+                warnings.warn(
+                    "The given reduced sampling range is equal or larger than the given total range. As "
+                    "default the given total sampling range is taken.",
+                    stacklevel=2,
+                )
+                self.__range = total_range
+            else:
+                sampled = self.__rng.uniform(
+                    np.min(total_range) + 0.5 * sampling_range, np.max(total_range) - 0.5 * sampling_range
+                )
+                if total_range[0] < total_range[1]:
+                    self.__range = (sampled - 0.5 * sampling_range, sampled + 0.5 * sampling_range)
+                else:
+                    self.__range = (sampled + 0.5 * sampling_range, sampled - 0.5 * sampling_range)
+        self.__scale = scale
+        self.__last_sample = None
+
+    def sample_parameter(self):
+        sampled = self.__range[0] + self.__rng.exponential(scale=self.__scale) * (self.__range[1] - self.__range[0])
+        # repeat sampling until the sampled value is in self.__range
+        while sampled < np.min(self.__range) or sampled > np.max(self.__range):
+            sampled = self.__range[0] + self.__rng.exponential(scale=self.__scale) * (self.__range[1] - self.__range[0])
+        self.__last_sample = sampled
+        return sampled
+
+    def last_sample(self):
+        return self.__last_sample
+
+    def __repr__(self) -> str:
+        return (
+                self.__class__.__name__
+                + f"(last_sample={self.last_sample()}, range={self.__range}, scale={self.__scale}, rng={self.__rng})"
+        )
