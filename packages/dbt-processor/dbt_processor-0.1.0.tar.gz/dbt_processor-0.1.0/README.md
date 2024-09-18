4e335b3c8a7ae75d52f6d7859de922c0ca65b675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 05:06:27 +0000

    Add package dbt-processor/0.1.0/dbt_processor-0.1.0.tar.gz

---
+++ packages/dbt-processor/dbt_processor-0.1.0.tar.gz/dbt_processor-0.1.0/README.md
@@ -0,0 +1,10 @@
+# dbt_processor
+
+A package to process dbt logs.
+
+## Installation
+
+You can install the package using pip:
+
+```sh
+pip install dbt_processor
---
+++ packages/dbt-processor/dbt_processor-0.1.0.tar.gz/dbt_processor-0.1.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dbt_processor',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'dbt_processor=dbt_processor.processor:main',
+        ],
+    },
+    author='Rakuten DO India',
+    author_email='ankur.kumar@rakuten.com',
+    description='A package to process dbt logs into Rakuten DO platform',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/dbt_processor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
