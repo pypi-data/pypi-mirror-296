722931607a2ccb334fe3596a2e8d10b44eaa20ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:10 +0000

    Add package onp2p/0.0.2.16/onp2p-0.0.2.16-py3-none-any.whl

---
+++ packages/onp2p/onp2p-0.0.2.16-py3-none-any.whl/onp2p/shell.py
@@ -0,0 +1,49 @@
+import argparse
+import pyfiglet
+
+# local imports
+from wrapper import add, multiply
+
+def shell():
+    # Creare un titolo in ASCII art con il font "slant"
+    ascii_title = pyfiglet.figlet_format("ONP2P", font="slant")
+    print(ascii_title)
+
+    print("Welcome to the ONP2P interctive shell. Type 'exit' to close.")
+
+    while True:
+        command = input(">>> ").strip().split()
+
+
+        if command[0].lower() == 'exit':
+            print("Bye")
+            break
+
+
+        parser = argparse.ArgumentParser(prog='shell', description="Shell interattiva per eseguire operazioni matematiche.")
+
+        # Aggiungiamo i sottocomandi per le diverse operazioni
+        subparsers = parser.add_subparsers(dest='command')
+
+        # Definizione del sottocomando 'somma'
+        parser_sum = subparsers.add_parser('sum', help='Add two numbers')
+        parser_sum.add_argument('a', type=float, help='First number')
+        parser_sum.add_argument('b', type=float, help='Second numer')
+
+        try:
+
+            # Parsing dell'input dell'utente
+            args = parser.parse_args(command)
+
+            # Esegui l'operazione corrispondente
+            if args.comando == 'sum':
+                print(add(args.a, args.b))
+            
+            else:
+                print("Command not recognized.")
+
+        except SystemExit:
+            # Gestione per evitare che argparse chiuda la shell su errore
+            pass
+        except Exception as e:
+            print(f"Error: {e}")
