499f18ec60379e5f97585a3a0644e366d18032f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:46 +0000

    Add package bvlogger/1.0.6/bvlogger-1.0.6.tar.gz

---
+++ packages/bvlogger/bvlogger-1.0.6.tar.gz/bvlogger-1.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bvlogger',
+    version='1.0.6',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your project',
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    url='https://github.com/obaidshk/bvlogger',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
