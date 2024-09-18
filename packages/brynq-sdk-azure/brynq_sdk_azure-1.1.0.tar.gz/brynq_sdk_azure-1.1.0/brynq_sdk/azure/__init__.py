cb0f17f042b3ab8e7e49a0c57a88948a91e8060e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:28 +0000

    Add package brynq-sdk-azure/1.1.0/brynq_sdk_azure-1.1.0.tar.gz

---
+++ packages/brynq-sdk-azure/brynq_sdk_azure-1.1.0.tar.gz/brynq_sdk_azure-1.1.0/brynq_sdk/azure/__init__.py
@@ -0,0 +1,3 @@
+from brynq_sdk.azure.entra import Entra
+from brynq_sdk.azure.azure_connection import AzureConnection
+from brynq_sdk.azure.blob_storage import BlobStorage
---
+++ packages/brynq-sdk-azure/brynq_sdk_azure-1.1.0.tar.gz/brynq_sdk_azure-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='brynq_sdk_azure',
+    version='1.1.0',
+    description='Azure wrapper from BrynQ',
+    long_description='Azure wrapper from BrynQ',
+    author='BrynQ',
+    author_email='support@brynq.com',
+    packages=["brynq_sdk.azure"],
+    license='BrynQ License',
+    install_requires=[
+        'brynq-sdk-brynq>=1',
+        'azure-storage-file-share>=12.6.0',
+        'azure-storage-blob>=12.16.0',
+        'msal==1.22.0'
+    ],
+    zip_safe=False,
+)
