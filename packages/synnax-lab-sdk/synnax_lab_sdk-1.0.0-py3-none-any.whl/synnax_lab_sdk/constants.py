526650525eceefc6987d85763359579b2a1c8d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:30 +0000

    Add package synnax-lab-sdk/1.0.0/synnax_lab_sdk-1.0.0-py3-none-any.whl

---
+++ packages/synnax-lab-sdk/synnax_lab_sdk-1.0.0-py3-none-any.whl/synnax_lab_sdk/constants.py
@@ -0,0 +1,4 @@
+PUBLIC_COMPANY_DATASET_URL: str = "https://public-company-dataset-api.synnax.app"
+PUBLIC_COMPANY_PREDICTION_SUBMISSION_URL: str = (
+    "https://public-company-prediction-submission-api.synnax.app"
+)
