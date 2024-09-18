d44d4c6432ef1e57ee2335cc16059d430f30f57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:50 +0000

    Add package mtmai-client/0.3.824/mtmai_client-0.3.824.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.824.tar.gz/mtmai_client-0.3.824/mtmai_client/models/agent_task_public.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="AgentTaskPublic")
+
+
+@_attrs_define
+class AgentTaskPublic:
+    """
+    Attributes:
+        id (str):
+        title (Union[None, Unset, str]):  Default: ''.
+    """
+
+    id: str
+    title: Union[None, Unset, str] = ""
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        id = self.id
+
+        title: Union[None, Unset, str]
+        if isinstance(self.title, Unset):
+            title = UNSET
+        else:
+            title = self.title
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "id": id,
+            }
+        )
+        if title is not UNSET:
+            field_dict["title"] = title
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        id = d.pop("id")
+
+        def _parse_title(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        title = _parse_title(d.pop("title", UNSET))
+
+        agent_task_public = cls(
+            id=id,
+            title=title,
+        )
+
+        agent_task_public.additional_properties = d
+        return agent_task_public
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
