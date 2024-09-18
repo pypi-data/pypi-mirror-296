ad24f4255c500976612e5c0987d6f6d69b0085f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 15:05:43 +0000

    Add package aac-init/0.3.51/aac_init-0.3.51.tar.gz

---
+++ packages/aac-init/aac_init-0.3.51.tar.gz/aac_init-0.3.51/aac_init/cli/options.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Wang Xiao <xiawang3@cisco.com>
+
+import click
+
+yaml_dir_path = click.option(
+    '--data',
+    '-d',
+    type=click.Path(),
+    required=True,
+    help='Path to aac-init YAML data files.'
+)
