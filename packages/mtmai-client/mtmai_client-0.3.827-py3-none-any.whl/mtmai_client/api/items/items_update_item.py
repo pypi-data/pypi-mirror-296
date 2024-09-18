d3a62bdff0fbac4d58ab6ad094193067630ef6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 14:05:35 +0000

    Add package mtmai-client/0.3.827/mtmai_client-0.3.827-py3-none-any.whl

---
+++ packages/mtmai-client/mtmai_client-0.3.827-py3-none-any.whl/mtmai_client/api/items/items_update_item.py
@@ -0,0 +1,186 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.http_validation_error import HTTPValidationError
+from ...models.item_public import ItemPublic
+from ...models.item_update import ItemUpdate
+from ...types import Response
+
+
+def _get_kwargs(
+    id: str,
+    *,
+    body: ItemUpdate,
+) -> Dict[str, Any]:
+    headers: Dict[str, Any] = {}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "put",
+        "url": f"/api/v1/items/{id}",
+    }
+
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[HTTPValidationError, ItemPublic]]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = ItemPublic.from_dict(response.json())
+
+        return response_200
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        response_422 = HTTPValidationError.from_dict(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[HTTPValidationError, ItemPublic]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    id: str,
+    *,
+    client: AuthenticatedClient,
+    body: ItemUpdate,
+) -> Response[Union[HTTPValidationError, ItemPublic]]:
+    """Update Item
+
+     Update an item.
+
+    Args:
+        id (str):
+        body (ItemUpdate):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[HTTPValidationError, ItemPublic]]
+    """
+
+    kwargs = _get_kwargs(
+        id=id,
+        body=body,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    id: str,
+    *,
+    client: AuthenticatedClient,
+    body: ItemUpdate,
+) -> Optional[Union[HTTPValidationError, ItemPublic]]:
+    """Update Item
+
+     Update an item.
+
+    Args:
+        id (str):
+        body (ItemUpdate):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[HTTPValidationError, ItemPublic]
+    """
+
+    return sync_detailed(
+        id=id,
+        client=client,
+        body=body,
+    ).parsed
+
+
+async def asyncio_detailed(
+    id: str,
+    *,
+    client: AuthenticatedClient,
+    body: ItemUpdate,
+) -> Response[Union[HTTPValidationError, ItemPublic]]:
+    """Update Item
+
+     Update an item.
+
+    Args:
+        id (str):
+        body (ItemUpdate):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[HTTPValidationError, ItemPublic]]
+    """
+
+    kwargs = _get_kwargs(
+        id=id,
+        body=body,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    id: str,
+    *,
+    client: AuthenticatedClient,
+    body: ItemUpdate,
+) -> Optional[Union[HTTPValidationError, ItemPublic]]:
+    """Update Item
+
+     Update an item.
+
+    Args:
+        id (str):
+        body (ItemUpdate):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[HTTPValidationError, ItemPublic]
+    """
+
+    return (
+        await asyncio_detailed(
+            id=id,
+            client=client,
+            body=body,
+        )
+    ).parsed
