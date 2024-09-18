9176add9c4c086e528c51daa993ed7bc2ce1dfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:28 +0000

    Add package CupDefs/1.6/CupDefs-1.6-py3-none-any.whl

---
+++ packages/CupDefs/CupDefs-1.6-py3-none-any.whl/CupDefs/vars.py
@@ -0,0 +1,11 @@
+import os
+
+PRINT_CUPDEFS_ERROR: bool = True
+PRINT_LOGS_CLI: bool = True
+
+VERGEN_HASH_SIZE = 2
+VERGEN_HASH_PATH = '.'
+VERGEN_FILE_LIMIT = 1000
+
+__PACKAGE_DIR: str = os.path.abspath(os.path.dirname(__file__))
+__CLEAR_COMMAND = 'clear'
