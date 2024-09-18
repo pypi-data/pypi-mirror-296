aedee6aa0cf2fec496ea2ce3f57f2256ddad954c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 04:05:32 +0000

    Add package physeng/0.2.0/physeng-0.2.0.tar.gz

---
+++ packages/physeng/physeng-0.2.0.tar.gz/physeng-0.2.0/physeng/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Andreas Mussgiller
+"""
+
+from .utilities import DateTimeFromDecimalYear
+from .singleton import Singleton
+from .materialdb import MaterialDB, MaterialProperty, Material
+from .iso286 import ISO286Hole, ISO286Shaft
+from .colors import Color
+from .DustMeter_xml_to_pandas import DustMeter_xml_to_pandas
+from .kalendars import CalDESY
