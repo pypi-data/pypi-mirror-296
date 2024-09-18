da89b39bca8b70b14842fc7c82cde3555230e895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 06:06:35 +0000

    Add package TEST-upload-to-test-functionality/0.0.1/test_upload_to_test_functionality-0.0.1.tar.gz

---
+++ packages/TEST-upload-to-test-functionality/test_upload_to_test_functionality-0.0.1.tar.gz/test_upload_to_test_functionality-0.0.1/TEST_upload_to_test_funcionality/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="TEST_upload_to_test_functionality",
+    version="0.0.1",
+    license="GNU PL",
+    description="just a test",
+    author="Adéla Dolníček",
+    packages=find_packages(),
+    include_package_data=True
+
+)
