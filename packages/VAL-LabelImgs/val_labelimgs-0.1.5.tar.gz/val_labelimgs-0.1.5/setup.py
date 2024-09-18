b54a88de068dad4718843075fe4039a9eac7bed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 16:05:20 +0000

    Add package VAL-LabelImgs/0.1.5/val_labelimgs-0.1.5.tar.gz

---
+++ packages/VAL-LabelImgs/val_labelimgs-0.1.5.tar.gz/val_labelimgs-0.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VAL_LabelImgs',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'requests',
+        'psutil',
+        'tqdm',
+        'matplotlib',
+        'pyyaml',
+        'pandas',
+        'numpy',
+        'torch',
+        'thop',
+        'torchvision',
+        'ultralytics-thop',
+        'urllib3',
+        'Werkzeug',
+        'yolov6',
+        'pycocotools',
+        'PyQt5',
+        'lxml'
+    ],
+    package_data={
+        'VAL_LabelImgs': ['default.yaml','coco.yaml','default_imgs.yaml','data/predefined_classes.txt'],
+        # Bao gồm file YAML trong gói
+    },
+    entry_points={
+        'console_scripts': [
+            'VAL_LabelImgs=VAL_LabelImgs.mainapp:main_app'
+        ]
+    }
+)
