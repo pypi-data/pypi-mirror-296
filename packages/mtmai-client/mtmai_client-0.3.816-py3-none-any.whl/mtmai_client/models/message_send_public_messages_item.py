5dbc689edffaa11433ff85ba30c8b49e0d744cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 03:05:21 +0000

    Add package mtmai-client/0.3.816/mtmai_client-0.3.816-py3-none-any.whl

---
+++ packages/mtmai-client/mtmai_client-0.3.816-py3-none-any.whl/mtmai_client/models/message_send_public_messages_item.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, List, Type, TypeVar
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+T = TypeVar("T", bound="MessageSendPublicMessagesItem")
+
+
+@_attrs_define
+class MessageSendPublicMessagesItem:
+    """ """
+
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        message_send_public_messages_item = cls()
+
+        message_send_public_messages_item.additional_properties = d
+        return message_send_public_messages_item
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
