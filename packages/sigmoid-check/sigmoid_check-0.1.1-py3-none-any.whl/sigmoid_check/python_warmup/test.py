fec35eecc942ce343a574dd513b46e4cd8b4dfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:07:01 +0000

    Add package sigmoid-check/0.1.1/sigmoid_check-0.1.1-py3-none-any.whl

---
+++ packages/sigmoid-check/sigmoid_check-0.1.1-py3-none-any.whl/sigmoid_check/python_warmup/test.py
@@ -0,0 +1,15 @@
+from sigmoid_check.python_warmup.python_warmup_checkers import *
+
+@check_exercise_75
+def exercise_75(amazing_string):
+    # This line is mandatory and should not be removed
+    ex_stat_init = True
+    
+    # replace the word "amazing!" with "amazing! Especially when is taught at Sigmoid" in the variable `amazing_string`;
+    # BELOW GOES YOUR CODE
+    print(amazing_string.replace("amazing!", "amazing! Especially when is taught at Sigmoid"))
+    # ABOVE GOES YOUR CODE
+
+# Run the exercise
+exercise_75()
+
