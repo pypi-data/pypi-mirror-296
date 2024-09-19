491f29a88b94391d40b110cb6215cbaadad8644c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:07:28 +0000

    Add package say-hello-vasanth/0.2/say_hello_vasanth-0.2.tar.gz

---
+++ packages/say-hello-vasanth/say_hello_vasanth-0.2.tar.gz/say_hello_vasanth-0.2/README.md
@@ -0,0 +1,1 @@
+This is a simple say hello file
---
+++ packages/say-hello-vasanth/say_hello_vasanth-0.2.tar.gz/say_hello_vasanth-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='say_hello_vasanth',
+    version='0.2',
+    packages=find_packages()
+)
