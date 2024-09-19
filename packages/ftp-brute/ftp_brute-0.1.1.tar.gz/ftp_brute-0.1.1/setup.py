223a606e910091f04e5b018529f5682680775bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:05:02 +0000

    Add package ftp-brute/0.1.1/ftp_brute-0.1.1.tar.gz

---
+++ packages/ftp-brute/ftp_brute-0.1.1.tar.gz/ftp_brute-0.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from os import rename, chmod
+from os.path import join
+from platform import system
+from subprocess import call
+from setuptools.command.install import install
+
+version = "0.1.1"
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        match system():
+            case 'Windows':
+                call(['pyinstaller', '--onefile', 'ftp_brute_force/ftp_brute.py'])
+                scripts_dir = join(self.install_scripts, 'ftp_brute.exe')
+                rename('dist/ftp_brute.exe', scripts_dir)
+
+            case 'Linux' | 'Darwin':
+                call(['pyinstaller', '--onefile', 'ftp_brute_force/ftp_brute.py'])
+                scripts_dir = join(self.install_scripts, 'ftp_brute')
+                rename('dist/ftp_brute', scripts_dir)
+                chmod(scripts_dir, 0o755)
+
+        install.run(self)
+
+
+setup(
+    name="ftp_brute",
+    version=version,
+    author="Jackson Ja",
+    author_email="jackson2937703346@163.com",
+    description="A FTP brute force tool",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacksonjapy/ftp_brute_force",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[],
+)
