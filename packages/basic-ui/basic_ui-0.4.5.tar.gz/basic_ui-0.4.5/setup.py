3e5b130c4e7f77b2399707f73e8d7eb47bcf0355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:05:21 +0000

    Add package basic-ui/0.4.5/basic_ui-0.4.5.tar.gz

---
+++ packages/basic-ui/basic_ui-0.4.5.tar.gz/basic_ui-0.4.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basic_ui',
+    version='0.4.5',
+    packages=find_packages(),
+    install_require=[
+        'pygame>=2.6.0'
+    ],
+    author='Legregz',
+    description="basic ui is a package for pygame to create interfaces easly based on json files",
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read()
+)
