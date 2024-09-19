bb10340a462150744b8fe6d1a9d4573d2eb8a8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:05:05 +0000

    Add package invenio-banners/3.1.1/invenio_banners-3.1.1-py2.py3-none-any.whl

---
+++ packages/invenio-banners/invenio_banners-3.1.1-py2.py3-none-any.whl/invenio_banners/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2024 CERN.
+#
+# Invenio-Banners is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Create and show banners with useful messages to users."""
+
+from .ext import InvenioBanners
+
+__version__ = "3.1.1"
+
+__all__ = ("__version__", "InvenioBanners")
