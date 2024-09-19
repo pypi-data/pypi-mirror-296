5336a9b3a8e27a0e896ad9e2bf54e2664198a7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:05:21 +0000

    Add package UniParse/0.1.1/uniparse-0.1.1.tar.gz

---
+++ packages/UniParse/uniparse-0.1.1.tar.gz/uniparse-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UniParse",
+    version="0.1.1",
+    author="Hridesh",
+    author_email="hridesh.khandal@gmail.com",
+    description="A library to parse PDF, DOCX, and TXT files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hridesh-net/praserlib.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "PyMuPDF>=1.18.0",
+        "python-docx>=0.8.10",
+    ]
+)
