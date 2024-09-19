181b5d1df08713b3cc62d582d99d52078e4c3564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:06:55 +0000

    Add package Image-dev/1.0.4/image_dev-1.0.4.tar.gz

---
+++ packages/Image-dev/image_dev-1.0.4.tar.gz/image_dev-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Image_dev",              # Module name
+    version="1.0.4",               # Version number
+    packages=find_packages(),      # Automatically find package directories
+    author="Hamdi Emad",
+    author_email="hamdy1721998@gmail.com",
+    description="Image Development project Project",
+    include_package_data=True
+)
