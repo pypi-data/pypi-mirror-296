20c50f4844e5c82fe3c33604c2d3241888de5d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:04:55 +0000

    Add package karpentermig/0.1.0/karpentermig-0.1.0.tar.gz

---
+++ packages/karpentermig/karpentermig-0.1.0.tar.gz/karpentermig-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='karpentermig',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    description='A tool for Karpenter migration',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Pugar Jayanegara',
+    license='MIT',
+    install_requires=[
+        'questionary',
+        'boto3',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'karpentermig=karpentermig.cli:cli',
+        ],
+    },
+)
---
+++ packages/karpentermig/karpentermig-0.1.0.tar.gz/karpentermig-0.1.0/src/karpentermig/__init__.py
@@ -0,0 +1,4 @@
+from .cli import cli
+from .discover_cluster import export_eks_config
+
+__all__ = ['cli', 'export_eks_config']
