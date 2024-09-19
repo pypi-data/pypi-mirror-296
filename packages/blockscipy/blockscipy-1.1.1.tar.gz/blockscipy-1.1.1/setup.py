d52e5d9804982859f1e0f1c54bbe9bb85fa25ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:07:41 +0000

    Add package blockscipy/1.1.1/blockscipy-1.1.1.tar.gz

---
+++ packages/blockscipy/blockscipy-1.1.1.tar.gz/blockscipy-1.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='blockscipy', #Package_Name
+      version='1.1.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
