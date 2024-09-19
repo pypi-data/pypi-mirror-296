bf09bbd5f91d7fa82681068fc3c94ce36e3cd511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:05:09 +0000

    Add package alloniamodel/1.0.0/alloniamodel-1.0.0-py3-none-any.whl

---
+++ packages/alloniamodel/alloniamodel-1.0.0-py3-none-any.whl/alloniamodel/__init__.py
@@ -0,0 +1,12 @@
+from .decorator import convert_self_kwarg
+from .model import AllOnIAModel
+from .utils import URL, SpecialDataFormat
+from .variables import Variables
+
+__all__ = [
+    "AllOnIAModel",
+    "Variables",
+    "SpecialDataFormat",
+    "URL",
+    "convert_self_kwarg",
+]
