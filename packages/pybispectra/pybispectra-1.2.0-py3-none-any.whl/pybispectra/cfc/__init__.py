784dc5c2db4433f03c056ee710a6ce0f2d01be21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:04:54 +0000

    Add package pybispectra/1.2.0/pybispectra-1.2.0-py3-none-any.whl

---
+++ packages/pybispectra/pybispectra-1.2.0-py3-none-any.whl/pybispectra/cfc/__init__.py
@@ -0,0 +1,7 @@
+"""Tools for handling cross-frequency coupling analysis."""
+
+__version__ = "1.2.0"
+
+from .aac import AAC
+from .pac import PAC
+from .ppc import PPC
---
+++ packages/pybispectra/pybispectra-1.2.0-py3-none-any.whl/pybispectra/waveshape/__init__.py
@@ -0,0 +1,5 @@
+"""Tools for handling waveshape analysis."""
+
+__version__ = "1.2.0"
+
+from .waveshape import WaveShape
