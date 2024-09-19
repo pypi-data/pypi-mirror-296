41b54463a8043f0f441a73631cc0ecc24f6cbe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:05:20 +0000

    Add package ScrollableContainers/2.2.3/scrollablecontainers-2.2.3.tar.gz

---
+++ packages/ScrollableContainers/scrollablecontainers-2.2.3.tar.gz/scrollablecontainers-2.2.3/src/ScrollableContainers/__init__.py
@@ -0,0 +1,10 @@
+import contextlib
+
+with contextlib.suppress(ImportError):
+    from ScrollableContainers._qt5 import ScrollableAreaQt5
+with contextlib.suppress(ImportError):
+    from ScrollableContainers._qt6 import ScrollableAreaQt6
+with contextlib.suppress(ImportError):
+    from ScrollableContainers._tk import ScrollableFrameTk
+with contextlib.suppress(ImportError):
+    from ScrollableContainers._wx import ScrollablePanelWx
