60cd0a38acdc9149899ff6e0a177cf74398eb892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:05:11 +0000

    Add package databricks-agents/0.5.1/databricks_agents-0.5.1-py3-none-any.whl

---
+++ packages/databricks-agents/databricks_agents-0.5.1-py3-none-any.whl/databricks/rag_eval/constants.py
@@ -0,0 +1,13 @@
+"""
+File containing all the constants needed for the agent utils.
+"""
+
+# Metrics
+GROUND_TRUTH_RETRIEVAL_METRIC_NAMES = ["recall"]
+
+# Configs
+EVALUATOR_CONFIG_EXAMPLES_KEY_NAME = "examples_df"
+
+DEFAULT_CONTEXT_CONCATENATION_DELIMITER = "\n"
+
+CHUNK_CONTENT_IS_EMPTY_RATIONALE = "Chunk content is empty"
