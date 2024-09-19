e8ba17fed6ac5f107b2b69471f2e5b74980afed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:07:20 +0000

    Add package reling/1.1.2/reling-1.1.2-py3-none-any.whl

---
+++ packages/reling/reling-1.1.2-py3-none-any.whl/reling/types/__init__.py
@@ -0,0 +1,13 @@
+from .audio import Reader, Speed, Transcriber
+from .dialogue_exchange_data import DialogueExchangeData
+from .input import Input
+from .word_with_sense import WordWithSense
+
+__all__ = [
+    'DialogueExchangeData',
+    'Input',
+    'Reader',
+    'Speed',
+    'Transcriber',
+    'WordWithSense',
+]
