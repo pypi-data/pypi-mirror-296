08c94cbb124455f967127bd3565b05481183fc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:07:15 +0000

    Add package XeroML/0.0.3/XeroML-0.0.3-py3-none-any.whl

---
+++ packages/XeroML/XeroML-0.0.3-py3-none-any.whl/XeroML/xApp/__init__.py
@@ -0,0 +1,4 @@
+from .utils import rm_outliers as iqr
+from .utils import fractional_abundances as fa
+from .utils import generate_standard_curves as gsc
+from .utils import natural_abundances as na
