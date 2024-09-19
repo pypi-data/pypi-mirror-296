9780c78c6f67634e9828f6ce7baec9ddd143302a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:04:52 +0000

    Add package PyCUC/1.1.0/PyCUC-1.1.0-py3-none-any.whl

---
+++ packages/PyCUC/PyCUC-1.1.0-py3-none-any.whl/pycuc/config/setting.py
@@ -0,0 +1,6 @@
+# version
+__version__ = '1.1.0'
+# author
+__author__ = 'Sina Gilassi'
+# email
+__email__ = 'sina.gilassi@gmail.com'
---
+++ packages/PyCUC/PyCUC-1.1.0-py3-none-any.whl/pycuc/docs/__init__.py
@@ -0,0 +1,4 @@
+from .cuc import CustomUnitConverter
+from .utils import Utils
+
+__all__ = ['CustomUnitConverter', 'Utils']
