a0dcbf41958afb43e3d36f42a587587eb942d314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 05:05:14 +0000

    Add package pygaming/0.2.0/pygaming-0.2.0.tar.gz

---
+++ packages/pygaming/pygaming-0.2.0.tar.gz/pygaming-0.2.0/pygaming/inputs/__init__.py
@@ -0,0 +1,3 @@
+"""The input module contains the Inputs, Controls and Click classes to interact with the user."""
+from .inputs import Click, Controls, Inputs
+__all__ = ["Click", "Controls", "Inputs"]
